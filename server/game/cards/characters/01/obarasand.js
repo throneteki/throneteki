adc0c14e2ebe9c447b51dc9f9f47c1f3f0a424d3
Ryan <Ryan@Ryan-PC>
2017-01-21 18:17:44 -0600

    Added isKneeled getter and Obara Sand

---
+++ server/game/cards/characters/01/obarasand.js
@@ -0,0 +1,14 @@
+const DrawCard = require('../../../drawcard.js');
+
+class ObaraSand extends DrawCard {
+	isKneeled() {
+		if(challenge.defendingPlayer == this.controller || challenge.challengeType == 'power' || !this.isBlank()) {
+			return false;
+		}
+		return this.kneeled;
+	}
+}
+
+ObaraSand.code = '01108';
+
+module.exports = ObaraSand;
