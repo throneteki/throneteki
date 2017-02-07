7c56d4e42260b2c2267cc83a08ad1242e6f4d8d1
DukeTax <DukeTax@users.noreply.github.com>
2017-02-07 16:35:44 +0100

    Update serilynpayne.js

--- server/game/cards/characters/02/serilynpayne.js
+++ server/game/cards/characters/02/serilynpayne.js
@@ -3,18 +3,18 @@ const DrawCard = require('../../../drawcard.js');
 class SerIlynPayne extends DrawCard {
     setupCardAbilities() {
         this.action({
-            title: 'Kneel Ser Ilyn Payne to kill a character with printed cost 3 or less',
-            method: 'killChud'
+            title: 'Kneel Ser Ilyn Payne to kill a character',
+            method: 'kneel'
         });
     }
     
-    killChud(player) {
-      if(this.controller !== player || this.location !== 'play area' || this.kneeled || player.phase !== 'marshal') {
-      return false;
-      }
+    kneel(player) {
+        if(this.controller !== player || this.location !== 'play area' || this.kneeled || player.phase !== 'marshal') {
+        return false;
+        }
       
-      this.game.promptForSelect(player, {
-            activePromptTitle: 'Select a character with printed cost 3 or less',
+        this.game.promptForSelect(player, {
+            activePromptTitle: 'Select a character',
             waitingPromptTitle: 'Waiting for opponent to use ' + this.name,
             cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.getCost() <= 3,
             onSelect: (p, card) => this.onCardSelected(p, card)
@@ -23,7 +23,7 @@ class SerIlynPayne extends DrawCard {
         return true;
     }
     
-      onCardSelected(player, card) {
+    onCardSelected(player, card) {
         this.game.addMessage('{0} kneels {1} to kill {2}', player, this, card);
         this.controller.kneelCard(this);
         card.controller.killCharacter(card);
