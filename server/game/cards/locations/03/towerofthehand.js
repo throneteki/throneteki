24937b719aeab905d6de5b4d53fd1970d0a257f8
Petar Petkov <pspetkov@progress.com>
2017-02-14 17:26:16 +0200

    changing the text of tower of the hand to include the kneel action.

--- server/game/cards/locations/03/towerofthehand.js
+++ server/game/cards/locations/03/towerofthehand.js
@@ -36,7 +36,7 @@ class TowerOfTheHand extends DrawCard {
         this.selectedCard.controller.returnCardToHand(this.selectedCard);
         this.controller.kneelCard(this);
 
-        this.game.addMessage('{0} uses {1} to return {2} to their hand and return {3} to {4}\'s hand', this.controller, this, this.selectedCard, card, card.controller);
+        this.game.addMessage('{0} kneels {1} to return {2} to their hand and return {3} to {4}\'s hand', this.controller, this, this.selectedCard, card, card.controller);
 
         return true;
     }
