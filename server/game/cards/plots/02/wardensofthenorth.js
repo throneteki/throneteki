ee4b1d65ba3566b4b10a7ce5813f540fa7d2bcdb
Brian Upton <ystros@gmail.com>
2017-02-26 20:43:09 -0800

    Wardens of the North can only add your own cards
    Previously Wardens of the North was allowing you to select your
    opponent's Stark cards to add to the challenge. This is invalid because
    kneeling the character is a cost and you can only pay costs using cards
    you control.

--- server/game/cards/plots/02/wardensofthenorth.js
+++ server/game/cards/plots/02/wardensofthenorth.js
@@ -4,41 +4,27 @@ class WardensOfTheNorth extends PlotCard {
     setupCardAbilities(ability) {
         this.action({
             title: 'Kneel a character to have it participate in the current challenge',
-            method: 'onClick',
             phase: 'challenge',
-            limit: ability.limit.perRound(2)
+            limit: ability.limit.perRound(2),
+            condition: () => this.isStarkCardParticipatingInChallenge(),
+            cost: ability.costs.kneel(card => card.getType() === 'character' && card.isFaction('stark')),
+            handler: context => {
+                var card = context.kneelingCostCard;
+                if(this.game.currentChallenge.attackingPlayer === context.player) {
+                    this.game.currentChallenge.addAttacker(card);
+                } else {
+                    this.game.currentChallenge.addDefender(card);
+                }
+
+                this.game.addMessage('{0} uses {1} to kneel {2} and add them to the challenge', context.player, this, card);
+            }
         });
     }
 
-    onClick(player) {
-        if(!this.game.currentChallenge || !this.controller.cardsInPlay.any(card => {
+    isStarkCardParticipatingInChallenge() {
+        return this.game.currentChallenge && this.controller.cardsInPlay.any(card => {
             return this.game.currentChallenge.isParticipating(card) && card.isFaction('stark');
-        })) {
-            return false;
-        }
-
-        this.game.promptForSelect(player, {
-            cardCondition: card => card.isFaction('stark') && !card.kneeled,
-            activePromptTitle: 'Select character',
-            source: this,
-            onSelect: (player, card) => this.onCardSelected(player, card)
         });
-
-        return true;
-    }
-
-    onCardSelected(player, card) {
-        player.kneelCard(card);
-
-        if(this.game.currentChallenge.attackingPlayer === this.controller) {
-            this.game.currentChallenge.addAttacker(card);
-        } else {
-            this.game.currentChallenge.addDefender(card);
-        }
-
-        this.game.addMessage('{0} uses {1} to kneel {2} and add them to the challenge', player, this, card);
-
-        return true;
     }
 }
 
