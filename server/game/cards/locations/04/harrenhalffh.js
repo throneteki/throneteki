92bcbc211743459e0467351f645eabe219c79d9e
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-02-28 20:14:46 +0100

    Fix Harrenhal and port to cost framework

--- server/game/cards/locations/04/harrenhalffh.js
+++ server/game/cards/locations/04/harrenhalffh.js
@@ -1,37 +1,35 @@
 const DrawCard = require('../../../drawcard.js');
 
 class Harrenhal extends DrawCard {
-    setupCardAbilities() {
+    setupCardAbilities(ability) {
         this.action({
             title: 'Kneel Harrenhal to put a character in play',
-            method: 'kneel',
-            phase: 'challenge'
+            cost: ability.costs.kneelSelf(),
+            phase: 'challenge',
+            handler: context => {
+                this.game.promptForSelect(context.player, {
+                    cardCondition: card => (
+                        card.location === 'hand' && 
+                        card.getType() === 'character' && 
+                        card.controller === this.controller &&
+                        (card.isFaction('lannister') || card.hasTrait('House Bolton'))),
+                    activePromptTitle: 'Select a character',
+                    source: this,
+                    onSelect: (player, card) => {
+                        player.playCard(card, 'play area');
+
+                        this.atEndOfPhase(ability => ({
+                            match: card,
+                            effect: ability.effects.killIfStillInPlay(false)
+                        }));
+
+                        this.game.addMessage('{0} kneels {1} to put {2} into play from their hand', player, this, card);
+
+                        return true;
+                    }
+                });
+            }
         });
-    }
-
-    kneel(player) {
-        this.game.promptForSelect(player, {
-            cardCondition: card => card.location === 'hand' && card.getType() === 'character' && card.controller === this.controller &&
-                (card.isFaction('lannister') || card.hasTrait('House Bolton')),
-            activePromptTitle: 'Select a character',
-            source: this,
-            onSelect: (player, card) => this.onCardSelected(player, card)
-        });
-    }
-
-    onCardSelected(player, card) {
-        player.kneelCard(this);
-
-        player.moveCard(card, 'play area');
-
-        this.atEndOfPhase(ability => ({
-            match: card,
-            effect: ability.effects.killIfStillInPlay(false)
-        }));
-
-        this.game.addMessage('{0} kneels {1} to put {2} into play from their hand', player, this, card);
-
-        return true;
     }
 }
 
