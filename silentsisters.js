27675aa0ae7b5520e143d709e2828df6f8b5b1f8
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-02-28 15:50:13 +0100

    Implement Silent Sisters

---
+++ silentsisters.js
@@ -0,0 +1,14 @@
+const DrawCard = require('../../../drawcard.js');
+
+class SilentSisters extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            effect: ability.effects.dynamicStrength(() => this.controller.deadPile.length)
+        });
+    }
+}
+
+SilentSisters.code = '04097';
+
+module.exports = SilentSisters;
