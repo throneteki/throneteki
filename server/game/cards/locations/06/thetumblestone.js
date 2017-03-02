ed35721c3e4cdfde134f4378672416053e2017b8
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-03-02 01:26:23 +0100

    Use cost framework

--- server/game/cards/locations/06/thetumblestone.js
+++ server/game/cards/locations/06/thetumblestone.js
@@ -1,7 +1,7 @@
 const DrawCard = require('../../../drawcard.js');
 
 class TheTumblestone extends DrawCard {
-    setupCardAbilities() {  
+    setupCardAbilities(ability) {  
         this.reaction({
             when: {
                 // Currently has false positive when power is moved to a character, should only trigger on 'gains'
@@ -14,8 +14,8 @@ class TheTumblestone extends DrawCard {
                     return true;
                 }
             },
+            cost: ability.costs.discardGold(),
             handler: () => {
-                this.removeToken('gold', 1);
                 this.standCard.controller.standCard(this.standCard);
                 this.game.addMessage('{0} discards a gold from {1} to stand {2}', this.controller, this, this.standCard);
             }
