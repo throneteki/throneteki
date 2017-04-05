4f5c859c7bc5558023bab324d8e3450f82a7bb6e
Stefano Zacchiroli <zack@upsilon.cc>
2017-04-05 11:33:49 +0200

    uniform coding style within different parts of Rattleshirt
    style-only, no functional changes

--- server/game/cards/characters/07/rattleshirt.js
+++ server/game/cards/characters/07/rattleshirt.js
@@ -3,10 +3,10 @@ const DrawCard = require('../../../drawcard.js');
 class Rattleshirt extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            condition: () => (
-                this.game.currentChallenge &&
-                this.game.currentChallenge.isAttacking(this) &&
-                this.game.currentChallenge.attackers.length === 1),
+            condition: () =>
+                this.game.currentChallenge
+                && this.game.currentChallenge.isAttacking(this)
+                && this.game.currentChallenge.attackers.length === 1,
             match: card =>
                 card.getType() === 'character'
                 && card.attachments.size() === 0,
