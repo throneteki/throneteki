0dc3cff77a134203f0d10240fe2b1aa07ef830ed
Brian Upton <ystros@gmail.com>
2017-04-20 09:52:32 -0700

    Make logic clearer for Beggar King trigger.

--- server/game/cards/attachments/04/beggarking.js
+++ server/game/cards/attachments/04/beggarking.js
@@ -1,5 +1,3 @@
-const _ = require('underscore');
-
 const DrawCard = require('../../../drawcard.js');
 
 class BeggarKing extends DrawCard {
@@ -9,12 +7,15 @@ class BeggarKing extends DrawCard {
         });
         this.reaction({
             when: {
-                onPlotsRevealed: event => (
-                    _.any(event.plots, plot => (
-                        plot.controller !== this.controller &&
-                        this.controller.activePlot.getIncome(true) < plot.getIncome(true)
-                    ))
-                )
+                onPlotsRevealed: event => {
+                    let opponent = this.game.getOtherPlayer(this.controller);
+
+                    if(!opponent || !event.plots.includes(opponent.activePlot)) {
+                        return false;
+                    }
+
+                    return this.controller.activePlot.getIncome(true) < opponent.activePlot.getIncome(true);
+                }
             },
             cost: ability.costs.kneelSelf(),
             handler: () => {
