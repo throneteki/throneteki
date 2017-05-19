b29c4f4a5cdfe0799e9647bf7da1025383f79afd
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-05-19 19:56:36 +0200

    Implement Dissension

---
+++ server/game/cards/events/04/dissension.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../../drawcard.js');
+
+class Dissension extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Discard Ally from play',
+            phase: 'marshal',
+            target: {
+                activePromptTitle: 'Select an Ally',
+                cardCondition: card => card.hasTrait('Ally') && card.getType() === 'character'
+            },
+            handler: context => {
+                context.target.controller.discardCard(context.target);
+                this.game.addMessage('{0} plays {1} to discard {2}',
+                                      this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+Dissension.code = '04059';
+
+module.exports = Dissension;
