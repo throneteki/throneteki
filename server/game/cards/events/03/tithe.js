e70efff9f856165a40cb6009182d1d54e703cf4a
DukeTax <DukeTax@users.noreply.github.com>
2017-05-20 20:01:44 +0200

    Implement Tithe (#920)

---
+++ server/game/cards/events/03/tithe.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../../drawcard.js');
+
+class Tithe extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Kneel character to gain 2 gold',
+            cost: ability.costs.kneel(card => card.isFaction('neutral') && card.getType() === 'character'),
+            handler: context => {
+                this.game.addGold(this.controller, 2);
+                this.game.addMessage('{0} uses {1} to kneel {2} to gain 2 gold',
+                                      this.controller, this, context.kneelingCostCard);
+            }
+        });
+    }
+}
+
+Tithe.code = '03045';
+
+module.exports = Tithe;
