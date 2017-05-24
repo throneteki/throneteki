21cab5f216973a6073aad609a6d96d0338218bd9
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-05-24 02:17:14 +0200

    Implement The Pack Survives

---
+++ server/game/cards/events/03/thepacksurvives.js
@@ -0,0 +1,39 @@
+const DrawCard = require('../../../drawcard.js');
+
+class ThePackSurvives extends DrawCard {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            canCancel: true,
+            title: () => 'Sac Direwolf',
+            when: {
+                onCardAbilityInitiated: event => event.source.getType() === 'event' && event.player !== this.controller
+            },
+            cost: ability.costs.sacrifice(card => card.hasTrait('Direwolf')),
+            handler: context => {
+                context.event.cancel();
+                
+                this.game.addMessage('{0} plays {1} and sacrifices {2} to cancel {3}', 
+                                      this.controller, this, context.sacrificeCostCard, context.event.source);
+            }
+        });
+
+        this.interrupt({
+            canCancel: true,
+            title: () => 'Kneel chars',
+            when: {
+                onCardAbilityInitiated: event => event.source.getType() === 'event' && event.player !== this.controller
+            },
+            cost: ability.costs.kneelMultiple(2, card => card.isFaction('stark') && card.getType() === 'character'),
+            handler: context => {
+                context.event.cancel();
+                
+                this.game.addMessage('{0} plays {1} and kneels {2} to cancel {3}', 
+                                      this.controller, this, context.kneelingCostCards, context.event.source);
+            }
+        });
+    }
+}
+
+ThePackSurvives.code = '03023';
+
+module.exports = ThePackSurvives;
