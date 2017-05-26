a7da96e4fbb6a8c4992edc860cd032b6a845085a
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-05-26 02:26:02 +0200

    Implement Underground Vault

---
+++ server/game/cards/locations/06/undergroundvault.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../../drawcard.js');
+
+class UndergroundVault extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Kneel this card to gain gold',
+            phase: 'marshal',
+            cost: ability.costs.kneelSelf(),
+            handler: context => {
+                let opponent = this.game.getOtherPlayer(this.controller);
+                let gold = opponent && opponent.activePlot.getIncome(true) >= 5 ? 2 : 1;
+
+                this.game.addGold(context.player, gold);
+                this.game.addMessage('{0} kneels {1} to gain {2} gold', context.player, this, gold);
+            }
+        });
+    }
+}
+
+UndergroundVault.code = '06046';
+
+module.exports = UndergroundVault;
