3bd54e626ebdd3f43fb120fbe2cedf75167e3e08
Samuel Linde <samuel@dontblink.se>
2017-05-30 18:07:43 +0200

    Implement Trial by Combat

---
+++ server/game/cards/events/02/trialbycombat.js
@@ -0,0 +1,36 @@
+const DrawCard = require('../../../drawcard.js');
+const ApplyClaim = require('../../../gamesteps/challenge/applyclaim.js');
+
+class TrialByCombat extends DrawCard {
+	setupCardAbilities(ability) {
+		this.interrupt({
+            when: {
+                onClaimApplied: (event, challenge) => (
+                    challenge.winner === this.controller &&
+                    challenge.attackingPlayer === this.controller &&
+                    challenge.challengeType === 'intrigue'
+                )
+            },
+            handler: context => {
+                let opponent = this.game.getOtherPlayer(this.controller);
+
+                context.skipHandler();
+
+                this.game.addMessage('{0} uses {1} to have {2} apply military claim instead of intrigue claim', this.controller, this, opponent);
+
+                let replacementChallenge = {
+                    challengeType: 'military',
+                    claim: this.controller.getClaim(),
+                    loser: opponent,
+                    winner: this.controller
+                };
+
+                this.game.queueStep(new ApplyClaim(this.game, replacementChallenge));
+            }            
+		})
+	}
+}
+
+TrialByCombat.code = '02090';
+
+module.exports = TrialByCombat;
