7017e26de83af6167e25aaf10eaf033cf1c5decb
Samuel Linde <samuel@dontblink.se>
2017-05-31 22:48:03 +0200

    Implement Tyrion's Chain

---
+++ server/game/cards/events/04/tyrionschain.js
@@ -0,0 +1,64 @@
+const _ = require('underscore');
+const DrawCard = require('../../../drawcard.js');
+
+class TyrionsChain extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                afterChallenge: (event, challenge) => (
+                    challenge.winner === this.controller &&
+                    this.hasParticipatingUniqueLannister()
+                )
+            },
+            max: ability.limit.perPhase(1),
+            condition: () => this.game.anyPlotHasTrait('War'),
+            handler: () => {
+                let warPlots = this.getRevealedWarPlots();
+
+                let buttons = _.map(warPlots, card => ({
+                    method: 'selectWarPlot', card: card
+                }));
+
+                this.game.promptWithMenu(this.controller, this, {
+                    activePrompt: {
+                        menuTitle: 'Select a War plot to resolve',
+                        buttons: buttons
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    hasParticipatingUniqueLannister() {
+        return this.controller.anyCardsInPlay(card => this.game.currentChallenge.isParticipating(card) && card.isUnique() && card.isFaction('lannister') && card.getType() === 'character');
+    }
+
+    getRevealedWarPlots() {
+        var revealedPlots = [];
+        _.each(this.game.getPlayers(), player => {
+            if(player.activePlot && player.activePlot.hasTrait('War')) {
+                revealedPlots = revealedPlots.concat(player.activePlot);
+            }
+        });
+        return revealedPlots;
+    }
+
+    selectWarPlot(player, card) {
+        let warPlot = this.controller.findCardByUuidInAnyList(card);
+        this.resolving = true;
+
+        this.game.addMessage('{0} uses {1} to initiate the When Revealed ability of {2}', this.controller, this, warPlot);
+        warPlot.controller = this.controller;
+        this.game.raiseMergedEvent('onPlotsWhenRevealed', { plots: [warPlot] });
+        this.game.queueSimpleStep(() => {
+            warPlot.controller = warPlot.owner;
+            this.resolving = false;
+        });
+        return true;
+    }
+}
+
+TyrionsChain.code = '04110';
+
+module.exports = TyrionsChain;
