fba310103b0b974bd5dbdb8da808d42892054b77
DukeTax <DukeTax@users.noreply.github.com>
2017-05-31 02:16:18 +0200

    Implement Maester of Sunspear (#980)

---
+++ server/game/cards/characters/06/maesterofsunspear.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../../drawcard.js');
+
+class MaesterOfSunspear extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: (event, challenge) => this.controller === challenge.loser && challenge.isParticipating(this)
+            },
+            target: {
+                activePromptTitle: 'Select an attachment',
+                cardCondition: card => card.location === 'play area' && card.getType() === 'attachment'
+            },
+            handler: context => {
+                context.target.owner.returnToHand(context.target);
+                this.game.addMessage('{0} uses {1} to return {2} to its owner\'s hand',
+                                      this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+MaesterOfSunspear.code = '06055';
+
+module.exports = MaesterOfSunspear;
