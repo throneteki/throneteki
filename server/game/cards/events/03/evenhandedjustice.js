62f8efe04f98610d551a3530d21abdfb5ab1657d
Brian Upton <ystros@gmail.com>
2017-06-01 16:49:39 -0700

    Convert Even Handed Justice to ability API.

--- server/game/cards/events/03/evenhandedjustice.js
+++ server/game/cards/events/03/evenhandedjustice.js
@@ -3,74 +3,36 @@ const _ = require('underscore');
 const DrawCard = require('../../../drawcard.js');
 
 class EvenHandedJustice extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Kneel a character for each player',
+            targets: {
+                yourCard: {
+                    activePromptTitle: 'Select a standing character of yours',
+                    cardCondition: card =>
+                        !card.kneeled
+                        && card.getType() === 'character'
+                        && card.controller === this.controller,  // event controller
+                    gameAction: 'kneel'
+                },
+                opponentCard: {
+                    activePromptTitle: 'Select a standing character controlled by your opponent',
+                    cardCondition: card =>
+                        !card.kneeled
+                        && card.getType() === 'character'
+                        && card.controller !== this.controller,  // not event controller
+                    gameAction: 'kneel'
+                }
+            },
+            handler: context => {
+                let kneeledCards = [context.targets.yourCard, context.targets.opponentCard];
 
-    canPlay(player, card) {
-        if(player !== this.controller || this !== card) {
-            return false;
-        }
-
-        return (_.every(this.game.getPlayers(), this.hasStandingCharacters)
-                && super.canPlay(player, card));
-    }
-
-    hasStandingCharacters(player) {
-        return player.anyCardsInPlay(card => card.getType() === 'character' && !card.kneeled);
-    }
-
-    play(player) {
-        this.toKneel = [];  // characters to kneel (later)
-
-        // step 1: select opponent character
-        this.game.promptForSelect(player, {
-            activePromptTitle: 'Select a standing character controlled by your opponent',
-            source: this,
-            cardCondition: card =>
-                !card.kneeled
-                && card.getType() === 'character'
-                && card.controller !== this.controller,  // not event controller
-            gameAction: 'kneel',
-            onSelect: (player, card) => this.onFirstCardSelected(player, card)
+                _.each(kneeledCards, card => card.controller.kneelCard(card));
+                this.game.addMessage('{0} uses {1} to kneel {2}',
+                                     this.controller, this, kneeledCards);
+            }
         });
-
-        return true;
     }
-
-    onFirstCardSelected(player, card) {
-        this.toKneel.push(card);
-
-        // step 2: select event controller character
-        this.game.promptForSelect(player, {
-            activePromptTitle: 'Select a standing character of yours',
-            source: this,
-            cardCondition: card =>
-                !card.kneeled
-                && card.getType() === 'character'
-                && card.controller === this.controller,  // event controller
-            gameAction: 'kneel',
-            onSelect: (player, card) => this.onSecondCardSelected(player, card)
-        });
-
-        return true;
-    }
-
-    onSecondCardSelected(player, card) {
-        this.toKneel.push(card);
-
-        if(! _.every(this.game.getPlayers(), player => _.some(this.toKneel, card => card.controller === player))) {
-            // proceed only if toKneel contain at least one card per player
-            return true;
-        }
-
-        // step 3: kneel selected characters
-        _.each(this.toKneel, card => card.controller.kneelCard(card));
-
-        var kneeledCards = _.map(this.toKneel, card => card.name);
-        this.game.addMessage('{0} uses {1} to kneel {2}',
-                             player, this, kneeledCards);
-
-        return true;
-    }
-
 }
 
 EvenHandedJustice.code = '03026';
