e5cd80d9af4767342694e4e5589ab7c9fe692e69
Brian Upton <ystros@gmail.com>
2017-06-09 17:17:40 -0700

    Change Pack Survives to use a `choose` cost.

--- server/game/cards/events/03/thepacksurvives.js
+++ server/game/cards/events/03/thepacksurvives.js
@@ -4,31 +4,23 @@ class ThePackSurvives extends DrawCard {
     setupCardAbilities(ability) {
         this.interrupt({
             canCancel: true,
-            title: () => 'Sacrifice Direwolf',
             when: {
                 onCardAbilityInitiated: event => event.source.getType() === 'event' && event.player !== this.controller
             },
-            cost: ability.costs.sacrifice(card => card.hasTrait('Direwolf')),
+            cost: ability.costs.choose({
+                'Sacrifice Direwolf': ability.costs.sacrifice(card => card.hasTrait('Direwolf')),
+                'Kneel 2 Starks': ability.costs.kneelMultiple(2, card => card.isFaction('stark') && card.getType() === 'character')
+            }),
             handler: context => {
                 context.event.cancel();
-                
-                this.game.addMessage('{0} plays {1} and sacrifices {2} to cancel {3}', 
-                                      this.controller, this, context.sacrificeCostCard, context.event.source);
-            }
-        });
 
-        this.interrupt({
-            canCancel: true,
-            title: () => 'Kneel characters',
-            when: {
-                onCardAbilityInitiated: event => event.source.getType() === 'event' && event.player !== this.controller
-            },
-            cost: ability.costs.kneelMultiple(2, card => card.isFaction('stark') && card.getType() === 'character'),
-            handler: context => {
-                context.event.cancel();
-                
-                this.game.addMessage('{0} plays {1} and kneels {2} to cancel {3}', 
-                                      this.controller, this, context.kneelingCostCards, context.event.source);
+                if(context.sacrificeCostCard) {
+                    this.game.addMessage('{0} plays {1} and sacrifices {2} to cancel {3}',
+                                          this.controller, this, context.sacrificeCostCard, context.event.source);
+                } else {
+                    this.game.addMessage('{0} plays {1} and kneels {2} to cancel {3}',
+                                          this.controller, this, context.kneelingCostCards, context.event.source);
+                }
             }
         });
     }
