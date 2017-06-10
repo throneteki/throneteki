e38459f6577376dd79cf10eca1c418a92eadec5f
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-06-10 17:45:53 +0200

    Modify to use new choose cost

--- server/game/cards/events/07/ishallwinnoglory.js
+++ server/game/cards/events/07/ishallwinnoglory.js
@@ -6,38 +6,15 @@ class IShallWinNoGlory extends DrawCard {
             when: {
                 onChallenge: (event, challenge) => challenge.defendingPlayer === this.controller
             },
-            title: () => 'Kneel Builders',
-            cost: ability.costs.kneelMultiple(3, card => card.hasTrait('Builder') && card.getType() === 'character'),
+            cost: ability.costs.choose({
+                'Kneel Builders': ability.costs.kneelMultiple(3, card => card.hasTrait('Builder') && card.getType() === 'character'),
+                'Kneel Rangers': ability.costs.kneelMultiple(3, card => card.hasTrait('Ranger') && card.getType() === 'character'),
+                'Kneel Stewards': ability.costs.kneelMultiple(3, card => card.hasTrait('Steward') && card.getType() === 'character')
+            }),
             handler: (context) => {
                 this.game.addMessage('{0} plays {1} and kneels {2} to end the challenge immediately with no winner or loser', 
                                       this.controller, this, context.kneelingCostCards);
 
-                this.game.currentChallenge.cancelChallenge();
-            }
-        });
-        this.reaction({
-            when: {
-                onChallenge: (event, challenge) => challenge.defendingPlayer === this.controller
-            },
-            title: () => 'Kneel Rangers',
-            cost: ability.costs.kneelMultiple(3, card => card.hasTrait('Ranger') && card.getType() === 'character'),
-            handler: (context) => {
-                this.game.addMessage('{0} plays {1} and kneels {2} to end the challenge immediately with no winner or loser', 
-                                      this.controller, this, context.kneelingCostCards);
-
-                this.game.currentChallenge.cancelChallenge();
-            }
-        });
-        this.reaction({
-            when: {
-                onChallenge: (event, challenge) => challenge.defendingPlayer === this.controller
-            },
-            title: () => 'Kneel Stewards',
-            cost: ability.costs.kneelMultiple(3, card => card.hasTrait('Steward') && card.getType() === 'character'),
-            handler: (context) => {
-                this.game.addMessage('{0} plays {1} and kneels {2} to end the challenge immediately with no winner or loser', 
-                                      this.controller, this, context.kneelingCostCards);
-                            
                 this.game.currentChallenge.cancelChallenge();
             }
         });
