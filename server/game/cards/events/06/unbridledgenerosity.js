85f6df6549a5af4beada6dc883f63c002de7eeca
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-06-24 22:04:51 +0200

    Use target API

--- server/game/cards/events/06/unbridledgenerosity.js
+++ server/game/cards/events/06/unbridledgenerosity.js
@@ -6,27 +6,21 @@ class UnbridledGenerosity extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Put gold on cards',
-            handler: () => {
-                this.game.promptForSelect(this.controller, {
-                    numCards: 3,
-                    multiSelect: true,
-                    activePromptTitle: 'Select up to 3 cards',
-                    source: this,
-                    cardCondition: card => card.location === 'play area',
-                    onSelect: (player, cards) => this.targetsSelected(player, cards)
+            target: {
+                activePromptTitle: 'Select up to 3 cards',
+                cardCondition: card => card.location === 'play area',
+                numCards: '3',
+                multiSelect: true
+            },
+            handler: context => {
+                _.each(context.target, card => {
+                    card.addToken('gold', 1);
                 });
+
+                this.game.addMessage('{0} plays {1} to move 1 gold from the treasury to {2}',
+                                      this.controller, this, context.target);
             }
         });
-    }
-    targetsSelected(player, cards) {
-        _.each(cards, card => {
-            card.addToken('gold', 1);
-        });
-
-        this.game.addMessage('{0} plays {1} to move 1 gold from the treasury to {2}',
-                              this.controller, this, cards);
-
-        return true;
     }
 }
 
