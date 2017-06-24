1c9d0ccaa3e105bffca1ec002a0547501c674b7d
DukeTax <DukeTax@users.noreply.github.com>
2017-06-24 22:23:59 +0200

    Implement Unbridled Generosity (#1095)

---
+++ server/game/cards/events/06/unbridledgenerosity.js
@@ -0,0 +1,29 @@
+const _ = require('underscore');
+
+const DrawCard = require('../../../drawcard.js');
+
+class UnbridledGenerosity extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Put gold on cards',
+            target: {
+                activePromptTitle: 'Select up to 3 cards',
+                cardCondition: card => card.location === 'play area',
+                numCards: '3',
+                multiSelect: true
+            },
+            handler: context => {
+                _.each(context.target, card => {
+                    card.addToken('gold', 1);
+                });
+
+                this.game.addMessage('{0} plays {1} to move 1 gold from the treasury to {2}',
+                                      this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+UnbridledGenerosity.code = '06118';
+
+module.exports = UnbridledGenerosity;
