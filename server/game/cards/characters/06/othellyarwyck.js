af0b4e64df291f4544c17dfcafe517904d2b68dc
DukeTax <DukeTax@users.noreply.github.com>
2017-07-05 22:13:26 +0200

    Implement remaining cards from The Red Wedding (#1114)
    * Implement Dacey Mormont
    * Implement Winterfell Archery Range
    * Implement Arbor Vineyard
    * Implement Othell Yarwyck
    * Implement Silence's Crew
    * Implement Plunder
    * Implement Breaker of Chains
    * Implement Improved Fortifications

---
+++ server/game/cards/characters/06/othellyarwyck.js
@@ -0,0 +1,30 @@
+const DrawCard = require('../../../drawcard.js');
+
+class OthellYarwyck extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Give icon',
+            cost: ability.costs.kneel(card => card.isFaction('thenightswatch') && card.getType() === 'location'),
+            target: {
+                activePromptTitle: 'Select a character',
+                cardCondition: card => card.location === 'play area' && card.isFaction('thenightswatch') && 
+                                       card.getType() === 'character'
+            },
+            handler: context => {
+                this.game.promptForIcon(this.controller, icon => {
+                    this.untilEndOfPhase(ability => ({
+                        match: context.target,
+                        effect: ability.effects.addIcon(icon)
+                    }));
+
+                    this.game.addMessage('{0} uses {1} and kneels {2} to give {3} {4} {5} icon until the end of the phase',
+                            this.controller, this, context.kneelingCostCard, context.target, icon === 'intrigue' ? 'an' : 'a', icon);
+                });
+            }
+        });
+    }
+}
+
+OthellYarwyck.code = '06065';
+
+module.exports = OthellYarwyck;
---
+++ server/game/cards/events/06/plunder.js
@@ -0,0 +1,36 @@
+const DrawCard = require('../../../drawcard.js');
+
+class Plunder extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Gain gold',
+            condition: () => this.getGold() >= 1,
+            cost: ability.costs.kneelFactionCard(),
+            handler: () => {
+                let gold = this.getGold();
+                this.game.addGold(this.controller, gold);
+                this.game.addMessage('{0} uses {1} and kneels their faction card to gain {2} gold', this.controller, this, gold);
+            }
+        });
+    }
+
+    getGold() {
+        let opponent = this.game.getOtherPlayer(this.controller);
+
+        if(!opponent) {
+            return 0;
+        }
+
+        return opponent.allCards.reduce((num, card) => {
+            if(card.location === 'discard pile' && (card.getType() === 'location' || card.getType() === 'attachment')) {
+                return num + 1;
+            }
+
+            return num;
+        }, 0);
+    }
+}
+
+Plunder.code = '06072';
+
+module.exports = Plunder;
