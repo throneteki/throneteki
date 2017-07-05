2e7885f0abb1bc7b01391e11671001e2124be7ba
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-07-05 15:26:00 +0200

    Implement Improved Fortifications

---
+++ server/game/cards/attachments/06/improvedfortifications.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../../drawcard.js');
+
+class ImprovedFortifications extends DrawCard {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            canCancel: true,
+            when: {
+                onCardLeftPlay: event => event.card === this.parent
+            },
+            cost: ability.costs.sacrificeSelf(),
+            handler: context => {
+                context.event.saveCard(this.parent);
+                this.game.addMessage('{0} sacrifices {1} to save {2}', this.controller, this, this.parent);
+            }
+        });
+    }
+
+    canAttach(player, card) {
+        if(card.getType() !== 'location') {
+            return false;
+        }
+
+        return super.canAttach(player, card);
+    }
+}
+
+ImprovedFortifications.code = '06066';
+
+module.exports = ImprovedFortifications;
