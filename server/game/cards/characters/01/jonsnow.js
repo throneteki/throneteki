0ae34e630621c02ada4f325bcfdeccf68c6d9be1
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-07-06 16:07:16 +0200

    Fix Jon Snow

--- server/game/cards/characters/01/jonsnow.js
+++ server/game/cards/characters/01/jonsnow.js
@@ -3,16 +3,17 @@ const DrawCard = require('../../../drawcard.js');
 class JonSnow extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            condition: () => !this.kneeled && this.game.currentChallenge && this.hasAttackingNightsWatch(),
+            condition: () => !this.kneeled && this.game.currentChallenge && this.hasOtherAttackingNightsWatch(),
             match: this,
             effect: ability.effects.consideredToBeAttacking()
         });
     }
 
-    hasAttackingNightsWatch() {
+    hasOtherAttackingNightsWatch() {
         return this.controller.anyCardsInPlay(card => this.game.currentChallenge.isAttacking(card) && 
-                                              card.isFaction('thenightswatch') &&
-                                              card.getType() === 'character');
+                                                      card.isFaction('thenightswatch') &&
+                                                      card.getType() === 'character' &&
+                                                      card !== this);
     }
 }
 
