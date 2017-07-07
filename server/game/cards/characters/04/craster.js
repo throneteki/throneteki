b58b83bf399ef664b996e91b42eb7db6f461e051
Brian Upton <ystros@gmail.com>
2017-07-06 19:27:49 -0700

    Implement Craster's resurrect ability.

--- server/game/cards/characters/04/craster.js
+++ server/game/cards/characters/04/craster.js
@@ -1,12 +1,42 @@
+const _ = require('underscore');
+
 const DrawCard = require('../../../drawcard.js');
 
+class KillTracker {
+    constructor(game) {
+        this.killedThisPhase = [];
+        game.on('onCharacterKilled', event => this.killedThisPhase.push(event.card));
+        game.on('onPhaseStarted', () => this.killedThisPhase = []);
+    }
+
+    anyKilled() {
+        return this.killedThisPhase.length !== 0;
+    }
+}
+
 class Craster extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+        this.tracker = new KillTracker(this.game);
+    }
+
     setupCardAbilities(ability) {
         this.persistentEffect({
             match: this,
             effect: ability.effects.immuneTo(card => card.hasTrait('Omen'))
         });
-        // TODO: Sacrifice to put characters killed this phase back into play.
+        this.action({
+            title: 'Sacrifice to resurrect',
+            cost: ability.costs.sacrificeSelf(),
+            condition: () => this.tracker.anyKilled(),
+            handler: () => {
+                let characters = this.tracker.killedThisPhase;
+                _.each(characters, character => {
+                    character.owner.putIntoPlay(character);
+                });
+                this.game.addMessage('{0} sacrifices {1} to put into play each character killed this phase', this.controller, this);
+            }
+        });
     }
 }
 
