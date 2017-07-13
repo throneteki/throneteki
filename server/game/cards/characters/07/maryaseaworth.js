0161d5c82a34bdf39b505a091abdf53b603a8318
Brian Upton <ystros@gmail.com>
2017-07-12 22:56:37 -0700

    Use correct target for Marya Seaworth.
    Fixes THRONETEKI-Z4

--- server/game/cards/characters/07/maryaseaworth.js
+++ server/game/cards/characters/07/maryaseaworth.js
@@ -9,8 +9,9 @@ class MaryaSeaworth extends DrawCard {
             cost: ability.costs.payGold(1),
             limit: ability.limit.perPhase(2),
             handler: context => {
-                context.target.controller.kneelCard(context.event.target);
-                this.game.addMessage('{0} uses {1} and pays 1 gold to kneel {2}', this.controller, this, context.event.target);
+                let target = context.event.target;
+                target.controller.kneelCard(target);
+                this.game.addMessage('{0} uses {1} and pays 1 gold to kneel {2}', this.controller, this, target);
             }
         });
     }
