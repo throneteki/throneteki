ae8e63411cc8338303a2ab46207ee76faa42aeb2
Stefano Zacchiroli <zack@upsilon.cc>
2017-07-13 17:06:19 +0200

    Balon GreyJoy (CtA): add log message for strength boost

--- server/game/cards/characters/04/balongreyjoy.js
+++ server/game/cards/characters/04/balongreyjoy.js
@@ -18,13 +18,16 @@ class BalonGreyjoy extends DrawCard {
         });
 
         this.action({
-            title: 'Kneel a greyjoy location',
+            title: 'Kneel a Greyjoy location to give +1 to loyal characters',
             cost: ability.costs.kneel(card => card.isFaction('greyjoy') && card.getType() === 'location'),
-            handler: () => {
+            handler: context => {
                 this.untilEndOfChallenge(ability => ({
                     match: card => card.isLoyal() && card.controller === this.controller && card.getType() === 'character',
                     effect: ability.effects.modifyStrength(1)
                 }));
+
+                this.game.addMessage('{0} uses {1} to kneel {2} and give +1 STR to each loyal character they control until the end of the challenge',
+                                     this.controller, this, context.kneelingCostCard);
             }
         });
     }
