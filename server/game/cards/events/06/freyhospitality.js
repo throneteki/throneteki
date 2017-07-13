30a54feb4fa9360d1e12e2ab8857d29a5e97e2da
DukeTax <DukeTax@users.noreply.github.com>
2017-07-13 19:50:06 +0200

    Fix Frey Hospitality (#1157)
    * Kill characters simultaneously
    * Fix prompt title
    * Close prompt after selecting targets

--- server/game/cards/events/06/freyhospitality.js
+++ server/game/cards/events/06/freyhospitality.js
@@ -1,15 +1,12 @@
-const _ = require('underscore');
-
 const DrawCard = require('../../../drawcard.js');
 
 class FreyHospitality extends DrawCard {
     setupCardAbilities() {
         this.reaction({
             when: {
-                afterChallenge: (event, challenge) => (
-                    challenge.winner === this.controller &&
-                    this.controller.getNumberOfChallengesInitiated() === 3 &&
-                    this.hasAttackingFrey())
+                afterChallenge: (event, challenge) => challenge.winner === this.controller &&
+                                                      this.controller.getNumberOfChallengesInitiated() === 3 &&
+                                                      this.hasAttackingFrey()
             },
             handler: () => {
                 let numTargets = this.game.currentChallenge.strengthDifference >= 20 ? 3 : 1;
@@ -17,13 +14,12 @@ class FreyHospitality extends DrawCard {
                 this.game.promptForSelect(this.controller, {
                     numCards: numTargets,
                     multiSelect: true,
-                    activePromptTitle: 'Select ' + numTargets + ' character(s)',
+                    activePromptTitle: 'Select character(s)',
                     source: this,
                     gameAction: 'kill',
-                    cardCondition: card => (
-                        card.location === 'play area' && 
-                        card.controller !== this.controller && 
-                        card.getType() === 'character'),
+                    cardCondition: card => card.location === 'play area' && 
+                                           card.controller !== this.controller && 
+                                           card.getType() === 'character',
                     onSelect: (player, cards) => this.targetsSelected(player, cards)
                 });
             }
@@ -34,12 +30,11 @@ class FreyHospitality extends DrawCard {
         if(this.game.currentChallenge.strengthDifference >= 20 && cards.length !== 3) {
             return false;
         }
-
-        _.each(cards, card => {
-            this.game.killCharacter(card);
-        });
-
+        
+        this.game.killCharacters(cards);
         this.game.addMessage('{0} plays {1} to kill {2}', this.controller, this, cards);
+
+        return true;
     }
 
     hasAttackingFrey() {
