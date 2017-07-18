e9db20364ff180e1ee63b9bf6ac84737c25e1bd5
Brian Upton <ystros@gmail.com>
2017-07-18 16:59:13 -0700

    Group keyword processing by keyword instead of card

--- server/game/gamesteps/keywordwindow.js
+++ server/game/gamesteps/keywordwindow.js
@@ -9,36 +9,45 @@ class KeywordWindow extends BaseStep {
     constructor(game, challenge) {
         super(game);
         this.challenge = challenge;
-        this.winnerCards = challenge.getWinnerCards();
+        this.winnerCardsWithContext = _.map(challenge.getWinnerCards(), card => {
+            return { card: card, context: { game: this.game, challenge: this.challenge, source: card } };
+        });
     }
 
     continue() {
-        let appliedIntimidate = false;
+        if(!this.challenge.winner) {
+            return;
+        }
 
-        _.each(this.winnerCards, card => {
-            let context = { game: this.game, challenge: this.challenge, source: card };
-
-            _.each(challengeKeywords, keyword => {
-                // It is necessary to check whether intimidate has been applied
-                // here instead of in the ability class because the individual
-                // keywords are resolved asynchronously but are queued up
-                // synchronously here. So two intimidates could be queued when
-                // only one is allowed.
-                if(keyword === 'intimidate' && appliedIntimidate) {
-                    return;
-                }
-
-                let ability = GameKeywords[keyword];
-                if(card.hasKeyword(keyword) && ability.meetsRequirements(context)) {
-                    appliedIntimidate = appliedIntimidate || (keyword === 'intimidate');
-                    this.game.resolveAbility(ability, context);
-                }
-            });
-
-            this.game.checkWinCondition(this.challenge.winner);
+        _.each(challengeKeywords, keyword => {
+            this.applyKeyword(keyword);
         });
 
         return true;
+    }
+
+    applyKeyword(keyword) {
+        let appliedIntimidate = false;
+        let ability = GameKeywords[keyword];
+        _.each(this.winnerCardsWithContext, participant => {
+            let {card, context} = participant;
+
+            // It is necessary to check whether intimidate has been applied
+            // here instead of in the ability class because the individual
+            // keywords are resolved asynchronously but are queued up
+            // synchronously here. So two intimidates could be queued when
+            // only one is allowed.
+            if(keyword === 'intimidate' && appliedIntimidate) {
+                return;
+            }
+
+            if(card.hasKeyword(keyword) && ability.meetsRequirements(context)) {
+                appliedIntimidate = appliedIntimidate || (keyword === 'intimidate');
+                this.game.resolveAbility(ability, context);
+            }
+        });
+
+        this.game.checkWinCondition(this.challenge.winner);
     }
 }
 
