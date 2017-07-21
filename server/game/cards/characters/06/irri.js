2026fbd4bccf0b42387ef7023e54a936dcd67f44
DukeTax <DukeTax@users.noreply.github.com>
2017-07-22 00:13:26 +0200

    Implement Irri & Overthrowing the Masters (#1213)
    * Implement Irri
    * Implement Overthrowing the Masters

---
+++ server/game/cards/characters/06/irri.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../../drawcard.js');
+
+class Irri extends DrawCard {
+    setupCardAbilities(ability) {  
+        this.action({
+            title: 'Stand Lord or Lady',
+            cost: ability.costs.discardGold(),
+            limit: ability.limit.perPhase(1),
+            target: {
+                activePromptTitle: 'Select a character to stand',
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' &&
+                                       (card.hasTrait('Lord') || card.hasTrait('Lady')) && card.kneeled,
+                gameAction: 'stand'
+            },
+            handler: context => {
+                context.target.controller.standCard(context.target);
+                this.game.addMessage('{0} discards 1 gold from {1} to stand {2}', this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+Irri.code = '06093';
+
+module.exports = Irri;
