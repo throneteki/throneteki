fd964d8f8cf17094fa05afe5e66ec9add0349c6f
DukeTax <DukeTax@users.noreply.github.com>
2017-07-24 09:31:55 +0200

    Convert and fix The Queen of Thorns (Core) (#1227)
    Convert to use target API and only allow printed cost 6 or lower characters to be put into play

--- server/game/cards/characters/01/thequeenofthorns.js
+++ server/game/cards/characters/01/thequeenofthorns.js
@@ -6,23 +6,16 @@ class TheQueenOfThorns extends DrawCard {
             when: {
                 afterChallenge: (event, challenge) => challenge.winner === this.controller && challenge.isParticipating(this) && challenge.challengeType === 'intrigue'
             },
-            handler: () => {
-                this.game.promptForSelect(this.controller, {
-                    activePromptTitle: 'Select a character to put in play',
-                    source: this,
-                    cardCondition: card => card.location === 'hand' && card.getCost() && card.getType() === 'character' && card.isFaction('tyrell'),
-                    onSelect: (p, card) => this.onCardSelected(p, card)
-                });
+            target: {
+                activePromptTitle: 'Select a character to put into play',
+                cardCondition: card => card.location === 'hand' && card.controller === this.controller &&
+                                       card.getPrintedCost() <= 6 && card.getType() === 'character' && card.isFaction('tyrell')
+            },
+            handler: context => {
+                this.controller.putIntoPlay(context.target);
+                this.game.addMessage('{0} uses {1} to put {2} in play from their hand', this.controller, this, context.target);
             }
         });
-    }
-
-    onCardSelected(player, card) {
-        player.putIntoPlay(card);
-
-        this.game.addMessage('{0} uses {1} to put {2} in play from their hand', player, this, card);
-
-        return true;
     }
 }
 
