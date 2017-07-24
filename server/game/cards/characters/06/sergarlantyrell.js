0d9fa80a96a0f6a244d3d0febc4134bbb620c029
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-07-24 13:30:17 +0200

    Implement Ser Garlan Tyrell (OR)

---
+++ server/game/cards/characters/06/sergarlantyrell.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../../drawcard.js');
+
+class SerGarlanTyrell extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            effect: ability.effects.dynamicStrength(() => this.tokens['gold'])
+        });
+
+        this.action({
+            title: 'Give character +2 STR',
+            cost: ability.costs.discardGold(),
+            target: {
+                activePromptTitle: 'Select a character',
+                cardCondition: card => card.location === 'play area' && card.isFaction('tyrell') && card.getType() === 'character'
+            },
+            handler: context => {
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.modifyStrength(2)
+                }));
+                this.game.addMessage('{0} discards 1 gold from {1} to give {2} +2 STR until the end of the phase',
+                    this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+SerGarlanTyrell.code = '06083';
+
+module.exports = SerGarlanTyrell;
