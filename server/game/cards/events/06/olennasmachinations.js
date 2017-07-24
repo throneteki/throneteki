b5019e53de5ccac6ed5b51a91b6eee57cc6df88f
DukeTax <DukeTax@users.noreply.github.com>
2017-07-24 20:02:12 +0200

    Implement Ser Garlan Tyrell (OR) & Olenna's Machinations (#1232)
    * Implement Ser Garlan Tyrell (OR)
    
    * Implement Olenna's Machinations

---
+++ server/game/cards/events/06/olennasmachinations.js
@@ -0,0 +1,37 @@
+const DrawCard = require('../../../drawcard.js');
+
+class OlennasMachinations extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            max: ability.limit.perPhase(1),
+            title: 'Raise power challenge limit',
+            handler: () => {
+                this.untilEndOfPhase(ability => ({
+                    targetType: 'player',
+                    targetController: 'current',
+                    effect: ability.effects.modifyChallengeTypeLimit('power', 1)
+                }));
+                this.game.addMessage('{0} plays {1} to be able to initiate an additional {2} challenge this phase',
+                    this.controller, this, 'power');
+            }
+        });
+
+        this.reaction({
+            location: 'discard pile',
+            when: {
+                afterChallenge: (event, challenge) => challenge.winner === this.controller && challenge.challengeType === 'intrigue' &&
+                                                      challenge.strengthDifference >= 5
+            },
+            ignoreEventCosts: true,
+            cost: ability.costs.payGold(1),
+            handler: () => {
+                this.game.addMessage('{0} pays 1 gold to move {1} back to their hand', this.controller, this);
+                this.controller.moveCard(this, 'hand');
+            }
+        });
+    }
+}
+
+OlennasMachinations.code = '06084';
+
+module.exports = OlennasMachinations;
