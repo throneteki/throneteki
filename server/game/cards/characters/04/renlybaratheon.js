c4827b1a9feaba72445ee3da848347157777e037
Brian Upton <ystros@gmail.com>
2017-07-25 14:36:54 -0700

    Convert onInsight to merged style

--- server/game/cards/characters/04/renlybaratheon.js
+++ server/game/cards/characters/04/renlybaratheon.js
@@ -4,23 +4,23 @@ class RenlyBaratheon extends DrawCard {
     setupCardAbilities() {
         this.reaction({
             when: {
-                onInsight: (event, challenge, insightTrigger, drawn) => {
-                    if(insightTrigger.controller !== this.controller) {
+                onInsight: event => {
+                    if(event.source.controller !== this.controller) {
                         return false;
                     }
-                    this.drawnCard = drawn;
 
                     // postpone the check about drawn card loyalty, to avoid
                     // leaking game state to the opponent
                     return true;
                 }
             },
-            handler: () => {
-                if(this.drawnCard.isLoyal()) {
+            handler: context => {
+                let drawnCard = context.event.drawnCard;
+                if(drawnCard.isLoyal()) {
                     this.controller.drawCardsToHand(1);
 
                     this.game.addMessage('{0} uses {1} to reveal {2} and draw a card',
-                        this.controller, this, this.drawnCard);
+                        this.controller, this, drawnCard);
                 }
             }
         });
