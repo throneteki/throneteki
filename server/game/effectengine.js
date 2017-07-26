d968f8af70d9f9cabee3d87fb3fcdcd300e1a5f3
Brian Upton <ystros@gmail.com>
2017-07-26 00:13:19 -0700

    Recalculate effects when traits change (#1236)
    Previously, if an effect were already in play that was limited to cards
    with a certain trait, and a card already in play gained or lost that
    trait, the effect would not be applied or removed from that card. For
    example, characters that gained the 'Knight' trait while Tourney for the
    King was revealed did not gain the renown keyword.
    
    Now, when a card gains or loses a trait, effects for that card are
    recalculated in a similar manner to the way they are recalculated for
    faction changes.

--- server/game/effectengine.js
+++ server/game/effectengine.js
@@ -6,7 +6,7 @@ class EffectEngine {
     constructor(game) {
         this.game = game;
         this.events = new EventRegistrar(game, this);
-        this.events.register(['onCardMoved', 'onCardFactionChanged', 'onCardTakenControl', 'onCardBlankToggled', 'onChallengeFinished', 'onPhaseEnded', 'onAtEndOfPhase', 'onRoundEnded']);
+        this.events.register(['onCardMoved', 'onCardTraitChanged', 'onCardFactionChanged', 'onCardTakenControl', 'onCardBlankToggled', 'onChallengeFinished', 'onPhaseEnded', 'onAtEndOfPhase', 'onRoundEnded']);
         this.effects = [];
         this.recalculateEvents = {};
         this.customDurationEvents = [];
@@ -67,14 +67,22 @@ class EffectEngine {
         this.addTargetForPersistentEffects(card, 'play area');
     }
 
+    onCardTraitChanged(event) {
+        this.recalculateTargetingChange(event.card);
+    }
+
     onCardFactionChanged(event) {
+        this.recalculateTargetingChange(event.card);
+    }
+
+    recalculateTargetingChange(card) {
         _.each(this.effects, effect => {
-            if(effect.duration === 'persistent' && effect.hasTarget(event.card) && !effect.isValidTarget(event.card)) {
-                effect.removeTarget(event.card);
+            if(effect.duration === 'persistent' && effect.hasTarget(card) && !effect.isValidTarget(card)) {
+                effect.removeTarget(card);
             }
         });
 
-        this.addTargetForPersistentEffects(event.card, 'play area');
+        this.addTargetForPersistentEffects(card, 'play area');
     }
 
     addTargetForPersistentEffects(card, targetLocation) {
