22e2d6937156fdb393f4f8ecea2a0b979c35fdd2
DukeTax <DukeTax@users.noreply.github.com>
2017-07-29 21:58:53 +0200

    Convert Jon Snow (WotN) to cost/target API (#1271)
    * Convert Jon Snow (WotN) to cost/target API
    * Has the additional benefit of not having identical prompt titles for the cost and target prompts.
    
    * Fix message

--- server/game/cards/characters/03/jonsnow.js
+++ server/game/cards/characters/03/jonsnow.js
@@ -3,45 +3,20 @@ const DrawCard = require('../../../drawcard.js');
 class JonSnow extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
-            title: 'Sacrifice character',
-            method: 'sacrifice',
-            limit: ability.limit.perRound(1)
+            title: 'Stand character',
+            limit: ability.limit.perRound(1),
+            cost: ability.costs.sacrifice(card => card.isFaction('stark') && card.getType() === 'character'),
+            target: {
+                activePromptTitle: 'Select a character',
+                cardCondition: card => card.location === 'play area' && card.isUnique() && card.isFaction('stark') && card.getType() === 'character',
+                gameAction: 'stand'
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} and sacrifices {2} to stand {3}', this.controller, this, context.sacrificeCostCard, context.target);
+                context.target.controller.standCard(context.target);
+            }
+
         });
-    }
-
-    sacrifice() {
-        this.game.promptForSelect(this.controller, {
-            activePromptTitle: 'Select a character',
-            source: this,
-            cardCondition: card => card.location === 'play area' && card.controller === this.controller && card.getType() === 'character' && card.isFaction('stark'),
-            onSelect: (p, card) => this.onSacrificeSelected(p, card)
-        });
-
-        return true;
-    }
-
-    onSacrificeSelected(player, card) {
-        this.toSacrifice = card;
-
-        this.game.promptForSelect(this.controller, {
-            activePromptTitle: 'Select a character',
-            source: this,
-            cardCondition: card => card.location === 'play area' && card.controller === this.controller && card.getType() === 'character' &&
-                card.isFaction('stark') && card.isUnique() && card.kneeled,
-            onSelect: (p, card) => this.onStandSelected(p, card)
-        });
-
-        return true;
-    }
-
-    onStandSelected(player, card) {
-        this.game.addMessage('{0} uses {1} to sacrifice {2} and stand {3}', player, this, this.toSacrifice, card);
-
-        player.standCard(card);
-        player.sacrificeCard(this.toSacrifice);
-        this.toSacrifice.selected = false;
-
-        return true;
     }
 }
 
