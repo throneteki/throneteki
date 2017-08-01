70ad10afbe61ef17ee48c5044ee628634bb0eea1
Brian Upton <ystros@gmail.com>
2017-08-01 10:34:48 -0700

    Convert cards that implicitly choose an opponent

--- server/game/cards/locations/07/queenscrown.js
+++ server/game/cards/locations/07/queenscrown.js
@@ -7,12 +7,9 @@ class Queenscrown extends DrawCard {
         this.action({
             title: 'Reveal top 3 cards of opponent\'s deck',
             cost: ability.costs.kneelSelf(),
-            handler: () => {
-                let opponent = this.game.getOtherPlayer(this.controller);
-
-                if(!opponent) {
-                    return;
-                }
+            chooseOpponent: true,
+            handler: context => {
+                let opponent = context.opponent;
 
                 this.remainingCards = opponent.drawDeck.first(3);
                 this.game.addMessage('{0} kneels {1} to reveal {2} from the top of {3}\'s deck', this.controller, this, this.remainingCards, opponent);
