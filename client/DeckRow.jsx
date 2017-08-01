5c8217d9290ad5f1bbb6401c66be63d8d893d301
Stuart Walsh <stu@ipng.org.uk>
2017-08-01 14:34:26 +0100

    lots of micro adjustments to make the deck rows look nicer

--- client/DeckRow.jsx
+++ client/DeckRow.jsx
@@ -4,11 +4,11 @@ import moment from 'moment';
 class DeckRow extends React.Component {
     render() {
         return (
-            <div className={ this.props.active ? 'row deck-row active' : 'row deck-row' } key={ this.props.deck.name } onClick={ this.props.onClick }>
-                <div className='col-xs-1'><img className='card-small' src={ '/img/cards/' + this.props.deck.faction.value + '.png' } /></div>
-                <span className='col-xs-8 deck-name'>{ this.props.deck.name }</span><span className='col-xs-2 deck-status'>{ this.props.deck.validation.status }</span>
+            <div className={ this.props.active ? 'deck-row active' : 'deck-row' } key={ this.props.deck.name } onClick={ this.props.onClick }>
+                <div className='col-xs-1 deck-image'><img className='card-small' src={ '/img/cards/' + this.props.deck.faction.value + '.png' } /></div>
+                <span className='col-xs-9 col-sm-8 col-lg-9 deck-name'>{ this.props.deck.name }</span><span className='col-xs-2 col-md-3 col-lg-2 deck-status text-right'>{ this.props.deck.validation.status }</span>
                 <div className='row small'>
-                    <span className='col-xs-7 deck-factionagenda'>{ this.props.deck.faction.name }{ this.props.deck.agenda && this.props.deck.agenda.label ? <span>/{ this.props.deck.agenda.label }</span> : null }</span>
+                    <span className='col-xs-7 col-sm-6 col-md-7 deck-factionagenda'>{ this.props.deck.faction.name }{ this.props.deck.agenda && this.props.deck.agenda.label ? <span>/{ this.props.deck.agenda.label }</span> : null }</span>
                     <span className='col-xs-3 deck-date text-right'>{ moment(this.props.deck.lastUpdated).format('Do MMMM YYYY') }</span>
                 </div>
             </div>);
