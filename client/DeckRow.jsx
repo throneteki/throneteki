a6dbeef0ac6adccbb3328b43174d2b8112d91f42
Stuart Walsh <stu@ipng.org.uk>
2017-08-01 20:42:09 +0100

    fix styling of deck row at all media sizes

--- client/DeckRow.jsx
+++ client/DeckRow.jsx
@@ -6,10 +6,10 @@ class DeckRow extends React.Component {
         return (
             <div className={ this.props.active ? 'deck-row active' : 'deck-row' } key={ this.props.deck.name } onClick={ this.props.onClick }>
                 <div className='col-xs-1 deck-image'><img className='card-small' src={ '/img/cards/' + this.props.deck.faction.value + '.png' } /></div>
-                <span className='col-xs-9 col-sm-8 col-lg-9 deck-name'>{ this.props.deck.name }</span><span className='col-xs-2 col-md-3 col-lg-2 deck-status text-right'>{ this.props.deck.validation.status }</span>
+                <span className='col-xs-8 col-md-7 col-lg-9 deck-name'>{ this.props.deck.name }</span><span className='col-xs-2 col-md-3 col-lg-2 deck-status text-right pull-right'>{ this.props.deck.validation.status }</span>
                 <div className='row small'>
-                    <span className='col-xs-7 col-sm-6 col-md-7 deck-factionagenda'>{ this.props.deck.faction.name }{ this.props.deck.agenda && this.props.deck.agenda.label ? <span>/{ this.props.deck.agenda.label }</span> : null }</span>
-                    <span className='col-xs-3 deck-date text-right'>{ moment(this.props.deck.lastUpdated).format('Do MMMM YYYY') }</span>
+                    <span className='col-md-7 deck-factionagenda'>{ this.props.deck.faction.name }{ this.props.deck.agenda && this.props.deck.agenda.label ? <span>/{ this.props.deck.agenda.label }</span> : null }</span>
+                    <span className='col-xs-4 col-md-3 deck-date text-right pull-right'>{ moment(this.props.deck.lastUpdated).format('Do MMM YYYY') }</span>
                 </div>
             </div>);
     }
