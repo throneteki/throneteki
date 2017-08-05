f3409e0852173dfd59a036f793a26d59d5177031
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-08-05 19:17:37 +0200

    Implement Support of Saltcliffe

---
+++ server/game/cards/attachments/00/supportofsaltcliffe.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../../drawcard.js');
+
+class SupportOfSaltcliffe extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.addKeyword('stealth')
+        });
+
+        this.reaction({
+            when: {
+                afterChallenge: ({challenge}) => this.controller === challenge.winner && challenge.isParticipating(this.parent) && challenge.isUnopposed()
+            },
+            handler: () => {
+                this.parent.controller.standCard(this.parent);
+                this.game.addMessage('{0} uses {1} to stand {2}', this.controller, this, this.parent);
+            }
+        });
+    }
+
+    canAttach(player, card) {
+        if(card.getType() !== 'character' || !card.isFaction('greyjoy')) {
+            return false;
+        }
+
+        return super.canAttach(player, card);
+    }
+}
+
+SupportOfSaltcliffe.code = '00010';
+
+module.exports = SupportOfSaltcliffe;
