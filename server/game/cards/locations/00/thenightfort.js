1c663b14c476917e0b5e5389227157083532a554
DukeTax <DukeTax@users.noreply.github.com>
2017-08-05 21:20:09 +0200

    Implement Valyrian draft only in-faction cards (#1291)
    * Implement Compelled by the King
    * Implement Support of Saltcliffe
    * Implement Tyrion Lannister (VDS)
    * Implement The Viper's Rage
    * Implement The Nightfort
    * Implement Deadly Khalasar
    * Implement Blackcrown Knights

---
+++ server/game/cards/locations/00/thenightfort.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../../drawcard.js');
+
+class TheNightfort extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Give character +STR',
+            condition: () => this.game.currentChallenge && this.game.currentChallenge.attackers.length >= 1,
+            phase: 'challenge',
+            cost: ability.costs.kneelSelf(),
+            target: {
+                activePromptTitle: 'Select a character',
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && 
+                                       this.game.currentChallenge.isDefending(card) && card.isFaction('thenightswatch')
+            },
+            handler: context => {
+                let strBoost = this.game.currentChallenge.attackers.length;
+                this.untilEndOfChallenge(ability => ({
+                    match: context.target,
+                    effect: ability.effects.modifyStrength(strBoost)
+                }));
+
+                this.game.addMessage('{0} kneels {1} to give {2} +{3} STR until the end of the challenge', 
+                    context.player, this, context.target, strBoost);
+            }
+        });
+    }
+}
+
+TheNightfort.code = '00013';
+
+module.exports = TheNightfort;
