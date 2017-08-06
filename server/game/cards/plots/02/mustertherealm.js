fe493b6452f6bb47e51cd3b34b8bdd3a4d88fcff
DukeTax <DukeTax@users.noreply.github.com>
2017-08-06 04:50:39 +0200

    Convert Muster the Realm to use an effect (#1295)

--- server/game/cards/plots/02/mustertherealm.js
+++ server/game/cards/plots/02/mustertherealm.js
@@ -1,24 +1,16 @@
-const _ = require('underscore');
-
 const PlotCard = require('../../../plotcard.js');
 
 class MusterTheRealm extends PlotCard {
-    modifyClaim(winner, challengeType, claim) {
-        var challenge = this.game.currentChallenge;
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.game.currentChallenge && this.hasAttackingArmy(),
+            match: this,
+            effect: ability.effects.modifyClaim(1)
+        });
+    }
 
-        if(!challenge || challenge.attackingPlayer !== this.controller) {
-            return claim;
-        }
-
-        if(!_.any(challenge.attackers, card => {
-            return card.hasTrait('Army');
-        })) {
-            return claim;
-        }
-
-        this.game.addMessage('{0} uses {1} to raise the claim of {1} by 1 for this challenge', this.controller, this);
-
-        return claim + 1;
+    hasAttackingArmy() {
+        return this.controller.anyCardsInPlay(card => this.game.currentChallenge.isAttacking(card) && card.hasTrait('Army'));
     }
 }
 
