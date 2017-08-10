e65659f06f23c95184024371a715b82ef9ee3a69
Brian Upton <ystros@gmail.com>
2017-08-10 08:22:13 -0700

    Add commands to manipulate faction for cards

--- server/game/chatcommands.js
+++ server/game/chatcommands.js
@@ -26,7 +26,9 @@ class ChatCommands {
             '/cancel-prompt': this.cancelPrompt,
             '/token': this.setToken,
             '/bestow': this.bestow,
-            '/disconnectme': this.disconnectMe
+            '/disconnectme': this.disconnectMe,
+            '/add-faction': this.addFaction,
+            '/remove-faction': this.removeFaction
         };
         this.tokens = [
             'power',
@@ -349,6 +351,44 @@ class ChatCommands {
 
     disconnectMe(player) {
         player.socket.disconnect();
+    }
+
+    addFaction(player, args) {
+        let faction = args[1];
+        if(!faction) {
+            return false;
+        }
+
+        this.game.promptForSelect(player, {
+            activePromptTitle: 'Select a card',
+            waitingPromptTitle: 'Waiting for opponent to add faction to card',
+            cardCondition: card => card.location === 'play area' && card.controller === player,
+            onSelect: (p, card) => {
+                card.addFaction(faction);
+
+                this.game.addMessage('{0} uses the /add-faction command to add the {1} faction to {2}', p, faction, card);
+                return true;
+            }
+        });
+    }
+
+    removeFaction(player, args) {
+        let faction = args[1];
+        if(!faction) {
+            return false;
+        }
+
+        this.game.promptForSelect(player, {
+            activePromptTitle: 'Select a card',
+            waitingPromptTitle: 'Waiting for opponent to add faction to card',
+            cardCondition: card => card.location === 'play area' && card.controller === player,
+            onSelect: (p, card) => {
+                card.removeFaction(faction);
+
+                this.game.addMessage('{0} uses the /remove-faction command to remove the {1} keyword from {2}', p, faction, card);
+                return true;
+            }
+        });
     }
 
     getNumberOrDefault(string, defaultNumber) {
