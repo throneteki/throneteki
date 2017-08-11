35a79eb49a71cc7acbf181a2a08e401b99dc04e0
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-08-12 01:29:55 +0200

    Only queue the attachment marshalling message

--- server/game/marshalcardaction.js
+++ server/game/marshalcardaction.js
@@ -27,15 +27,15 @@ class MarshalCardAction extends BaseAbility {
     executeHandler(context) {
         context.player.putIntoPlay(context.source, 'marshal');
 
-        context.game.queueSimpleStep(() => {
-            if(context.costs.isDupe) {
-                context.game.addMessage('{0} duplicates {1} for free', context.player, context.source);
-            } else if(context.source.getType() === 'attachment') {
+        if(context.costs.isDupe) {
+            context.game.addMessage('{0} duplicates {1} for free', context.player, context.source);
+        } else if(context.source.getType() === 'attachment') {
+            context.game.queueSimpleStep(() => {
                 context.game.addMessage('{0} marshals {1} on {2} costing {3}', context.player, context.source, context.source.parent, context.costs.gold);
-            } else {
-                context.game.addMessage('{0} marshals {1} costing {2}', context.player, context.source, context.costs.gold);
-            }            
-        });
+            });
+        } else {
+            context.game.addMessage('{0} marshals {1} costing {2}', context.player, context.source, context.costs.gold);
+        }
     }
 
     isCardAbility() {
