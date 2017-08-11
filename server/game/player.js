cc0bba698132f101b43c7a826d442b0c150f14fd
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-08-11 18:40:30 +0200

    Wrap method in a applyGameAction for immunity purposes

--- server/game/player.js
+++ server/game/player.js
@@ -897,16 +897,18 @@ class Player extends Spectator {
     }
 
     moveCardToBottomOfDeck(card, allowSave = true) {
-        if(!card.dupes.isEmpty() && allowSave) {
-            if(!this.removeDuplicate(card)) {
-                this.moveCard(card, 'draw deck', { bottom: true });
+        this.game.applyGameAction('moveToBottomOfDeck', card, card => {
+            if(!card.dupes.isEmpty() && allowSave) {
+                if(!this.removeDuplicate(card)) {
+                    this.moveCard(card, 'draw deck', { bottom: true });
+                } else {
+                    this.game.addMessage('{0} discards a duplicate to save {1}', this, card);
+                    this.game.raiseEvent('onCardSaved', { card: card });
+                }
             } else {
-                this.game.addMessage('{0} discards a duplicate to save {1}', this, card);
-                this.game.raiseEvent('onCardSaved', { card: card });
+                this.moveCard(card, 'draw deck', { bottom: true });
             }
-        } else {
-            this.moveCard(card, 'draw deck', { bottom: true });
-        }
+        });
     }
 
     /**
