b749dbdc7a28e30663f5c334bb8809289f09d850
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-08-21 14:50:22 +0200

    Have Nute the Barber target loser discard pile Instead of the defender

--- server/game/cards/characters/06/nutethebarber.js
+++ server/game/cards/characters/06/nutethebarber.js
@@ -9,12 +9,11 @@ class NuteTheBarber extends DrawCard {
             limit: ability.limit.perPhase(3),
             target: {
                 activePromptTitle: 'Select a card',
-                cardCondition: card => card.location === 'discard pile' && card.controller === this.game.currentChallenge.defendingPlayer
+                cardCondition: (card, context) => card.location === 'discard pile' && card.controller === context.event.challenge.loser
             },
             handler: context => {
                 context.target.owner.moveCard(context.target, 'hand');
-                this.game.addMessage('{0} uses {1} to return {2} to {3}\'s hand',
-                    this.controller, this, context.target, context.target.owner);
+                this.game.addMessage('{0} uses {1} to return {2} to {3}\'s hand', this.controller, this, context.target, context.target.owner);
                 this.controller.drawCardsToHand(1);
                 this.game.addMessage('{0} then draws 1 card', this.controller);
             }
