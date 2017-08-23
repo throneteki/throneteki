9f98f9c33f7834529e173074b1c8600c2397afe7
Brian Upton <ystros@gmail.com>
2017-08-23 01:51:04 -0700

    Remove incorrect limit from Wildling Scout (#1365)
    Also converts it to the cost / target API.

--- server/game/cards/characters/02/wildlingscout.js
+++ server/game/cards/characters/02/wildlingscout.js
@@ -3,39 +3,19 @@ const DrawCard = require('../../../drawcard.js');
 class WildlingScout extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
-            title: 'Sacrifice this character to give another character stealth',
-            method: 'sacrifice',
-            limit: ability.limit.perPhase(1)
+            title: 'Give another character stealth',
+            cost: ability.costs.sacrificeSelf(),
+            target: {
+                cardCondition: card => card.getType() === 'character' && card.location === 'play area'
+            },
+            handler: context => {
+                this.game.addMessage('{0} sacrifices {1} to make {2} gain stealth', context.player, this, context.target);
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.addKeyword('Stealth')
+                }));
+            }
         });
-    }
-
-    sacrifice(player) {
-        if(this.location !== 'play area') {
-            return false;
-        }
-
-        this.game.promptForSelect(player, {
-            cardCondition: card => this.cardCondition(card),
-            activePromptTitle: 'Select character to gain stealth',
-            source: this,
-            onSelect: (player, card) => this.onCardSelected(player, card)
-        });
-    }
-
-    cardCondition(card) {
-        return card.getType() === 'character' && card.location === 'play area';
-    }
-
-    onCardSelected(player, card) {
-        player.sacrificeCard(this);
-
-        this.game.addMessage('{0} sacrifices {1} to make {2} gain stealth', player, this, card);
-        this.untilEndOfPhase(ability => ({
-            match: card,
-            effect: ability.effects.addKeyword('Stealth')
-        }));
-
-        return true;
     }
 }
 
