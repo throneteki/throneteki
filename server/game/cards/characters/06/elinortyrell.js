712fbb54be3a452d03a6dd2f4fa63ba938045e4f
Stuart Walsh <stu@ipng.org.uk>
2017-08-24 13:39:25 +0100

    Do not allow elinor tyrell's ability to be active on setup. Fixes #1174

--- server/game/cards/characters/06/elinortyrell.js
+++ server/game/cards/characters/06/elinortyrell.js
@@ -3,6 +3,7 @@ const DrawCard = require('../../../drawcard.js');
 class ElinorTyrell extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
+            condition: this.controller.phase !== 'setup',
             targetType: 'player',
             effect: ability.effects.modifyMaxLimited(1)
         });
