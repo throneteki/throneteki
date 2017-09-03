d1d9ee357a71a854c232d01039b813cd4f6fc6e4
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-09-03 20:11:22 +0200

    Implement Ranger's Bow

---
+++ server/game/cards/attachments/06/rangersbow.js
@@ -0,0 +1,38 @@
+const DrawCard = require('../../../drawcard.js');
+
+class RangersBow extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.modifyStrength(1)
+        });
+
+        this.action({
+            title: 'Give defending character +2 STR',
+            condition: () => this.game.currentChallenge,
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: card => card.isFaction('thenightswatch') && card.getType() === 'character' && this.game.currentChallenge.isDefending(card)
+            },
+            handler: context => {
+                this.untilEndOfChallenge(ability => ({
+                    match: context.target,
+                    effect: ability.effects.modifyStrength(2)
+                }));
+
+                this.game.addMessage('{0} kneels {1} to give {2} +2 STR until the end of the challenge', this.controller, this, context.target);
+            }
+        });
+    }
+
+    canAttach(player, card) {
+        if(card.getType() !== 'character' || !card.isFaction('thenightswatch')) {
+            return false;
+        }
+
+        return super.canAttach(player, card);
+    }
+}
+
+RangersBow.code = '06106';
+
+module.exports = RangersBow;
