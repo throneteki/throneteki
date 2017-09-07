ad4828429080d5606f614b004bd3a02dd527b0a9
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-09-07 21:05:01 +0200

    Disallow attaching to opponent's characters during setup

--- server/game/gamesteps/attachmentprompt.js
+++ server/game/gamesteps/attachmentprompt.js
@@ -11,13 +11,17 @@ class AttachmentPrompt extends UiPrompt {
     continue() {
         this.game.promptForSelect(this.player, {
             activePromptTitle: 'Select target for attachment',
-            cardCondition: card => this.attachmentCard.owner.canAttach(this.attachmentCard, card),
+            cardCondition: card => this.attachmentCard.owner.canAttach(this.attachmentCard, card) && this.setupRestriction(card),
             onSelect: (player, card) => {
                 let targetPlayer = card.controller;
                 targetPlayer.attach(player, this.attachmentCard, card, this.playingType);
                 return true;
             }
         });
+    }
+
+    setupRestriction(card) {
+        return this.game.currentPhase === 'setup' ? card.controller === this.controller : true;
     }
 }
 
