7c1d46056873997f7975e90f35069c127d31d9d3
Brian Upton <ystros@gmail.com>
2017-09-06 19:27:08 -0700

    Move API error handling to middleware

--- server/api/decks.js
+++ server/api/decks.js
@@ -1,6 +1,5 @@
 const monk = require('monk');
 const config = require('../config.js');
-const logger = require('../log.js');
 const DeckService = require('../services/DeckService.js');
 const {wrapAsync} = require('../util.js');
 
@@ -8,7 +7,7 @@ let db = monk(config.dbPath);
 let deckService = new DeckService(db);
 
 module.exports.init = function(server) {
-    server.get('/api/decks/:id', wrapAsync(async function(req, res, next) {
+    server.get('/api/decks/:id', wrapAsync(async function(req, res) {
         if(!req.user) {
             return res.status(401).send({ message: 'Unauthorized' });
         }
@@ -17,113 +16,78 @@ module.exports.init = function(server) {
             return res.status(404).send({ message: 'No such deck' });
         }
 
-        try {
-            let deck = await deckService.getById(req.params.id);
+        let deck = await deckService.getById(req.params.id);
 
-            if(!deck) {
-                res.status(404).send({ message: 'No such deck' });
-
-                return next();
-            }
-
-            if(deck.username !== req.user.username) {
-                return res.status(401).send({ message: 'Unauthorized' });
-            }
-
-            res.send({ success: true, deck: deck });
-        } catch(err) {
-            res.send({ success: false, message: 'Error fetching deck' });
-            logger.info(err.message);
-            return next(err);
+        if(!deck) {
+            return res.status(404).send({ message: 'No such deck' });
         }
+
+        if(deck.username !== req.user.username) {
+            return res.status(401).send({ message: 'Unauthorized' });
+        }
+
+        res.send({ success: true, deck: deck });
     }));
 
-    server.get('/api/decks', wrapAsync(async function(req, res, next) {
+    server.get('/api/decks', wrapAsync(async function(req, res) {
         if(!req.user) {
             return res.status(401).send({ message: 'Unauthorized' });
         }
 
-        try {
-            let decks = await deckService.findByUserName(req.user.username);
-            res.send({ success: true, decks: decks });
-        } catch(err) {
-            res.send({ success: false, message: 'Error fetching decks' });
-            logger.info(err);
-            return next(err);
-        }
+        let decks = await deckService.findByUserName(req.user.username);
+        res.send({ success: true, decks: decks });
     }));
 
-    server.put('/api/decks/:id', wrapAsync(async function(req, res, next) {
+    server.put('/api/decks/:id', wrapAsync(async function(req, res) {
         if(!req.user) {
             return res.status(401).send({ message: 'Unauthorized' });
         }
 
-        try {
-            let deck = await deckService.getById(req.params.id);
+        let deck = await deckService.getById(req.params.id);
 
-            if(!deck) {
-                res.status(404).send({ message: 'No such deck' });
-
-                return next();
-            }
-
-            if(deck.username !== req.user.username) {
-                return res.status(401).send({ message: 'Unauthorized' });
-            }
-
-            let data = Object.assign({ id: req.params.id }, JSON.parse(req.body.data));
-
-            deckService.update(data);
-
-            res.send({ success: true, message: 'Saved' });
-        } catch(err) {
-            res.send({ success: false, message: 'Error saving deck' });
-            logger.info(err.message);
-            return next(err);
+        if(!deck) {
+            return res.status(404).send({ message: 'No such deck' });
         }
+
+        if(deck.username !== req.user.username) {
+            return res.status(401).send({ message: 'Unauthorized' });
+        }
+
+        let data = Object.assign({ id: req.params.id }, JSON.parse(req.body.data));
+
+        deckService.update(data);
+
+        res.send({ success: true, message: 'Saved' });
     }));
 
-    server.post('/api/decks', wrapAsync(async function(req, res, next) {
+    server.post('/api/decks', wrapAsync(async function(req, res) {
         if(!req.user) {
             return res.status(401).send({ message: 'Unauthorized' });
         }
 
-        try {
-            let deck = Object.assign(JSON.parse(req.body.data), { username: req.user.username });
-            await deckService.create(deck);
-            res.send({ success: true });
-        } catch(err) {
-            logger.info(err);
-            return next(err);
-        }
+        let deck = Object.assign(JSON.parse(req.body.data), { username: req.user.username });
+        await deckService.create(deck);
+        res.send({ success: true });
     }));
 
-    server.delete('/api/decks/:id', wrapAsync(async function(req, res, next) {
+    server.delete('/api/decks/:id', wrapAsync(async function(req, res) {
         if(!req.user) {
             return res.status(401).send({ message: 'Unauthorized' });
         }
 
         let id = req.params.id;
 
-        try {
-            let deck = await deckService.getById(id);
+        let deck = await deckService.getById(id);
 
-            if(!deck) {
-                res.status(404).send({ success: false, message: 'No such deck' });
-
-                return next();
-            }
-
-            if(deck.username !== req.user.username) {
-                return res.status(401).send({ message: 'Unauthorized' });
-            }
-
-            await deckService.delete(id);
-            res.send({ success: true, message: 'Deck deleted successfully', deckId: id });
-        } catch(err) {
-            res.send({ success: false, message: 'Error deleting deck' });
-            logger.info(err.message);
-            return next(err);
+        if(!deck) {
+            return res.status(404).send({ success: false, message: 'No such deck' });
         }
+
+        if(deck.username !== req.user.username) {
+            return res.status(401).send({ message: 'Unauthorized' });
+        }
+
+        await deckService.delete(id);
+        res.send({ success: true, message: 'Deck deleted successfully', deckId: id });
     }));
 };
