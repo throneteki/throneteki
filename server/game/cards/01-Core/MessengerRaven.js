c14f52703af26ab92ef331e2efb7506bad133d53
Brian Upton <ystros@gmail.com>
2017-09-11 16:06:36 -0700

    Convert Messenger Raven to cost API

--- server/game/cards/01-Core/MessengerRaven.js
+++ server/game/cards/01-Core/MessengerRaven.js
@@ -1,21 +1,19 @@
 const DrawCard = require('../../drawcard.js');
 
 class MessengerRaven extends DrawCard {
-    setupCardAbilities() {
+    setupCardAbilities(ability) {
         this.action({
-            title: 'Return Messanger Raven to your hand and draw 1 card',
-            method: 'return',
-            phase: 'dominance'
+            title: 'Draw 1 card',
+            phase: 'dominance',
+            cost: ability.costs.returnSelfToHand(),
+            handler: context => {
+                let player = context.player;
+
+                player.drawCardsToHand(1);
+
+                this.game.addMessage('{0} returns {1} to their hand and draws 1 card', player, this);
+            }
         });
-    }
-
-    return(player) {
-        player.moveCard(this, 'hand');
-        player.drawCardsToHand(1);
-
-        this.game.addMessage('{0} returns {1} to their hand and draws 1 card', player, this);
-
-        return true;
     }
 }
 
