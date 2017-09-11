5f71ceea0873c2e63db2ea5cfe3128393d1680c8
Brian Upton <ystros@gmail.com>
2017-09-11 15:57:00 -0700

    Convert Lordsport Shipwright to target API

--- server/game/cards/01-Core/LordsportShipright.js
+++ server/game/cards/01-Core/LordsportShipright.js
@@ -3,37 +3,27 @@ const DrawCard = require('../../drawcard.js');
 class LordsportShipright extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
-            title: 'Kneel Lordsport Shipwright to kneel a location',
+            title: 'Kneel a location',
+            phase: 'marshal',
             cost: ability.costs.kneelSelf(),
-            method: 'kneelLocation',
-            phase: 'marshal'
-        });
-    }
+            target: {
+                activePromptTitle: 'Select a location',
+                cardCondition: card => this.cardCondition(card)
+            },
+            handler: context => {
+                let player = context.player;
+                let card = context.target;
+                player.controller.kneelCard(card);
 
-    kneelLocation(player) {
-        this.game.promptForSelect(player, {
-            cardCondition: card => this.cardCondition(card),
-            activePromptTitle: 'Select location',
-            source: this,
-            gameAction: 'kneel',
-            onSelect: (player, card) => this.onCardSelected(player, card)
+                this.game.addMessage('{0} uses {1} to kneel {2}', player, this, card);
+            }
         });
-
-        return true;
     }
 
     cardCondition(card) {
         var cost = this.controller.firstPlayer ? 3 : 2;
 
         return !card.kneeled && card.getType() === 'location' && card.getCost() <= cost;
-    }
-
-    onCardSelected(player, card) {
-        player.kneelCard(card);
-
-        this.game.addMessage('{0} uses {1} to kneel {2}', player, this, card);
-
-        return true;
     }
 }
 
