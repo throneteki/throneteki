f47ecde6f55fb2da512d8e657e1291aba493518b
Brian Upton <ystros@gmail.com>
2017-09-11 16:23:16 -0700

    Convert Syrio Forel to target API

--- server/game/cards/02.2-TRtW/SyrioForel.js
+++ server/game/cards/02.2-TRtW/SyrioForel.js
@@ -4,41 +4,25 @@ class SyrioForel extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
             title: 'Give a character military icon and stealth',
-            method: 'giveIcon',
-            limit: ability.limit.perPhase(1)
+            limit: ability.limit.perPhase(1),
+            phase: 'challenge',
+            target: {
+                cardCondition: card => card.getType() === 'character' && card.controller === this.controller
+            },
+            handler: context => {
+                let player = context.player;
+                let card = context.target;
+
+                this.game.addMessage('{0} uses {1} to give {2} a {3} icon and stealth until the end of the phase', player, this, card, 'military');
+                this.untilEndOfPhase(ability => ({
+                    match: card,
+                    effect: [
+                        ability.effects.addIcon('military'),
+                        ability.effects.addKeyword('Stealth')
+                    ]
+                }));
+            }
         });
-    }
-
-    giveIcon(player) {
-        if(this.location !== 'play area' || player.phase !== 'challenge' || this.controller !== player) {
-            return false;
-        }
-
-        this.game.promptForSelect(player, {
-            cardCondition: card => this.cardCondition(card),
-            activePromptTitle: 'Select a character',
-            source: this,
-            onSelect: (player, card) => this.onCardSelected(player, card)
-        });
-
-        return true;
-    }
-
-    cardCondition(card) {
-        return card.getType() === 'character' && card.controller === this.controller;
-    }
-
-    onCardSelected(player, card) {
-        this.game.addMessage('{0} uses {1} to give {2} a {3} icon and stealth until the end of the phase', player, this, card, 'military');
-        this.untilEndOfPhase(ability => ({
-            match: card,
-            effect: [
-                ability.effects.addIcon('military'),
-                ability.effects.addKeyword('Stealth')
-            ]
-        }));
-
-        return true;
     }
 }
 
