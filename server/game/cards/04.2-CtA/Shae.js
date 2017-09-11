34a25d1a82d033cc41a863269deaacab90951dd3
Brian Upton <ystros@gmail.com>
2017-09-11 16:33:54 -0700

    Convert Shae to cost API

--- server/game/cards/04.2-CtA/Shae.js
+++ server/game/cards/04.2-CtA/Shae.js
@@ -3,22 +3,16 @@ const DrawCard = require('../../drawcard.js');
 class Shae extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
-            title: 'Pay 1 gold to stand Shae',
-            method: 'stand',
+            title: 'Stand Shae',
             phase: 'challenge',
-            limit: ability.limit.perPhase(2)
+            limit: ability.limit.perPhase(2),
+            cost: ability.costs.payGold(1),
+            handler: () => {
+                this.controller.standCard(this);
+
+                this.game.addMessage('{0} pays 1 gold to stand {1}', this.controller, this);
+            }
         });
-    }
-
-    stand(player) {
-        if(player.gold <= 0 || !this.kneeled) {
-            return false;
-        }
-
-        this.game.addGold(this.controller, -1);
-        player.standCard(this);
-
-        this.game.addMessage('{0} pays 1 gold to stand {1}', this.controller, this);
     }
 }
 
