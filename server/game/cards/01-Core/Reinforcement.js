9987a4233fce15a14448faa026272c9931efa948
Brian Upton <ystros@gmail.com>
2017-09-15 01:18:55 -0700

    Convert Reinforcements to target API

--- server/game/cards/01-Core/Reinforcement.js
+++ server/game/cards/01-Core/Reinforcement.js
@@ -3,33 +3,19 @@ const PlotCard = require('../../plotcard.js');
 class Reinforcements extends PlotCard {
     setupCardAbilities() {
         this.whenRevealed({
-            handler: () => {
-                this.game.promptForSelect(this.controller, {
-                    activePromptTitle: 'Select a character',
-                    source: this,
-                    cardCondition: card => this.cardCondition(card),
-                    onSelect: (player, card) => this.onCardClicked(player, card)
-                });
+            target: {
+                cardCondition: card => (
+                    this.controller === card.controller &&
+                    card.getCost() <= 5 &&
+                    card.getType() === 'character' &&
+                    ['hand', 'discard pile'].includes(card.location)
+                )
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to put {2} into play from their {3}', this.controller, this, context.target, context.target.location);
+                this.controller.putIntoPlay(context.target);
             }
         });
-    }
-
-    cardCondition(card) {
-        var player = card.controller;
-        return this.controller === player &&
-            card.getCost() <= 5 &&
-            card.getType() === 'character' &&
-            (player.findCardByUuid(player.discardPile, card.uuid) || player.findCardByUuid(player.hand, card.uuid));
-    }
-
-    onCardClicked(player, card) {
-        var hand = !!player.findCardByUuid(player.hand, card.uuid);
-
-        this.game.addMessage('{0} uses {1} to put {2} into play from their {3}', player, this, card, hand ? 'hand' : 'discard pile');
-
-        player.putIntoPlay(card);
-
-        return true;
     }
 }
 
