58603858e4a50a58124afff9b0e8aea9005afe4c
Brian Upton <ystros@gmail.com>
2017-09-15 01:58:01 -0700

    Convert Chett to target API

--- server/game/cards/02.5-COW/Chett.js
+++ server/game/cards/02.5-COW/Chett.js
@@ -3,21 +3,22 @@ const DrawCard = require('../../drawcard.js');
 class Chett extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
-            title: 'Kneel a steward to return a direwolf or raven',
+            title: 'Return a card to hand',
             cost: ability.costs.kneel(card => card.hasTrait('Steward') && card.getType() === 'character'),
             phase: 'dominance',
             limit: ability.limit.perPhase(1),
+            target: {
+                activePromptTitle: 'Select a card',
+                cardCondition: card => (
+                    card.location === 'discard pile' &&
+                    card.controller === this.controller &&
+                    (card.hasTrait('Direwolf') || card.hasTrait('Raven'))
+                )
+            },
             handler: context => {
-                this.game.promptForSelect(context.player, {
-                    cardCondition: card => card.location === 'discard pile' && (card.hasTrait('Direwolf') || card.hasTrait('Raven')),
-                    activePromptTitle: 'Select Direwolf or Raven card',
-                    source: this,
-                    onSelect: (player, card) => {
-                        player.moveCard(card, 'hand');
-                        this.game.addMessage('{0} uses {1} to kneel {2} to return {3} to their hand',
-                            context.player, this, context.kneelingCostCard, card);
-                    }
-                });
+                this.controller.moveCard(context.target, 'hand');
+                this.game.addMessage('{0} uses {1} to kneel {2} to return {3} to their hand',
+                    context.player, this, context.kneelingCostCard, context.target);
             }
         });
     }
