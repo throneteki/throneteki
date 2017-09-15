af716b6fd05247f018ef9d0c0936ae76c0263d4d
Brian Upton <ystros@gmail.com>
2017-09-15 01:41:19 -0700

    Convert Halder to target API

--- server/game/cards/02.4-NMG/Halder.js
+++ server/game/cards/02.4-NMG/Halder.js
@@ -8,26 +8,18 @@ class Halder extends DrawCard {
                 card.isFaction('thenightswatch') &&
                 (card.getType() === 'attachment' || card.getType() === 'location')
             )),
+            target: {
+                cardCondition: card => card.isFaction('thenightswatch') && card.getType() === 'character'
+            },
             handler: (context) => {
-                this.game.promptForSelect(context.player, {
-                    cardCondition: card => card.isFaction('thenightswatch') && card.getType() === 'character',
-                    activePromptTitle: 'Select character',
-                    source: this,
-                    onSelect: (player, card) => this.onStrCardSelected(player, card, context.kneelingCostCard)
-                });
+                this.game.addMessage('{0} uses {1} to kneels {2} to give {3} +1 STR until the end of the phase', this.controller, this, context.kneelingCostCard, context.target);
+
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.modifyStrength(1)
+                }));
             }
         });
-    }
-
-    onStrCardSelected(player, card, kneelingCard) {
-        this.game.addMessage('{0} uses {1} to kneels {2} to give {3} +1 STR until the end of the phase', player, this, kneelingCard, card);
-
-        this.untilEndOfPhase(ability => ({
-            match: card,
-            effect: ability.effects.modifyStrength(1)
-        }));
-
-        return true;
     }
 }
 
