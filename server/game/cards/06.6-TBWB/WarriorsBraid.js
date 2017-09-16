37a5e4367d36704ce4049096bc018d44020859b6
DukeTax <DukeTax@users.noreply.github.com>
2017-09-16 18:52:27 +0200

    Implement Freedmen & Warrior's Braid (#1448)
    * Implement Freedmen
    
    * Implement Warrior's Braid

---
+++ server/game/cards/06.6-TBWB/WarriorsBraid.js
@@ -0,0 +1,34 @@
+const DrawCard = require('../../drawcard.js');
+
+class WarriorsBraid extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: [
+                ability.effects.addKeyword('renown'),
+                ability.effects.dynamicStrength(() => this.tokens['bell'])
+            ]
+        });
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.challengeType === 'military' && event.challenge.winner === this.controller &&
+                                         event.challenge.isAttacking(this.parent)
+            },
+            handler: () => {
+                this.addToken('bell', 1);
+                this.game.addMessage('{0} places 1 bell token on {1}', this.controller, this);
+            }
+        });
+    }
+
+    canAttach(player, card) {
+        if(card.getType() !== 'character' || !card.isFaction('targaryen')) {
+            return false;
+        }
+
+        return super.canAttach(player, card);
+    }
+}
+
+WarriorsBraid.code = '06114';
+
+module.exports = WarriorsBraid;
--- server/game/chatcommands.js
+++ server/game/chatcommands.js
@@ -41,7 +41,8 @@ class ChatCommands {
             'vengeance',
             'ear',
             'venom',
-            'kiss'
+            'kiss',
+            'bell'
         ];
     }
 
