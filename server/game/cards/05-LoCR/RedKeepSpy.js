3719f3cd50305cb8a434c77ea365c039997c222d
Brian Upton <ystros@gmail.com>
2017-09-16 12:27:30 -0700

    Make Red Keep Spy melee friendly

--- server/game/cards/05-LoCR/RedKeepSpy.js
+++ server/game/cards/05-LoCR/RedKeepSpy.js
@@ -4,16 +4,13 @@ class RedKeepSpy extends DrawCard {
     setupCardAbilities() {
         this.reaction({
             when: {
-                onCardEntersPlay: event => (
-                    event.playingType === 'ambush' &&
-                    this === event.card &&
-                    this.hasMoreCardsInHand()
-                )
+                onCardEntersPlay: event => event.playingType === 'ambush' && this === event.card
             },
             target: {
                 cardCondition: card => (
                     card.location === 'play area' &&
                     card.controller !== this.controller &&
+                    card.controller.hand.size() < this.controller.hand.size() &&
                     card.getType() === 'character' &&
                     card.getCost() <= 3)
             },
@@ -22,16 +19,6 @@ class RedKeepSpy extends DrawCard {
                 this.game.addMessage('{0} uses {1} to return {2} to {3}\'s hand', this.controller, this, context.target, context.target.controller);
             }
         });
-    }
-
-    hasMoreCardsInHand() {
-        var otherPlayer = this.game.getOtherPlayer(this.controller);
-
-        if(!otherPlayer) {
-            return false;
-        }
-
-        return this.controller.hand.size() > otherPlayer.hand.size();
     }
 }
 
