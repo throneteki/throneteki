70bbecb98ac5bfec466d296791709b45978b067b
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-09-16 18:03:07 +0200

    Implement Freedmen

---
+++ server/game/cards/06.6-TBWB/Freedmen.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+
+class Freedmen extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && event.challenge.attackingPlayer === this.controller &&
+                                         event.challenge.strengthDifference >= 5 && this.controller.canPutIntoPlay(this)
+            },
+            location: 'discard pile',
+            handler: context => {
+                this.controller.putIntoPlay(this);
+                this.game.addMessage('{0} puts {1} into play from their discard pile', context.player, this);
+            }
+        });
+    }
+}
+
+Freedmen.code = '06113';
+
+module.exports = Freedmen;
