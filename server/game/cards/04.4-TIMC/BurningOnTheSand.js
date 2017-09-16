705af603e04563b44fc96c9ce54607878ad65a80
Brian Upton <ystros@gmail.com>
2017-09-16 11:45:22 -0700

    Make Burning on the Sand melee friendly

--- server/game/cards/04.4-TIMC/BurningOnTheSand.js
+++ server/game/cards/04.4-TIMC/BurningOnTheSand.js
@@ -4,16 +4,16 @@ class BurningOnTheSand extends DrawCard {
     setupCardAbilities() {
         this.reaction({
             when: {
-                afterChallenge: ({challenge}) => challenge.loser === this.controller && challenge.isUnopposed()
+                afterChallenge: event => event.challenge.loser === this.controller && event.challenge.isUnopposed()
             },
-            handler: () => {
+            handler: context => {
+                let opponent = context.event.winner;
                 this.untilEndOfChallenge(ability => ({
                     match: card => card === card.controller.activePlot,
-                    targetController: 'opponent',
+                    targetController: opponent,
                     effect: ability.effects.setClaim(0)
                 }));
 
-                let opponent = this.game.getOtherPlayer(this.controller);
                 this.game.addMessage('{0} plays {1} to set the claim value on {2}\'s revealed plot card to 0 until the end of the challenge',
                     this.controller, this, opponent);
             }
