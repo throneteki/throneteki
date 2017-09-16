a934f8283a665ccdb4cba430e7a760609797454f
Brian Upton <ystros@gmail.com>
2017-09-16 12:34:27 -0700

    Make Tommen Baratheon melee friendly

--- server/game/cards/05-LoCR/TommenBaratheon.js
+++ server/game/cards/05-LoCR/TommenBaratheon.js
@@ -3,27 +3,11 @@ const DrawCard = require('../../drawcard.js');
 class TommenBaratheon extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            condition: () => this.controller.hand.size() === 0,
             targetType: 'player',
+            targetController: 'any',
+            match: player => player.hand.size() === 0,
             effect: ability.effects.cannotGainChallengeBonus()
         });
-
-        this.persistentEffect({
-            condition: () => this.opponentHasNoCardsInHand(),
-            targetType: 'player',
-            targetController: 'opponent',
-            effect: ability.effects.cannotGainChallengeBonus()
-        });
-    }
-
-    opponentHasNoCardsInHand() {
-        var opponent = this.game.getOtherPlayer(this.controller);
-
-        if(!opponent) {
-            return true;
-        }
-
-        return opponent.hand.size() === 0;
     }
 }
 
