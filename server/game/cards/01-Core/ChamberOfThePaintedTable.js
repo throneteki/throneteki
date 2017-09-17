b312b64cb51c8f6133cfccbdc9afdd4b23c3dcf8
Brian Upton <ystros@gmail.com>
2017-09-17 14:10:50 -0700

    Make Chamber of the Painted Table melee friendly

--- server/game/cards/01-Core/ChamberOfThePaintedTable.js
+++ server/game/cards/01-Core/ChamberOfThePaintedTable.js
@@ -8,17 +8,35 @@ class ChamberOfThePaintedTable extends DrawCard {
             },
             cost: ability.costs.kneelSelf(),
             handler: () => {
-                let opponent = this.game.getOtherPlayer(this.controller);
-                this.game.addMessage('{0} kneels {1} to move 1 power from {2}\'s faction card to their own',
-                    this.controller, this, opponent);
-                this.game.transferPower(this.controller, opponent, 1);
+                let opponents = this.game.getOpponents(this.controller).filter(card => card.power > 0);
+
+                if(opponents.length === 1) {
+                    this.stealPowerFromOpponent(opponents[0]);
+                    return;
+                }
+
+                let factionCards = opponents.map(opponent => opponent.faction);
+
+                this.promptForSelect(this.controller, {
+                    activePromptTitle: 'Select a faction card',
+                    cardCondition: card => factionCards.includes(card),
+                    cardType: 'faction',
+                    onSelect: (player, card) => this.stealPowerFromOpponent(card.owner)
+                });
             }
         });
     }
 
+    stealPowerFromOpponent(opponent) {
+        this.game.addMessage('{0} kneels {1} to move 1 power from {2}\'s faction card to their own',
+            this.controller, this, opponent);
+        this.game.transferPower(this.controller, opponent, 1);
+        return true;
+    }
+
     opponentHasPower() {
-        let opponent = this.game.getOtherPlayer(this.controller);
-        return opponent && opponent.faction.power > 0;
+        let opponents = this.game.getOpponents(this.controller);
+        return opponents.some(opponent => opponent.faction.power > 0);
     }
 }
 
