98578eb9e2557816c15ae064fd449404d9f36b26
Brian Upton <ystros@gmail.com>
2017-09-18 00:17:37 -0700

    Update first player order to work with melee (#1457)

--- test/server/game/getplayers.spec.js
+++ test/server/game/getplayers.spec.js
@@ -13,7 +13,7 @@ describe('Game', function() {
         this.setPlayer2 = { id: '2', name: 'test2', firstPlayer: true };
     });
 
-    describe('getPlayersInFirstPlayer Order', function() {
+    describe('getPlayersInFirstPlayerOrder()', function() {
         describe('when there are no players', function() {
             beforeEach(function() {
                 this.players = this.game.getPlayersInFirstPlayerOrder();
@@ -105,6 +105,54 @@ describe('Game', function() {
                 it('should return player 2 then player 1', function() {
                     expect(this.players[0]).toBe(this.setPlayer2);
                     expect(this.players[1]).toBe(this.setFalsePlayer1);
+                });
+            });
+        });
+
+        describe('when there are more than two players', function() {
+            beforeEach(function() {
+                this.player1 = { id: '1', name: 'test1', firstPlayer: false };
+                this.player2 = { id: '2', name: 'test2', firstPlayer: false };
+                this.player3 = { id: '3', name: 'test1', firstPlayer: false };
+
+                this.game.playersAndSpectators['test1'] = this.player1;
+                this.game.playersAndSpectators['test2'] = this.player2;
+                this.game.playersAndSpectators['test3'] = this.player3;
+            });
+
+            describe('and there is no first player', function() {
+                it('should return the players in default order', function() {
+                    expect(this.game.getPlayersInFirstPlayerOrder()).toEqual([this.player1, this.player2, this.player3]);
+                });
+            });
+
+            describe('and the first player in the list is first player', function() {
+                beforeEach(function() {
+                    this.player1.firstPlayer = true;
+                });
+
+                it('should return the players in correct order', function() {
+                    expect(this.game.getPlayersInFirstPlayerOrder()).toEqual([this.player1, this.player2, this.player3]);
+                });
+            });
+
+            describe('and the last player in the list is first player', function() {
+                beforeEach(function() {
+                    this.player3.firstPlayer = true;
+                });
+
+                it('should return the players in correct order', function() {
+                    expect(this.game.getPlayersInFirstPlayerOrder()).toEqual([this.player3, this.player1, this.player2]);
+                });
+            });
+
+            describe('and a middle player in the list is first player', function() {
+                beforeEach(function() {
+                    this.player2.firstPlayer = true;
+                });
+
+                it('should return the players in clockwise order', function() {
+                    expect(this.game.getPlayersInFirstPlayerOrder()).toEqual([this.player2, this.player3, this.player1]);
                 });
             });
         });
