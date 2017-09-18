d4af66b7f47ce833b091de5f79ce879cfc8d9bc4
Stefano Zacchiroli <zack@upsilon.cc>
2017-09-18 09:50:51 +0200

    Xaro Xohan Daxos: use ability parameter instead of abilitylimit.js

--- server/game/cards/04.5-GoH/XaroXhoanDaxos.js
+++ server/game/cards/04.5-GoH/XaroXhoanDaxos.js
@@ -1,13 +1,12 @@
 const DrawCard = require('../../drawcard.js');
-const AbilityLimit = require('../../abilitylimit.js');
 
 class XaroXhoanDaxos extends DrawCard {
-    setupCardAbilities() {
+    setupCardAbilities(ability) {
         this.reaction({
             when: {
                 onCardEntersPlay: event => event.card.getType() === 'attachment' && event.card.isUnique() && event.playingType === 'marshal'
             },
-            limit: AbilityLimit.perPhase(1),
+            limit: ability.limit.perPhase(1),
             handler: () => {
                 this.game.addGold(this.controller, 2);
                 this.game.addMessage('{0} uses {1} to gain 2 gold as a unique attachment has been marshalled', this.controller, this);
