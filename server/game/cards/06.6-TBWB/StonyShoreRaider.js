ba212ff631133daf3d9686b9a894e3a50116fe3c
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-09-19 15:11:49 +0200

    Implement Stony Shore Raider

---
+++ server/game/cards/06.6-TBWB/StonyShoreRaider.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard.js');
+
+class StonyShoreRaider extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Kneel location',
+            cost: ability.costs.discardGold(),
+            target: {
+                activePromptTitle: 'Select a location',
+                cardCondition: card => card.location === 'play area' && card.getType() === 'location' &&
+                                       card.getPrintedCost() <= 3 && !card.kneeled
+            },
+            handler: context => {
+                context.target.controller.kneelCard(context.target);
+                this.game.addMessage('{0} discards 1 gold from {1} to kneel {2}',
+                    this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+StonyShoreRaider.code = '06111';
+
+module.exports = StonyShoreRaider;
