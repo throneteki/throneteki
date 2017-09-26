8613aa497b266983c571ba38eb3e1793cf4ac711
DukeTax <DukeTax@users.noreply.github.com>
2017-09-26 19:10:22 +0200

    Implement Sparring in Secret, Asha Greyjoy (Kingsmoot) & Kingsmoot (#1492)
    * Implement Sparring in Secret
    
    * Implement Asha GreyJoy (Kingsmoot)
    
    * Implement Kingsmoot

---
+++ server/game/cards/08.3-Km/AshaGreyJoy.js
@@ -0,0 +1,37 @@
+const DrawCard = require('../../drawcard.js');
+
+class AshaGreyjoy extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onPillage: event => event.source === this && event.challenge.loser.discardPile.size() >= 1
+            },
+            handler: context => {
+                let amount = context.event.challenge.loser.discardPile.size();
+
+                this.game.promptForDeckSearch(this.controller, {
+                    numCards: amount,
+                    activePromptTitle: 'Select a card',
+                    onSelect: (player, card) => this.cardSelected(player, card, amount),
+                    onCancel: player => this.doneSelecting(player, amount),
+                    source: this
+                });
+            }
+        });
+    }
+
+    cardSelected(player, card, amount) {
+        player.moveCard(card, 'hand');
+        this.game.addMessage('{0} uses {1} to search the top {2} cards of their deck and add {2} to their hand',
+            player, this, amount, card);
+    }
+
+    doneSelecting(player, amount) {
+        this.game.addMessage('{0} uses {1} to search the top {2} cards of their deck, but does not add any card to their hand',
+            player, this, amount);
+    }
+}
+
+AshaGreyjoy.code = '08051';
+
+module.exports = AshaGreyjoy;
---
+++ server/game/cards/08.3-Km/Kingsmoot.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard.js');
+
+class Kingsmoot extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            max: ability.limit.perRound(1),
+            when: {
+                onDominanceDetermined: event => this.controller === event.winner
+            },
+            target: {
+                cardCondition: card => card.location === 'play area' && card.controller === this.controller &&
+                                           card.isUnique() && card.isFaction('greyjoy') && card.getType() === 'character'
+            },
+            handler: context => {
+                let amount = context.player.getNumberOfCardsInPlay(card => card.isUnique() && card.isFaction('greyjoy') && card.getType() === 'character');
+                context.target.modifyPower(amount);
+                this.game.addMessage('{0} plays {1} to have {2} gain {3} power', context.player, this, context.target, amount);
+            }
+        });
+    }
+}
+
+Kingsmoot.code = '08052';
+
+module.exports = Kingsmoot;
