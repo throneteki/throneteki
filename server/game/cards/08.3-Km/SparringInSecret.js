6c2385672df21c54513f7e856ab3ae17c2180351
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-09-26 17:04:59 +0200

    Implement Sparring in Secret

---
+++ server/game/cards/08.3-Km/SparringInSecret.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard.js');
+
+class SparringInSecret extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Stand Knight character',
+            cost: ability.costs.kneel(card => card.hasTrait('Knight') && card.getType() === 'character'),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.hasTrait('Knight') &&
+                                       card.getType() === 'character' && card.kneeled
+            },
+            handler: context => {
+                context.target.controller.standCard(context.target);
+                this.game.addMessage('{0} plays {1} and kneels {2} to stand {3}',
+                    context.player, this, context.kneelingCostCard, context.target);
+            }
+        });
+    }
+}
+
+SparringInSecret.code = '08050';
+
+module.exports = SparringInSecret;
