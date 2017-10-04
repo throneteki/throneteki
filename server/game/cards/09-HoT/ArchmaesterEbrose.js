4befee5942570f2caa86f0650156def599d55f6a
DukeTax <DukeTax@users.noreply.github.com>
2017-10-04 19:41:38 +0200

    Implement Archmaester Ebrose & Words Are Wind (#1517)
    * Implement Archmaester Ebrose
    
    * Implement Words Are Wind
    
    * Use string interpolation instead of concatenation
    Clarify button text

---
+++ server/game/cards/09-HoT/ArchmaesterEbrose.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard.js');
+
+class ArchmaesterEbrose extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Stand Maester character',
+            limit: ability.limit.perPhase(1),
+            cost: ability.costs.kneel(card => card.hasTrait('Maester') && card.getType() === 'character'),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.hasTrait('Maester') &&
+                                       card.getType() === 'character' && card.kneeled
+            },
+            handler: context => {
+                context.target.controller.standCard(context.target);
+                this.game.addMessage('{0} uses {1} and kneels {2} to stand {3}',
+                    context.player, this, context.kneelingCostCard, context.target);
+            }
+        });
+    }
+}
+
+ArchmaesterEbrose.code = '09039';
+
+module.exports = ArchmaesterEbrose;
---
+++ server/game/cards/09-HoT/WordsAreWind.js
@@ -0,0 +1,45 @@
+const DrawCard = require('../../drawcard.js');
+
+class WordsAreWind extends DrawCard {
+    setupCardAbilities() {
+        this.interrupt({
+            canCancel: true,
+            when: {
+                onCardAbilityInitiated: event => event.source.getType() === 'event' && event.player !== this.controller
+            },
+            handler: context => {
+                this.context = context;
+
+                this.game.addMessage('{0} plays {1} to have {2} choose whether to cancel {3} or have {0} draw 2 cards',
+                    context.player, this, context.event.player, context.event.source);
+
+                this.game.promptWithMenu(context.event.player, this, {
+                    activePrompt: {
+                        menuTitle: `Cancel ${context.event.source.name} or ${context.player.name} draws 2 cards?`,
+                        buttons: [
+                            { text: `Cancel ${context.event.source.name}`, method: 'cancel' },
+                            { text: `${context.player.name} draws 2 cards`, method: 'draw' }
+                        ]
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    cancel() {
+        this.context.event.cancel();
+        this.game.addMessage('{0} chooses to have {1} cancelled for {2}', this.context.event.player, this.context.event.source, this);
+        return true;
+    }
+
+    draw() {
+        this.context.player.drawCardsToHand(2);
+        this.game.addMessage('{0} chooses to have {1} draw 2 cards for {2}', this.context.event.player, this.context.player, this);
+        return true;
+    }
+}
+
+WordsAreWind.code = '09044';
+
+module.exports = WordsAreWind;
