6801ff2e086ae56dfaa7c8ba7e958c1c42a0221c
Stuart Walsh <stu@ipng.org.uk>
2017-10-06 08:55:48 +0100

    Remove raven script from the html and use npm instead to put it in the bundle.  update usage to the new api version and add a default blocklist

--- index.prod.jsx
+++ index.prod.jsx
@@ -1,4 +1,4 @@
-/*global user, authToken, Raven */
+/*global user, authToken */
 import React from 'react';
 import { render } from 'react-dom';
 import Application from './Application.jsx';
@@ -7,13 +7,55 @@ import configureStore from './configureStore';
 import { navigate, login } from './actions';
 import 'bootstrap/dist/js/bootstrap';
 import ReduxToastr from 'react-redux-toastr';
+import Raven from 'raven-js';
 
 import version from '../version.js';
 
-Raven.config('https://f5286cd580bf46898e7180c7a46de2f6@sentry.io/123019', {
-    ignoreErrors: ['/recaptcha/api2'],
+const ravenOptions = {
+    ignoreErrors: [
+        // Random plugins/extensions
+        'top.GLOBALS',
+        // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error. html
+        'originalCreateNotification',
+        'canvas.contentDocument',
+        'MyApp_RemoveAllHighlights',
+        'http://tt.epicplay.com',
+        'Can\'t find variable: ZiteReader',
+        'jigsaw is not defined',
+        'ComboSearch is not defined',
+        'http://loading.retry.widdit.com/',
+        'atomicFindClose',
+        // Facebook borked
+        'fb_xd_fragment',
+        // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to
+        // reduce this. (thanks @acdha)
+        // See http://stackoverflow.com/questions/4113268
+        'bmi_SafeAddOnload',
+        'EBCallBackMessageReceived',
+        // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
+        'conduitPage'
+    ],
+    ignoreUrls: [
+        // Facebook flakiness
+        /graph\.facebook\.com/i,
+        // Facebook blocked
+        /connect\.facebook\.net\/en_US\/all\.js/i,
+        // Woopra flakiness
+        /eatdifferent\.com\.woopra-ns\.com/i,
+        /static\.woopra\.com\/js\/woopra\.js/i,
+        // Chrome extensions
+        /extensions\//i,
+        /^chrome:\/\//i,
+        // Other plugins
+        /127\.0\.0\.1:4001\/isrunning/i, // Cacaoweb
+        /webappstoolbarba\.texthelp\.com\//i,
+        /metrics\.itunes\.apple\.com\.edgesuite\.net\//i
+    ],
     release: version
-}).install();
+};
+
+Raven.config('https://f5286cd580bf46898e7180c7a46de2f6@sentry.io/123019', ravenOptions)
+    .install();
 
 const store = configureStore();
 
