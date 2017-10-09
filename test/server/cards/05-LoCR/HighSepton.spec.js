3f17d22d65f2999b21ad26c72a49882d00b20165
Brian Upton <ystros@gmail.com>
2017-10-09 14:32:09 -0700

    Implement High Septon

---
+++ test/server/cards/05-LoCR/HighSepton.spec.js
@@ -0,0 +1,89 @@
+describe('High Septon', function() {
+    integration(function() {
+        beforeEach(function() {
+            const deck1 = this.buildDeck('stark', [
+                'A Noble Cause',
+                'High Septon', 'House Tully Septon', 'Winterfell Steward'
+            ]);
+            const deck2 = this.buildDeck('targaryen', [
+                'A Noble Cause',
+                'Mirri Maz Duur', 'Tears of Lys', 'Even Handed Justice', 'His Viper Eyes'
+            ]);
+
+            this.player1.selectDeck(deck1);
+            this.player2.selectDeck(deck2);
+            this.startGame();
+            this.keepStartingHands();
+
+            this.septon = this.player1.findCardByName('High Septon', 'hand');
+            this.sevenCharacter = this.player1.findCardByName('House Tully Septon', 'hand');
+            this.nonSevenCharacter = this.player1.findCardByName('Winterfell Steward', 'hand');
+
+
+            this.player1.clickCard(this.septon);
+            this.player1.clickCard(this.sevenCharacter);
+            this.player2.clickCard('Mirri Maz Duur');
+
+            this.completeSetup();
+
+            this.player1.selectPlot('A Noble Cause');
+            this.player2.selectPlot('A Noble Cause');
+
+        });
+
+        describe('vs a single target ability', function() {
+            beforeEach(function() {
+                this.selectFirstPlayer(this.player2);
+
+                this.completeMarshalPhase();
+
+                this.unopposedChallenge(this.player2, 'intrigue', 'Mirri Maz Duur');
+                this.player2.clickPrompt('Pass');
+                this.player2.clickPrompt('Apply Claim');
+
+                this.player2.clickPrompt('Mirri Maz Duur');
+                this.player2.clickCard(this.septon);
+            });
+
+            it('should allow it be redirected to a The Seven character', function() {
+                this.player1.clickPrompt('High Septon');
+                this.player1.clickCard(this.sevenCharacter);
+
+                expect(this.sevenCharacter.location).toBe('dead pile');
+                expect(this.septon.location).not.toBe('dead pile');
+            });
+        });
+
+        describe('vs a non-targeting ability', function() {
+            beforeEach(function() {
+                this.selectFirstPlayer(this.player2);
+
+                this.player2.clickPrompt('Done');
+                this.player1.clickCard(this.nonSevenCharacter);
+                this.player1.clickPrompt('Done');
+
+                this.unopposedChallenge(this.player2, 'intrigue', 'Mirri Maz Duur');
+                this.player2.clickPrompt('Tears of Lys');
+                this.player2.clickCard(this.nonSevenCharacter);
+            });
+
+            it('should not allow it be redirected', function() {
+                expect(this.player1).not.toHavePromptButton('High Septon');
+            });
+        });
+
+        describe('vs a multi target ability', function() {
+            beforeEach(function() {
+                this.selectFirstPlayer(this.player2);
+
+                this.player2.clickCard('Even Handed Justice', 'hand');
+                this.player2.clickCard('Mirri Maz Duur');
+                this.player2.clickCard(this.septon);
+            });
+
+            it('should not allow it be redirected', function() {
+                expect(this.player1).not.toHavePromptButton('High Septon');
+            });
+        });
+    });
+});
