7b041d6ba2cf0e550885c07b2b53270641d660bb
Stefano Zacchiroli <zack@upsilon.cc>
2017-10-11 20:25:05 +0200

    Implement Corpse Lake (#1526)

---
+++ server/game/cards/09-HoT/CorpseLake.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard.js');
+
+class CorpseLake extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardsDiscarded: event =>
+                    event.originalLocation === 'draw deck'
+                    && event.card.getType() === 'character'
+                    && event.card.controller !== this.controller
+            },
+            limit: ability.limit.perRound(3),
+            handler: () => {
+                this.modifyPower(1);
+                this.game.addMessage('{0} gains 1 power on {1}', this.controller, this);
+            }
+        });
+    }
+}
+
+CorpseLake.code = '09028';
+
+module.exports = CorpseLake;
