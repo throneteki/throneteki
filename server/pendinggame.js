935b6e52434c4f1a0caf161d665a0bf757286b49
Grugn0 <grugno.mezzorco@gmail.com>
2017-10-19 22:31:53 +0200

    minor bug fix that resolve user being stuck on game list. (#1544)
    Bug description:
    When user click on "watch" on a game still pending nothing happen, nevertheless he could no watch nor join any other game in list. Page refresh
    was needed

--- server/pendinggame.js
+++ server/pendinggame.js
@@ -89,7 +89,7 @@ class PendingGame {
     addSpectator(id, user) {
         this.spectators[user.username] = {
             id: id,
-            name: user.userame,
+            name: user.username,
             user: user,
             emailHash: user.emailHash
         };
