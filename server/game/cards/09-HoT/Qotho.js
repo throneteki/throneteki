169d7f556ec938e47e16e32ff198733f9f7dfce2
Brian Upton <ystros@gmail.com>
2017-10-19 14:43:36 -0700

    Implement Qotho

---
+++ server/game/cards/09-HoT/Qotho.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+
+class Qotho extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Put Qotho into play',
+            location: 'hand',
+            condition: () => this.controller.canPutIntoPlay(this),
+            cost: ability.costs.discardMultipleFromHand(2, card => card !== this && card.isFaction('targaryen')),
+            handler: context => {
+                this.controller.putIntoPlay(this);
+                this.game.addMessage('{0} discards {1} to put {2} into play',
+                    this.controller, context.costs.discardFromHand, this);
+            }
+        });
+    }
+}
+
+Qotho.code = '09037';
+
+module.exports = Qotho;
