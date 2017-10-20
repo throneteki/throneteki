a99aa0998102adde16fae2f5c121e8617bfe8cb7
Brian Upton <ystros@gmail.com>
2017-10-20 00:12:42 -0700

    Implement Hellholt (#1547)

---
+++ server/game/cards/09-HoT/Hellholt.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard.js');
+
+class Hellholt extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.loser === this.controller && event.challenge.strengthDifference >= 5
+            },
+            handler: () => {
+                this.modifyPower(1);
+                this.game.addMessage('{0} gains 1 power on {1}', this.controller, this);
+            }
+        });
+    }
+}
+
+Hellholt.code = '09032';
+
+module.exports = Hellholt;
