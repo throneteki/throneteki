68b549209a3c72b337806b27434d4b7a1a5de394
Brian Upton <ystros@gmail.com>
2017-10-23 10:39:25 -0700

    Implement Your King Commands It

---
+++ server/game/cards/09-HoT/YourKingCommandsIt.js
@@ -0,0 +1,15 @@
+const PlotCard = require('../../plotcard.js');
+
+class YourKingCommandsIt extends PlotCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: card => card.getType() === 'character' && !card.hasTrait('King') && !card.hasTrait('Queen'),
+            targetController: 'any',
+            effect: ability.effects.cannotGainPower()
+        });
+    }
+}
+
+YourKingCommandsIt.code = '09048';
+
+module.exports = YourKingCommandsIt;
