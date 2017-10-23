ae684cffa607bb8e26dda283671ef72ee219a657
Brian Upton <ystros@gmail.com>
2017-10-23 02:14:00 -0700

    Implement Hidden Thorns

---
+++ server/game/cards/09-HoT/HiddenThorns.js
@@ -0,0 +1,54 @@
+const DrawCard = require('../../drawcard.js');
+
+class HiddenThorns extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            max: ability.limit.perChallenge(1),
+            when: {
+                afterChallenge: event => (
+                    event.challenge.winner === this.controller &&
+                    event.challenge.challengeType === 'intrigue' &&
+                    event.challenge.strengthDifference >= 5
+                )
+            },
+            handler: context => {
+                let loser = context.event.challenge.loser;
+                this.game.promptForSelect(loser, {
+                    numCards: 2,
+                    activePromptTitle: 'Select 2 cards',
+                    cardCondition: card => card.controller === loser && card.location === 'hand',
+                    onSelect: (player, cards) => this.discardCards(player, cards),
+                    onCancel: player => this.cancelResolution(player)
+                });
+            }
+        });
+    }
+
+    discardCards(player, cards) {
+        player.discardCards(cards);
+        this.game.addMessage('{0} uses {1} to make {2} choose and discard {3}', this.controller, this, player, cards);
+
+        if(this.controller.anyCardsInPlay(card => card.name === 'The Queen of Thorns')) {
+            this.lastingEffect(ability => ({
+                until: {
+                    onCardPlayed: event => event.card === this
+                },
+                targetLocation: 'any',
+                match: this,
+                effect: ability.effects.setEventPlacementLocation('hand')
+            }));
+        }
+
+        return true;
+    }
+
+    cancelResolution(player) {
+        this.game.addAlert('danger', '{0} cancels resolution of {1}', player, this);
+
+        return true;
+    }
+}
+
+HiddenThorns.code = '09024';
+
+module.exports = HiddenThorns;
---
+++ test/server/cards/09-HoT/HiddenThorns.spec.js
@@ -0,0 +1,74 @@
+describe('Hidden Thorns', function() {
+    integration(function() {
+        beforeEach(function() {
+            const deck1 = this.buildDeck('tyrell', [
+                'A Noble Cause',
+                'Margaery Tyrell (AMAF)', 'The Queen of Thorns (HoT)', 'Hidden Thorns'
+            ]);
+            const deck2 = this.buildDeck('stark', [
+                'A Noble Cause',
+                'Hedge Knight', 'Varys'
+            ]);
+
+            this.player1.selectDeck(deck1);
+            this.player2.selectDeck(deck2);
+            this.startGame();
+            this.keepStartingHands();
+
+            this.character = this.player1.findCardByName('Margaery Tyrell', 'hand');
+            this.queenOfThorns = this.player1.findCardByName('The Queen of Thorns', 'hand');
+            this.hiddenThorns = this.player1.findCardByName('Hidden Thorns', 'hand');
+
+            this.card1 = this.player2.findCardByName('Hedge Knight', 'hand');
+            this.card2 = this.player2.findCardByName('Varys', 'hand');
+
+            this.player1.clickCard(this.character);
+
+            this.completeSetup();
+
+            this.player1.selectPlot('A Noble Cause');
+            this.player2.selectPlot('A Noble Cause');
+            this.selectFirstPlayer(this.player1);
+        });
+
+        describe('when winning a challenge by 5 or more', function() {
+            beforeEach(function() {
+                this.completeMarshalPhase();
+
+                this.unopposedChallenge(this.player1, 'Intrigue', this.character);
+                this.player1.clickPrompt('Hidden Thorns');
+
+                this.player2.clickCard(this.card1);
+                this.player2.clickCard(this.card2);
+                this.player2.clickPrompt('Done');
+            });
+
+            it('should discard the cards', function() {
+                expect(this.card1.location).toBe('discard pile');
+                expect(this.card2.location).toBe('discard pile');
+            });
+
+            it('should place the event in discard', function() {
+                expect(this.hiddenThorns.location).toBe('discard pile');
+            });
+        });
+
+        describe('when the Queen of Thorns is controlled', function() {
+            beforeEach(function() {
+                this.player1.clickCard(this.queenOfThorns);
+                this.completeMarshalPhase();
+
+                this.unopposedChallenge(this.player1, 'Intrigue', this.character);
+                this.player1.clickPrompt('Hidden Thorns');
+
+                this.player2.clickCard(this.card1);
+                this.player2.clickCard(this.card2);
+                this.player2.clickPrompt('Done');
+            });
+
+            it('should return the event to hand', function() {
+                expect(this.hiddenThorns.location).toBe('hand');
+            });
+        });
+    });
+});
