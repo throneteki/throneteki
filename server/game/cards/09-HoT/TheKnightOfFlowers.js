29e466b1d70671f05c9471816661a9d58360a677
Brian Upton <ystros@gmail.com>
2017-10-24 09:25:55 -0700

    Implement The Knight of Flowers

---
+++ server/game/cards/09-HoT/TheKnightOfFlowers.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheKnightOfFlowers extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardPlayed: event => event.player === this.controller
+            },
+            handler: () => {
+                this.game.addMessage('{0} uses {1} to gain +2 STR until the end of the phase', this.controller, this);
+                this.untilEndOfPhase(ability =>({
+                    match: this,
+                    effect: ability.effects.modifyStrength(2)
+                }));
+            }
+        });
+    }
+}
+
+TheKnightOfFlowers.code = '09007';
+
+module.exports = TheKnightOfFlowers;
