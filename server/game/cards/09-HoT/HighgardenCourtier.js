c96aecebd85ee682f037f97ef7398c9708fe33a5
Brian Upton <ystros@gmail.com>
2017-10-24 09:47:04 -0700

    Implement Highgarden Courtier

---
+++ server/game/cards/09-HoT/HighgardenCourtier.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class HighgardenCourtier extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardStrengthChanged: event => (
+                    event.card !== this &&
+                    event.card.controller === this.controller &&
+                    event.amount > 0 &&
+                    event.applying &&
+                    event.card.kneeled
+                )
+            },
+            limit: ability.limit.perPhase(1),
+            handler: context => {
+                this.game.addMessage('{0} kneels {1} to stand {2}', this.controller, this, context.event.card);
+                this.controller.standCard(context.event.card);
+            }
+        });
+    }
+}
+
+HighgardenCourtier.code = '09015';
+
+module.exports = HighgardenCourtier;
