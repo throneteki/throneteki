2d8174a0c3cc59fa65f1a288f82e4b81325be7f5
Brian Upton <ystros@gmail.com>
2017-10-23 17:58:00 -0700

    Implement Nothing Burns Like The Cold

---
+++ server/game/cards/09-HoT/NothingBurnsLikeTheCold.js
@@ -0,0 +1,104 @@
+const PlotCard = require('../../plotcard.js');
+
+class NothingBurnsLikeTheCold extends PlotCard {
+    setupCardAbilities() {
+        this.whenRevealed({
+            handler: () => {
+                this.selections = [];
+                this.remainingPlayers = this.game.getPlayersInFirstPlayerOrder();
+                this.proceedToNextStep();
+            }
+        });
+    }
+
+    proceedToNextStep() {
+        if(this.remainingPlayers.length > 0) {
+            let currentPlayer = this.remainingPlayers.shift();
+            this.promptForAttachment(currentPlayer);
+        } else {
+            this.doDiscard();
+        }
+    }
+
+    promptForAttachment(currentPlayer) {
+        if(!this.hasValidAttachments(currentPlayer)) {
+            this.onSelectAttachment(currentPlayer, null);
+            return;
+        }
+
+        this.game.promptForSelect(currentPlayer, {
+            activePromptTitle: 'Select an attachment',
+            source: this,
+            cardCondition: card => card.location === 'play area' && card.controller === currentPlayer && card.getType() === 'attachment',
+            onSelect: (player, card) => this.onSelectAttachment(player, card),
+            onCancel: (player) => this.onSelectAttachment(player, null)
+        });
+    }
+
+    hasValidAttachments(player) {
+        return this.game.anyCardsInPlay(card => card.controller === player && card.getType() === 'attachment');
+    }
+
+    onSelectAttachment(currentPlayer, attachment) {
+        this.promptForLocation(currentPlayer, attachment);
+        return true;
+    }
+
+    promptForLocation(currentPlayer, attachment) {
+        if(!this.hasValidLocations(currentPlayer)) {
+            this.onSelectLocation(currentPlayer, attachment, null);
+            return;
+        }
+
+        this.game.promptForSelect(currentPlayer, {
+            activePromptTitle: 'Select a location',
+            source: this,
+            cardCondition: card => card.location === 'play area' && card.controller === currentPlayer && card.getType() === 'location',
+            onSelect: (player, card) => this.onSelectLocation(player, attachment, card),
+            onCancel: (player) => this.onSelectLocation(player, attachment, null)
+        });
+    }
+
+    hasValidLocations(player) {
+        return this.game.anyCardsInPlay(card => card.controller === player && card.getType() === 'location' && !card.isLimited());
+    }
+
+    onSelectLocation(player, attachment, location) {
+        let cards = [attachment, location].filter(card => card !== null);
+
+        if(cards.length === 0) {
+            this.game.addMessage('{0} does not choose any cards for {1}', player, this);
+        } else {
+            this.game.addMessage('{0} chooses {1} for {2}', player, cards, this);
+        }
+
+        if(!attachment && this.hasValidAttachments(player)) {
+            this.game.addAlert('danger', '{0} has attachments for {1} but did not select one', player, this);
+        }
+
+        if(!location && this.hasValidLocations(player)) {
+            this.game.addAlert('danger', '{0} has locations for {1} but did not select one', player, this);
+        }
+
+        this.selections.push({ player: player, cards: cards });
+        this.proceedToNextStep();
+        return true;
+    }
+
+    doDiscard() {
+        for(let selection of this.selections) {
+            let player = selection.player;
+
+            if(selection.cards.length !== 0) {
+                this.game.addMessage('{0} discards {1} for {2}', player, selection.cards, this);
+                player.discardCards(selection.cards, false);
+            }
+        }
+
+        this.selections = [];
+    }
+}
+
+NothingBurnsLikeTheCold.code = '09052';
+
+module.exports = NothingBurnsLikeTheCold;
