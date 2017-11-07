981605c6e407825bc86b8d850114685949c57af8
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-11-07 22:55:08 +0100

    Implement Selyse Baratheon (FotOG)

---
+++ server/game/cards/08.4-FotOG/SelyseBaratheon.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard.js');
+
+class SelyseBaratheon extends DrawCard {
+    setupCardAbilities() {
+        this.forcedReaction({
+            when: {
+                onCardEntersPlay: event => event.card === this && event.playingType === 'marshal' && !this.controller.faction.kneeled
+            },
+            handler: () => {
+                this.controller.kneelCard(this.controller.faction);
+                this.game.addMessage('{0} is forced by {1} to kneel their faction card', this.controller, this);
+
+                let nonLadies = this.game.filterCardsInPlay(card => !card.hasTrait('Lady') && card.getType() === 'character');
+                
+                if(nonLadies.length > 0) {
+                    for(let card of nonLadies) {
+                        card.controller.kneelCard(card);
+                    }
+                    this.game.addMessage('{0} then kneels each non-Lady character', this.controller);
+                }
+            }
+        });
+    }
+}
+
+SelyseBaratheon.code = '08067';
+
+module.exports = SelyseBaratheon;
