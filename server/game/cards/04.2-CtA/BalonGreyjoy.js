814741f09e1e0d35800b4347ad0891a72da22616
DukeTax <DukeTax@users.noreply.github.com>
2017-11-11 00:30:06 +0100

    Add limit to Balon Greyjoy (CtA) (#1589)
    Also standardise action label and message

--- server/game/cards/04.2-CtA/BalonGreyjoy.js
+++ server/game/cards/04.2-CtA/BalonGreyjoy.js
@@ -15,15 +15,16 @@ class BalonGreyjoy extends DrawCard {
         });
 
         this.action({
-            title: 'Kneel a Greyjoy location to give +1 to loyal characters',
+            title: 'Give loyal characters +1 STR',
             cost: ability.costs.kneel(card => card.isFaction('greyjoy') && card.getType() === 'location'),
+            limit: ability.limit.perChallenge(1),
             handler: context => {
                 this.untilEndOfChallenge(ability => ({
                     match: card => card.isLoyal() && card.controller === this.controller && card.getType() === 'character',
                     effect: ability.effects.modifyStrength(1)
                 }));
 
-                this.game.addMessage('{0} uses {1} to kneel {2} and give +1 STR to each loyal character they control until the end of the challenge',
+                this.game.addMessage('{0} uses {1} and kneels {2} to give +1 STR to each loyal character they control until the end of the challenge',
                     this.controller, this, context.costs.kneel);
             }
         });
