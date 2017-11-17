e8d9504084f321dc7f1ddad6f6914b144117a930
Brian Upton <ystros@gmail.com>
2017-11-17 10:03:03 -0800

    Use cost API for Janos Slynt

--- server/game/cards/04.1-AtSK/JanosSlynt.js
+++ server/game/cards/04.1-AtSK/JanosSlynt.js
@@ -1,12 +1,12 @@
 const DrawCard = require('../../drawcard.js');
 
 class JanosSlynt extends DrawCard {
-    setupCardAbilities() {
+    setupCardAbilities(ability) {
         this.action({
             title: 'Pay 1 gold to give Janos Slynt +2 strength',
+            cost: ability.costs.payGold(1),
             handler: context => {
                 this.game.addMessage('{0} pays 1 gold to give {1} +2 STR until the end of the phase', context.player, this);
-                context.player.gold -= 1;
                 this.untilEndOfPhase(ability => ({
                     match: this,
                     effect: ability.effects.modifyStrength(2)
