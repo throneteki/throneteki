3fbd4b9b9572be496d30c26672a1326ffa852413
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-11-20 19:44:49 +0100

    Implement Daario Naharis

---
+++ server/game/cards/08.1-TAK/DaarioNaharis.js
@@ -0,0 +1,33 @@
+const DrawCard = require('../../drawcard.js');
+
+class DaarioNaharis extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && event.challenge.isParticipating(this)
+            },
+            target: {
+                cardCondition: card => card.location === 'play area' && card !== this && (card.kneeled || card.controller !== this.controller) &&
+                                       (card.hasTrait('Ally') || card.hasTrait('Companion') || card.hasTrait('Mercenary'))
+            },
+            handler: context => {
+                if(context.target.kneeled) {
+                    context.target.controller.standCard(context.target);
+                }
+
+                if(context.target.controller !== this.controller) {
+                    this.untilEndOfRound(ability => ({
+                        match: context.target,
+                        effect: ability.effects.takeControl(this.controller)
+                    }));
+                }
+
+                this.game.addMessage('{0} uses {1} to stand and take control of {2}', context.player, this, context.target);
+            }
+        });
+    }
+}
+
+DaarioNaharis.code = '08014';
+
+module.exports = DaarioNaharis;
