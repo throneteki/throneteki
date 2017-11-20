42dc0c1e20919d566fb942cffdee7061cbc50090
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-11-20 19:44:12 +0100

    Implement Nagga's Ribs

---
+++ server/game/cards/08.1-TAK/NaggasRibs.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class NaggasRibs extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            effect: ability.effects.modifyDominanceStrength(2 * this.controller.deadPile.size())
+        });
+
+        this.reaction({
+            when: {
+                onCardPlaced: event => event.card.getType() === 'character' && event.location === 'discard pile' &&
+                                       event.card.owner === this.controller
+            },
+            handler: context => {
+                let card = context.event.card;
+                context.player.moveCard(card, 'dead pile');
+                this.game.addMessage('{0} uses {1} to move {2} to their dead pile', context.player, this, card);
+            }
+        });
+    }
+}
+
+NaggasRibs.code = '08013';
+
+module.exports = NaggasRibs;
