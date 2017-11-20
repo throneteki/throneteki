8d74a2183dfdb544357c1bef2abdadd940c0ed9a
DukeTax <yorickvogels@iMac-van-Yorick.local>
2017-11-20 19:42:56 +0100

    Implement At Night They Howl

---
+++ server/game/cards/08.1-TAK/AtNightTheyHowl.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class AtNightTheyHowl extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Stand Direwolves',
+            phase: 'challenge',
+            target: {
+                mode: 'unlimited',
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.kneeled &&
+                                       (card.hasTrait('Direwolf') || card.attachments.any(attachment => attachment.hasTrait('Direwolf')))
+            },
+            handler: context => {
+                for(let card of context.target) {
+                    card.controller.standCard(card);
+                }
+
+                this.game.addMessage('{0} plays {1} to stand {2}', context.player, this, context.target);
+            }
+        });
+    }
+}
+
+AtNightTheyHowl.code = '08003';
+
+module.exports = AtNightTheyHowl;
