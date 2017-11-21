e7ce7c81a8e5d424f4eb2122e41330d751e8d474
DukeTax <DukeTax@users.noreply.github.com>
2017-11-21 04:05:29 +0100

    Implement Mag the Mighty (#1606)
    * Implement Mag the Mighty
    
    * Add Mag vs saves explanation to How To Play page

---
+++ server/game/cards/08.1-TAK/MagTheMighty.js
@@ -0,0 +1,35 @@
+const DrawCard = require('../../drawcard.js');
+
+class MagTheMighty extends DrawCard {
+    setupCardAbilities() {
+        this.forcedReaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && event.challenge.isParticipating(this)
+            },
+            target: {
+                cardCondition: card => card.location === 'play area' && card.controller === this.controller && card.getType() === 'character'
+            },
+            handler: context => {
+                this.game.killCharacter(context.target);
+                this.game.addMessage('{0} is forced by {1} to kill {2}', context.player, this, context.target);
+
+                //TODO Technically should only trigger when the first kill was not saved
+                this.game.promptForSelect(context.event.challenge.loser, {
+                    cardCondition: card => card.location === 'play area' && card.controller === context.event.challenge.loser && card.getType() === 'character',
+                    source: this,
+                    onSelect: (player, card) => this.onCardSelected(player, card)
+                });
+            }
+        });
+    }
+
+    onCardSelected(player, card) {
+        this.game.killCharacter(card);
+        this.game.addMessage('{0} is then forced by {1} to kill {2}', player, this, card);
+        return true;
+    }
+}
+
+MagTheMighty.code = '08018';
+
+module.exports = MagTheMighty;
