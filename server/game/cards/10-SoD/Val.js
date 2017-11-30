ae86e882e69bafb4a55618acce3c5ae085d4e34b
DukeTax <yorickvogels@gmail.com>
2017-11-30 14:25:16 +0100

    Implement Val

---
+++ server/game/cards/10-SoD/Val.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+
+class Val extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onAttackersDeclared: event => event.challenge.isAttacking(this)
+            },
+            target: {
+                cardCondition: card => card.location === 'hand' && card.controller === this.controller &&
+                                       card.getType() === 'character' && card.hasTrait('Wildling') && card.getPrintedCost() <= 4
+            },
+            handler: context => {
+                context.player.putIntoPlay(context.target);
+                context.target.kneeled = true;
+                this.game.currentChallenge.addAttacker(context.target);
+
+                this.game.addMessage('{0} uses {1} to put {2} into play from their hand knelt, participating as an attacker',
+                    context.player, this, context.target);
+            }
+        });
+    }
+}
+
+Val.code = '10039';
+
+module.exports = Val;
