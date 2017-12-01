f7cfefc9bc448d2cf546acb8ce6ccdce55e4d88d
DukeTax <yorickvogels@gmail.com>
2017-12-01 02:55:31 +0100

    Implement Gates of the Moon

---
+++ server/game/cards/08.2-JtO/GatesOfTheMoon.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard.js');
+
+class GatesOfTheMoon extends DrawCard {
+    setupCardAbilities(ability) {
+        this.plotModifiers({
+            gold: 2
+        });
+
+        this.persistentEffect({
+            match: card => card === card.controller.activePlot,
+            targetController: 'opponent',
+            effect: ability.effects.modifyGold(1)
+        });
+    }
+}
+
+GatesOfTheMoon.code = '08038';
+
+module.exports = GatesOfTheMoon;
