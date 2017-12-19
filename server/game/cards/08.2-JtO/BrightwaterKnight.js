3d742c5f05902271352b939799b0cc736c9faefd
DukeTax <yorickvogels@gmail.com>
2017-12-19 16:58:17 +0100

    Implement Brightwater Knight

---
+++ server/game/cards/08.2-JtO/BrightwaterKnight.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard.js');
+
+class BrightWaterKnight extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Reduce Knight character',
+            phase: 'marshal',
+            limit: ability.limit.perPhase(1),
+            cost: ability.costs.discardGold(),
+            handler: context => {
+                this.untilEndOfPhase(ability => ({
+                    targetType: 'player',
+                    effect: ability.effects.reduceNextMarshalledCardCost(
+                        2,
+                        card => card.getType() === 'character' && card.hasTrait('Knight')
+                    )
+                }));
+
+                this.game.addMessage('{0} discards 1 gold from {1} to reduce the next knight character they marshal this phase by 2',
+                    context.player, this);
+            }
+        });
+    }
+}
+
+BrightWaterKnight.code = '08023';
+
+module.exports = BrightWaterKnight;
