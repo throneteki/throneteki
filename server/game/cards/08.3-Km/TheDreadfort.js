86a76cb376f823a8b31d9bfabf0601d0a0043b94
DukeTax <yorickvogels@gmail.com>
2018-01-07 14:48:49 +0100

    Implement The Dreadfort

---
+++ server/game/cards/08.3-Km/TheDreadfort.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheDreadfort extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onSacrificed: event => event.cardStateWhenSacrificed.controller === this.controller &&
+                                       event.cardStateWhenSacrificed.getType() === 'character'
+            },
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' &&
+                                       (card.hasTrait('House Bolton') || !card.isFaction('stark')) && card.kneeled,
+                gameAction: 'stand'
+            },
+            handler: context => {
+                context.target.controller.standCard(context.target);
+                this.game.addMessage('{0} kneels {1} to stand {2}', context.player, this, context.target);
+            }
+        });
+    }
+}
+
+TheDreadfort.code = '08042';
+
+module.exports = TheDreadfort;
