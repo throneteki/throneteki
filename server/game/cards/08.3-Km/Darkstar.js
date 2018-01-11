f2b3bf7d732a79d9613b81ac081f595d6995f711
DukeTax <yorickvogels@gmail.com>
2018-01-11 01:09:55 +0100

    Implement Darkstar

---
+++ server/game/cards/08.3-Km/Darkstar.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class Darkstar extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onBypassedByStealth: event => event.source === this
+            },
+            handler: context => {
+                this.game.promptForIcon(context.player, this, icon => {
+                    this.untilEndOfPhase(ability => ({
+                        match: context.event.target,
+                        effect: ability.effects.removeIcon(icon)
+                    }));
+
+                    this.game.addMessage('{0} uses {1} to remove {2} {3} icon from {4}',
+                        context.player, this, icon === 'intrigue' ? 'an' : 'a', icon, context.event.target);
+                });
+            }
+        });
+    }
+}
+
+Darkstar.code = '08055';
+
+module.exports = Darkstar;
