08ca279dfd8695fbc4575386fc5a0d863e528ca6
DukeTax <yorickvogels@gmail.com>
2018-01-11 01:54:51 +0100

    Implement Haunted Forest Scout

---
+++ server/game/cards/08.3-Km/HauntedForestScout.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard.js');
+
+class HauntedForestScout extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            effect: ability.effects.cannotBeDeclaredAsDefender()
+        });
+        this.persistentEffect({
+            condition: () => this.getNumberOfRangers() >= 3,
+            match: this,
+            effect: ability.effects.doesNotKneelAsAttacker()
+        });
+    }
+
+    getNumberOfRangers() {
+        return this.controller.getNumberOfCardsInPlay(card => card.getType() === 'character' && card.hasTrait('Ranger'));
+    }
+}
+
+HauntedForestScout.code = '08045';
+
+module.exports = HauntedForestScout;
