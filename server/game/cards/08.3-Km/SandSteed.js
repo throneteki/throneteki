b5157fa1eece0a4e1aa9fda6dbf4226fc0409838
DukeTax <DukeTax@users.noreply.github.com>
2018-01-11 20:27:29 +0100

    Implement Martell cards from Kingsmoot (#1677)
    * Implement Darkstar
    
    * Implement Sand Steed

---
+++ server/game/cards/08.3-Km/SandSteed.js
@@ -0,0 +1,20 @@
+const DrawCard = require('../../drawcard.js');
+
+class SandSteed extends DrawCard {
+    setupCardAbilities() {
+        this.attachmentRestriction(card => card.getType() === 'character' && card.attachments.every(attachment => attachment === this || attachment.name !== 'Sand Steed')),
+        this.reaction({
+            when: {
+                onCardPlaced: event => event.card.hasTrait('Summer') && event.location === 'revealed plots' && event.player === this.controller
+            },
+            handler: context => {
+                this.parent.modifyPower(1);
+                this.game.addMessage('{0} uses {1} to gain 1 power on {2}', context.player, this, this.parent);
+            }
+        });
+    }
+}
+
+SandSteed.code = '08056';
+
+module.exports = SandSteed;
--- server/game/player.js
+++ server/game/player.js
@@ -1042,7 +1042,7 @@ class Player extends Spectator {
             targetPile.push(card);
         }
 
-        if(['dead pile', 'discard pile'].includes(targetLocation)) {
+        if(['dead pile', 'discard pile', 'revealed plots'].includes(targetLocation)) {
             this.game.raiseEvent('onCardPlaced', { card: card, location: targetLocation, player: this });
         }
     }
