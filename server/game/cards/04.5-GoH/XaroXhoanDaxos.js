9d9b259c30747039ed38d02f6e310a30fe2c7bde
DukeTax <DukeTax@users.noreply.github.com>
2018-01-12 05:21:55 +0100

    Only trigger XaroXD after marshalling own attachments (#1679)
    Also trim message.

--- server/game/cards/04.5-GoH/XaroXhoanDaxos.js
+++ server/game/cards/04.5-GoH/XaroXhoanDaxos.js
@@ -4,12 +4,13 @@ class XaroXhoanDaxos extends DrawCard {
     setupCardAbilities(ability) {
         this.reaction({
             when: {
-                onCardEntersPlay: event => event.card.getType() === 'attachment' && event.card.isUnique() && event.playingType === 'marshal'
+                onCardEntersPlay: event => event.card.getType() === 'attachment' && event.card.isUnique() &&
+                                           event.playingType === 'marshal' && event.card.controller === this.controller
             },
             limit: ability.limit.perPhase(1),
             handler: () => {
                 this.game.addGold(this.controller, 2);
-                this.game.addMessage('{0} uses {1} to gain 2 gold as a unique attachment has been marshalled', this.controller, this);
+                this.game.addMessage('{0} uses {1} to gain 2 gold', this.controller, this);
             }
         });
     }
