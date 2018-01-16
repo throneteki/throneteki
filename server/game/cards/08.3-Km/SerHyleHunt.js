91edd0c2c47f364ebc9bfa0d621d8891400f254e
DukeTax <yorickvogels@gmail.com>
2018-01-17 00:58:49 +0100

    Implement Ser Hyle Hunt

---
+++ server/game/cards/08.3-Km/SerHyleHunt.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard.js');
+
+class SerHyleHunt extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Remove character from challenge',
+            condition: () => this.game.currentChallenge && this.game.currentChallenge.isParticipating(this),
+            cost: ability.costs.payGold(1),
+            target: {
+                cardCondition: card => this.game.currentChallenge.isParticipating(card) && card.getStrength() < this.getStrength()
+            },
+            handler: context => {
+                context.target.controller.standCard(context.target);
+                this.game.currentChallenge.removeFromChallenge(context.target);
+                this.game.addMessage('{0} uses {1} and pays 1 gold to stand and remove {2} from the challenge',
+                    context.player, this, context.target);
+            }
+        });
+    }
+}
+
+SerHyleHunt.code = '08043';
+
+module.exports = SerHyleHunt;
