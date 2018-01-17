85503a77f0c7ee9a0ad50ed3b93c23f5e61eb18c
DukeTax <yorickvogels@gmail.com>
2018-01-17 22:13:32 +0100

    Implement Tom of Sevenstreams

---
+++ server/game/cards/08.3-Km/TomOfSevenstreams.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard.js');
+
+class TomOfSevenstreams extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardPlayed: event => event.card.hasTrait('Song') && event.card.controller === this.controller
+            },
+            limit: ability.limit.perPhase(1),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' &&
+                                       card.controller === this.controller && !card.isLoyal() && card.kneeled,
+                gameAction: 'stand'
+            },
+            handler: context => {
+                context.target.controller.standCard(context.target);
+                this.game.addMessage('{0} uses {1} to stand {2}', context.player, this, context.target);
+            }
+        });
+    }
+}
+
+TomOfSevenstreams.code = '08057';
+
+module.exports = TomOfSevenstreams;
