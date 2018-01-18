99aeb94dedb61505fa4f29ca18c14fff4ac2242d
Stefano Zacchiroli <zack@upsilon.cc>
2018-01-18 22:29:18 +0100

    implement Targaryen cards from Kingsmoot (#1697)
    - Jhiqui
    - The Skahazadhan

---
+++ server/game/cards/08.3-Km/Jhiqui.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard.js');
+
+class Jhiqui extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                afterChallenge: ({challenge}) => challenge.winner === this.controller
+            },
+            cost: ability.costs.discardFromHand(),
+            target: {
+                type: 'select',
+                cardCondition: card =>
+                    card.location === 'play area' && card.getType() === 'character'
+                        && this.game.currentChallenge.isParticipating(card)
+                        && (card.hasTrait('Lord') || card.hasTrait('Lady'))
+            },
+            handler: context => {
+                context.target.modifyPower(1);
+                this.game.addMessage('{0} uses {1} and discards {2} to have {3} gain 1 power',
+                    this.controller, this, context.costs.discardFromHand, context.target);
+            }
+        });
+    }
+}
+
+Jhiqui.code = '08053';
+
+module.exports = Jhiqui;
