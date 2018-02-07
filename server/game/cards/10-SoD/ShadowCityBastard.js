3b84c33a48b9bcd9870fe066947edc384e2d6954
DukeTax <DukeTax@users.noreply.github.com>
2018-02-07 23:33:30 +0100

    Implement cards from Sands of Dorne 2nd preview article (#1726)
    * Implement Obara Sand (SoD)
    
    * Implement AreoHotah (SoD)
    
    * Implement Shadow City Bastard
    
    * Implement Hotah's Axe

---
+++ server/game/cards/10-SoD/ShadowCityBastard.js
@@ -0,0 +1,32 @@
+const DrawCard = require('../../drawcard.js');
+
+class ShadowCityBastard extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Remove icons from character',
+            phase: 'challenge',
+            cost: ability.costs.sacrificeSelf(),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' &&
+                                       card.getPrintedCost() <= 5
+            },
+            handler: context => {
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: [
+                        ability.effects.removeIcon('military'),
+                        ability.effects.removeIcon('intrigue'),
+                        ability.effects.removeIcon('power')
+                    ]
+                }));
+
+                this.game.addMessage('{0} sacrifices {1} to remove a {2}, an {3}, and a {4} icon from {5} until the end of the phase',
+                    context.player, this, 'military', 'intrigue', 'power', context.target);
+            }
+        });
+    }
+}
+
+ShadowCityBastard.code = '10016';
+
+module.exports = ShadowCityBastard;
