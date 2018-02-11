f077cdf4cd4ff0d6a8eb515af22993dc580a528b
DukeTax <yorickvogels@gmail.com>
2018-02-11 03:21:44 +0100

    Implement Wise Master

---
+++ server/game/cards/08.4-FotOG/WiseMaster.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class WiseMaster extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Return attachment/event to hand',
+            limit: ability.limit.perRound(1),
+            cost: ability.costs.discardGold(),
+            target: {
+                type: 'select',
+                activePromptTitle: 'Select an attachment or event',
+                cardCondition: card => card.location === 'discard pile' && card.controller === this.controller &&
+                                       ['attachment', 'event'].includes(card.getType()) && !card.isFaction(this.controller.getFaction())
+            },
+            handler: context => {
+                context.target.owner.returnCardToHand(context.target);
+                this.game.addMessage('{0} discards 1 gold from {1} to return {2} to their hand from their discard pile',
+                    context.player, this, context.target);
+            }
+        });
+    }
+}
+
+WiseMaster.code = '08073';
+
+module.exports = WiseMaster;
