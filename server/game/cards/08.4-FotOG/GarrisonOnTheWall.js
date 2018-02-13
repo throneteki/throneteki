8cd877ba1832ec251cf0066a54afb5dd22dee652
DukeTax <yorickvogels@gmail.com>
2018-02-13 20:09:57 +0100

    Implement Garrison on the Wall

---
+++ server/game/cards/08.4-FotOG/GarrisonOnTheWall.js
@@ -0,0 +1,20 @@
+const DrawCard = require('../../drawcard.js');
+
+class GarrisonOnTheWall extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onDeclaredAsDefender: event => event.card === this
+            },
+            cost: ability.costs.kneel(card => !card.isFaction('thenightswatch') && card.getType() === 'character'),
+            handler: context => {
+                context.player.standCard(this);
+                this.game.addMessage('{0} kneels {1} to stand {2}', context.player, context.costs.kneel, this);
+            }
+        });
+    }
+}
+
+GarrisonOnTheWall.code = '08065';
+
+module.exports = GarrisonOnTheWall;
