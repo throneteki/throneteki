39505ab7ab89f4fcf7cbf2d25adf67f6d2c3de41
DukeTax <yorickvogels@gmail.com>
2018-02-13 20:10:44 +0100

    Implement Coastal Reaver

---
+++ server/game/cards/08.4-FotOG/CoastalReaver.js
@@ -0,0 +1,41 @@
+const DrawCard = require('../../drawcard.js');
+
+class CoastalReaver extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card === this
+            },
+            chooseOpponent: opponent => opponent.hand.size() > 0,
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to choose {2}', context.player, this, context.opponent);
+
+                this.game.promptForSelect(context.opponent, {
+                    acticePromptTitle: 'Select a card',
+                    source: this,
+                    cardCondition: card => card.location === 'hand' && card.controller === context.opponent,
+                    onSelect: (player, card) => this.onCardSelected(player, card),
+                    onCancel: (player) => this.cancelSelection(player)
+                });
+            }
+        });
+    }
+
+    onCardSelected(player, card) {
+        card.owner.moveCardToTopOfDeck(card);
+        this.game.addMessage('{0} moves {1} to the top of their deck for {2}', player, card, this);
+
+        return true;
+    }
+
+    cancelSelection(player) {
+        this.game.addAlert('danger', '{0} cancels the resolution of {1} even though they had card(s) in their hand',
+            player, this);
+
+        return true;
+    }
+}
+
+CoastalReaver.code = '08071';
+
+module.exports = CoastalReaver;
