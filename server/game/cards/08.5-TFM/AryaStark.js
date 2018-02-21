88db647d5394e1a933c3786ad57f774488a7c8c1
Stefano Zacchiroli <zack@upsilon.cc>
2018-02-21 14:25:34 +0100

    Implement Arya Stark (TFM) (#1757)

---
+++ server/game/cards/08.5-TFM/AryaStark.js
@@ -0,0 +1,16 @@
+const DrawCard = require('../../drawcard.js');
+
+class AryaStark extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => !this.kneeled,
+            targetController: 'any',
+            match: card => card.getType() === 'character',
+            effect: ability.effects.cannotBeSaved()
+        });
+    }
+}
+
+AryaStark.code = '08081';
+
+module.exports = AryaStark;
