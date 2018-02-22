9b2a1dd702637ca1daf33c087e53cf0442bffd7c
hanneshelge <36401591+hanneshelge@users.noreply.github.com>
2018-02-22 11:34:38 +0100

    Partly implementation of The Iron Bank
    This is a partly implementation of  'The Iron Bank' (The Archmasters Key).
    
    What this should do: Calculate the interest for the Iron bank.
    What this does not do: Provide ways to use the gold

---
+++ server/game/cards/08.1-TAK/TheIronBank.js
@@ -0,0 +1,20 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheIronBank extends DrawCard {
+   setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onIncomeCollected: event => event.player === this.controller
+			      },
+            handler: () => {
+				        var interest = this.tokens['gold'];
+                this.modifyToken('gold', interest);
+                this.game.addMessage('{0} uses {1} to place {2} gold from the treasury on {1}', this.controller, this, interest);
+            }
+        });
+    }
+}
+
+TheIronBank.code = '08019'; 
+
+module.exports = TheIronBank;
