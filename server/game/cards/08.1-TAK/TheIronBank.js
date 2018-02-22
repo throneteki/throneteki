7a2164ded012c01c702d04defb708bdfc011a3cb
hanneshelge <36401591+hanneshelge@users.noreply.github.com>
2018-02-22 12:50:04 +0100

    Update TheIronBank.js
    changed tabs to spaces

--- server/game/cards/08.1-TAK/TheIronBank.js
+++ server/game/cards/08.1-TAK/TheIronBank.js
@@ -1,18 +1,18 @@
 const DrawCard = require('../../drawcard.js');
 
 class TheIronBank extends DrawCard {
-	setupCardAbilities(ability) {
-		this.reaction({
-			when: {
-				onIncomeCollected: event => event.player === this.controller
-			},
-			handler: () => {
-				var interest = this.tokens['gold'];
-				this.modifyToken('gold', interest);
-				this.game.addMessage('{0} uses {1} to place {2} gold from the treasury on {1}', this.controller, this, interest);
-			}
-		});
-	}
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onIncomeCollected: event => event.player === this.controller
+            },
+            handler: () => {
+                var interest = this.tokens['gold'];
+                this.modifyToken('gold', interest);
+                this.game.addMessage('{0} uses {1} to place {2} gold from the treasury on {1}', this.controller, this, interest);
+            }
+        });
+    }
 }
 
 TheIronBank.code = '08019'; 
