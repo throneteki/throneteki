19b897fd0220ff89cc376dfda0ac8abee2e5f659
hanneshelge <36401591+hanneshelge@users.noreply.github.com>
2018-02-22 05:40:49 +0100

    Implement gold refill for Sweet Cersei (#1753)

---
+++ SweetCersei.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard.js');
+
+class SweetCersei extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && event.challenge.challengeType === 'intrigue'
+            },
+            handler: () => {
+                this.modifyToken('gold', 1);
+                this.game.addMessage('{0} uses {1} to place 1 gold from the treasury on {1}', this.controller, this);
+            }
+        });
+    }
+}
+
+SweetCersei.code = '08070'; 
+
+module.exports = SweetCersei;
