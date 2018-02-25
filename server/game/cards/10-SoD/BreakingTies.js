030ab5557b2ad8dfb12e8a14bb34c19929320033
DukeTax <DukeTax@users.noreply.github.com>
2018-02-25 04:19:54 +0100

    Implement Breaking Ties (#1767)

---
+++ server/game/cards/10-SoD/BreakingTies.js
@@ -0,0 +1,26 @@
+const PlotCard = require('../../plotcard.js');
+
+class BreakingTies extends PlotCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Return character/location to hand',
+            limit: ability.limit.perRound(2),
+            cost: ability.costs.sacrifice(card => card.isLoyal() && card.getType() === 'character'),
+            target: {
+                activePromptTitle: 'Select character or location',
+                cardCondition: card => card.location === 'play area' && !card.isLoyal() &&
+                                       ['character', 'location'].includes(card.getType()),
+                gameAction: 'returnToHand'
+            },
+            handler: context => {
+                context.target.owner.returnCardToHand(context.target);
+                this.game.addMessage('{0} uses {1} and sacrifices {2} to return {3} to {4}\'s hand',
+                    context.player, this, context.costs.sacrifice, context.target, context.target.owner);
+            }
+        });
+    }
+}
+
+BreakingTies.code = '10050';
+
+module.exports = BreakingTies;
