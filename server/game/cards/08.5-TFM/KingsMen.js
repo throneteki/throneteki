b0dbc7790b2a2adaa7737c61ccfb98026597bed9
Stefano Zacchiroli <zack@upsilon.cc>
2018-02-26 01:07:28 +0100

    implement King's Men (TFM) (#1763)

---
+++ server/game/cards/08.5-TFM/KingsMen.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard.js');
+
+class KingsMen extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onPhaseStarted: () => true
+            },
+            limit: ability.limit.perRound(1),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.kneeled
+            },
+            handler: context => {
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.blank
+                }));
+
+                this.game.addMessage('{0} uses {1} to treat the text box of {2} as blank until the end of the phase',
+                    this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+KingsMen.code = '08087';
+
+module.exports = KingsMen;
