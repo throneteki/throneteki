a344e61055f7daed3e9dbd050915f42cdb2c083c
Stefano Zacchiroli <zack@upsilon.cc>
2018-03-03 17:56:06 +0100

    implement "The Song of the Seven" (TFM)

---
+++ server/game/cards/08.5-TFM/TheSongOfTheSeven.js
@@ -0,0 +1,34 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheSongOfTheSeven extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            max: ability.limit.perRound(1),
+            when: {
+                onDominanceDetermined: event =>
+                    event.winner &&
+                    this.controller !== event.winner &&
+                    event.winner.faction.power >= 1
+            },
+            target: {
+                cardCondition: card =>
+                    card.location === 'play area' &&
+                    card.controller === this.controller &&
+                    card.getType() === 'character' &&
+                    card.hasTrait('The Seven')
+            },
+            handler: context => {
+                let otherPlayer = context.event.winner;
+                let power = Math.min(otherPlayer.faction.power, 2);
+                this.game.movePower(otherPlayer.faction, context.target, power);
+
+                this.game.addMessage('{0} plays {1} to move {2} power from {3}\'s faction to {4}',
+                    this.controller, this, power, otherPlayer, context.target);
+            }
+        });
+    }
+}
+
+TheSongOfTheSeven.code = '08098';
+
+module.exports = TheSongOfTheSeven;
