1aa20a7d668a720fa056bba5e9efb2b783902b08
Stefano Zacchiroli <zack@upsilon.cc>
2018-03-03 17:16:01 +0100

    implement The High Sparrow (TFM)

---
+++ server/game/cards/08.5-TFM/TheHighSparrow.js
@@ -0,0 +1,18 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheHighSparrow extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            targetType: 'player',
+            targetController: 'any',
+            effect: [
+                ability.effects.setMaxGoldGain(7),
+                ability.effects.setMaxCardDraw(3)
+            ]
+        });
+    }
+}
+
+TheHighSparrow.code = '08097';
+
+module.exports = TheHighSparrow;
