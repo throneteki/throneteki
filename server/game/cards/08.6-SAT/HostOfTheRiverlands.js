ec65f4ce48724633b80371c8e25178e7f487d3b3
Stefano Zacchiroli <zack@upsilon.cc>
2018-03-04 19:19:46 +0100

    implement Stark cards from Someone Always Tells chapter pack (#1754)
    * implement Godswood (SAT)
    
    * implement Host of the Riverlands (SAT)

---
+++ server/game/cards/08.6-SAT/HostOfTheRiverlands.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard.js');
+
+class HostOfTheRiverlands extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardPowerGained: event =>
+                    event.card.getType() === 'character'
+                    && event.card.controller === this.controller
+                    && event.card.hasTrait('House Tully')
+            },
+            limit: ability.limit.perPhase(1),
+            cost: ability.costs.discardGold(),
+            handler: context => {
+                this.game.addPower(context.event.card, 1);
+                this.game.addMessage('{0} discards a gold from {1} to have {2} gain 1 power',
+                    this.controller, this, context.event.card);
+            }
+        });
+    }
+}
+
+HostOfTheRiverlands.code = '08101';
+
+module.exports = HostOfTheRiverlands;
