a2d310fae6b33ce40697a77d574e4c6e5a6fb904
DukeTax <DukeTax@users.noreply.github.com>
2018-03-04 19:17:45 +0100

    Implement Lannister cards from Sands of Dorne (#1795)
    * Implement Myrcella Baratheon (SoD)
    
    * Implement Extortion

---
+++ server/game/cards/10-SoD/Extortion.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class Extortion extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.attackingPlayer === this.controller && event.challenge.winner === this.controller &&
+                                         event.challenge.challengeType === 'intrigue'
+            },
+            handler: context => {
+                this.game.addGold(context.player, 3);
+
+                let loser = context.event.challenge.loser;
+                let returnGold = Math.min(loser.gold, 3);
+                this.game.addGold(loser, -returnGold);
+
+                this.game.addMessage('{0} plays {1} to gain 3 gold and return {2} gold from {3}\'s gold pool to the treasury',
+                    context.player, this, returnGold, loser);
+            }
+        });
+    }
+}
+
+Extortion.code = '10030';
+
+module.exports = Extortion;
