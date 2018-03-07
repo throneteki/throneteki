c54b99f8c5b969ae2e1a851534b6d741cdb0bbf6
DukeTax <yorickvogels@gmail.com>
2018-03-07 17:59:26 +0100

    Implement Beacon of the South

---
+++ server/game/cards/10-SoD/BeaconOfTheSouth.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard.js');
+
+class BeaconOfTheSouth extends DrawCard {
+    setupCardAbilities() {
+        this.attachmentRestriction({ faction: 'tyrell' });
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller &&
+                                         event.challenge.strengthDifference >= 5 &&
+                                         event.challenge.isAttacking(this.parent)
+            },
+            handler: context => {
+                this.parent.modifyPower(1);
+                this.game.addMessage('{0} uses {1} to gain 1 power on {2}', context.player, this, this.parent);
+            }
+        });
+    }
+}
+
+BeaconOfTheSouth.code = '10038';
+
+module.exports = BeaconOfTheSouth;
