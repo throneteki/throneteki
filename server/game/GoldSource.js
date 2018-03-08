7594c24c5dea7ce1b7dc0ed13e9dc09e85fea754
Brian Upton <ystros@gmail.com>
2018-03-07 23:00:56 -0800

    Introduce additional gold sources
    Adds an effect that allows gold to be spent from a source other than the
    player's gold pool. When the player goes to spend money, they will be
    prompted how much from the separate sources they want to spend. If the
    amount of gold they are spending can only be covered by all gold from
    all sources, or if only one source has enough gold to cover it, then
    they will not be prompted and the gold will be automatically spent.

---
+++ server/game/GoldSource.js
@@ -0,0 +1,28 @@
+class GoldSource {
+    constructor(playerOrCard, allowSpendingFunc = () => true) {
+        this.playerOrCard = playerOrCard;
+        this.allowSpendingFunc = allowSpendingFunc;
+    }
+
+    get gold() {
+        return this.playerOrCard.gold;
+    }
+
+    get name() {
+        if(this.playerOrCard.getGameElementType() === 'player') {
+            return `${this.playerOrCard.name}'s gold pool`;
+        }
+
+        return this.playerOrCard.name;
+    }
+
+    allowSpendingFor(spendParams) {
+        return this.allowSpendingFunc(spendParams);
+    }
+
+    modifyGold(amount) {
+        this.playerOrCard.modifyGold(amount);
+    }
+}
+
+module.exports = GoldSource;
---
+++ server/game/gamesteps/ChooseGoldSourceAmounts.js
@@ -0,0 +1,60 @@
+const _ = require('underscore');
+
+const BaseStep = require('./basestep');
+
+class ChooseGoldSourceAmounts extends BaseStep {
+    constructor(game, spendParams, callback) {
+        super(game);
+
+        this.remainingAmount = spendParams.amount;
+        this.player = spendParams.player;
+        this.sources = this.player.getSpendableGoldSources(spendParams);
+        this.spendParams = spendParams;
+        this.callback = callback;
+    }
+
+    continue() {
+        while(this.sources.length > 0) {
+            if(this.remainingAmount === 0) {
+                return;
+            }
+
+            this.currentSource = this.sources.shift();
+            let currentAvailable = this.currentSource.gold;
+            let maxAmount = Math.min(this.remainingAmount, currentAvailable);
+            let minAmount = Math.max(0, this.remainingAmount - this.getMaxRemainingAvailable());
+
+            if(this.sources.length > 0 && minAmount !== maxAmount) {
+                let buttons = _.range(minAmount, maxAmount + 1).reverse().map(amount => {
+                    return { text: amount.toString(), method: 'payGold', arg: amount };
+                });
+                this.game.promptWithMenu(this.player, this, {
+                    activePrompt: {
+                        menuTitle: `Select amount from ${this.currentSource.name}`,
+                        buttons: buttons
+                    }
+                });
+                return false;
+            }
+
+            this.payGold(this.player, maxAmount);
+        }
+    }
+
+    getMaxRemainingAvailable() {
+        return this.sources.reduce((sum, source) => sum + source.gold, 0);
+    }
+
+    payGold(player, amount) {
+        this.remainingAmount -= amount;
+        this.currentSource.modifyGold(-amount);
+
+        if(this.remainingAmount === 0) {
+            this.callback();
+        }
+
+        return true;
+    }
+}
+
+module.exports = ChooseGoldSourceAmounts;
