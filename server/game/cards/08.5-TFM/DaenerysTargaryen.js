eec9f35bc43059987c514f24a4bdbed91aba009a
Stefano Zacchiroli <zack@upsilon.cc>
2018-03-11 13:59:37 +0100

    implement strength reduction immunity for Daenerys Targaryen (TFM)
    to that end introduce immunities cannotReduceStrength and
    cannotIncreaseStrength and make strength effects enforce them

--- server/game/cards/08.5-TFM/DaenerysTargaryen.js
+++ server/game/cards/08.5-TFM/DaenerysTargaryen.js
@@ -1,22 +1,11 @@
-// french spoiler image:
-// https://i.redditmedia.com/SxCGctsBb71h87NWgeUy8uFPE0JKbC_U2rPjGr-N2vg.jpg?w=500&s=e8cdc218e218f28254767e63c69aba20
-//
-// TODO: implement immunity to strength reduction
-//
-// Effects that should check new immunity:
-//
-// - modifyStrength
-// - modifyStrengthMultiplier
-//
-// Effects that should be OK as is:
-//
-// - killByStrength (use ModifyStrength)
-// - dynamicStrength (use modifyStrength)
-
 const DrawCard = require('../../drawcard.js');
 
 class DaenerysTargaryen extends DrawCard {
     setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            effect: ability.effects.cannotDecreaseStrength(context => context.resolutionStage === 'effect')
+        });
         this.reaction({
             when: {
                 onCardPlayed: event => event.card.controller === this.controller && event.card.isFaction('targaryen')
