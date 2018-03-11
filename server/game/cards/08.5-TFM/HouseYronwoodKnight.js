9c6dab67d845c7180942159ad3ad6e5c5d404804
Stefano Zacchiroli <zack@upsilon.cc>
2018-03-11 09:47:19 +0100

    implement House Yronwood Knight (TFM)

---
+++ server/game/cards/08.5-TFM/HouseYronwoodKnight.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard.js');
+
+class HouseYronwoodKnight extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.loser === this.controller && event.challenge.isParticipating(this)
+            },
+            handler: () => {
+                let winner = this.game.currentChallenge.winner;
+                winner.discardAtRandom(1);
+
+                this.game.addMessage('{0} uses {1} to discard 1 card at random from {2}\'s hand',
+                    this.controller, this, winner);
+            }
+        });
+    }
+}
+
+HouseYronwoodKnight.code = '08095';
+
+module.exports = HouseYronwoodKnight;
