df1955a456d6107e6bc9d829afcd03759b1178e7
Brian Upton <ystros@gmail.com>
2018-03-13 14:55:32 -0700

    Remove unused card field 'faction_name'

--- server/scripts/fetchdata/JsonCardSource.js
+++ server/scripts/fetchdata/JsonCardSource.js
@@ -7,7 +7,6 @@ class JsonCardSource {
         let files = fs.readdirSync('thronesdb-json-data/pack');
         let totalCards = [];
         let types = JSON.parse(fs.readFileSync('thronesdb-json-data/types.json'));
-        let factions = JSON.parse(fs.readFileSync('thronesdb-json-data/factions.json'));
 
         _.each(files, file => {
             let cards = JSON.parse(fs.readFileSync('thronesdb-json-data/pack/' + file));
@@ -26,19 +25,9 @@ class JsonCardSource {
                 card.label = card.name;
             }
 
-            let faction = _.find(factions, faction => {
-                return faction.code === card.faction_code;
-            });
-
             let type = _.find(types, type => {
                 return type.code === card.type_code;
             });
-
-            if(faction) {
-                card.faction_name = faction.name;
-            } else {
-                console.info(faction, card.faction_code);
-            }
 
             if(type) {
                 card.type_name = type.name;
