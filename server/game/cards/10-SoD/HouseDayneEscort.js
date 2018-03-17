c7417e155ef529747afb166ff6c5b045cd1ddf25
DukeTax <yorickvogels@gmail.com>
2018-03-17 23:46:48 +0100

    Implement House Dayne Escort

---
+++ server/game/cards/10-SoD/HouseDayneEscort.js
@@ -0,0 +1,17 @@
+const DrawCard = require('../../drawcard.js');
+
+class HouseDayneEscort extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            location: 'any',
+            condition: () => this.location === 'discard pile' && this.controller.getNumberOfUsedPlots() >= 3,
+            targetType: 'player',
+            targetController: 'current',
+            effect: ability.effects.canMarshal(card => card === this && card.location === 'discard pile')
+        });
+    }
+}
+
+HouseDayneEscort.code = '10015';
+
+module.exports = HouseDayneEscort;
