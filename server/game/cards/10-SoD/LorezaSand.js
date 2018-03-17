8845aeb318499683114a6f9d49f9d001b4b26de7
DukeTax <yorickvogels@gmail.com>
2018-03-17 23:43:39 +0100

    Implement Loreza Sand

---
+++ server/game/cards/10-SoD/LorezaSand.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+
+class LorezaSand extends DrawCard {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            when: {
+                onCharacterKilled: event => event.card.isUnique() && event.card.isFaction('martell') &&
+                                            event.card.controller === this.controller && this.controller.canDraw()
+            },
+            limit: ability.limit.perPhase(1),
+            handler: context => {
+                context.player.drawCardsToHand(1);
+                this.game.addMessage('{0} uses {1} to draw 1 card', context.player, this);
+            }
+        });
+    }
+}
+
+LorezaSand.code = '10013';
+
+module.exports = LorezaSand;
