3e149bd216d62192233585cc909e93a7b49b6b02
DukeTax <yorickvogels@gmail.com>
2018-03-17 23:42:55 +0100

    Implement Quentyn Martell (SoD)

---
+++ server/game/cards/10-SoD/QuentynMartell.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard.js');
+
+class QuentynMartell extends DrawCard {
+    setupCardAbilities() {
+        this.forcedReaction({
+            when: {
+                onPhaseStarted: event => event.phase === 'challenge' && this.controller.getNumberOfUsedPlots() < 3
+            },
+            handler: context => {
+                context.player.kneelCard(this);
+                this.game.addMessage('{0} is forced to kneel {1}', context.player, this);
+            }
+        });
+    }
+}
+
+QuentynMartell.code = '10011';
+
+module.exports = QuentynMartell;
