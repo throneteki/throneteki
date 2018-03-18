ade3934f05b5e604a8bf5059ccd42ff1bf095e0c
DukeTax <yorickvogels@gmail.com>
2018-03-18 17:55:56 +0100

    Implement Ser Cortnay Penrose

---
+++ server/game/cards/10-SoD/SerCortnayPenrose.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+
+class SerCortnayPenrose extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.challengeType === 'power' &&
+                                         event.challenge.winner === this.controller &&
+                                         event.challenge.isParticipating(this)
+            },
+            handler: context => {
+                context.player.standCard(this);
+                this.game.addMessage('{0} stands {1}', context.player, this);
+            }
+        });
+    }
+}
+
+SerCortnayPenrose.code = '10025';
+
+module.exports = SerCortnayPenrose;
