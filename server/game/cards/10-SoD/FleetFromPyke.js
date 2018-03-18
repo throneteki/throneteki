1f14ab79a02497a080d6120757f665c5bd4ef7ea
DukeTax <yorickvogels@gmail.com>
2018-03-18 17:57:36 +0100

    Implement Fleet From Pyke

---
+++ server/game/cards/10-SoD/FleetFromPyke.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+
+class FleetFromPyke extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onPillage: event => event.source.controller === this.controller &&
+                                    ['location', 'attachment'].includes(event.discardedCard.getType())
+            },
+            handler: context => {
+                this.game.promptForIcon(context.player, this, icon => {
+                    this.untilEndOfPhase(ability => ({
+                        match: this,
+                        effect: ability.effects.addIcon(icon)
+                    }));
+
+                    this.game.addMessage('{0} gains {1} {2} icon on {3}',
+                        context.player, icon === 'intrigue' ? 'an' : 'a', icon, this);
+                });
+            }
+        });
+    }
+}
+
+FleetFromPyke.code = '10027';
+
+module.exports = FleetFromPyke;
