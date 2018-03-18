64e5cee51e7318cd50acd812e09db6e1af322e8a
DukeTax <yorickvogels@gmail.com>
2018-03-18 18:21:49 +0100

    Implement Plundered

---
+++ server/game/cards/10-SoD/Plundered.js
@@ -0,0 +1,48 @@
+const DrawCard = require('../../drawcard.js');
+
+class Plundered extends DrawCard {
+    setupCardAbilities() {
+        this.attachmentRestriction(card => card.getType() === 'location' && card.controller !== this.controller);
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.loser === this.parent.controller &&
+                                         event.challenge.defendingPlayer === this.parent.controller &&
+                                         !this.parent.kneeled
+            },
+            handler: context => {
+                this.parent.controller.kneelCard(this.parent);
+                this.game.addMessage('{0} uses {1} to kneel {2}', context.player, this, this.parent);
+
+                if(!context.player.canGainGold()) {
+                    return true;
+                }
+
+                this.game.promptWithMenu(context.player, this, {
+                    activePrompt: {
+                        menuTitle: 'Sacrifice Plundered to gain 3 gold?',
+                        buttons: [
+                            { text: 'Yes', method: 'sacrifice' },
+                            { text: 'No', method: 'decline' }
+                        ]
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    sacrifice() {
+        this.controller.sacrificeCard(this);
+        let gold = this.game.addGold(this.controller, 3);
+        this.game.addMessage('{0} then sacrifices {1} to gain {2} gold', this.controller, this, gold);
+        return true;
+    }
+
+    decline() {
+        return true;
+    }
+}
+
+Plundered.code = '10028';
+
+module.exports = Plundered;
