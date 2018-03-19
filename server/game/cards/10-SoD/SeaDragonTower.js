351fce0a2643434ed1ff6d8139663fe79c92185a
DukeTax <DukeTax@users.noreply.github.com>
2018-03-19 19:21:01 +0100

    Implement Baratheon & Greyjoy cards from Sands of Dorne (#1831)
    * Implement Ser Cortnay Penrose
    
    * Implement Sea Dragon Tower
    
    * Implement Fleet From Pyke
    
    * Implement Plundered

---
+++ server/game/cards/10-SoD/SeaDragonTower.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+
+class SeaDragonTower extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Give power icon',
+            phase: 'challenge',
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character'
+            },
+            handler: context => {
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.addIcon('power')
+                }));
+
+                this.game.addMessage('{0} kneels {1} to give a {2} icon to {3}',
+                    context.player, this, 'power', context.target);
+            }
+        });
+    }
+}
+
+SeaDragonTower.code = '10026';
+
+module.exports = SeaDragonTower;
