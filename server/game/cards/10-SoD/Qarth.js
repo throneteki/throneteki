b6a48bbd6d0251a316315f041f4b1ff1ade1a29f
DukeTax <DukeTax@users.noreply.github.com>
2018-03-19 02:28:41 +0100

    Implement Stark cards from Sands of Dorne & Qarth (#1833)
    * Implement Greatjon Umber
    
    * Implement Taste For Flesh
    
    * Implement Qarth

---
+++ server/game/cards/10-SoD/Qarth.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard.js');
+
+class Qarth extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Discard attachment',
+            cost: [
+                ability.costs.kneelSelf(),
+                ability.costs.payGold(1)
+            ],
+            target: {
+                activePromptTitle: 'Select an attachment',
+                cardCondition: card => card.location === 'play area' && card.getType() === 'attachment' &&
+                                       card.parent && card.parent.controller === this.controller,
+                gameAction: 'discard'
+            },
+            handler: context => {
+                context.target.owner.discardCard(context.target);
+                this.game.addMessage('{0} kneels {1} and pays 1 gold to discard {2} from play',
+                    context.player, this, context.target);
+            }
+        });
+    }
+}
+
+Qarth.code = '10036';
+
+module.exports = Qarth;
---
+++ server/game/cards/10-SoD/TasteForFlesh.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../drawcard.js');
+
+class TasteForFlesh extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller &&
+                                         this.isAttackingDirewolfOrHasAttachment(event.challenge)
+            },
+            target: {
+                cardCondition: card => this.game.currentChallenge.isDefending(card),
+                gameAction: 'kill'
+            },
+            handler: context => {
+                this.game.killCharacter(context.target);
+                this.game.addMessage('{0} plays {1} to kill {2}', context.player, this, context.target);
+            }
+        });
+    }
+
+    isAttackingDirewolfOrHasAttachment(challenge) {
+        return this.controller.anyCardsInPlay(card => challenge.isAttacking(card) && 
+        (card.hasTrait('Direwolf') || card.attachments.any(attachment => attachment.hasTrait('Direwolf'))));
+    }
+}
+
+TasteForFlesh.code = '10034';
+
+module.exports = TasteForFlesh;
