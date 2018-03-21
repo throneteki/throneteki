56806adf3ed4d7698078e14b386aa3cec7391035
Brian Upton <ystros@gmail.com>
2018-03-21 05:09:12 -0700

    Extract components for Decks page (#1837)
    * Move confirmed button into component
    
    * Move selected deck to component
    
    * Extract deck list into a component
    
    * Remove unused state from Decks
    
    * Use same level of abstraction within components
    
    Previously Decks was handling click handler events set up by ViewDeck.
    Now ViewDeck publishes events at an abstraction level of editing /
    deleting a deck rather than clicking edit / delete buttons and handles
    the button logic internally.
    
    * Check optional props / mark others as required
    
    * Coerce inline render condition to boolean

--- client/DeckRow.jsx
+++ client/DeckRow.jsx
@@ -3,9 +3,21 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 
 class DeckRow extends React.Component {
+    constructor() {
+        super();
+
+        this.handleDeckClick = this.handleDeckClick.bind(this);
+    }
+
+    handleDeckClick() {
+        if(this.props.onSelect) {
+            this.props.onSelect(this.props.deck);
+        }
+    }
+
     render() {
         return (
-            <div className={ this.props.active ? 'deck-row active' : 'deck-row' } key={ this.props.deck.name } onClick={ this.props.onClick }>
+            <div className={ this.props.active ? 'deck-row active' : 'deck-row' } key={ this.props.deck.name } onClick={ this.handleDeckClick }>
                 <div className='col-xs-1 deck-image'><img className='card-small' src={ '/img/cards/' + this.props.deck.faction.value + '.png' } /></div>
                 <span className='col-xs-8 col-md-7 col-lg-9 deck-name'>{ this.props.deck.name }</span><span className='col-xs-2 col-md-3 col-lg-2 deck-status-label text-right pull-right'>{ this.props.deck.validation.status }</span>
                 <div className='row small'>
@@ -19,8 +31,8 @@ class DeckRow extends React.Component {
 DeckRow.displayName = 'DeckRow';
 DeckRow.propTypes = {
     active: PropTypes.bool,
-    deck: PropTypes.object,
-    onClick: PropTypes.func
+    deck: PropTypes.object.isRequired,
+    onSelect: PropTypes.func
 };
 
 export default DeckRow;
