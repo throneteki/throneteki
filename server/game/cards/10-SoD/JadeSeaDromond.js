bc46c2ea1ee07b6368761ed7f514a48fb20da9b8
DukeTax <yorickvogels@gmail.com>
2018-03-22 19:55:58 +0100

    Implement Jade Sea Dromond

---
+++ server/game/cards/10-SoD/JadeSeaDromond.js
@@ -0,0 +1,33 @@
+const DrawCard = require('../../drawcard.js');
+
+class JadeSeaDromond extends DrawCard {
+    setupCardAbilities(ability) {
+        this.plotModifiers({
+            gold: 1
+        });
+
+        this.action({
+            title: 'Discard location',
+            phase: 'dominance',
+            cost: [
+                ability.costs.kneelSelf(),
+                ability.costs.sacrificeSelf()
+            ],
+            target: {
+                activePromptTitle: 'Select a location',
+                cardCondition: card => card.location === 'play area' && !card.isLimited() &&
+                                       card.getType() === 'location' && card.getPrintedCost() <= 3,
+                gameAction: 'discard'
+            },
+            handler: context => {
+                context.target.controller.discardCard(context.target);
+                this.game.addMessage('{0} kneels and sacrifices {1} to discard {2} from play',
+                    context.player, this, context.target);
+            }
+        });
+    }
+}
+
+JadeSeaDromond.code = '10042';
+
+module.exports = JadeSeaDromond;
