632c48b86f7865df92c46bdaa101ba33ec19257b
DukeTax <yorickvogels@gmail.com>
2018-03-22 14:31:25 +0100

    Fix Peace And Prosperity & A Noble Cause

--- server/game/cards/10-SoD/PeaceAndProsperity.js
+++ server/game/cards/10-SoD/PeaceAndProsperity.js
@@ -6,9 +6,9 @@ class PeaceAndProsperity extends PlotCard {
             targetType: 'player',
             targetController: 'current',
             effect: [
-                ability.effects.reduceNextMarshalledCardCost(3, card => card.getType() === 'character'),
-                ability.effects.reduceNextMarshalledCardCost(2, card => card.getType() === 'location'),
-                ability.effects.reduceNextMarshalledCardCost(1, card => card.getType() === 'attachment')
+                ability.effects.reduceFirstMarshalledCardCostEachRound(3, card => card.getType() === 'character'),
+                ability.effects.reduceFirstMarshalledCardCostEachRound(2, card => card.getType() === 'location'),
+                ability.effects.reduceFirstMarshalledCardCostEachRound(1, card => card.getType() === 'attachment')
             ]
         });
     }
