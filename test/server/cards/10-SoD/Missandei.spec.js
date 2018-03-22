573ba5b03e77826d55e52547704e3b9f58d92d9c
Brian Upton <ystros@gmail.com>
2018-03-22 15:31:05 -0700

    Implement Missandei

---
+++ test/server/cards/10-SoD/Missandei.spec.js
@@ -0,0 +1,65 @@
+describe('Missandei', function() {
+    integration(function() {
+        beforeEach(function() {
+            const deck1 = this.buildDeck('targaryen', [
+                'A Noble Cause',
+                'Missandei', 'Missandei', 'Missandei', 'Missandei', 'Missandei', 'Missandei',
+                'Missandei', 'Missandei', 'Missandei', 'Missandei', 'Missandei', 'Missandei'
+            ]);
+            const deck2 = this.buildDeck('lannister', [
+                'A Noble Cause',
+                'Ser Gregor\'s Marauders'
+            ]);
+
+            this.player1.selectDeck(deck1);
+            this.player2.selectDeck(deck2);
+            this.startGame();
+            this.keepStartingHands();
+
+            this.opponentPillager = this.player2.findCardByName('Ser Gregor\'s Marauders', 'hand');
+
+            this.player2.clickCard(this.opponentPillager);
+
+            this.completeSetup();
+
+            this.player1.selectPlot('A Noble Cause');
+            this.player2.selectPlot('A Noble Cause');
+            this.selectFirstPlayer(this.player2);
+
+            this.completeMarshalPhase();
+        });
+
+        describe('when Missandei is discarded from hand', function() {
+            beforeEach(function() {
+                this.unopposedChallenge(this.player2, 'Intrigue', this.opponentPillager);
+                this.player2.clickPrompt('Apply Claim');
+
+                this.player1.clickPrompt('Missandei');
+            });
+
+            it('should put her into play', function() {
+                let missandei = this.player1.findCardByName('Missandei', 'play area');
+                expect(!!missandei).toBe(true);
+            });
+        });
+
+        describe('when Missandei is discarded from deck', function() {
+            beforeEach(function() {
+                // this.player1.dragCard(this.missandei, 'draw deck');
+                this.unopposedChallenge(this.player2, 'Military', this.opponentPillager);
+                this.player2.clickPrompt('Apply Claim');
+
+                // No characters to kill
+                this.player1.clickPrompt('Done');
+
+                // Pillage occurs, which would discard Missandei from deck
+                this.player1.clickPrompt('Missandei');
+            });
+
+            it('should put her into play', function() {
+                let missandei = this.player1.findCardByName('Missandei', 'play area');
+                expect(!!missandei).toBe(true);
+            });
+        });
+    });
+});
