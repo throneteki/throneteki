ef380168137e1f99f5cc21e32ba7915a8d7d5362
DukeTax <yorickvogels@gmail.com>
2018-04-01 20:55:57 +0200

    Implement Someone Always Tells

---
+++ server/game/cards/08.6-SAT/SomeoneAlwaysTells.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard.js');
+
+class SomeoneAlwaysTells extends DrawCard {
+    setupCardAbilities() {
+        this.interrupt({
+            canCancel: true,
+            when: {
+                onCardAbilityInitiated: event => event.source.controller !== this.controller &&
+                                                 (event.source.getType() === 'event' || 
+                                                 (event.source.getType() === 'plot' && event.ability.isTriggeredAbility()))
+            },
+            handler: context => {
+                context.event.cancel();
+                this.game.addMessage('{0} plays {1} to cancel {2}', 
+                    context.player, this, context.event.source);
+            }
+        });
+    }
+}
+
+SomeoneAlwaysTells.code = '08116';
+
+module.exports = SomeoneAlwaysTells;
---
+++ test/server/cards/08.6-SAT/SomeoneAlwaysTells.spec.js
@@ -0,0 +1,56 @@
+describe('Someone Always Tells', function() {
+    integration(function() {
+        beforeEach(function() {
+            const deck = this.buildDeck('baratheon', [
+                'A Noble Cause', 'Trading with the Pentoshi', 'Valar Morghulis',
+                'Tumblestone Knight', 'Someone Always Tells'
+            ]);
+
+            this.player1.selectDeck(deck);
+            this.player2.selectDeck(deck);
+            this.startGame();
+            this.keepStartingHands();
+
+            this.knight = this.player1.findCardByName('Tumblestone Knight', 'hand');
+            this.player1.clickCard(this.knight);
+
+            this.completeSetup();
+
+            this.player1Object.gold = 2;
+
+            this.player1.selectPlot('A Noble Cause');
+        });
+
+        describe('when an opponent reveals a non-cancelable plot when revealed', function() {
+            beforeEach(function() {
+                this.player2.selectPlot('Trading with the Pentoshi');
+                this.selectFirstPlayer(this.player2);
+            });
+
+            it('should not prompt to trigger someone always tells', function() {
+                expect(this.player1).not.toHavePromptButton('Someone Always Tells');
+            });
+        });
+
+        describe('when an opponent reveals a cancelable plot when revealed', function() {
+            beforeEach(function() {
+                this.player2.selectPlot('Valar Morghulis');
+                this.selectFirstPlayer(this.player2);
+            });
+
+            it('should prompt to trigger someone always tells', function() {
+                expect(this.player1).toHavePromptButton('Someone Always Tells');
+            });
+
+            describe('and when someone always tells is used to cancel', function() {
+                beforeEach(function() {
+                    this.player1.clickPrompt('Someone Always Tells');
+                });
+    
+                it('should cancel the when revealed ability', function() {
+                    expect(this.knight.location).toBe('play area');
+                });
+            });
+        });
+    });
+});
