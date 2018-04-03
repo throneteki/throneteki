d45df29d0d642f2e821b29b6c1955474b01133ea
Brian Upton <ystros@gmail.com>
2018-04-03 04:41:08 -0700

    Do not reveal strength / icons for facedown cards (#1865)
    Returning information about a card's strength and the icons while it's
    facedown can make it guessable what card it may be. This is a potential
    problem during setup, for cards in hand, and cards in the upcoming
    Shadow zone.

--- test/server/card/drawcard.getsummary.spec.js
+++ test/server/card/drawcard.getsummary.spec.js
@@ -1,11 +1,17 @@
 const DrawCard = require('../../../server/game/drawcard.js');
 
 describe('DrawCard', function () {
+    function createPlayerSpy(name) {
+        let player = jasmine.createSpyObj('player', ['getCardSelectionState']);
+        player.name = name;
+        return player;
+    }
+
     beforeEach(function () {
         this.testCard = { code: '111', label: 'test 1(some pack)', name: 'test 1' };
         this.card = new DrawCard({}, this.testCard);
-        this.activePlayer = {};
-        this.activePlayer.name = 'bla';
+        this.activePlayer = createPlayerSpy('player1');
+        this.card.owner = this.activePlayer;
     });
 
     describe('getSummary', function() {
@@ -13,7 +19,7 @@ describe('DrawCard', function () {
             describe('when the card has non-zero strength', function() {
                 beforeEach(function() {
                     this.testCard.strength = 5;
-                    this.summary = this.card.getSummary(this.activePlayer, true);
+                    this.summary = this.card.getSummary(this.activePlayer, false);
                 });
 
                 it('should include the strength', function() {
@@ -24,12 +30,36 @@ describe('DrawCard', function () {
             describe('when the card has a zero strength', function() {
                 beforeEach(function() {
                     this.testCard.strength = 0;
-                    this.summary = this.card.getSummary(this.activePlayer, true);
+                    this.summary = this.card.getSummary(this.activePlayer, false);
                 });
 
                 it('should include the strength', function() {
                     expect(this.summary.strength).toBe(0);
                 });
+            });
+        });
+
+        describe('when a card is facedown', function() {
+            beforeEach(function() {
+                this.testCard.strength = 5;
+                let anotherPlayer = createPlayerSpy('player2');
+                this.summary = this.card.getSummary(anotherPlayer, true);
+            });
+
+            it('should not include baseStrength', function() {
+                expect(this.summary.baseStrength).toBeUndefined();
+            });
+
+            it('should not include iconsAdded', function() {
+                expect(this.summary.iconsAdded).toBeUndefined();
+            });
+
+            it('should not include iconsRemoved', function() {
+                expect(this.summary.iconsRemoved).toBeUndefined();
+            });
+
+            it('should not include strength', function() {
+                expect(this.summary.strength).toBeUndefined();
             });
         });
     });
