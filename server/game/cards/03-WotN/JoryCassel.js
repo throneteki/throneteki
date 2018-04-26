a535dbfcd5b300c349093e7a6e44deeecf861303
DukeTax <yorickvogels@gmail.com>
2018-04-27 00:37:00 +0200

    Convert Jory Cassel to costs API Also standardize message, fix gameAction and remove underscore usage.

--- server/game/cards/03-WotN/JoryCassel.js
+++ server/game/cards/03-WotN/JoryCassel.js
@@ -1,37 +1,34 @@
-const _ = require('underscore');
-
 const DrawCard = require('../../drawcard.js');
 
 class JoryCassel extends DrawCard {
-    setupCardAbilities() {
+    setupCardAbilities(ability) {
         this.interrupt({
             canCancel: true,
             when: {
                 onCharactersKilled: event => event.allowSave
             },
+            cost: ability.costs.sacrificeSelf(),
             target: {
                 cardCondition: (card, context) => (
+                    card.location === 'play area' &&
                     context.event.cards.includes(card) &&
                     card.canBeSaved() &&
                     card.controller === this.controller &&
                     card.isUnique() &&
                     card.isFaction('stark')
-                ),
-                gameAction: 'gainPower'
+                )
             },
             handler: context => {
-                let message = '{0} uses {1} to save {2}';
+                let message = '{0} sacrifices {1} to save {2}';
                 let toKill = context.target;
 
                 context.event.saveCard(toKill);
-                this.controller.sacrificeCard(this);
 
-                if(_.any(this.game.getPlayers(), player => {
+                if(this.game.getPlayers().some(player => {
                     return player.activePlot.hasTrait('Winter');
-                })) {
+                }) && toKill.canGainPower()) {
                     toKill.modifyPower(1);
-
-                    message += ' and make it gain 1 power';
+                    message += ' and have it gain 1 power';
                 }
 
                 this.game.addMessage(message, this.controller, this, toKill);
