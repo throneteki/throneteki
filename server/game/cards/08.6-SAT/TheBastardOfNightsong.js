8c0d1acdc37050f4b3ba73585b5833bff08096c6
Stefano Zacchiroli <zack@upsilon.cc>
2018-04-29 10:54:42 +0200

    implement The Bastard of Nightsong (SAT)

---
+++ server/game/cards/08.6-SAT/TheBastardOfNightsong.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheBastardOfNightsong extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => (
+                    event.challenge.winner === this.controller &&
+                    event.challenge.isParticipating(this) &&
+                    event.challenge.loser.faction.power > 0)
+            },
+            handler: context => {
+                let otherPlayer = context.event.challenge.loser;
+                this.game.movePower(otherPlayer.faction, this.controller.faction, 1);
+
+                this.game.addMessage('{0} uses {1} to move 1 power from {2}\'s faction to their own',
+                    this.controller, this, otherPlayer);
+            }
+        });
+    }
+}
+
+TheBastardOfNightsong.code = '08107';
+
+module.exports = TheBastardOfNightsong;
