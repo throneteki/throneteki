e1dd4fbbd9fccd35bdba1876fd2af3e2beb49280
Stefano Zacchiroli <zack@upsilon.cc>
2018-04-30 13:57:11 +0200

    implement Driftwood Cudgel (SAT)

---
+++ server/game/cards/08.6-SAT/DriftwoodCudgel.js
@@ -0,0 +1,34 @@
+const DrawCard = require('../../drawcard.js');
+
+class DriftwoodCudgel extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ trait: 'Drowned God' });
+
+        this.whileAttached({
+            effect: ability.effects.dynamicStrength(() => this.controller.deadPile.length)
+        });
+
+        this.interrupt({
+            when: {
+                onCharacterKilled: event => event.card === this.parent
+            },
+            target: {
+                type: 'select',
+                cardCondition: card =>
+                    card.location === 'play area' && card.getType() === 'character' &&
+                    card !== this.parent && this.controller.canAttach(this, card)
+            },
+            handler: context => {
+                this.controller.attach(this.controller, this, context.target);
+                context.target.modifyPower(1);
+
+                this.game.addMessage('{0} uses {1} to attach {1} to {2} and have it gain 1 power',
+                    this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+DriftwoodCudgel.code = '08112';
+
+module.exports = DriftwoodCudgel;
