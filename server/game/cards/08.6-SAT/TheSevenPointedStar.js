ac512ba9e3d6157b47e857f2978eedee17ae06cb
Brian Upton <ystros@gmail.com>
2018-05-05 13:00:31 -0700

    Implement The Seven-Pointed Star

---
+++ server/game/cards/08.6-SAT/TheSevenPointedStar.js
@@ -0,0 +1,30 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheSevenPointedStar extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.addTrait('The Seven')
+        });
+
+        this.action({
+            title: 'Reduce cost',
+            phase: 'marshal',
+            cost: ability.costs.kneelParent(),
+            handler: context => {
+                let currentController = context.player;
+                this.game.addMessage('{0} uses {1} and kneels {2} to reduce the cost of the next The Seven character by 2', currentController, this, context.costs.kneel);
+                this.untilEndOfPhase(ability => ({
+                    condition: () => !context.abilityDeactivated,
+                    targetType: 'player',
+                    targetController: 'current',
+                    match: player => player === currentController,
+                    effect: ability.effects.reduceNextMarshalledCardCost(2, card => card.getType() === 'character' && card.hasTrait('The Seven'))
+                }));
+            }
+        });
+    }
+}
+
+TheSevenPointedStar.code = '08119';
+
+module.exports = TheSevenPointedStar;
