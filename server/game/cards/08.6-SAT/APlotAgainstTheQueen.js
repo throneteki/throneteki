cb41458b98922cf36f0c6a955a57bc85b6397c4e
Brian Upton <ystros@gmail.com>
2018-05-07 10:47:36 -0700

    Implement remaining Someone Always Tells cards (#1945)
    * Implement The Seven-Pointed Star
    
    * Implement A Plot Against the Queen

---
+++ server/game/cards/08.6-SAT/APlotAgainstTheQueen.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard.js');
+
+class APlotAgainstTheQueen extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.loser === this.controller && event.challenge.challengeType === 'power'
+            },
+            cost: ability.costs.returnToHand(card => card.getType() === 'character' && card.power > 0),
+            target: {
+                cardCondition: card => this.game.currentChallenge.isParticipating(card) && card.power > 0
+            },
+            handler: context => {
+                context.target.owner.returnCardToHand(context.target);
+                this.game.addMessage('{0} plays {1} and returns {2} to their hand to return {3} to {4}\'s hand',
+                    context.player, this, context.costs.returnToHand, context.target, context.target.owner);
+            }
+        });
+    }
+}
+
+APlotAgainstTheQueen.code = '08110';
+
+module.exports = APlotAgainstTheQueen;
