85d03785d19be8b785f20e453cd963cafa7e144f
Stefano Zacchiroli <zack@upsilon.cc>
2018-05-07 20:41:23 +0200

    implement The Great Sept of Baelor (SAT) (#1941)

---
+++ server/game/cards/08.6-SAT/TheGreatSeptOfBaelor.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheGreatSeptOfBaelor extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card.getType() === 'character' && event.card.controller !== this.controller
+            },
+            cost: [
+                ability.costs.kneelSelf(),
+                ability.costs.discardGold()
+            ],
+            handler: context => {
+                this.untilEndOfRound(ability => ({
+                    match: context.event.card,
+                    effect: ability.effects.blankExcludingTraits
+                }));
+                this.game.addMessage('{0} kneels {1} and discards 1 gold from it to treat the text box of {2} as blank until the end of the phase',
+                    context.player, this, context.event.card);
+            }
+        });
+    }
+}
+
+TheGreatSeptOfBaelor.code = '08118';
+
+module.exports = TheGreatSeptOfBaelor;
