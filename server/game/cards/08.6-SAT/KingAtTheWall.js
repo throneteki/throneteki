9e263bc1dbae787823edc3dd2fd9a583cb47dc54
Stefano Zacchiroli <zack@upsilon.cc>
2018-05-08 13:27:33 +0200

    implement Baratheon cards from Someone Always Tells chapter pack (#1939)
    * implement The Bastard of Nightsong (SAT)
    
    * implement King at the Wall (SAT)

---
+++ server/game/cards/08.6-SAT/KingAtTheWall.js
@@ -0,0 +1,36 @@
+const DrawCard = require('../../drawcard.js');
+
+class KingAtTheWall extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ faction: 'baratheon', unique: true });
+        this.whileAttached({
+            effect: ability.effects.addTrait('King')
+        });
+
+        this.reaction({
+            when: {
+                afterChallenge: event =>
+                    event.challenge.winner === this.controller &&
+                    event.challenge.challengeType === 'power' &&
+                    event.challenge.isParticipating(this.parent) &&
+                    this.numberOfOutOfFactionLocations() > 0 &&
+                    this.controller.canDraw()
+            },
+            handler: () => {
+                let cards = this.numberOfOutOfFactionLocations();
+                cards = this.controller.drawCardsToHand(cards).length;
+
+                this.game.addMessage('{0} uses {1} to draw {2} {3}',
+                    this.controller, this, cards, cards > 1 ? 'cards' : 'card');
+            }
+        });
+    }
+
+    numberOfOutOfFactionLocations() {
+        return this.controller.getNumberOfCardsInPlay(card => card.getType() === 'location' && card.isOutOfFaction());
+    }
+}
+
+KingAtTheWall.code = '08108';
+
+module.exports = KingAtTheWall;
