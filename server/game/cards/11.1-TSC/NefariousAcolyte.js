c5c5e6a4532b0a473e9b3387f5a016ba75436aa3
Brian Upton <ystros@gmail.com>
2018-05-30 15:06:12 -0700

    Implement Nefarious Acolyte

---
+++ server/game/cards/11.1-TSC/NefariousAcolyte.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../drawcard');
+
+class NefariousAcolyte extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Blank a plot',
+            cost: [
+                ability.costs.kneelSelf(),
+                ability.costs.payGold(1)
+            ],
+            target: {
+                activePromptTitle: 'Select a plot',
+                cardCondition: card => card.location === 'active plot',
+                cardType: 'plot'
+            },
+            handler: context => {
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.blankExcludingTraits
+                }));
+                this.game.addMessage('{0} kneels {1} and pays 1 gold to blank {2} until the end of the phase', this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+NefariousAcolyte.code = '11018';
+
+module.exports = NefariousAcolyte;
