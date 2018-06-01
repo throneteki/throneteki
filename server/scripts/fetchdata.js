13d847d1fba229680cc9cb6936fbd63509e82ce7
Brian Upton <ystros@gmail.com>
2018-06-01 01:50:34 -0700

    Allow fetchdata from other directories (#2000)

--- server/scripts/fetchdata.js
+++ server/scripts/fetchdata.js
@@ -10,6 +10,7 @@ const NoImageSource = require('./fetchdata/NoImageSource.js');
 
 const optionsDefinition = [
     { name: 'card-source', type: String, defaultValue: 'json' },
+    { name: 'card-dir', type: String, defaultValue: path.join(__dirname, '..', '..', 'throneteki-json-data') },
     { name: 'image-source', type: String, defaultValue: 'cardgamedb' },
     { name: 'image-dir', type: String, defaultValue: path.join(__dirname, '..', '..', 'public', 'img', 'cards') },
     { name: 'no-images', type: Boolean, defaultValue: false }
@@ -18,7 +19,7 @@ const optionsDefinition = [
 function createDataSource(options) {
     switch(options['card-source']) {
         case 'json':
-            return new JsonCardSource();
+            return new JsonCardSource(options['card-dir']);
     }
 
     throw new Error(`Unknown card source '${options['card-source']}'`);
