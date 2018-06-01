dd4bd628cdf79f3600db12345ddbd68a978e3362
Brian Upton <ystros@gmail.com>
2018-06-01 01:43:22 -0700

    Implement Nighttime Marauders (#2001)

---
+++ server/game/cards/11.1-TSC/NighttimeMarauders.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard');
+
+class NighttimeMarauders extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card === this && event.playingType === 'outOfShadows'
+            },
+            target: {
+                activePromptTitle: 'Select a card',
+                cardCondition: card => card.controller !== this.controller && card.location === 'discard pile'
+            },
+            handler: context => {
+                let opponent = context.target.controller;
+                let cardsToDiscard = opponent.hand.filter(card => card.getPrintedCost() === context.target.getPrintedCost());
+                opponent.discardCards(cardsToDiscard, false);
+                this.game.addMessage('{0} uses {1} to choose {2} and have {3} reveal their hand: {4}', this.controller, this, context.target, opponent, opponent.hand);
+                this.game.addMessage('{0} discards {1}', opponent, cardsToDiscard);
+            }
+        });
+    }
+}
+
+NighttimeMarauders.code = '11012';
+
+module.exports = NighttimeMarauders;
