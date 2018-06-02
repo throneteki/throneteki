b99456a6c3c95e16b495b9b25e7ed3b855f7b6ab
Brian Upton <ystros@gmail.com>
2018-06-02 02:15:46 -0700

    Implement Stark cards from The Shadow City (#2002)

---
+++ server/game/cards/11.1-TSC/NorthernArmory.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../drawcard');
+
+class NorthernArmory extends DrawCard {
+    setupCardAbilities(ability) {
+        this.plotModifiers({
+            gold: 1
+        });
+        this.persistentEffect({
+            match: this,
+            effect: ability.effects.cannotBeStood()
+        });
+        this.action({
+            title: 'Stand a character',
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: card => card.getType() === 'character' && card.isFaction('stark'),
+                gameAction: 'stand'
+            },
+            handler: context => {
+                this.controller.standCard(context.target);
+                this.game.addMessage('{0} kneels {1} to stand {2}', this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+NorthernArmory.code = '11003';
+
+module.exports = NorthernArmory;
