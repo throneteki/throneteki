cdd3a5dbcabde0b4f35c5fd1a1ab135abbe32418
Brian Upton <ystros@gmail.com>
2018-06-02 02:13:08 -0700

    Implement Margaery's Influence (#2003)

---
+++ server/game/cards/11.1-TSC/MargaerysInfluence.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard');
+
+class MargaerysInfluence extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Stand parent and remove from challenge',
+            condition: () => this.game.currentChallenge && this.game.currentChallenge.isParticipating(this.parent),
+            cost: [
+                ability.costs.kneelSelf(),
+                ability.costs.payGold(1)
+            ],
+            handler: () => {
+                this.parent.controller.standCard(this.parent);
+                this.game.currentChallenge.removeFromChallenge(this.parent);
+                this.game.addMessage('{0} kneels {1} and pays 1 gold to stand {2} and remove it from the challenge', this.controller, this, this.parent);
+            }
+        });
+    }
+}
+
+MargaerysInfluence.code = '11005';
+
+module.exports = MargaerysInfluence;
