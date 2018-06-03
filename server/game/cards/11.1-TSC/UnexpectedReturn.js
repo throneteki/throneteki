1b5e1922dc08d201087c43aedeb5ea768828b41c
Brian Upton <ystros@gmail.com>
2018-06-03 10:53:58 -0700

    Implement Unexpected Return

---
+++ server/game/cards/11.1-TSC/UnexpectedReturn.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard');
+
+class UnexpectedReturn extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            phase: 'challenge',
+            title: 'Put character into play',
+            target: {
+                cardCondition: card => card.getType() === 'character' && card.location === 'discard pile' && card.controller === this.controller
+            },
+            handler: context => {
+                this.controller.putIntoPlay(context.target);
+                this.game.addMessage('{0} uses {1} to put {2} into play', this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+UnexpectedReturn.code = '11015';
+
+module.exports = UnexpectedReturn;
