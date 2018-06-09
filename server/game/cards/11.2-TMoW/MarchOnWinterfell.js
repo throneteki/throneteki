bfbc055e34a86ef14b9803352bdf577364e7b0b4
DukeTax <yorickvogels@gmail.com>
2018-06-09 21:44:52 +0200

    Implement March on Winterfell

---
+++ server/game/cards/11.2-TMoW/MarchOnWinterfell.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard');
+
+class MarchOnWinterfell extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'End the challenge',
+            condition: () => this.game.currentChallenge && this.game.currentChallenge.attackingPlayer === this.controller,
+            handler: context => {
+                this.game.currentChallenge.cancelChallenge();
+
+                this.untilEndOfPhase(ability => ({
+                    targetType: 'player',
+                    targetController: 'current',
+                    effect: ability.effects.modifyChallengeTypeLimit('power', 1)
+                }));
+
+                this.game.addMessage('{0} plays {1} to end this challenge with no winner or loser',
+                    context.player, this);
+                this.game.addMessage('{0} may initiate an additional {1} challenge this phase', context.player, 'power');
+            }
+        });
+    }
+}
+
+MarchOnWinterfell.code = '11028';
+
+module.exports = MarchOnWinterfell;
