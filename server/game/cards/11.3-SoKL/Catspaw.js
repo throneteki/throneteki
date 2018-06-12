0b2b316c276ca2a5ae2bb6912a8f5c35172d5205
DukeTax <yorickvogels@gmail.com>
2018-06-12 17:17:00 +0200

    Implement Catspaw

---
+++ server/game/cards/11.3-SoKL/Catspaw.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../drawcard');
+
+class Catspaw extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card === this && event.playingType === 'outOfShadows'
+            },
+            cost: ability.costs.payXGold(() => this.getMinimumCost(), () => 99),
+            target: {
+                cardCondition: (card, context) => card.location === 'play area' && card.getType() === 'character' &&
+                                                  (context.xValue ? (card.getPrintedCost() <= context.xValue) : (card.getPrintedCost() <= this.controller.getSpendableGold()))
+            },
+            handler: context => {
+                context.target.controller.returnCardToHand(context.target);
+                this.game.addMessage('{0} uses {1} and pays {2} gold to return {3} to {4}\'s hand',
+                    context.player, this, context.goldCost, context.target, context.target.owner);
+            }
+        });
+    }
+
+    getMinimumCost() {
+        return this.game.filterCardsInPlay(card => card !== this && card.getType() === 'character')
+            .map(card => card.getPrintedCost())
+            .reduce(((acc, val) => Math.min(acc, val)));
+    }
+}
+
+Catspaw.code = '11049';
+
+module.exports = Catspaw;
