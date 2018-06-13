8f8267d779d99514aea0c44686b4f97a2b95f505
DukeTax <DukeTax@users.noreply.github.com>
2018-06-13 03:47:37 +0200

    Implement spoilers from Streets of King's Landing (#2034)
    * Update submodules
    
    * Implement Catspaw
    
    * Implement Poor Fellows
    
    * Implement Glass Candle

---
+++ server/game/cards/11.3-SoKL/PoorFellows.js
@@ -0,0 +1,48 @@
+const DrawCard = require('../../drawcard');
+
+class PoorFellows extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && event.challenge.isAttacking(this) &&
+                                         this.canChangeGameState()
+            },
+            handler: context => {
+                let opponent = context.event.challenge.loser;
+                this.game.addMessage('{0} uses {1} to have {2} choose between drawing 1 card or gaining 1 power',
+                    context.player, this, opponent);
+
+                this.game.promptWithMenu(opponent, this, {
+                    activePrompt: {
+                        menuTitle: 'Opponent draws 1 card or Poor Fellows gains 1 power?',
+                        buttons: [
+                            { text: 'Draws card', method: 'draw' },
+                            { text: 'Gains power', method: 'gainsPower' }
+                        ]
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    draw() {
+        this.controller.drawCardsToHand(1);
+        this.game.addMessage('{0} draws 1 card', this.controller);
+        return true;
+    }
+
+    gainsPower() {
+        this.modifyPower(1);
+        this.game.addMessage('{0} gains 1 power on {2}', this.controller, this);
+        return true;
+    }
+
+    canChangeGameState() {
+        return this.canGainPower() || this.controller.canDraw();
+    }
+}
+
+PoorFellows.code = '11057';
+
+module.exports = PoorFellows;
