f96dcb80778128ea07e4fc53537099a8e220729e
Brian Upton <ystros@gmail.com>
2018-06-20 23:01:02 -0700

    Fixes to lasting effects (#2040)
    * Fix immunity vs lasting effects
    
    If a lasting effect is applied to a card that has immunity from that
    effect, it should re-assert itself if immunity ever wears off.
    
    * Only apply lasting effects to in-play cards
    
    Several card implementations were setting up lasting effects that could
    potentially be applied to new cards coming into play.

--- test/server/integration/effects.spec.js
+++ test/server/integration/effects.spec.js
@@ -1,5 +1,84 @@
 describe('effects', function() {
     integration(function() {
+        describe('lasting effects', function() {
+            describe('when a lasting effect applies to a group of cards', function() {
+                beforeEach(function() {
+                    const deck = this.buildDeck('martell', [
+                        'Trading with the Pentoshi',
+                        'Nymeria Sand (TRtW)', 'Dorea Sand', 'Hedge Knight'
+                    ]);
+                    this.player1.selectDeck(deck);
+                    this.player2.selectDeck(deck);
+                    this.startGame();
+                    this.keepStartingHands();
+
+                    let nymeria = this.player1.findCardByName('Nymeria Sand', 'hand');
+                    this.player1.clickCard(nymeria);
+
+                    let opponentCharacter = this.player2.findCardByName('Hedge Knight', 'hand');
+                    this.player2.clickCard(opponentCharacter);
+
+                    this.completeSetup();
+                    this.selectFirstPlayer(this.player1);
+                    this.selectPlotOrder(this.player1);
+                    this.completeMarshalPhase();
+
+                    // Steal Military icon and give it to Sand Snake cards
+                    this.player1.clickMenu(nymeria, 'Remove and gain icon');
+                    this.player1.clickCard(opponentCharacter);
+                    this.player1.clickPrompt('Military');
+                });
+
+                it('should not apply the effect to new cards that come into play', function() {
+                    let dorea = this.player1.findCardByName('Dorea Sand', 'hand');
+                    this.player1.dragCard(dorea, 'play area');
+
+                    expect(dorea.hasIcon('Military')).toBe(false);
+                });
+            });
+
+            describe('when losing immunity during a lasting effect', function() {
+                beforeEach(function() {
+                    const deck = this.buildDeck('targaryen', [
+                        'Trading with the Pentoshi',
+                        'Daenerys Targaryen (TFM)', 'Drogon (Core)', 'Nightmares', 'Dracarys!'
+                    ]);
+                    this.player1.selectDeck(deck);
+                    this.player2.selectDeck(deck);
+                    this.startGame();
+                    this.keepStartingHands();
+
+                    this.dany = this.player1.findCardByName('Daenerys Targaryen (TFM)', 'hand');
+                    this.dragon = this.player1.findCardByName('Drogon', 'hand');
+                    this.player1.clickCard(this.dany);
+                    this.player1.clickCard(this.dragon);
+
+                    this.completeSetup();
+                    this.selectFirstPlayer(this.player1);
+                    this.selectPlotOrder(this.player1);
+                    this.completeMarshalPhase();
+
+                    this.player1.clickPrompt('Intrigue');
+                    this.player1.clickCard(this.dany);
+                    this.player1.clickPrompt('Done');
+
+                    this.player1.clickCard('Dracarys!', 'hand');
+                    this.player1.clickCard(this.dragon);
+                    this.player1.clickCard(this.dany);
+                });
+
+                it('should apply the previously immune effect', function() {
+                    // Blanking Dany should remove her immunity, and the -4 from
+                    // Dracarys! then takes effect, instantly killing her.
+                    // Ruling: http://www.cardgamedb.com/forums/index.php?/topic/39830-ruling-faq-21-cannot-be-variableed/
+                    this.player1.clickCard('Nightmares', 'hand');
+                    this.player1.clickCard(this.dany);
+
+                    expect(this.dany.location).toBe('dead pile');
+                });
+            });
+        });
+
         describe('when blanking / unblanking a dynamically calculated conditional effect', function() {
             beforeEach(function() {
                 const deck = this.buildDeck('targaryen', [
