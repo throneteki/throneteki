78b641a1da4f60c8bb716ea701ec1b76a112d8a1
Stefano Zacchiroli <zack@upsilon.cc>
2018-06-22 22:10:43 +0200

    implement Drogon (In Daznak's Pit) (#2049)

---
+++ server/game/cards/11.5-IDP/Drogon.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../drawcard.js');
+
+class Drogon extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && event.challenge.isAttacking(this)
+            },
+            target: {
+                cardCondition: card => card.location === 'play area' &&
+                    card.controller === this.game.currentChallenge.loser &&
+                    card.getType() === 'character'
+            },
+            handler: context => {
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.killByStrength(-4)
+                }));
+
+                this.game.addMessage('{0} uses {1} to give {2} -4 STR until the end of the phase',
+                    context.player, this, context.target);
+            }
+        });
+    }
+}
+
+Drogon.code = '11093';
+
+module.exports = Drogon;
