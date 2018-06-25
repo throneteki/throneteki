dd3e818d2906d9e4e81e72c342392a769ebc3a87
DukeTax <yorickvogels@gmail.com>
2018-06-25 20:35:16 +0200

    Implement Jon Snow (MoD)

---
+++ server/game/cards/11.4-MoD/JonSnow.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../drawcard.js');
+
+class JonSnow extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.hasCharacterWithTrait('builder'),
+            match: this,
+            effect: ability.effects.addKeyword('stealth')
+        });
+
+        this.persistentEffect({
+            condition: () => this.hasCharacterWithTrait('ranger'),
+            match: this,
+            effect: ability.effects.addKeyword('intimidate')
+        });
+
+        this.persistentEffect({
+            condition: () => this.hasCharacterWithTrait('steward'),
+            match: this,
+            effect: ability.effects.addKeyword('insight')
+        });
+    }
+
+    hasCharacterWithTrait(trait) {
+        return this.controller.anyCardsInPlay(card => card.getType() === 'character' && card.hasTrait(trait));
+    }
+}
+
+JonSnow.code = '11065';
+
+module.exports = JonSnow;
