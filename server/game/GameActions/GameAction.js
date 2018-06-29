fcb90531013cabda7d859822b7b6cf387bcab6d0
Brian Upton <ystros@gmail.com>
2018-06-28 23:48:47 -0700

    Add infrastructure for game actions

---
+++ server/game/GameActions/GameAction.js
@@ -0,0 +1,72 @@
+const AtomicEvent = require('../AtomicEvent');
+const Event = require('../event');
+
+/**
+ * A game action is anything that changes the state of the game, e.g. draw X
+ * cards, kill this character, etc. This class acts as a base class for such
+ * actions. Inheritors should override the `canChangeGameState` and
+ * `createEvent` methods as necessary, and then export a singleton object
+ * instead of the class itself.
+ *
+ * ```
+ * module.exports = new PlaceCard();
+ * ```
+ */
+class GameAction {
+    constructor(name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns whether the action would result in the game state changing and
+     * thus be allowed to resolve.
+     *
+     * @param {*} props
+     */
+    allow(props) {
+        return this.canChangeGameState(props) && (!this.isImmune(props) || !!props.force);
+    }
+
+    /**
+     * Returns whether the game is in a state that would allow the action to be
+     * applied (e.g. a card must be standing in order to be kneeled). Inheritors
+     * should override.
+     *
+     * @param {*} props
+     */
+    canChangeGameState() {
+        return true;
+    }
+
+    /**
+     * Returns whether the target is immune to the action being applied.
+     *
+     * @param {*} props
+     */
+    isImmune(props) {
+        let { card, context } = props;
+        return !!card && !card.allowGameAction(this.name, context);
+    }
+
+    /**
+     * Creates an event object related to the triggering condition for the
+     * action being applied. Inheritors must override.
+     */
+    createEvent() {
+        throw new Error('abstract method `createEvent`');
+    }
+
+    event(name, params, handler) {
+        return new Event(name, params, handler);
+    }
+
+    atomic(...events) {
+        let event = new AtomicEvent();
+        for(let childEvent of events) {
+            event.addChildEvent(childEvent);
+        }
+        return event;
+    }
+}
+
+module.exports = GameAction;
---
+++ test/server/GameActions/GameAction.spec.js
@@ -0,0 +1,69 @@
+const GameAction = require('../../../server/game/GameActions/GameAction');
+
+describe('GameAction', function() {
+    beforeEach(function() {
+        this.action = new GameAction('actionName');
+    });
+
+    describe('allow()', function() {
+        beforeEach(function() {
+            this.cardSpy = jasmine.createSpyObj('card', ['allowGameAction']);
+            this.cardSpy.allowGameAction.and.returnValue(true);
+            this.context = { context: 1 };
+            this.props = { card: this.cardSpy, context: this.context };
+
+            // Inheritors should override canChangeGameState, so explicitly spy
+            // on it in these tests to simulate that.
+            spyOn(this.action, 'canChangeGameState').and.returnValue(true);
+        });
+
+        describe('when game state can be changed and target is not immune', function() {
+            it('should check the game state', function() {
+                this.action.allow(this.props);
+
+                expect(this.action.canChangeGameState).toHaveBeenCalledWith(this.props);
+            });
+
+            it('should check the card for immunity', function() {
+                this.action.allow(this.props);
+
+                expect(this.cardSpy.allowGameAction).toHaveBeenCalledWith('actionName', this.context);
+            });
+
+            it('should return true', function() {
+                expect(this.action.allow(this.props)).toBe(true);
+            });
+        });
+
+        describe('when game state cannot be changed', function() {
+            beforeEach(function() {
+                this.action.canChangeGameState.and.returnValue(false);
+            });
+
+            it('should return false', function() {
+                expect(this.action.allow(this.props)).toBe(false);
+            });
+        });
+
+        describe('when the card is immune', function() {
+            beforeEach(function() {
+                this.cardSpy.allowGameAction.and.returnValue(false);
+            });
+
+            it('should return false', function() {
+                expect(this.action.allow(this.props)).toBe(false);
+            });
+
+            it('should ignore immunity if the force flag is passed', function() {
+                let props = Object.assign({ force: true }, this.props);
+                expect(this.action.allow(props)).toBe(true);
+            });
+        });
+    });
+
+    describe('createEvent()', function() {
+        it('should force inheritors to override', function() {
+            expect(() => this.action.createEvent({})).toThrow();
+        });
+    });
+});
