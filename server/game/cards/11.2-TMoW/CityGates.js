3a780ca6307af2a96c2366ac3f39306014e878be
DukeTax <yorickvogels@gmail.com>
2018-07-02 23:46:50 +0200

    Implement City Gates

---
+++ server/game/cards/11.2-TMoW/CityGates.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+
+class CityGates extends DrawCard {
+    setupCardAbilities(ability) {
+        this.plotModifiers({
+            gold: 1
+        });
+        this.action({
+            title: 'Draw 1 card',
+            condition: () => this.controller.canDraw(),
+            cost: [
+                ability.costs.payGold(1),
+                ability.costs.discardSelfFromHand()
+            ],
+            location: 'hand',
+            handler: context => {
+                context.player.drawCardsToHand(1);
+                this.game.addMessage('{0} pays 1 gold and discards {1} from their hand to draw 1 card',
+                    context.player, this);
+            }
+        });
+    }
+}
+
+CityGates.code = '11038';
+
+module.exports = CityGates;
