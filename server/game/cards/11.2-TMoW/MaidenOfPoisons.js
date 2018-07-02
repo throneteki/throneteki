f7f0db84b558f061f9f61ad6a8d439aef867aa24
DukeTax <yorickvogels@gmail.com>
2018-07-02 23:44:32 +0200

    Implement Maiden of Poisons

---
+++ server/game/cards/11.2-TMoW/MaidenOfPoisons.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard.js');
+
+class MaidenOfPoisons extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.loser === this.controller && event.challenge.isParticipating(this)
+            },
+            cost: ability.costs.sacrificeSelf(),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' &&
+                                       this.game.currentChallenge.isParticipating(card) && card.getNumberOfIcons() < 2
+            },
+            handler: context => {
+                context.target.owner.returnCardToHand(context.target);
+                this.game.addMessage('{0} sacrifices {1} to return {2} to {3}\'s hand',
+                    context.player, this, context.target, context.target.owner);
+            }
+        });
+    }
+}
+
+MaidenOfPoisons.code = '11035';
+
+module.exports = MaidenOfPoisons;
