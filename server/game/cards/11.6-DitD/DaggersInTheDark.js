b2db570c550db7419a28384b54742dc17c1bc6dc
DukeTax <yorickvogels@gmail.com>
2018-07-11 20:16:37 +0200

    Implement Daggers in the Dark

---
+++ server/game/cards/11.6-DitD/DaggersInTheDark.js
@@ -0,0 +1,59 @@
+const DrawCard = require('../../drawcard.js');
+
+class DaggersInTheDark extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && event.challenge.defendingPlayer === this.controller
+            },
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.controller === this.game.currentChallenge.loser,
+                gameAction: 'kill'
+            },
+            handler: context => {
+                let loser = context.event.challenge.loser;
+                let target = context.target;
+
+                if(!this.playerHas2Characters(loser)) {
+                    this.resolveKill(target);
+                    return;
+                }
+
+                this.game.promptForSelect(loser, {
+                    mode: 'exactly',
+                    numCards: 2,
+                    activePromptTitle: 'Sacrifice 2 characters to cancel "Daggers in the Dark"?',
+                    source: this,
+                    cardCondition: card => card.location === 'play area' && card.controller === loser && card.getType() === 'character',
+                    onSelect: (player, cards) => this.onCardsSelected(player, cards, target),
+                    onCancel: () => this.resolveKill(target)
+                });
+            }
+        });
+    }
+
+    playerHas2Characters(player) {
+        return player.getNumberOfCardsInPlay(card => card.getType() === 'character') >= 2;
+    }
+
+    resolveKill(target) {
+        this.game.killCharacter(target);
+        this.game.addMessage('{0} plays {1} to kill {2}', this.controller, this, target);
+        return true;
+    }
+
+    onCardsSelected(player, cards, target) {
+        for(let card of cards) {
+            player.sacrificeCard(card);
+        }
+
+        this.game.addMessage('{0} plays {1} to kill {2}, but the effect is cancelled by {3} sacrificing {4}',
+            this.controller, this, target, player, cards);
+
+        return true;
+    }
+}
+
+DaggersInTheDark.code = '11106';
+
+module.exports = DaggersInTheDark;
