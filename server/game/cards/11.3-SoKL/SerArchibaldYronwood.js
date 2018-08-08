72a3095d951741147ed034fbb53df190e4bbc21c
DukeTax <yorickvogels@gmail.com>
2018-08-09 00:58:06 +0200

    Implement Ser Archibald Yronwood

---
+++ server/game/cards/11.3-SoKL/SerArchibaldYronwood.js
@@ -0,0 +1,35 @@
+const DrawCard = require('../../drawcard.js');
+
+class SerArchibaldYronwood extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onDominanceDetermined: event => event.winner && this.controller !== event.winner
+            },
+            handler: context => {
+                this.game.promptForSelect(context.event.winner, {
+                    source: this,
+                    cardCondition: card => card.location === 'play area' && card.controller === context.event.winner &&
+                                           card.getType() === 'character',
+                    gameAction: 'kill',
+                    onSelect: (player, card) => this.onCardSelected(player, card),
+                    onCancel: (player) => this.cancelSelection(player)
+                });
+            }
+        });
+    }
+    onCardSelected(player, card) {
+        this.game.killCharacter(card);
+        this.game.addMessage('{0} uses {1} to have {2} choose and kill {3}', this.controller, this, player, card);
+        return true;
+    }
+
+    cancelSelection(player) {
+        this.game.addAlert('danger', '{0} cancels the resolution of {1}', player, this);
+        return true;
+    }
+}
+
+SerArchibaldYronwood.code = '11055';
+
+module.exports = SerArchibaldYronwood;
