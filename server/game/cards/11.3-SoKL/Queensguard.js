0b6e087cfd586623866fddc2b40c6b110b4e486b
DukeTax <DukeTax@users.noreply.github.com>
2018-08-09 07:37:22 +0200

    Implement Targaryen cards from Streets of King's Landing (#2147)
    * Implement Hired Assassin
    
    * Implement Queensguard

---
+++ server/game/cards/11.3-SoKL/Queensguard.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard.js');
+
+class Queensguard extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction(card => card.getType() === 'character' && !card.hasTrait('Lady'));
+        this.action({
+            title: 'Stand attached character',
+            condition: () => this.parent.kneeled,
+            cost: ability.costs.discardFromHand(),
+            limit: ability.limit.perRound(3),
+            handler: context => {
+                context.player.standCard(this.parent);
+                this.game.addMessage('{0} uses {1} and discards {2} from their hand to stand {3}',
+                    context.player, this, context.costs.discardFromHand, this.parent);
+            }
+        });
+    }
+}
+
+Queensguard.code = '11054';
+
+module.exports = Queensguard;
