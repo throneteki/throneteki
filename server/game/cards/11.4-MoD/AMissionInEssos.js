bfe324e7d1e573a8ba877b1b95d2fe5e423c2dde
Brian Upton <ystros@gmail.com>
2018-09-04 16:11:20 -0700

    Implement Martell cards for Music of Dragons (#2191)
    * Implement House Manwoody Guard
    
    * Implement A Mission in Essos

---
+++ server/game/cards/11.4-MoD/AMissionInEssos.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../drawcard');
+const TextHelper = require('../../TextHelper');
+
+class AMissionInEssos extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Remove deck from game',
+            handler: () => {
+                let deck = this.controller.drawDeck.slice(0, -1);
+                for(let card of deck) {
+                    this.controller.moveCard(card, 'out of game');
+                    card.facedown = true;
+                }
+
+                for(let card of this.controller.discardPile) {
+                    this.controller.moveCard(card, 'draw deck');
+                }
+
+                this.controller.shuffleDrawDeck();
+                let cardsDrawn = this.controller.drawCardsToHand(3);
+                this.game.addMessage('{0} uses {1} to replace their deck with their discard pile, shuffle it, and draw {2}', this.controller, this, TextHelper.count(cardsDrawn.length, 'card'));
+            }
+        });
+    }
+}
+
+AMissionInEssos.code = '11076';
+
+module.exports = AMissionInEssos;
