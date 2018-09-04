8f069189f0f2ceec5958aba2fabcbea3cb3ff0c1
Brian Upton <ystros@gmail.com>
2018-09-04 14:41:35 -0700

    Implement Winter Reserves (#2190)

---
+++ server/game/cards/11.4-MoD/WinterReserves.js
@@ -0,0 +1,19 @@
+const PlotCard = require('../../plotcard');
+
+class WinterReserves extends PlotCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            location: 'any',
+            condition: () => this.location === 'revealed plots',
+            match: card => card === card.controller.activePlot && card.hasTrait('Winter'),
+            effect: [
+                ability.effects.modifyGold(1),
+                ability.effects.modifyReserve(1)
+            ]
+        });
+    }
+}
+
+WinterReserves.code = '11080';
+
+module.exports = WinterReserves;
