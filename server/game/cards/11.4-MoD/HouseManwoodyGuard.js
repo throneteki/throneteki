7947b2ecc18def92f8649851894f9d2b31a4a4fe
Brian Upton <ystros@gmail.com>
2018-09-04 14:57:41 -0700

    Implement House Manwoody Guard

---
+++ server/game/cards/11.4-MoD/HouseManwoodyGuard.js
@@ -0,0 +1,15 @@
+const DrawCard = require('../../drawcard');
+
+class HouseManwoodyGuard extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => !this.controller.firstPlayer,
+            match: card => card.getType() === 'character' && card.hasTrait('Guard'),
+            effect: ability.effects.doesNotKneelAsDefender()
+        });
+    }
+}
+
+HouseManwoodyGuard.code = '11075';
+
+module.exports = HouseManwoodyGuard;
