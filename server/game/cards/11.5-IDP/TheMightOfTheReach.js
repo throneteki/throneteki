e60568070bf564c8b38fbd67b8d30f2103b2013e
Brian Upton <ystros@gmail.com>
2018-10-04 11:16:12 -0700

    Implement The Might of the Reach

---
+++ server/game/cards/11.5-IDP/TheMightOfTheReach.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard');
+
+class TheMightOfTheReach extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Give +10 STR to participating character',
+            phase: 'challenge',
+            target: {
+                cardCondition: card => card.getType() === 'character' && card.isParticipating()
+            },
+            handler: context => {
+                this.game.addMessage('{0} plays {1} to give +10 STR to {2} until the end of the challenge', context.player, this, context.target);
+                this.untilEndOfChallenge(ability => ({
+                    match: context.target,
+                    effect: ability.effects.modifyStrength(10)
+                }));
+            }
+        });
+    }
+}
+
+TheMightOfTheReach.code = '11084';
+
+module.exports = TheMightOfTheReach;
