bf2e843b066f2bdfa5f44721e547d6795ad30fde
Brian Upton <ystros@gmail.com>
2018-10-04 09:06:17 -0700

    Implement Hardhome

---
+++ server/game/cards/11.5-IDP/Hardhome.js
@@ -0,0 +1,40 @@
+const DrawCard = require('../../drawcard');
+
+class Hardhome extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Force opponent to sacrifice',
+            phase: 'dominance',
+            cost: [
+                ability.costs.kneelSelf(),
+                ability.costs.sacrificeSelf()
+            ],
+            chooseOpponent: opponent => opponent.anyCardsInPlay(card => card.getType() === 'character' && !card.kneeled),
+            handler: context => {
+                this.game.addMessage('{0} kneels and sacrifices {1} to force {2} to choose a standing character to sacrifice', context.player, this, context.opponent);
+                this.game.promptForSelect(context.opponent, {
+                    cardCondition: card => card.controller === context.opponent && card.location === 'play area' && card.getType() === 'character' && !card.kneeled,
+                    gameAction: 'sacrifice',
+                    onSelect: (player, card) => this.cardSelected(player, card),
+                    onCancel: (player) => this.doneSelecting(player),
+                    source: this
+                });
+            }
+        });
+    }
+
+    cardSelected(player, card) {
+        this.game.addMessage('{0} sacrifices {1} for {2}', player, card, this);
+        player.sacrificeCard(card);
+        return true;
+    }
+
+    doneSelecting(player) {
+        this.game.addAlert('danger', '{0} cancels the resolution of {1}', player, this);
+        return true;
+    }
+}
+
+Hardhome.code = '11086';
+
+module.exports = Hardhome;
