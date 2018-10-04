15d7dd0a362d33c86eca37f53bf3a87cff3c0a4e
Stuart Walsh <stu@ipng.org.uk>
2018-10-04 09:08:04 +0100

    Implement A Pinch of Powder and Bear Island Scout from IDP Update card data

---
+++ server/game/cards/11.5-IDP/APinchOfPowder.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+
+class APinchOfPowder extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && event.challenge.attackingPlayer === this.controller &&
+                                         event.challenge.strengthDifference >= 5 && ['intrigue', 'power'].contains(event.challenge.challengeType)
+            },
+            handler: context => {
+                this.parent.controller.returnCardToHand(this.parent, true);
+                this.game.addMessage('{0} uses {1} to return {2} to {3}\'s hand',
+                    context.player, this, this.parent, this.parent.controller);
+            }
+        });
+    }
+}
+
+APinchOfPowder.code = '11099';
+
+module.exports = APinchOfPowder;
---
+++ server/game/cards/11.5-IDP/BearIslandScout.js
@@ -0,0 +1,40 @@
+const DrawCard = require('../../drawcard.js');
+
+class BearIslandScout extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card === this && event.playingType === 'marshal' && this.eachCharacterIsStark()
+            },
+            handler: () => {
+                this.game.promptForDeckSearch(this.controller, {
+                    activePromptTitle: 'Select a card',
+                    cardCondition: card =>card.hasTrait('House Mormont'),
+                    onSelect: (player, card) => this.cardSelected(player, card),
+                    onCancel: player => this.doneSelecting(player),
+                    source: this
+                });
+            }
+        });
+    }
+
+    eachCharacterIsStark() {
+        let charactersInPlay = this.game.allCards.filter(card => card.controller === this && card.location === 'play area' && card.getType() === 'character');
+
+        return charactersInPlay.every(card => card.hasFaction('stark'));
+    }
+
+    cardSelected(player, card) {
+        player.moveCard(card, 'hand');
+
+        this.game.addMessage('{0} uses {1} to search their deck and add {2} to their hand', player, this, card);
+    }
+
+    doneSelecting(player) {
+        this.game.addMessage('{0} uses {1} to search their deck, but does not add a card to their hand', player, this);
+    }
+}
+
+BearIslandScout.code = '11081';
+
+module.exports = BearIslandScout;
