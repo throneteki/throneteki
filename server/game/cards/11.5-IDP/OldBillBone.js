ea897fd7a8e9d92cd5ba95653e7e3f0eafa309e1
Brian Upton <ystros@gmail.com>
2018-10-04 12:05:55 -0700

    Implement neutral cards from In Daznak's Pit (#2256)
    * Implement Old Bill Bone
    
    * Implement The Inn at the Crossroads

---
+++ server/game/cards/11.5-IDP/OldBillBone.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard');
+
+class OldBillBone extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardOutOfShadows: event => event.card === this
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to kneel each opponent\'s faction card', context.player, this);
+                for(let opponent of this.game.getOpponents(context.player)) {
+                    opponent.kneelCard(opponent.faction);
+                }
+            }
+        });
+    }
+}
+
+OldBillBone.code = '11097';
+
+module.exports = OldBillBone;
---
+++ server/game/cards/11.5-IDP/TheInnAtTheCrossroads.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard');
+const TextHelper = require('../../TextHelper');
+
+class TheInnAtTheCrossroads extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Draw 3 cards',
+            phase: 'dominance',
+            cost: ability.costs.kneelSelf(),
+            chooseOpponent: true,
+            handler: context => {
+                let numCardsDrawn = context.player.drawCardsToHand(3).length;
+                this.game.takeControl(context.opponent, this);
+                this.game.addMessage('{0} kneels {1} to draw {2} and give control of {1} to {3}', context.player, this, TextHelper.count(numCardsDrawn, 'card'), context.opponent);
+            }
+        });
+    }
+}
+
+TheInnAtTheCrossroads.code = '11098';
+
+module.exports = TheInnAtTheCrossroads;
