ac36a5752bedb3e380ed76491df56cef4f94c637
Brian Upton <ystros@gmail.com>
2018-10-04 01:04:05 -0700

    Implement Painted Dogs (#2248)

---
+++ server/game/cards/11.5-IDP/PaintedDogs.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard');
+
+class PaintedDogs extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Stand Clansman or Tyrion',
+            phase: 'challenge',
+            cost: ability.costs.returnSelfToHand(),
+            target: {
+                cardCondition: card => card.getType() === 'character' && (card.hasTrait('Clansman') || card.name === 'Tyrion Lannister'),
+                gameAction: 'stand'
+            },
+            handler: context => {
+                this.game.addMessage('{0} returns {1} to hand to stand {2}', context.player, this, context.target);
+                context.player.standCard(context.target);
+            }
+        });
+    }
+}
+
+PaintedDogs.code = '11089';
+
+module.exports = PaintedDogs;
