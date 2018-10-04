184e6f2ea5f0f80f2d87ddd9e8b6f7a446072e36
Brian Upton <ystros@gmail.com>
2018-10-04 01:04:54 -0700

    Implement Glamor (#2249)
    * Implement Glamor
    
    * Extract common event trackers to namespace

---
+++ server/game/cards/11.5-IDP/Glamor.js
@@ -0,0 +1,39 @@
+const DrawCard = require('../../drawcard');
+const KillTracker = require('../../EventTrackers/KillTracker');
+
+class Glamor extends DrawCard {
+    constructor(owner, cardData) {
+        super(owner, cardData);
+        this.tracker = new KillTracker(this.game);
+    }
+
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ controller: 'current' });
+
+        this.whileAttached({
+            effect: ability.effects.modifyStrength(2)
+        });
+
+        this.action({
+            title: 'Resurrect character',
+            cost: ability.costs.killParent(),
+            target: {
+                cardCondition: (card, context) => (
+                    card.controller === context.player &&
+                    card.location === 'dead pile' &&
+                    this.tracker.wasKilledThisPhase(card) &&
+                    context.player.canPutIntoPlay(card)
+                )
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} and kills {2} to put {3} into play',
+                    context.player, this, context.costs.kill, context.target);
+                context.player.putIntoPlay(context.target);
+            }
+        });
+    }
+}
+
+Glamor.code = '11088';
+
+module.exports = Glamor;
