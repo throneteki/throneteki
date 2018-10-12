523ab15f5fab5c98eaaa49796fb2b4343b235085
Tomáš Hrnčiar <tomas.hrnciar@me.com>
2018-10-12 22:49:51 +0200

    Implement Cunning Diplomat - Tyrell card from Daggers in the Dark  (#2279)

---
+++ server/game/cards/11.6-DitD/CunningDiplomat.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+
+class CunningDiplomat extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => (
+                    event.card === this &&
+                    this.game.isDuringChallenge()
+                )
+            },
+            target: {
+                cardCondition: card => card.getType() === 'character' && card.isParticipating()
+            },
+            handler: context => {
+                context.target.controller.standCard(context.target);
+                this.game.currentChallenge.removeFromChallenge(context.target);
+
+                this.game.addMessage('{0} uses {1} to stand and remove {2} from the challenge', context.player, this, context.target);
+            }
+        });
+    }
+}
+
+CunningDiplomat.code = '11103';
+
+module.exports = CunningDiplomat;
