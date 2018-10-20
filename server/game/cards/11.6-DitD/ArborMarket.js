8a79b5dd24aa5ca430940c18a177d978784d8d70
Tomáš Hrnčiar <tomas.hrnciar@me.com>
2018-10-20 23:18:26 +0200

    Implement Arbor Market - Tyrell card from Daggers in the Dark  (#2282)
    Arbor Market(my translation, may be different); 2 cost; non-unique; loyal; House Redwyne; 
    Challenges phase action: Kneel Arbor Market to choose participating character. That character gains +1 STR until the end of challenge.

---
+++ server/game/cards/11.6-DitD/ArborMarket.js
@@ -0,0 +1,33 @@
+const DrawCard = require('../../drawcard.js');
+
+class ArborMarket extends DrawCard {
+    setupCardAbilities(ability) {
+        this.plotModifiers({
+            gold: 1
+        });
+
+        this.action({
+            title: 'Give character +1 STR',
+            cost: ability.costs.kneelSelf(),
+            condition: () => this.game.isDuringChallenge(),
+            phase: 'challenge',
+
+            target: {
+                cardCondition: card => card.location === 'play area' && card.isParticipating()
+            },
+            handler: context => {
+                this.game.addMessage('{0} kneels {1} to give +1 STR to {2} until the end of the challenge',
+                    this.controller, this, context.target);
+
+                this.untilEndOfChallenge(ability => ({
+                    match: context.target,
+                    effect: ability.effects.modifyStrength(1)
+                }));
+            }
+        });
+    }
+}
+
+ArborMarket.code = '11104';
+
+module.exports = ArborMarket;
