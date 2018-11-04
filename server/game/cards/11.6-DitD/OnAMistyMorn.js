f91cf7e89ba91ba0839990fad6f75c7ffc5b148b
Brian Upton <ystros@gmail.com>
2018-11-04 14:05:49 -0800

    Implement "On a Misty Morn"

---
+++ server/game/cards/11.6-DitD/OnAMistyMorn.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard');
+
+class OnAMistyMorn extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Return characters to hand',
+            target: {
+                mode: 'upTo',
+                numCards: 2,
+                cardCondition: card => card.controller === this.controller && card.location === 'dead pile' && card.getType() === 'character'
+            },
+            handler: context => {
+                this.game.addMessage('{0} plays {1} to return {2} to hand', context.player, this, context.target);
+                for(let card of context.target) {
+                    context.player.returnCardToHand(card);
+                }
+            }
+        });
+    }
+}
+
+OnAMistyMorn.code = '11117';
+
+module.exports = OnAMistyMorn;
