21c757e75d5d9695be2042e33b129e3cfd7ad4c2
luky <luky982006@gmail.com>
2018-11-12 16:12:18 +0100

    Implement We Take Westeros and What is dead May Never Die

---
+++ server/game/cards/12-KotI/WeTakeWesteros.js
@@ -0,0 +1,24 @@
+const PlotCard = require('../../plotcard.js');
+
+class WeTakeWesteros extends PlotCard {
+    setupCardAbilities() {
+        this.whenRevealed({
+            target: {
+                activePromptTitle: 'Select a card',
+                cardCondition: card => card.getType()==='location' && card.location === 'discard pile' && this.controller.canPutIntoPlay(card)
+            },
+            handler: (context) => {
+               context.player.putIntoPlay(context.target);
+               this.game.addMessage("{0} uses {1} to put {2} into play ",context.player,this,context.target);
+            }
+        });
+    }
+
+ 
+
+  
+}
+
+WeTakeWesteros.code = '12046';
+
+module.exports = WeTakeWesteros;
---
+++ server/game/cards/12-KotI/WhatIsDeadMayNeverDie.js
@@ -0,0 +1,33 @@
+const DrawCard = require('../../drawcard.js');
+
+class WhatIsDeadMayNeverDie extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onDominanceDetermined: event => this.controller === event.winner
+            },
+            target: {
+                activePromptTitle: 'Select character',
+                cardCondition: card => this.cardCondition(card)
+            },
+            handler: context => {
+                context.player.putIntoPlay(context.target);
+                this.game.addMessage('{0} uses {1} to put {2} into play from their dead pile', context.player, this, context.target);
+            }
+        });
+    }
+
+    cardCondition(card) {
+        return (
+            card.controller === this.controller &&
+            card.getType() === 'character' &&
+            card.location === 'dead pile' &&
+            card.isFaction('greyjoy') &&
+            this.controller.canPutIntoPlay(card)
+        );
+    }
+}
+
+WhatIsDeadMayNeverDie.code = '12023';
+
+module.exports = WhatIsDeadMayNeverDie;
