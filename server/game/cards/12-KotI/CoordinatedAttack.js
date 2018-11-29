cbde8b9f7c342600591e85e0b7564993491a896f
Stuart Walsh <stu@ipng.org.uk>
2018-11-29 10:16:53 +0000

    Implement coordinated attack

---
+++ server/game/cards/12-KotI/CoordinatedAttack.js
@@ -0,0 +1,36 @@
+const PlotCard = require('../../plotcard.js');
+const ChallengeTypes = require('../../ChallengeTypes');
+
+class CoordinatedAttack extends PlotCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                afterChallenge: event => (
+                    event.challenge.winner === this.controller && event.challenge.attackingPlayer === this.controller
+                )
+            },
+            limit: ability.limit.perPhase(1),
+            handler: context => {
+                this.game.promptWithMenu(context.player, this, {
+                    activePrompt: {
+                        menuTitle: 'Select a challenge type',
+                        buttons: ChallengeTypes.asButtons({ method: 'selectChallengeType' }).filter(button => button.arg !== context.event.challenge.challengeType)
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    selectChallengeType(player, selectedType) {
+        this.untilEndOfPhase(ability => ({
+            targetController: 'current',
+            effect: ability.effects.mayInitiateAdditionalChallenge(selectedType)
+        }));
+        this.game.addMessage('{0} uses {1} to be able to initate an additional {2} challenge this phase', player, this, selectedType);
+    }
+}
+
+CoordinatedAttack.code = '12049';
+
+module.exports = CoordinatedAttack;
