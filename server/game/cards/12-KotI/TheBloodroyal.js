b431ed2e59f0fef8e6f232f6e643dadc67e290cd
Stuart Walsh <stu@ipng.org.uk>
2018-11-29 10:25:36 +0000

    Implement the bloodroyal

---
+++ server/game/cards/12-KotI/TheBloodroyal.js
@@ -0,0 +1,18 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheBloodroyal extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ faction: 'martell' });
+        this.whileAttached({
+            condition: () => !this.controller.firstPlayer,
+            effect: [
+                ability.effects.addKeyword('stealth'),
+                ability.effects.addKeyword('renown')
+            ]
+        });
+    }
+}
+
+TheBloodroyal.code = '12030';
+
+module.exports = TheBloodroyal;
