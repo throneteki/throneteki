76330f812cfe76e04d3f2cb37782e104b9e5884a
Stuart Walsh <stu@ipng.org.uk>
2018-11-30 09:09:57 +0000

    Implement Andrik The Unsmiling

---
+++ server/game/cards/12-KotI/AndrikTheUnsmiling.js
@@ -0,0 +1,42 @@
+const DrawCard = require('../../drawcard.js');
+
+class AndrikTheUnsmiling extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onPillage: event => event.challenge.winner === this.controller && event.source === this
+            },
+            target: {
+                activePromptTitle: 'Select location',
+                cardCondition: card => this.cardCondition(card)
+            },
+            handler: context => {
+                this.game.promptForSelect(this.controller, {
+                    activePromptTitle: 'Select a copy of ' + context.target.name,
+                    source: this,
+                    cardCondition: card => card.location === 'play area' && card.isCopyOf(context.target),
+                    onSelect: (player, card) => this.onCardSelected(player, card)
+                });
+            }
+        });
+    }
+
+    cardCondition(card) {
+        return card.controller === this.game.currentChallenge.loser && 
+                                   !card.isLimited() && 
+                                   ['location', 'attachment'].includes(card.getType()) && 
+                                   card.location === 'discard pile';
+    }
+
+    onCardSelected(player, card) {
+        card.controller.discardCard(card);
+
+        this.game.addMessage('{0} uses {1} to discard a copy of {2} from play', player, this, card);
+
+        return true;
+    }
+}
+
+AndrikTheUnsmiling.code = '12008';
+
+module.exports = AndrikTheUnsmiling;
