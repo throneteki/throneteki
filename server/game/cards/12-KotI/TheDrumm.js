8239fdb889771bf882cd677992b1233e826005b2
Brian Upton <ystros@gmail.com>
2018-12-02 11:18:57 -0800

    Even more Kings of the Isles cards (#2345)
    * Implement Bronze Yohn Royce
    
    * Implement The Drumm
    
    * Implement The Starry Sept

---
+++ server/game/cards/12-KotI/TheDrumm.js
@@ -0,0 +1,16 @@
+const DrawCard = require('../../drawcard');
+
+class TheDrumm extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.game.isDuringChallenge({ challengeType: ['military', 'power'] }) && this.isAttacking(),
+            match: card => card.hasIcon('intrigue'),
+            targetController: 'any',
+            effect: ability.effects.cannotBeDeclaredAsDefender()
+        });
+    }
+}
+
+TheDrumm.code = '12006';
+
+module.exports = TheDrumm;
---
+++ server/game/cards/12-KotI/TheStarrySept.js
@@ -0,0 +1,35 @@
+const DrawCard = require('../../drawcard');
+
+class TheStarrySept extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Move 1 power + blank',
+            condition: () => this.power > 0,
+            target: {
+                cardCondition: card => card.getType() === 'character' && card.location === 'play area'
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to move 1 power to {2} and blank them until the end of the phase', context.player, this, context.target);
+                this.game.movePower(this, context.target, 1);
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.blankExcludingTraits
+                }));
+            }
+        });
+
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.isMatch({ winner: this.controller, challengeType: 'power' }) && this.canGainPower()
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to have {1} gain 1 power', context.player, this);
+                this.modifyPower(1);
+            }
+        });
+    }
+}
+
+TheStarrySept.code = '12042';
+
+module.exports = TheStarrySept;
