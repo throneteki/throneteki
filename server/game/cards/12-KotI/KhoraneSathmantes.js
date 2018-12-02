0cc7ff21c0ad967fdd8d9c91f44f2150ad170e0f
Brian Upton <ystros@gmail.com>
2018-12-02 01:52:25 -0800

    Implement Baratheon cards for Kings of the Isles (#2340)
    * Implement Khorane Sathmantes
    
    * Implement Fury

---
+++ server/game/cards/12-KotI/KhoraneSathmantes.js
@@ -0,0 +1,33 @@
+const DrawCard = require('../../drawcard');
+
+class KhoraneSathmantes extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Kneel character and location',
+            cost: [
+                ability.costs.kneelSelf(),
+                ability.costs.kneel(card => card.getType() === 'location' && card.hasTrait('Warship'))
+            ],
+            targets: {
+                character: {
+                    cardCondition: card => card.location === 'play area' && card.getType() === 'character' && !card.kneeled,
+                    gameAction: 'kneel'
+                },
+                location: {
+                    activePromptTitle: 'Select a location',
+                    cardCondition: card => card.location === 'play area' && card.getType() === 'location' && !card.kneeled,
+                    gameAction: 'kneel'
+                }
+            },
+            handler: context => {
+                this.game.addMessage('{0} kneels {1} to kneel {2} and {3}', context.player, context.costs.kneel, context.targets.character, context.targets.location);
+                context.targets.character.controller.kneelCard(context.targets.character);
+                context.targets.location.controller.kneelCard(context.targets.location);
+            }
+        });
+    }
+}
+
+KhoraneSathmantes.code = '12025';
+
+module.exports = KhoraneSathmantes;
