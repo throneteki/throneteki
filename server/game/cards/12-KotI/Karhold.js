3b0814b0a9e78415524c65bbac72892e6226f057
Brian Upton <ystros@gmail.com>
2018-12-02 01:52:49 -0800

    Implement Stark cards for Kings of the Isles (#2341)
    * Implement Maester Vyman
    
    * Implement Karhold

---
+++ server/game/cards/12-KotI/Karhold.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard');
+
+class Karhold extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: card => ['character', 'location'].includes(card.getType()) && !this.hasWinterPlotRevealed(card.controller),
+            targetController: 'any',
+            effect: ability.effects.cannotGainPower()
+        });
+    }
+
+    hasWinterPlotRevealed(controller) {
+        return controller.activePlot && controller.activePlot.hasTrait('Winter');
+    }
+}
+
+Karhold.code = '12034';
+
+module.exports = Karhold;
