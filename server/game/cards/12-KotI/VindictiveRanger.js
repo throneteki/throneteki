ce9579e6c8a84ed187d97b7d36a1d7a41d35882e
Brian Upton <ystros@gmail.com>
2018-12-01 21:15:44 -0800

    Implement Vindictive Ranger

---
+++ server/game/cards/12-KotI/VindictiveRanger.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard');
+
+class VindictiveRanger extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.isMatch({ loser: this.controller, defendingPlayer: this.controller })
+            },
+            handler: context => {
+                this.game.promptForIcon(context.player, this, icon => {
+                    this.game.addMessage('{0} uses {1} to gain stealth and {2} {3} icon', context.player, this, icon === 'intrigue' ? 'an' : 'a', icon);
+                    this.untilEndOfPhase(ability => ({
+                        match: context.target,
+                        effect: [
+                            ability.effects.addIcon(icon),
+                            ability.effects.addKeyword('Stealth')
+                        ]
+                    }));
+                });
+            }
+        });
+    }
+}
+
+VindictiveRanger.code = '12031';
+
+module.exports = VindictiveRanger;
