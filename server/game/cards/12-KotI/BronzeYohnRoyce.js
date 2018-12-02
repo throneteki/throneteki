5bce678f9c4501ded8548d882fa96aa67480bde7
Brian Upton <ystros@gmail.com>
2018-12-02 09:25:17 -0800

    Implement Bronze Yohn Royce

---
+++ server/game/cards/12-KotI/BronzeYohnRoyce.js
@@ -0,0 +1,20 @@
+const DrawCard = require('../../drawcard');
+
+class BronzeYohnRoyce extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && this.isDefending() && this.kneeled
+            },
+            cost: ability.costs.kneel(card => card.getType() === 'location' && card.isFaction('neutral') && card.getPrintedCost() >= 1),
+            handler: context => {
+                this.game.addMessage('{0} uses {1} and kneels {2} to stand {1}', context.player, this, context.costs.kneel);
+                this.controller.standCard(this);
+            }
+        });
+    }
+}
+
+BronzeYohnRoyce.code = '12039';
+
+module.exports = BronzeYohnRoyce;
