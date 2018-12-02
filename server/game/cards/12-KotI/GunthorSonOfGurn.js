b5980f260e3fc6e4df08966e921149d462f0b9ac
Stuart Walsh <stu@ipng.org.uk>
2018-12-02 11:12:03 +0000

    implement gunthor son of gurn - can't read what the code is on the image

---
+++ server/game/cards/12-KotI/GunthorSonOfGurn.js
@@ -0,0 +1,35 @@
+const DrawCard = require('../../drawcard.js');
+
+class GunthorSonOfGurn extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => (
+                    event.challenge.attackingPlayer === this.controller &&
+                    event.challenge.winner === this.controller &&
+                    this.isAttacking()
+                )
+            },
+            handler: context => {
+                this.game.promptForSelect(context.opponent, {
+                    activePromptTitle: 'Select a card',
+                    source: this,
+                    cardCondition: card => card.location === 'hand' && card.controller === context.opponent,
+                    onSelect: (player, card) => this.cardSelected(context, player, card)
+                });
+            }
+        });
+    }
+
+    onCardSelected(context, player, card) {
+        player.discardCard(card);
+        this.game.addMessage('{0} uses {1} to have {2} discard {3} from their hand',
+            context.player, this, context.opponent, card);
+
+        return true;
+    }
+}
+
+GunthorSonOfGurn.code = '120??';
+
+module.exports = GunthorSonOfGurn;
