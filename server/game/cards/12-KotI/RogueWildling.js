ba5d6e50ff0316dd13cebce76e39a033df1aa1a8
Brian Upton <ystros@gmail.com>
2018-12-02 21:43:03 -0800

    Implement Rogue Wildling

---
+++ server/game/cards/12-KotI/RogueWildling.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard');
+
+class RogueWildling extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card === this
+            },
+            target: {
+                cardCondition: card => card.controller === this.controller && card.getType() === 'character' && card.hasTrait('Wildling') && card.kneeled,
+                gameAction: 'stand'
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to stand {2}', context.player, this, context.target);
+                this.controller.standCard(context.target);
+            }
+        });
+    }
+}
+
+RogueWildling.code = '12040';
+
+module.exports = RogueWildling;
