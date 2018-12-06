358c1fbe106e1723c2222e2fe2916cb355b93da9
luky <luky982006@gmail.com>
2018-12-06 20:02:07 +0100

    Implement the Knight

---
+++ server/game/cards/12-KotI/TheKnight.js
@@ -0,0 +1,18 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheKnight extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.game.isDuringChallenge({attackingPlayer: this.controller, attackingAlone: this }),
+            match: this,
+            effect: [
+                ability.effects.addKeyword('renown'),
+                ability.effects.addKeyword('stealth')
+            ]
+        });
+    }
+}
+
+TheKnight.code = '12004';
+
+module.exports = TheKnight;
