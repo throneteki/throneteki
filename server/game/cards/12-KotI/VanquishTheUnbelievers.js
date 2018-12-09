36657bb00d153b447aede34443ebcd8fa864d899
Brian Upton <ystros@gmail.com>
2018-12-09 10:11:00 -0800

    Implement Vanquish the Unbelievers (#2348)

---
+++ server/game/cards/12-KotI/VanquishTheUnbelievers.js
@@ -0,0 +1,38 @@
+const PlotCard = require('../../plotcard');
+
+class VanquishTheUnbelievers extends PlotCard {
+    setupCardAbilities() {
+        this.forcedReaction({
+            when: {
+                onPhaseStarted: event => event.phase === 'challenge'
+            },
+            handler: () => {
+                this.game.promptWithMenu(this.controller, this, {
+                    activePrompt: {
+                        menuTitle: 'Name a trait',
+                        controls: [
+                            { type: 'trait-name', command: 'menuButton', method: 'selectTraitName' }
+                        ]
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    selectTraitName(player, traitName) {
+        let characters = player.filterCardsInPlay(card => card.getType() === 'character' && !card.hasTrait(traitName));
+
+        this.game.addMessage('{0} is forced by {1} to kill characters they control without the {2} trait', player, this, traitName);
+
+        if(characters.length > 0) {
+            this.game.killCharacters(characters, { allowSave: false });
+        }
+
+        return true;
+    }
+}
+
+VanquishTheUnbelievers.code = '12052';
+
+module.exports = VanquishTheUnbelievers;
