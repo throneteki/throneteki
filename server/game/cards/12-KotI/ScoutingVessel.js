c806467cb18ca36db959f7bd443bd3f13a2f7e02
Brian Upton <ystros@gmail.com>
2018-12-21 12:12:47 -0800

    Allow Scouting Vessel to be chained

--- server/game/cards/12-KotI/ScoutingVessel.js
+++ server/game/cards/12-KotI/ScoutingVessel.js
@@ -4,15 +4,15 @@ class ScoutingVessel extends DrawCard {
     setupCardAbilities(ability) {
         this.interrupt({
             when: {
-                onPillage: event => event.source.getType() === 'character' && event.source.controller === this.controller && event.numCards === 1
+                onPillage: event => event.source.getType() === 'character' && event.source.controller === this.controller
             },
             cost: [
                 ability.costs.kneelSelf(),
                 ability.costs.sacrificeSelf()
             ],
+            message: '{player} kneels and sacrifices {source} to discard 3 cards with pillage instead of 1',
             handler: context => {
-                this.game.addMessage('{0} kneels and sacrifices {1} to discard 3 cards with pillage instead', context.player, this);
-                context.event.numCards = 3;
+                context.event.numCards += 2;
             }
         });
     }
