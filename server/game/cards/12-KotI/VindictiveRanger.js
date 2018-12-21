5b5085f72b3f127894892d3f2b76fd2cbf277b95
Stuart Walsh <stu@ipng.org.uk>
2018-12-21 17:08:01 +0000

    Fix improper match in vindictive ranger

--- server/game/cards/12-KotI/VindictiveRanger.js
+++ server/game/cards/12-KotI/VindictiveRanger.js
@@ -10,7 +10,7 @@ class VindictiveRanger extends DrawCard {
                 this.game.promptForIcon(context.player, this, icon => {
                     this.game.addMessage('{0} uses {1} to gain stealth and {2} {3} icon', context.player, this, icon === 'intrigue' ? 'an' : 'a', icon);
                     this.untilEndOfPhase(ability => ({
-                        match: context.target,
+                        match: this,
                         effect: [
                             ability.effects.addIcon(icon),
                             ability.effects.addKeyword('Stealth')
