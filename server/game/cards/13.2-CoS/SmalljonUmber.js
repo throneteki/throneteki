d7d676dd48b41ada0148729e2b2ac3397e75c8a4
weber-knecht <weber.jakob@gmx.de>
2019-01-05 11:12:42 +0100

    Implement Smalljon Umber #2377

---
+++ server/game/cards/13.2-CoS/SmalljonUmber.js
@@ -0,0 +1,14 @@
+const DrawCard = require('../../drawcard.js');
+
+class SmalljonUmber extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            effect: ability.effects.dynamicDecreaseStrength(() => this.controller.getNumberOfUsedPlots())
+        });
+    }
+}
+
+SmalljonUmber.code = '13021';
+
+module.exports = SmalljonUmber;
