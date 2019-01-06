0d64156db808a4c32b3f7924cb7a61e28a982410
weber-knecht <16719940+weber-knecht@users.noreply.github.com>
2019-01-06 05:11:47 +0100

    Fix Red Rain - Once per phase #2375 (#2381)

--- server/game/cards/12-KotI/RedRain.js
+++ server/game/cards/12-KotI/RedRain.js
@@ -13,6 +13,7 @@ class RedRain extends DrawCard {
             when: {
                 afterChallenge: event => event.challenge.isMatch({ winner: this.controller, unopposed: true }) && this.parent.isAttacking() && this.parent.canGainPower()
             },
+            limit: ability.limit.perPhase(1),
             handler: context => {
                 this.game.addMessage('{0} uses {1} to have {2} gain 2 power', context.player, this, this.parent);
                 this.parent.modifyPower(2);
