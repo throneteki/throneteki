96a6d221db891835ac1d0f6aea8d5b30b32bfef6
weber-knecht <16719940+weber-knecht@users.noreply.github.com>
2019-01-12 23:44:39 +0100

    Fix  the way Core Jaime gains renown #2386 (#2392)

--- test/server/cards/06.6-TBWB/Patchface.spec.js
+++ test/server/cards/06.6-TBWB/Patchface.spec.js
@@ -3,7 +3,7 @@ describe('Patchface', function() {
         beforeEach(function() {
             const deck = this.buildDeck('tyrell', [
                 'Trading with the Pentoshi',
-                'Patchface', 'Moon Boy', 'Motley', 'Renly Baratheon (FFH)'
+                'Patchface', 'Moon Boy', 'Motley', 'Motley', 'Renly Baratheon (FFH)', 'Ser Jaime Lannister (Core)'
             ]);
 
             this.player1.selectDeck(deck);
@@ -14,19 +14,21 @@ describe('Patchface', function() {
             this.patchface = this.player1.findCardByName('Patchface', 'hand');
             this.boy = this.player1.findCardByName('Moon Boy', 'hand');
             this.motley = this.player1.findCardByName('Motley', 'hand');
+            this.motley2 = this.player1.findCardByName('Motley', 'hand');
             this.renly = this.player2.findCardByName('Renly Baratheon (FFH)', 'hand');
+            this.jaime = this.player2.findCardByName('Ser Jaime Lannister (Core)', 'hand');
 
             this.player1.clickCard(this.boy);
             this.player2.clickCard(this.renly);
-
+            
             this.completeSetup();
-
-            this.selectFirstPlayer(this.player1);
-            this.selectPlotOrder(this.player1);
         });
-
+        
         describe('when another fool is in play', function() {
             beforeEach(function() {
+                this.selectFirstPlayer(this.player1);
+                this.selectPlotOrder(this.player1);
+
                 this.player1.clickCard(this.patchface);
             });
 
@@ -37,6 +39,9 @@ describe('Patchface', function() {
 
         describe('when the fool trait is added to a character', function() {
             beforeEach(function() {
+                this.selectFirstPlayer(this.player1);
+                this.selectPlotOrder(this.player1);
+
                 this.player1.clickCard(this.patchface);
                 this.player1.clickCard(this.motley);
                 this.player1.clickCard(this.renly);
@@ -49,5 +54,47 @@ describe('Patchface', function() {
                 expect(this.patchface.hasKeyword('renown')).toBe(true);
             });
         });
+        
+        describe('when the fool trait is added to a character with conditional keywords', function() {
+            beforeEach(function() {
+                this.selectFirstPlayer(this.player2);
+                this.selectPlotOrder(this.player1);
+
+                this.player2.clickCard(this.jaime);
+                this.player2.clickPrompt('Done');
+                
+                this.player1.clickCard(this.patchface);
+                this.player1.clickCard(this.motley2);
+                this.player1.clickCard(this.jaime);
+                this.player1.clickPrompt('Done');
+            });
+
+            it('should not match keywords and icons if condition is not met even if it is a fool', function() {
+                expect(this.jaime.hasTrait('Fool')).toBe(true);
+                expect(this.patchface.hasIcon('military')).toBe(true);
+                expect(this.patchface.hasIcon('intrigue')).toBe(true);
+                expect(this.patchface.hasKeyword('renown')).toBe(false);
+            });
+            
+            it('should not match keywords and icons if condition is not met', function() {
+                this.player2.clickPrompt('Military');
+                this.player2.clickCard(this.renly);
+                this.player2.clickPrompt('Done');
+
+                expect(this.patchface.hasIcon('military')).toBe(true);
+                expect(this.patchface.hasIcon('intrigue')).toBe(true);
+                expect(this.patchface.hasKeyword('renown')).toBe(false);
+            });
+
+            it('should only match keywords and icons if condition is met', function() {
+                this.player2.clickPrompt('Military');
+                this.player2.clickCard(this.jaime);
+                this.player2.clickPrompt('Done');
+
+                expect(this.patchface.hasIcon('military')).toBe(true);
+                expect(this.patchface.hasIcon('intrigue')).toBe(true);
+                expect(this.patchface.hasKeyword('renown')).toBe(true);
+            });
+        });        
     });
 });
