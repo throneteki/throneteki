be54afe82606091be6e20bc77c5491e2ae8d29dc
weber-knecht <knecht_weber@yahoo.com>
2019-01-20 17:10:41 +0100

    Implement Littlefinger (AtG)  #2376

---
+++ server/game/cards/13.1-AtG/Littlefinger.js
@@ -0,0 +1,80 @@
+const DrawCard = require('../../drawcard');
+const TextHelper = require('../../TextHelper');
+const _ = require('underscore');
+
+class Littlefinger extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardOutOfShadows: event => event.card.controller === this.controller && event.card.getType() === 'character' 
+            },
+            limit: ability.limit.perRound(1),            
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to select up to 3 cards from their hand', context.player, this);
+                this.game.promptForSelect(context.player, {
+                    activePromptTitle: 'Select up to 3 cards',
+                    numCards: 3,
+                    cardCondition: card => card.controller === context.player && card.location === 'hand',
+                    onSelect: (player, cards) => this.onCardsSelected(player, cards),
+                    onCancel: player => this.cancelResolution(player)
+                });
+            }
+        });
+    }
+
+    onCardsSelected(player, cards) {
+        this.remainingCards = cards;
+        this.numSelectedCards = cards.length;
+        this.promptToPlaceNextCard(cards);
+        return true;
+    }
+
+    promptToPlaceNextCard() {
+        if(this.remainingCards.length === 0) {
+            return true;
+        }
+
+        let buttons = _.map(this.remainingCards, card => ({
+            method: 'selectCardForBottom', card: card
+        }));
+
+        this.game.promptWithMenu(this.controller, this, {
+            activePrompt: {
+                menuTitle: 'Choose card to place on bottom of deck',
+                buttons: buttons
+            },
+            source: this
+        });
+
+        return true;
+    }
+
+    selectCardForBottom(player, cardId) {
+        let card = _.find(this.remainingCards, card => card.uuid === cardId);
+        if(!card) {
+            return false;
+        }
+
+        this.remainingCards = _.reject(this.remainingCards, card => card.uuid === cardId);
+        this.controller.moveCard(card, 'draw deck', { bottom: true });
+
+        if(this.remainingCards.length > 0) {
+            this.promptToPlaceNextCard();
+        } else {
+            let drawnCards = player.drawCardsToHand(this.numSelectedCards);
+            this.game.addMessage('{0} places {1} on bottom of their deck to draw {2}', 
+                player, TextHelper.count(this.numSelectedCards, 'card'), TextHelper.count(drawnCards.length, 'card'));
+        }
+
+        return true;
+    }
+
+    cancelResolution(player) {
+        this.game.addAlert('danger', '{0} cancels resolution of {1}', player, this);
+        return true;
+    }
+}
+
+Littlefinger.code = '13017';
+
+module.exports = Littlefinger;
