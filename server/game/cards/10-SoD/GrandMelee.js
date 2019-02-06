3bae51364eb8bf8c37e85b3479deda6b6725f62b
Brian Upton <ystros@gmail.com>
2019-02-05 20:49:42 -0800

    Fix Grand Melee when adding/removing participants
    Recalculates Grand Melee's effect after every game step in order to
    ensure that its "does not contribute STR" effect is added or removed
    when the number of participants change. Additionally, challenge strength
    is now recalculated when the "does not contribute STR" effect is applied
    to a card.

--- server/game/cards/10-SoD/GrandMelee.js
+++ server/game/cards/10-SoD/GrandMelee.js
@@ -3,6 +3,9 @@ const PlotCard = require('../../plotcard.js');
 class GrandMelee extends PlotCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
+            // Add always-on condition so that attacking / defending alone is
+            // rechecked after participants are added or removed from challenges
+            condition: () => true,
             match: card => this.game.isDuringChallenge({ attackingAlone: card }) || this.game.isDuringChallenge({ defendingAlone: card }),
             targetController: 'any',
             effect: ability.effects.doesNotContributeStrength()
