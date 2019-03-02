2b2a131f18616fa13fa51c88af0a201dc51adfb8
Brian Upton <ystros@gmail.com>
2019-03-02 10:06:55 -0800

    Add tests around entering play knelt effect

---
+++ test/server/cards/01-Core/SansaStark.spec.js
@@ -0,0 +1,51 @@
+describe('Sansa Stark (Core)', function() {
+    integration(function() {
+        describe('when setup', function() {
+            beforeEach(function() {
+                const deck = this.buildDeck('baratheon', [
+                    'A Noble Cause',
+                    'Sansa Stark (Core)'
+                ]);
+                this.player1.selectDeck(deck);
+                this.player2.selectDeck(deck);
+                this.startGame();
+                this.keepStartingHands();
+
+                this.sansa = this.player1.findCardByName('Sansa Stark', 'hand');
+
+                this.player1.clickCard(this.sansa);
+                this.completeSetup();
+            });
+
+            it('is setup standing', function() {
+                expect(this.sansa.location).toBe('play area');
+                expect(this.sansa.kneeled).toBe(false);
+            });
+        });
+
+        describe('when marshalling', function() {
+            beforeEach(function() {
+                const deck = this.buildDeck('baratheon', [
+                    'A Noble Cause',
+                    'Sansa Stark (Core)'
+                ]);
+                this.player1.selectDeck(deck);
+                this.player2.selectDeck(deck);
+                this.startGame();
+                this.keepStartingHands();
+
+                this.sansa = this.player1.findCardByName('Sansa Stark', 'hand');
+
+                this.completeSetup();
+                this.selectFirstPlayer(this.player1);
+
+                this.player1.clickCard(this.sansa);
+            });
+
+            it('enters play knelt', function() {
+                expect(this.sansa.location).toBe('play area');
+                expect(this.sansa.kneeled).toBe(true);
+            });
+        });
+    });
+});
