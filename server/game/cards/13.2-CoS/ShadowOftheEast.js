b1bb005411d0694fa75c5e1aadac5b7e5eb759ee
weber-knecht <knecht_weber@yahoo.com>
2019-03-02 20:56:19 +0100

    Implement ShadowOfTheEast #2377

---
+++ server/game/cards/13.2-CoS/ShadowOftheEast.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class ShadowOfTheWall extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Select an attachment',
+            target: {
+                activePromptTitle: 'Select an attachment',
+                cardCondition: card => card.location === 'play area' && card.getType() === 'attachment'
+            },
+            handler: context => {
+                let attachment = context.target;
+                attachment.owner.discardCard(attachment);
+                this.game.addMessage('{0} plays {1} to discard {2}', this.controller, this, context.target);
+                if(this.game.getPlayers().some(player => player.activePlot && player.activePlot.hasTrait('Summer'))) {
+                    this.game.addMessage('{0} uses {1} to return {1} to their hand instead of their discard pile', this.controller, this);
+                    this.controller.moveCard(this, 'hand');                    
+                }
+            }
+        });
+    }
+}
+
+ShadowOfTheWall.code = '13034';
+
+module.exports = ShadowOfTheWall;
