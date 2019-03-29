8911caa43b894f47ca808de9650e7f0b1187f9aa
Brian Upton <ystros@gmail.com>
2019-03-28 22:49:05 -0700

    Implement R'hllor Infiltrator

---
+++ server/game/cards/13.1-AtG/RhllorInfiltrator.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard');
+
+class RhllorInfiltrator extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Give control and gain 1 power',
+            phase: 'dominance',
+            condition: () => this.controller.canGainFactionPower(),
+            chooseOpponent: true,
+            costs: ability.costs.kneelSelf(),
+            message: {
+                format: '{player} kneels {source} to give control of {source} to {opponent} and gain 1 power',
+                args: { opponent: context => context.opponent }
+            },
+            handler: context => {
+                this.game.takeControl(context.opponent, this);
+                this.game.addPower(context.player, 1);
+            }
+        });
+    }
+}
+
+RhllorInfiltrator.code = '13007';
+
+module.exports = RhllorInfiltrator;
