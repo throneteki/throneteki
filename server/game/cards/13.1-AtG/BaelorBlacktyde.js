4ddd24b368c1eab1807c5e48167add67a493765b
Brian Upton <ystros@gmail.com>
2019-03-29 00:09:06 -0700

    Implement Baelor Blacktyde (#2451)

---
+++ server/game/cards/13.1-AtG/BaelorBlacktyde.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard');
+
+class BaelorBlacktyde extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            targetController: 'opponent',
+            effect: ability.effects.cannotPlay(card => this.hasCopyInDiscard(card))
+        });
+    }
+
+    hasCopyInDiscard(card) {
+        let discardPile = card.controller.discardPile;
+        return discardPile.some(discardedCard => card.isCopyOf(discardedCard));
+    }
+}
+
+BaelorBlacktyde.code = '13011';
+
+module.exports = BaelorBlacktyde;
