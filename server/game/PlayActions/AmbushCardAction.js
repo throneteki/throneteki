9f877d27fbe197028381205a59389e2e14ea2bbf
Brian Upton <ystros@gmail.com>
2019-03-30 22:12:38 -0700

    Clarify message for ambush / shadow dupes

--- server/game/PlayActions/AmbushCardAction.js
+++ server/game/PlayActions/AmbushCardAction.js
@@ -25,7 +25,7 @@ class AmbushCardAction extends BaseAbility {
 
     executeHandler(context) {
         if(context.costs.isDupe) {
-            context.game.addMessage('{0} duplicates {1} costing {2} gold', context.player, context.source, context.costs.gold);
+            context.game.addMessage('{0} ambushes a duplicate of {1} costing {2} gold', context.player, context.source, context.costs.gold);
         } else {
             context.game.addMessage('{0} ambushes with {1} costing {2} gold', context.player, context.source, context.costs.gold);
         }
