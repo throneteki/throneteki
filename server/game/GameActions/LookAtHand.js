c2db59658389d02fa7551ba03e833b4774e982d0
Brian Upton <ystros@gmail.com>
2019-04-22 21:59:43 -0700

    Add look at hand game action

---
+++ server/game/GameActions/LookAtHand.js
@@ -0,0 +1,25 @@
+const GameAction = require('./GameAction');
+
+class LookAtHand extends GameAction {
+    constructor() {
+        super('lookAtHand');
+    }
+
+    canChangeGameState({ opponent }) {
+        return opponent.hand.length > 0;
+    }
+
+    createEvent({ player, opponent, context }) {
+        return this.event('onLookAtHand', { player, opponent }, event => {
+            context.game.promptForSelect(event.player, {
+                activePromptTitle: `Look at ${event.opponent.name}'s hand`,
+                source: context.source,
+                revealTargets: true,
+                cardCondition: card => card.location === 'hand' && card.controller === event.opponent,
+                onSelect: () => true
+            });
+        });
+    }
+}
+
+module.exports = new LookAtHand();
--- server/game/GameActions/index.js
+++ server/game/GameActions/index.js
@@ -1,11 +1,13 @@
 const AbilityAdapter = require('./AbilityAdapter');
 const DiscardCard = require('./DiscardCard');
+const LookAtHand = require('./LookAtHand');
 const RemoveFromGame = require('./RemoveFromGame');
 const SacrificeCard = require('./SacrificeCard');
 const SimultaneousAction = require('./SimultaneousAction');
 
 const GameActions = {
     discardCard: props => new AbilityAdapter(DiscardCard, props),
+    lookAtHand: props => new AbilityAdapter(LookAtHand, props),
     removeFromGame: props => new AbilityAdapter(RemoveFromGame, props),
     sacrificeCard: props => new AbilityAdapter(SacrificeCard, props),
     simultaneously: function(actions) {
