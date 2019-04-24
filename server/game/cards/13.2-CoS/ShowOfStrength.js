6d760874af1bcf00d1f1f7cfd03e474ed4cceaca
Brian Upton <ystros@gmail.com>
2019-04-23 20:55:05 -0700

    Implement Show of Strength

---
+++ server/game/cards/13.2-CoS/ShowOfStrength.js
@@ -0,0 +1,30 @@
+const DrawCard = require('../../drawcard');
+
+class ShowOfStrength extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            phase: 'challenge',
+            condition: () => this.getLowStrengthCharacters().length > 0,
+            message: {
+                format: '{player} plays {source} to treat {lowStrengthCharacters} as blank',
+                args: { lowStrengthCharacters: () => this.getLowStrengthCharacters() }
+            },
+            handler: () => {
+                let characters = this.getLowStrengthCharacters();
+
+                this.untilEndOfPhase(ability => ({
+                    match: characters,
+                    effect: ability.effects.blankExcludingTraits
+                }));
+            }
+        });
+    }
+
+    getLowStrengthCharacters() {
+        return this.game.filterCardsInPlay(card => card.getType() === 'character' && card.getStrength() <= 3);
+    }
+}
+
+ShowOfStrength.code = '13024';
+
+module.exports = ShowOfStrength;
