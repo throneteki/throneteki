fe1309015c4e8d984fed739d1de8f38cf84bb2d9
Brian Upton <ystros@gmail.com>
2019-04-23 21:04:41 -0700

    Implement The Fowler Twins

---
+++ server/game/cards/13.2-CoS/TheFowlerTwins.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../drawcard');
+
+class TheFowlerTwins extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character'
+            },
+            message: '{player} uses {source} to force {target} to be declared as a participant in the next challenge',
+            handler: context => {
+                this.lastingEffect(ability => ({
+                    until: {
+                        afterChallenge: () => true,
+                        onPhaseEnd: () => true
+                    },
+                    match: context.target,
+                    effect: [
+                        ability.effects.mustBeDeclaredAsAttacker(),
+                        ability.effects.mustBeDeclaredAsDefender()
+                    ]
+                }));
+            }
+        });
+    }
+}
+
+TheFowlerTwins.code = '13035';
+
+module.exports = TheFowlerTwins;
