cbf9d30f16b7ac688dca4757b9e109f44d4df5b1
Brian Upton <ystros@gmail.com>
2019-04-25 06:57:02 -0700

    Implement Arrogant Contender

---
+++ server/game/cards/13.2-CoS/ArrogantContender.js
@@ -0,0 +1,38 @@
+const DrawCard = require('../../drawcard');
+
+class ArrogantContender extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.game.isDuringChallenge() && this.isParticipating(),
+            match: this,
+            effect: ability.effects.dynamicStrength(() => this.numOfOpponentParticipants())
+        });
+
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.isMatch({ winner: this.controller, attackingAlone: this }) && this.canGainPower()
+            },
+            message: {
+                format: '{player} uses {source} to have it gain {gainedPower}',
+                args: { gainedPower: () => this.gainedPowerAmount() }
+            },
+            handler: () => {
+                this.modifyPower(this.gainedPowerAmount());
+            }
+        });
+    }
+
+    gainedPowerAmount() {
+        return Math.min(this.numOfOpponentParticipants(), 3);
+    }
+
+    numOfOpponentParticipants() {
+        let participants = this.game.currentChallenge.getParticipants();
+        let opponentParticipants = participants.filter(card => card.controller !== this.controller);
+        return opponentParticipants.length;
+    }
+}
+
+ArrogantContender.code = '13023';
+
+module.exports = ArrogantContender;
