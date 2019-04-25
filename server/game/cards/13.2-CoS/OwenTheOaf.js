968c38b9a5c609ad65799dd0aaa128b46033ec55
Brian Upton <ystros@gmail.com>
2019-04-24 21:01:48 -0700

    Implement Owen the Oaf

---
+++ server/game/cards/13.2-CoS/OwenTheOaf.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard');
+
+class OwenTheOaf extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.isParticipating(),
+            match: card => (
+                card.isParticipating() &&
+                card.getType() === 'character' &&
+                card.getNumberOfIcons() > 1
+            ),
+            targetController: 'any',
+            effect: ability.effects.doesNotContributeStrength()
+        });
+    }
+}
+
+OwenTheOaf.code = '13025';
+
+module.exports = OwenTheOaf;
+
