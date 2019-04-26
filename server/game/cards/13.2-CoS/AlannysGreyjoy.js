be048e629ed966223638b9aa4fcdd439808746df
Brian Upton <ystros@gmail.com>
2019-04-25 23:07:47 -0700

    Implement Alannys Greyjoy (CoS) (#2490)

---
+++ server/game/cards/13.2-CoS/AlannysGreyjoy.js
@@ -0,0 +1,20 @@
+const DrawCard = require('../../drawcard');
+
+class AlannysGreyjoy extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card.isFaction('greyjoy') && event.card.getType() === 'location' && event.card.controller === this.controller && this.controller.canDraw()
+            },
+            message: '{player} uses {source} to draw 1 card',
+            handler: context => {
+                context.player.drawCardsToHand(1);
+            },
+            limit: ability.limit.perPhase(1)
+        });
+    }
+}
+
+AlannysGreyjoy.code = '13031';
+
+module.exports = AlannysGreyjoy;
