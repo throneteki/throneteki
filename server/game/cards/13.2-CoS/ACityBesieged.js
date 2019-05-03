8ba512c5772a2703933dfd1c52e78827c1931c91
weber-knecht <knecht_weber@yahoo.com>
2019-05-03 16:20:46 +0200

    changing whenRevealed handler so it actually works

--- server/game/cards/13.2-CoS/ACityBesieged.js
+++ server/game/cards/13.2-CoS/ACityBesieged.js
@@ -3,27 +3,31 @@ const PlotCard = require('../../plotcard.js');
 class ACityBesieged extends PlotCard {
     setupCardAbilities() {
         this.whenRevealed({
-            target: {
-                numCards: this.hasUsedCityPlot() ? 2 : 1,
-                activePromptTitle: this.hasUsedCityPlot() ? 'Select up to 2 locations' : 'Select a location',
-                ifAble: true,
-                cardCondition: card => card.location === 'play area' && 
-                                        card.getType() === 'location' && 
-                                        !card.kneeled,
-                gameAction: 'kneel'
-            },
-            handler: context => {
-                if(this.hasUsedCityPlot()) {
-                    for(let card of context.target) {
-                        this.controller.kneelCard(card);
-                    }
-                } else {
-                    this.controller.kneelCard(context.target);
-                }
+            handler: () => {
+                let numTargets = this.hasUsedCityPlot() ? 2 : 1;
 
-                this.game.addMessage('{0} uses {1} to kneel {2}', this.controller, this, context.target);
+                this.game.promptForSelect(this.controller, {
+                    mode: 'upTo',
+                    numCards: numTargets,
+                    activePromptTitle: this.hasUsedCityPlot() ? 'Select up to 2 locations' : 'Select a location',
+                    source: this,
+                    gameAction: 'kneel',
+                    cardCondition: card => card.location === 'play area' &&
+                                            card.getType() === 'location' &&
+                                            !card.kneeled,
+                    onSelect: (player, cards) => this.targetsSelected(player, cards)
+                })
             }
         });
+    }
+
+    targetsSelected(player, cards) {
+        for(let card of cards) {
+            this.controller.kneelCard(card);
+        }
+
+        this.game.addMessage('{0} uses {1} to kneel {2}', player, this, cards);
+        return true;
     }
 
     hasUsedCityPlot() {
