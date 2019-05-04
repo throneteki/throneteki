81788da8fe9eb9602ef33ce4fa675655cab42369
weber-knecht <16719940+weber-knecht@users.noreply.github.com>
2019-05-04 19:41:38 +0200

    Implement A City Besieged #2377 (#2507)

---
+++ server/game/cards/13.2-CoS/ACityBesieged.js
@@ -0,0 +1,44 @@
+const PlotCard = require('../../plotcard.js');
+
+class ACityBesieged extends PlotCard {
+    setupCardAbilities() {
+        this.whenRevealed({
+            handler: () => {
+                let numTargets = this.hasUsedCityPlot() ? 2 : 1;
+
+                this.game.promptForSelect(this.controller, {
+                    mode: 'upTo',
+                    numCards: numTargets,
+                    activePromptTitle: this.hasUsedCityPlot() ? 'Select up to 2 locations' : 'Select a location',
+                    source: this,
+                    gameAction: 'kneel',
+                    cardCondition: card => card.location === 'play area' &&
+                                            card.getType() === 'location' &&
+                                            !card.kneeled,
+                    onSelect: (player, cards) => this.targetsSelected(player, cards)
+                });
+            }
+        });
+    }
+
+    targetsSelected(player, cards) {
+        for(let card of cards) {
+            this.controller.kneelCard(card);
+        }
+
+        this.game.addMessage('{0} uses {1} to kneel {2}', player, this, cards);
+        return true;
+    }
+
+    hasUsedCityPlot() {
+        return this.game.allCards.some(card => (
+            card.controller === this.controller &&
+            card.location === 'revealed plots' &&
+            card.hasTrait('City')
+        ));
+    }
+}
+
+ACityBesieged.code = '13040';
+
+module.exports = ACityBesieged;
