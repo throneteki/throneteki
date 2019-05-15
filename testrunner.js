eb9adb5bdadb426a9fd70067f6a2001beeeeaf88
Stuart Walsh <stu@ipng.org.uk>
2019-05-15 08:33:54 +0100

    Fix the test runner to use the existing json

--- testrunner.js
+++ testrunner.js
@@ -2,23 +2,12 @@ var Jasmine = require('jasmine'),
     reporters = require('jasmine-reporters');
 
 var junitReporter = new reporters.JUnitXmlReporter({
-  savePath: __dirname + '/testoutput',
-  consolidateAll: false
+    savePath: __dirname + '/testoutput',
+    consolidateAll: false
 });
 
 var jasmine = new Jasmine();
 
-jasmine.loadConfig({
-  "spec_dir": "test",
-  "spec_files": [
-    "**/*[sS]pec.js"
-  ],
-  "helpers": [
-    "helpers/**/*.js"
-  ],
-  "stopSpecOnExpectationFailure": false,
-  "random": false
-});
-//jasmine.loadConfigFile("jasmine.json");
+jasmine.loadConfigFile('jasmine.json');
 jasmine.addReporter(junitReporter);
 jasmine.execute();
