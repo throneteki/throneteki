18e92dfb73ece6c44eaf6ad6c254741a1dec5236
Stuart Walsh <stu@ipng.org.uk>
2019-05-15 19:11:34 +0100

    amend deployment script

--- pack.js
+++ pack.js
@@ -1,14 +1,16 @@
 /*eslint no-console:0 */
 const octo = require('@octopusdeploy/octopackjs');
+const version = require('./packagever');
 
-octo.pack()
+octo.pack('targz', { version: version })
     .append('./version.js')
     .append('./index.js')
     .append('./package.json')
     .append('./server/**/*.js')
     .append('./public/**/*')
+    .append('./node_modules/**/*')
     .append('./throneteki-json-data/**/*')
     .append('./views/**/*.pug')
-    .toFile('./dist', function (err, data) {
+    .toFile('./dist', function(err, data) {
         console.log('Package Saved: ' + data.name);
     });
