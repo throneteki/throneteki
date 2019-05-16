f0482e84c2eb1db43ebddd9244eed41f9e640e5b
Stuart Walsh <stu@ipng.org.uk>
2019-05-16 17:57:16 +0100

    some fudging required to get the assets.json in as it's created later on

--- OctoWebpackPlugin.js
+++ OctoWebpackPlugin.js
@@ -9,24 +9,30 @@ function OctoWebpackPlugin(options) {
 OctoWebpackPlugin.prototype.apply = function(compiler) {
     let options = this.options;
 
+    let pkg;
+
     compiler.plugin('after-emit', function(compilation, callback) {
         let files = compilation.assets;
-        let pkg = octo.pack(options.type, { id: options.id, version: options.version });
+        pkg = octo.pack(options.type, { id: options.id, version: options.version });
 
         for(var name in compilation.assets) {
             pkg.append(name, compilation.assets[name].existsAt);
         }
 
         pkg.append('vendor-assets.json', './vendor-assets.json');
-        pkg.append('assets.json', './assets.json');
         pkg.append(assets.vendor.js.slice(1), './dist' + assets.vendor.js);
+
+        callback();
+    });
+
+    compiler.plugin('done', function(foo) {
+        pkg.append('assets.json', './assets.json');
 
         pkg.toFile('./out', function(error, data) {
             if(error) {
-                compilation.errors.push(new Error(error));
-                callback();
+                console.error(error);
             } else {
-                console.log('Packed \'' + data.name + '\' with ' + Object.keys(files).length + ' files');
+                console.log('Packed ' + data.name);
             }
         });
     });
