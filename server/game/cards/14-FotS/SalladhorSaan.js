2a0d33d58d878bac794c2bd45ce163a00b0784e7
Brian Upton <ystros@gmail.com>
2019-05-24 22:49:56 -0700

    Implement Salladhor Saan (FotS)

---
+++ server/game/cards/14-FotS/SalladhorSaan.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../drawcard');
+
+class SalladhorSaan extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.playingType === 'marshal' && event.card.controller === this.controller && event.card.hasTrait('Smuggler')
+            },
+            target: {
+                activePromptTitle: 'Select a location',
+                cardCondition: card => card.location === 'play area' && card.getType() === 'location' && !card.kneeled,
+                gameAction: 'kneel'
+            },
+            message: '{player} uses {source} to kneel {target}',
+            handler: context => {
+                this.controller.kneelCard(context.target);
+
+                if(context.player.canGainGold()) {
+                    let gold = this.game.addGold(context.player, context.target.getPrintedCost());
+                    this.game.addMessage('Then {0} uses {1} to gain {2} gold', context.player, this, gold);
+                }
+            },
+            limit: ability.limit.perRound(1)
+        });
+    }
+}
+
+SalladhorSaan.code = '14005';
+
+module.exports = SalladhorSaan;
+
