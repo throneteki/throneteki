62ad76dd07b1beba98294c161b01efcc67c699ab
Brian Upton <ystros@gmail.com>
2019-05-24 20:39:58 -0700

    Implement Outwit

---
+++ server/game/cards/14-FotS/Outwit.js
@@ -0,0 +1,30 @@
+const PlotCard = require('../../plotcard');
+
+class Outwit extends PlotCard {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            canCancel: true,
+            when: {
+                onCardAbilityInitiated: event => event.ability.eventType === 'whenrevealed'
+            },
+            cost: [
+                ability.costs.kneelFactionCard(),
+                ability.costs.kneel(card => card.getType() === 'character' && card.hasTrait('Maester') && card.isUnique())
+            ],
+            message: {
+                format: '{player} uses {source}, kneels their faction card, and kneels {kneeledCard} to cancel {plot}',
+                args: {
+                    kneeledCard: context => context.costs.kneel,
+                    plot: context => context.event.source
+                }
+            },
+            handler: context => {
+                context.event.cancel();
+            }
+        });
+    }
+}
+
+Outwit.code = '14047';
+
+module.exports = Outwit;
