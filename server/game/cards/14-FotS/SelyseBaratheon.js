60471bc704ce1fed8a38980f81c45f562f31611b
Brian Upton <ystros@gmail.com>
2019-05-25 11:21:57 -0700

    Implement Selyse Baratheon (FotS)

---
+++ server/game/cards/14-FotS/SelyseBaratheon.js
@@ -0,0 +1,18 @@
+const DrawCard = require('../../drawcard');
+
+class SelyseBaratheon extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            targetController: 'current',
+            effect: ability.effects.reduceCost({
+                playingTypes: 'marshal',
+                amount: 3,
+                match: card => card.getType() === 'attachment' && card.hasTrait('R\'hllor')
+            })
+        });
+    }
+}
+
+SelyseBaratheon.code = '14008';
+
+module.exports = SelyseBaratheon;
