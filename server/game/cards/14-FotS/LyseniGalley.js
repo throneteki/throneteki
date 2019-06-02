d64d22b4c303d2acc46b5a7dbf0aaeac6db61cf6
Brian Upton <ystros@gmail.com>
2019-06-01 19:15:47 -0700

    Implement Lyseni Galley

---
+++ server/game/cards/14-FotS/LyseniGalley.js
@@ -0,0 +1,39 @@
+const DrawCard = require('../../drawcard');
+
+class LyseniGalley extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Give +1 STR',
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character'
+            },
+            message: {
+                format: '{player} kneels {source} to give {target} +1 STR {additionalEffect}',
+                args: {
+                    additionalEffect: context => this.isSmugglerOrCaptain(context.target) ? 'and stealth' : ''
+                }
+            },
+            handler: context => {
+                let effects = [ability.effects.modifyStrength(1)];
+
+                if(this.isSmugglerOrCaptain(context.target)) {
+                    effects.push(ability.effects.addKeyword('Stealth'));
+                }
+
+                this.untilEndOfPhase(() => ({
+                    match: context.target,
+                    effect: effects
+                }));
+            }
+        });
+    }
+
+    isSmugglerOrCaptain(card) {
+        return ['Smuggler', 'Captain'].some(trait => card.hasTrait(trait));
+    }
+}
+
+LyseniGalley.code = '14019';
+
+module.exports = LyseniGalley;
