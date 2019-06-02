425f2a6f74c9683f10c6d19ba92cf309a7ab0c1c
Brian Upton <ystros@gmail.com>
2019-06-01 17:47:49 -0700

    Implement Bastard of Robert

---
+++ server/game/cards/14-FotS/BastardOfRobert.js
@@ -0,0 +1,30 @@
+const DrawCard = require('../../drawcard');
+
+class BastardOfRobert extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            location: 'any',
+            condition: () => this.controller.anyCardsInPlay(card => card.name === 'Robert Baratheon'),
+            targetController: 'current',
+            effect: ability.effects.reduceSelfCost('marshal', 2)
+        });
+
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => (
+                    event.card === this &&
+                    this.controller.anyCardsInPlay(card => card.getType() === 'character' && card.hasTrait('King')) &&
+                    this.controller.canDraw()
+                )
+            },
+            message: '{player} uses {source} to draw 1 card',
+            hander: context => {
+                context.player.drawCardsToHand(1);
+            }
+        });
+    }
+}
+
+BastardOfRobert.code = '14015';
+
+module.exports = BastardOfRobert;
