3b75ef1aec285debfe1a3433380d532f21eec03b
Brian Upton <ystros@gmail.com>
2019-06-01 17:40:35 -0700

    Implement Shyra Errol

---
+++ server/game/cards/14-FotS/ShyraErrol.js
@@ -0,0 +1,33 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class ShyraErrol extends DrawCard {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            when: {
+                onCharacterKilled: event => event.card.power > 0
+            },
+            cost: ability.costs.sacrificeSelf(),
+            message: {
+                format: '{player} sacrifices {source} to move power from {powerCard} to {powerController}\'s faction card',
+                args: {
+                    powerCard: context => context.event.card,
+                    powerController: context => context.event.card.controller
+                }
+            },
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.movePower(context => ({
+                        from: context.event.card,
+                        to: context.event.card.controller.faction
+                    })),
+                    context
+                );
+            }
+        });
+    }
+}
+
+ShyraErrol.code = '14013';
+
+module.exports = ShyraErrol;
