a1edb2ec6bfd6ee2b41a6c6e5e87ad6b8388d03c
Brian Upton <ystros@gmail.com>
2019-06-01 19:31:11 -0700

    Implement Warhammer

---
+++ server/game/cards/14-FotS/Warhammer.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard');
+
+class Warhammer extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            match: card => card.getPrintedStrength() <= 4,
+            effect: ability.effects.modifyStrength(2)
+        });
+
+        this.whileAttached({
+            match: card => card.getPrintedStrength() >= 5,
+            effect: ability.effects.addKeyword('Intimidate')
+        });
+    }
+}
+
+Warhammer.code = '14022';
+
+module.exports = Warhammer;
