d20c7320f18f02deb2d2d3a67a718780e589bebe
Brian Upton <ystros@gmail.com>
2019-06-02 20:39:20 -0700

    Implement Desperate Attack

---
+++ server/game/cards/14-FotS/DesperateAttack.js
@@ -0,0 +1,15 @@
+const PlotCard = require('../../plotcard');
+
+class DesperateAttack extends PlotCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.game.isDuringChallenge({ defendingPlayer: this.controller }),
+            match: card => card === this.game.currentChallenge.attackingPlayer.activePlot,
+            effect: ability.effects.modifyClaim(1)
+        });
+    }
+}
+
+DesperateAttack.code = '14050';
+
+module.exports = DesperateAttack;
