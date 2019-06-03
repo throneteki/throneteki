0ea723b9bab25fcbe75b3e243fb08c52769c3784
Brian Upton <ystros@gmail.com>
2019-06-03 11:26:20 -0700

    Implement Renly Baratheon (FotS) (#2539)

---
+++ server/game/cards/14-FotS/RenlyBaratheon.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard');
+
+class RenlyBaratheon extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Put character into play',
+            cost: ability.costs.kneelFactionCard(),
+            target: {
+                cardCondition: card => (
+                    card.controller === this.controller &&
+                    card.location === 'hand' &&
+                    card.getType() === 'character' &&
+                    !card.isFaction('baratheon') &&
+                    card.getPrintedCost() <= this.getStrength() &&
+                    this.controller.canPutIntoPlay(card)
+                )
+            },
+            message: '{player} uses {source} and kneels their faction card to put {target} into play',
+            handler: context => {
+                context.player.putIntoPlay(context.target);
+            }
+        });
+    }
+}
+
+RenlyBaratheon.code = '14004';
+
+module.exports = RenlyBaratheon;
