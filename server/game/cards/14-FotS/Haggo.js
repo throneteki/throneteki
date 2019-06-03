9f915b3ad99db4448b77f5c7c79e0d46e1af73f2
Brian Upton <ystros@gmail.com>
2019-06-02 17:04:36 -0700

    Implement Haggo

---
+++ server/game/cards/14-FotS/Haggo.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class Haggo extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                afterChallenge: event => this.isParticipating() && event.challenge.isMatch({ winner: this.controller })
+            },
+            cost: ability.costs.discardFromHand(),
+            target: {
+                activePromptTitle: 'Select a card',
+                cardCondition: card => card.location === 'discard pile' && card.controller === this.controller && card.hasTrait('Dothraki')
+            },
+            message: {
+                format: '{player} uses {source} and discards {discardedCard} to return {target} to hand',
+                args: { discardedCard: context => context.costs.discardFromHand }
+            },
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.returnCardToHand(context => ({ card: context.target })),
+                    context
+                );
+            }
+        });
+    }
+}
+
+Haggo.code = '14035';
+
+module.exports = Haggo;
