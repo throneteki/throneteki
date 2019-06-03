c33ddaf81943391e630c52b4c5e762a64617f05c
Brian Upton <ystros@gmail.com>
2019-06-02 19:06:08 -0700

    Implement Fields of the Reach

---
+++ server/game/cards/14-FotS/FieldsOfTheReach.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard');
+
+class FieldsOfTheReach extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.isMatch({ winner: this.controller, challengeType: 'power' })
+            },
+            cost: ability.costs.kneelMultiple(3, card => card.getType() === 'location' && card.hasTrait('The Reach')),
+            message: {
+                format: '{player} uses {source} and kneels {kneeledCards} to raise their claim by 1',
+                args: { kneeledCards: context => context.costs.kneel }
+            },
+            handler: () => {
+                this.untilEndOfPhase(ability => ({
+                    match: card => card === this.controller.activePlot,
+                    effect: ability.effects.modifyClaim(1)
+                }));
+            }
+        });
+    }
+}
+
+FieldsOfTheReach.code = '14038';
+
+module.exports = FieldsOfTheReach;
