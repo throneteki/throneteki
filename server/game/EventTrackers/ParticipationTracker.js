c342c5f074d5f383335de23fcd183ec4dd298a0f
Brian Upton <ystros@gmail.com>
2019-06-02 20:21:50 -0700

    Implement Defiance

---
+++ server/game/EventTrackers/ParticipationTracker.js
@@ -0,0 +1,26 @@
+class ParticipationTracker {
+    static forPhase(game) {
+        return new ParticipationTracker(game, 'onPhaseEnded');
+    }
+
+    constructor(game, endingEvent) {
+        this.cards = [];
+        game.on('onDeclaredAsAttacker', event => this.trackCard(event.card));
+        game.on('onDeclaredAsDefender', event => this.trackCard(event.card));
+        game.on(endingEvent, () => this.clearCards());
+    }
+
+    trackCard(card) {
+        this.cards.push(card);
+    }
+
+    clearCards() {
+        this.cards = [];
+    }
+
+    hasParticipated(card) {
+        return this.cards.includes(card);
+    }
+}
+
+module.exports = ParticipationTracker;
