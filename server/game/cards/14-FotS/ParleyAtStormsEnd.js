ba630b5244f04f5d0a78f19b4721962b6dc40c66
Brian Upton <ystros@gmail.com>
2019-06-03 10:56:48 -0700

    Implement Parley at Storm's End (#2535)

---
+++ server/game/cards/14-FotS/ParleyAtStormsEnd.js
@@ -0,0 +1,36 @@
+const PlotCard = require('../../plotcard');
+const ChallengeTypes = require('../../ChallengeTypes');
+
+class ParleyAtStormsEnd extends PlotCard {
+    setupCardAbilities() {
+        this.whenRevealed({
+            handler: () => {
+                this.game.promptWithMenu(this.controller, this, {
+                    activePrompt: {
+                        menuTitle: 'Select a challenge type',
+                        buttons: ChallengeTypes.asButtons({ method: 'setChallengeType' })
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    setChallengeType(player, challengeType) {
+        this.game.addMessage('{0} uses {1} to prevent {2} challenges', player, this, challengeType);
+
+        this.lastingEffect(ability => ({
+            until: {
+                onCardEntersPlay: event => event.card.getType() === 'plot' && event.card.controller === player
+            },
+            targetController: 'any',
+            effect: ability.effects.cannotInitiateChallengeType(challengeType)
+        }));
+
+        return true;
+    }
+}
+
+ParleyAtStormsEnd.code = '14046';
+
+module.exports = ParleyAtStormsEnd;
