9da8bbbc27b38dc0e71f3ab8b72eb36083af500b
Brian Upton <ystros@gmail.com>
2019-06-04 09:59:22 -0700

    Implement Lionstar (#2566)

---
+++ server/game/cards/14-FotS/Lionstar.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../drawcard');
+
+class Lionstar extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Put a character into play',
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: card => (
+                    card.location === 'hand' &&
+                    card.controller === this.controller &&
+                    card.getType() === 'character' &&
+                    card.isFaction('lannister') &&
+                    this.controller.canPutIntoPlay(card)
+                )
+            },
+            message: '{player} kneels {source} to put {target} into play',
+            handler: context => {
+                context.player.putIntoPlay(context.target);
+                this.atEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.discardIfStillInPlay()
+                }));
+            }
+        });
+    }
+}
+
+Lionstar.code = '14028';
+
+module.exports = Lionstar;
