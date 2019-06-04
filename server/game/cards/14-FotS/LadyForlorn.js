c6ce229b8f13702341a4a7a3aa66c0c27b0ef83a
Brian Upton <ystros@gmail.com>
2019-06-04 10:05:07 -0700

    Implement Lady Forlorn (#2581)

---
+++ server/game/cards/14-FotS/LadyForlorn.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard');
+
+class LadyForlorn extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.dynamicStrength(() => this.getSTR())
+        });
+
+        this.whileAttached({
+            match: card => card.name === 'Lyn Corbray',
+            effect: ability.effects.addIcon('power')
+        });
+    }
+
+    getSTR() {
+        return this.controller.getNumberOfCardsInPlay(card => card.getType() === 'location' && card.isFaction('neutral'));
+    }
+}
+
+LadyForlorn.code = '14043';
+
+module.exports = LadyForlorn;
