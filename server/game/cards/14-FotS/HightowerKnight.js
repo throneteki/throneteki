4e4d8a869ffcc58a59bae21657c80f58c729fb94
Brian Upton <ystros@gmail.com>
2019-06-04 10:02:47 -0700

    Implement Hightower Knight (#2575)

---
+++ server/game/cards/14-FotS/HightowerKnight.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard');
+
+class HightowerKnight extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Put into play',
+            location: 'hand',
+            condition: () => this.controller.canPutIntoPlay(this),
+            cost: ability.costs.kneel(card => card.getType() === 'character' && !card.isFaction('tyrell') && card.hasTrait('Knight')),
+            message: {
+                format: '{player} uses {source} and kneels {kneeledCard} to put {source} into play',
+                args: { kneeledCard: context => context.costs.kneel }
+            },
+            handler: context => {
+                context.player.putIntoPlay(this);
+            }
+        });
+    }
+}
+
+HightowerKnight.code = '14037';
+
+module.exports = HightowerKnight;
