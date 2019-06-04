31c9de2ed172c6cc4442ff81b0fc2c169aa2ffc6
Brian Upton <ystros@gmail.com>
2019-06-04 09:59:05 -0700

    Implement Ser Gregor Clegane (FotS) (#2565)

---
+++ server/game/cards/14-FotS/SerGregorClegane.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard');
+
+class SerGregorClegane extends DrawCard {
+    setupCardAbilities() {
+        this.forcedReaction({
+            when: {
+                afterChallenge: event => this.isParticipating() && event.challenge.isMatch({ winner: this.controller })
+            },
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.controller === this.controller,
+                gameAction: 'kill'
+            },
+            message: '{player} is forced to choose and kill {target} for {source}',
+            handler: context => {
+                this.game.killCharacter(context.target);
+            }
+        });
+    }
+}
+
+SerGregorClegane.code = '14027';
+
+module.exports = SerGregorClegane;
