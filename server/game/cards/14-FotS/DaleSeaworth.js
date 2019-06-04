15417ac5aa3bc9e1bf312a52b0262cab3f06c2e7
Brian Upton <ystros@gmail.com>
2019-06-04 09:51:02 -0700

    Implement Dale Seaworth (FotS) (#2552)

---
+++ server/game/cards/14-FotS/DaleSeaworth.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class DaleSeaworth extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card === this && event.playingType === 'mashal'
+            },
+            target: {
+                activePromptTitle: 'Select a location',
+                cardCondition: card => card.location === 'discard pile' && card.getType() === 'location' && card.controller === this.controller,
+            },
+            message: '{player} uses {source} to return {target} to hand',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.returnCardToHand(context => ({ card: context.target })),
+                    context
+                );
+            }
+        });
+    }
+}
+
+DaleSeaworth.code = '14010';
+
+module.exports = DaleSeaworth;
