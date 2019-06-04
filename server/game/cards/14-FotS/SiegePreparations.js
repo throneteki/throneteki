b68e95012e082121db727e5633654c6af515ca5f
Brian Upton <ystros@gmail.com>
2019-06-04 10:06:32 -0700

    Implement Siege Preparations (#2583)

---
+++ server/game/cards/14-FotS/SiegePreparations.js
@@ -0,0 +1,21 @@
+const PlotCard = require('../../plotcard');
+const TextHelper = require('../../TextHelper');
+
+class SiegePreparations extends PlotCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Draw cards',
+            condition: () => this.controller.canDraw() && this.controller.getTotalReserve() > this.controller.hand.length,
+            handler: context => {
+                let cardsToDraw = context.player.getTotalReserve() - context.player.hand.length;
+                let numDrawn = context.player.drawCardsToHand(cardsToDraw).length;
+                this.game.addMessage('{0} uses {1} to draw {2}', context.player, this, TextHelper.count(numDrawn, 'card'));
+            },
+            limit: ability.limit.perPhase(1)
+        });
+    }
+}
+
+SiegePreparations.code = '14047';
+
+module.exports = SiegePreparations;
