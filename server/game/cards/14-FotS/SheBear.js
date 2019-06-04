bcfda9f9ce83c5219d0202a94867ae88ac66bc0e
Brian Upton <ystros@gmail.com>
2019-06-04 10:01:34 -0700

    Implement She-Bear (#2571)

---
+++ server/game/cards/14-FotS/SheBear.js
@@ -0,0 +1,32 @@
+const DrawCard = require('../../drawcard');
+
+class SheBear extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card === this && event.playingType === 'marshal'
+            },
+            target: {
+                activePromptTitle: 'Select a card',
+                cardCondition: card => (
+                    card.location === 'hand' &&
+                    card.controller === this.controller &&
+                    ['character', 'attachment'].includes(card.getType()) &&
+                    card.isFaction('stark'),
+                    card.hasPrintedCost() &&
+                    card.getPrintedCost() <= 3 &&
+                    this.controller.canPutIntoPlay(card)
+                )
+            },
+            message: '{player} uses {source} to put {target} into play',
+            hander: context => {
+                context.player.putIntoPlay(context.target);
+            }
+        });
+    }
+}
+
+SheBear.code = '14033';
+
+module.exports = SheBear;
+
