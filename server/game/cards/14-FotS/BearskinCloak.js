37c80bd44a72db3900c34321f5b356acfc425ac6
Brian Upton <ystros@gmail.com>
2019-06-04 10:01:51 -0700

    Implement Bearskin Cloak (#2572)

---
+++ server/game/cards/14-FotS/BearskinCloak.js
@@ -0,0 +1,18 @@
+const DrawCard = require('../../drawcard');
+
+class BearskinCloak extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.modifyStrength(1)
+        });
+
+        this.whileAttached({
+            match: card => card.hasTrait('House Mormont'),
+            effect: ability.effects.addKeyword('Stealth')
+        });
+    }
+}
+
+BearskinCloak.code = '14034';
+
+module.exports = BearskinCloak;
