e81345abd62a314bb3d7c4ebf11ce56fcc286869
Brian Upton <ystros@gmail.com>
2019-06-04 10:03:18 -0700

    Implement Lyn Corbray (#2577)

---
+++ server/game/cards/14-FotS/LynCorbray.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class LynCorbray extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onChallengeInitiated: event => event.challenge.isMatch({ defendingPlayer: this.controller, challengeType: 'power' }) && this.allowGameAction('stand')
+            },
+            message: '{player} uses {source} to stand {source}',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.standCard({ card: this }),
+                    context
+                );
+            }
+        });
+    }
+}
+
+LynCorbray.code = '14039';
+
+module.exports = LynCorbray;
+
