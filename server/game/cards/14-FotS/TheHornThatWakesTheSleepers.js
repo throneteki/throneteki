ebfcbe185866362ff389c7c906bd0aa371d85ca7
Brian Upton <ystros@gmail.com>
2019-06-04 10:01:19 -0700

    Implement The Horn That Wakes the Sleepers (#2570)

---
+++ server/game/cards/14-FotS/TheHornThatWakesTheSleepers.js
@@ -0,0 +1,56 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class TheHornThatWakesTheSleepers extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onChallengeInitiated: event => event.challenge.isMatch({ defendingPlayer: this.controller })
+            },
+            targets: {
+                builder: {
+                    cardCondition: card => this.isKneeledWithTrait(card, 'Builder'),
+                    gameAction: 'stand'
+                },
+                ranger: {
+                    cardCondition: card => this.isKneeledWithTrait(card, 'Ranger'),
+                    gameAction: 'stand'
+                },
+                steward: {
+                    cardCondition: card => this.isKneeledWithTrait(card, 'Steward'),
+                    gameAction: 'stand'
+                }
+            },
+            message: {
+                format: '{player} plays {source} to stand {targets}',
+                args: { targets: context => this.getCharactersToStand(context) }
+            },
+            handler: context => {
+                let targets = this.getCharactersToStand(context);
+                this.game.resolveGameAction(
+                    GameActions.simultaneously(
+                        targets.map(card => GameActions.standCard({ card }))
+                    )
+                );
+            }
+        });
+    }
+
+    isKneeledWithTrait(card, trait) {
+        return (
+            card.location === 'play area' &&
+            card.controller === this.controller &&
+            card.getType() === 'character' &&
+            card.kneeled &&
+            card.hasTrait(trait)
+        );
+    }
+
+    getCharactersToStand(context) {
+        return [context.targets.builder, context.targets.ranger, context.targets.steward];
+    }
+}
+
+TheHornThatWakesTheSleepers.code = '14032';
+
+module.exports = TheHornThatWakesTheSleepers;
