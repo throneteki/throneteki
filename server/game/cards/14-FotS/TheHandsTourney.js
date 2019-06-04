9380cc9653a2b18b9f94bce89385440dbd9c098a
Brian Upton <ystros@gmail.com>
2019-06-04 10:07:31 -0700

    Implement The Hand's Tourney (#2585)

---
+++ server/game/cards/14-FotS/TheHandsTourney.js
@@ -0,0 +1,26 @@
+const PlotCard = require('../../plotcard');
+const ApplyClaim = require('../../gamesteps/challenge/applyclaim');
+
+class TheHandsTourney extends PlotCard {
+    setupCardAbilities() {
+        this.forcedInterrupt({
+            when: {
+                onClaimApplied: event => event.challenge.challengeType === 'military'
+            },
+            handler: context => {
+                this.game.addMessage('{0} is forced by {1} to apply {2} claim instead of {3} claim', context.event.challenge.attackingPlayer, this, 'power', 'military');
+
+                context.replaceHandler(() => {
+                    let replacementClaim = context.event.claim.clone();
+                    replacementClaim.challengeType = 'power';
+
+                    this.game.queueStep(new ApplyClaim(this.game, replacementClaim));
+                });
+            }
+        });
+    }
+}
+
+TheHandsTourney.code = '14051';
+
+module.exports = TheHandsTourney;
