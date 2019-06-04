ca83150dd4f62a2dedc7c61a2b814c58c9551828
Brian Upton <ystros@gmail.com>
2019-06-04 10:02:24 -0700

    Implement Dothraki Steed (#2574)

---
+++ server/game/cards/14-FotS/DothrakiSteed.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard');
+
+class DothrakiSteed extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction(card => card.getType() === 'character' && card.attachments.every(attachment => attachment === this || attachment.name !== 'Dothraki Steed'));
+
+        this.whileAttached({
+            effect: ability.effects.dynamicStrength(() => this.getSTR())
+        });
+    }
+
+    getSTR() {
+        return this.parent.hasTrait('Dothraki') ? 3 : 1;
+    }
+}
+
+DothrakiSteed.code = '14036';
+
+module.exports = DothrakiSteed;
