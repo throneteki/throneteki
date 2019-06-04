9c067a10b677951ddbdfd24588bc70d9f15a1b37
Brian Upton <ystros@gmail.com>
2019-06-04 09:59:49 -0700

    Implement Prince's Attendant (#2567)

---
+++ server/game/cards/14-FotS/PrincesAttendant.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard');
+
+class PrincesAttendant extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Reduce next marshalled character',
+            clickToActivate: true,
+            phase: 'marshal',
+            cost: ability.costs.kneelSelf(),
+            message: '{player} kneels {source} to reduce the cost of the next Martell Lord or Lady they marshal this phase by 2',
+            handler: () => {
+                this.untilEndOfPhase(ability => ({
+                    effect: ability.effects.reduceNextMarshalledCardCost(
+                        2,
+                        card => (card.hasTrait('Lord') || card.hasTrait('Lady')) && card.getType() === 'character' && card.isFaction('martell')
+                    )
+                }));
+            }
+        });
+    }
+}
+
+PrincesAttendant.code = '14029';
+
+module.exports = PrincesAttendant;
