38d1d96866f41f007e0b739a3380c89b4740d659
Brian Upton <ystros@gmail.com>
2019-06-04 10:03:30 -0700

    Implement Unlikely Champion (#2578)

---
+++ server/game/cards/14-FotS/UnlikelyChampion.js
@@ -0,0 +1,15 @@
+const DrawCard = require('../../drawcard');
+
+class UnlikelyChampion extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.controller.anyCardsInPlay(card => card.hasTrait('Lord') || card.hasTrait('Lady')),
+            match: this,
+            effect: ability.effects.addIcon('power')
+        });
+    }
+}
+
+UnlikelyChampion.code = '14040';
+
+module.exports = UnlikelyChampion;
