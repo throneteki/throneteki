bfa4515444979c957aefab0274251dcec0e51d9b
Brian Upton <ystros@gmail.com>
2019-06-04 23:06:19 -0700

    Implement Acolyte of the Flame (#2549)

---
+++ server/game/GameActions/LookAtDeck.js
@@ -0,0 +1,26 @@
+const GameAction = require('./GameAction');
+
+class LookAtDeck extends GameAction {
+    constructor() {
+        super('lookAtHand');
+    }
+
+    canChangeGameState({ lookingAt, amount = 1 }) {
+        return lookingAt.drawDeck.length > 0 && amount > 0;
+    }
+
+    createEvent({ player, lookingAt, context, amount = 1 }) {
+        return this.event('onLookAtDeck', { player, lookingAt, amount }, event => {
+            let topCards = event.lookingAt.drawDeck.slice(0, amount);
+            context.game.promptForSelect(event.player, {
+                activePromptTitle: `Look at ${event.lookingAt.name}'s deck`,
+                source: context.source,
+                revealTargets: true,
+                cardCondition: card => card.location === 'draw deck' && card.controller === event.lookingAt && topCards.includes(card),
+                onSelect: () => true
+            });
+        });
+    }
+}
+
+module.exports = new LookAtDeck();
---
+++ test/server/GameActions/LookAtDeck.spec.js
@@ -0,0 +1,67 @@
+const LookAtDeck = require('../../../server/game/GameActions/LookAtDeck');
+
+describe('LookAtDeck', function() {
+    beforeEach(function() {
+        this.playerSpy = { player: 1 };
+        this.opponentSpy = { player: 2, name: 'opponent', drawDeck: ['card1', 'card2'] };
+        this.gameSpy = jasmine.createSpyObj('game', ['promptForSelect']);
+        this.contextSpy = {
+            game: this.gameSpy,
+            source: 'SOURCE'
+        };
+        this.props = { player: this.playerSpy, lookingAt: this.opponentSpy, amount: 1, context: this.contextSpy };
+    });
+
+    describe('allow()', function() {
+        describe('when the opponent has cards in their deck', function() {
+            beforeEach(function() {
+                this.opponentSpy.drawDeck = ['card1', 'card2'];
+            });
+
+            it('returns true', function() {
+                expect(LookAtDeck.allow(this.props)).toBe(true);
+            });
+        });
+
+        describe('when the opponent has no cards in their deck', function() {
+            beforeEach(function() {
+                this.opponentSpy.drawDeck = [];
+            });
+
+            it('returns false', function() {
+                expect(LookAtDeck.allow(this.props)).toBe(false);
+            });
+        });
+    });
+
+    describe('createEvent()', function() {
+        beforeEach(function() {
+            this.event = LookAtDeck.createEvent(this.props);
+        });
+
+        it('creates a onLookAtDeck event', function() {
+            expect(this.event.name).toBe('onLookAtDeck');
+            expect(this.event.player).toBe(this.playerSpy);
+            expect(this.event.lookingAt).toBe(this.opponentSpy);
+            expect(this.event.amount).toBe(1);
+        });
+
+        describe('the event handler', function() {
+            beforeEach(function() {
+                this.event.executeHandler();
+            });
+
+            it('prompts the player', function() {
+                expect(this.gameSpy.promptForSelect).toHaveBeenCalledWith(this.playerSpy, jasmine.any(Object));
+            });
+
+            it('uses the correct prompt properties', function() {
+                expect(this.gameSpy.promptForSelect).toHaveBeenCalledWith(jasmine.anything(), jasmine.objectContaining({
+                    activePromptTitle: 'Look at opponent\'s deck',
+                    source: 'SOURCE',
+                    revealTargets: true
+                }));
+            });
+        });
+    });
+});
