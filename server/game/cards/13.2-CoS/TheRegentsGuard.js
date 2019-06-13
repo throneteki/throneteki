244d5466bdfb4340b3c5ccce3cd980643bbb0be9
Brian Upton <ystros@gmail.com>
2019-06-12 19:58:02 -0700

    Add proper printed cost limit for Regent Guard

--- server/game/cards/13.2-CoS/TheRegentsGuard.js
+++ server/game/cards/13.2-CoS/TheRegentsGuard.js
@@ -5,7 +5,7 @@ class TheRegentsGuard extends DrawCard {
         this.action({
             title: 'Put into play',
             location: 'shadows',
-            cost: ability.costs.returnToHand(card => card.getType() === 'character' && card.isFaction('Lannister') && !card.hasTrait('Ally')),
+            cost: ability.costs.returnToHand(card => card.getType() === 'character' && card.isFaction('Lannister') && !card.hasTrait('Ally') && card.getPrintedCost() >= 4),
             message: {
                 format: '{player} returns {returnedCard} to hand to put {source} into play from shadows',
                 args: { returnedCard: context => context.costs.returnToHand }
