1f7299623c621e1cc6162a017b187e3dd0e5ffa1
Brian Upton <ystros@gmail.com>
2019-06-18 22:33:23 -0700

    Implement Chiswyck

---
+++ server/game/cards/13.3-PoS/Chiswyck.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard');
+
+class Chiswyck extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                afterChallenge: event => this.isParticipating() && event.challenge.isMatch({ winner: this.controller }) && this.controller.canDraw()
+            },
+            cost: ability.costs.discardFromDeck(),
+            message: {
+                format: '{player} uses {source} and discards {discardFromDeck} to draw 1 card',
+                args: { discardFromDeck: context => context.costs.discardFromDeck }
+            },
+            handler: context => {
+                context.player.drawCardsToHand(1);
+            }
+        });
+    }
+}
+
+Chiswyck.code = '13049';
+
+module.exports = Chiswyck;
---
+++ server/game/costs/DiscardFromDeckCost.js
@@ -0,0 +1,33 @@
+const GameActions = require('../GameActions');
+
+class DiscardFromDeckCost {
+    canPay(context) {
+        return context.player.drawDeck.length > 0;
+    }
+
+    resolve(context, result = { resolved: false }) {
+        let topCard = context.player.drawDeck[0];
+
+        context.addCost('discardFromDeck', topCard);
+
+        result.resolved = true;
+        result.value = topCard;
+        return result;
+    }
+
+    pay(context) {
+        context.game.resolveGameAction(
+            GameActions.discardCard(context => ({ card: context.player.drawDeck[0] })),
+            context
+        );
+    }
+
+    canUnpay() {
+        return false;
+    }
+
+    unpay() {
+    }
+}
+
+module.exports = DiscardFromDeckCost;
