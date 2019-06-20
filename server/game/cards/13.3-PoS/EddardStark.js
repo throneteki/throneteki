0f698ea2936779bb72145e69fd25185689c2e522
Brian Upton <ystros@gmail.com>
2019-06-20 06:49:29 -0700

    Clean up Eddard Stark

--- server/game/cards/13.3-PoS/EddardStark.js
+++ server/game/cards/13.3-PoS/EddardStark.js
@@ -1,24 +1,29 @@
 const DrawCard = require('../../drawcard.js');
 
 class EddardStark extends DrawCard {
-	setupCardAbilities(ability) {
-			this.action({
-				condition: () => this.allCharactersHaveStarkAffiliation()
-				title: 'Kill a character'
-				phase: 'dominance'
-				cost: ability.costs.kneelSelf()
-				target: {
-					cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.getPrintedCost() <= 4
-					gameAction: 'kill'
-				},
-				handler: context => {
-					this.game.addMessage('{0} kneels {1} to kill {2}', this.controller, this, context.target);
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Kill a character',
+            phase: 'dominance',
+            condition: context => this.allCharactersHaveStarkAffiliation(context.player),
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.getPrintedCost() <= 4,
+                gameAction: 'kill'
+            },
+            message: '{player} kneels {source} to kill {target}',
+            handler: context => {
+                this.game.killCharacter(context.target);
+            },
+            limit: ability.limit.perPhase(1)
+        });
+    }
 
-					this.game.killCharacter(context.target);
-				}
-				limit: ability.limit.perPhase(1)
-			});
-	}
+    allCharactersHaveStarkAffiliation(player) {
+        let characters = player.filterCardsInPlay(card => card.getType() === 'character');
+
+        return characters.length > 0 && characters.every(card => card.isFaction('stark'));
+    }
 }
 
 EddardStark.code = '13041';
