639e3d8dcd19ec501d8f992db1294afb09da1635
Brian Upton <ystros@gmail.com>
2019-06-19 22:26:47 -0700

    Implement Shadow of the Throne

---
+++ server/game/cards/13.3-PoS/ShadowOfTheThrone.js
@@ -0,0 +1,34 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class ShadowOfTheThrone extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Kneel locations',
+            target: {
+                maxStat: () => 3,
+                cardStat: card => card.getPrintedCost(),
+                cardCondition: card => card.location === 'play area' && card.getType() === 'location' && !card.kneeled,
+                gameAction: 'kneel'
+            },
+            message: '{player} plays {source} to kneel {target}',
+            handler: context => {
+                let actions = context.target.map(card => GameActions.kneelCard({ card }));
+
+                this.game.resolveGameAction(
+                    GameActions.simultaneously(actions),
+                    context
+                );
+
+                if(this.game.anyPlotHasTrait('Kingdom')) {
+                    this.game.addMessage('{0} uses {1} to return {1} to their hand instead of their discard pile', context.player, this);
+                    context.player.moveCard(this, 'hand');
+                }
+            }
+        });
+    }
+}
+
+ShadowOfTheThrone.code = '13048';
+
+module.exports = ShadowOfTheThrone;
