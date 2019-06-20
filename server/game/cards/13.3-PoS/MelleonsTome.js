1cebcb484dc3740e7d9cd830784bb2214b5561b8
Stuart Walsh <stu@ipng.org.uk>
2019-06-20 14:33:04 +0100

    Implement malleons tome (#2622)
    * Implement malleons tome
    
    * Check for parent and update json data

---
+++ server/game/cards/13.3-PoS/MelleonsTome.js
@@ -0,0 +1,39 @@
+const DrawCard = require('../../drawcard.js');
+const GameActions = require('../../GameActions');
+
+class MalleonsTome extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.addIcon('intrigue')
+        });
+
+        this.reaction({
+            when: {
+                afterChallenge: event =>
+                    event.challenge.attackingPlayer === this.controller &&
+                    event.challenge.winner === this.controller && this.parent &&
+                    this.parent.isAttacking()
+            },
+            handler: context => {
+                let message = '{0} plays {1} to look at {2}\'s hand';
+                let messageArgs = [context.player, this, context.event.challenge.loser];
+
+                this.game.resolveGameAction(
+                    GameActions.lookAtHand({ player: context.player, opponent: context.event.challenge.loser, context })
+                );
+
+                if(this.parent && this.parent.name === 'Eddard Stark' || this.parent.hasTrait('Maester')) {
+                    this.parent.controller.standCard(this.parent);
+                    message += ' and stand {3}';
+                    messageArgs.push(this.parent);
+                }
+
+                this.game.addMessage(message, ...messageArgs);
+            }
+        });
+    }
+}
+
+MalleonsTome.code = '13042';
+
+module.exports = MalleonsTome;
