40383169d2911b26a40234cd094fcdc675493af7
Brian Upton <ystros@gmail.com>
2019-06-21 13:23:40 -0700

    Implement The Dragonpit

---
+++ server/game/cards/13.3-PoS/TheDragonpit.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard');
+
+class TheDragonpit extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            // Add an explicit condition so the effect gets recalculated as the
+            // number of shadow cards change.
+            condition: () => this.game.getOpponents().some(opponent => opponent.shadows.length < this.controller.shadows.length),
+            match: card => card.location === 'play area' && card.getType() === 'character' && this.controllerHasFewerShadows(card),
+            targetController: 'any',
+            effect: ability.effects.modifyStrength(-1)
+        });
+    }
+
+    controllerHasFewerShadows(card) {
+        return card.controller !== this.controller && card.controller.shadows.length < this.controller.shadows.length;
+    }
+}
+
+TheDragonpit.code = '13054';
+
+module.exports = TheDragonpit;
