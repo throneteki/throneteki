579eac4d6becb42f1582bf7eb46f38187645ee89
Brian Upton <ystros@gmail.com>
2019-06-20 20:15:43 -0700

    Implement Outfitted for War

---
+++ server/game/cards/13.3-PoS/OutfittedForWar.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+
+class OutfittedForWar extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ type: 'location', trait: 'Warship' });
+
+        this.action({
+            title: 'Give +3 STR',
+            condition: () => !!this.parent && this.game.isDuringChallenge({ challengeType: 'military' }),
+            cost: ability.costs.kneelParent(),
+            target: {
+                cardCondition: card => card.isParticipating() && card.isFaction('greyjoy') && card.getType() === 'character'
+            },
+            message: '{player} kneels {source} to give {target} +3 STR',
+            handler: context => {
+                this.untilEndOfChallenge(ability => ({
+                    match: context.target,
+                    effect: ability.effects.modifyStrength(3)
+                }));
+            }
+        });
+    }
+}
+
+OutfittedForWar.code = '13052';
+
+module.exports = OutfittedForWar;
