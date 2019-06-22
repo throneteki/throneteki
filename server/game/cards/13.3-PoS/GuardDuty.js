65f2062abd9041db8b57510e4cc2f45c7eea32dc
Stuart Walsh <stu@ipng.org.uk>
2019-06-22 17:46:40 +0100

    implement guard duty (#2629)

---
+++ server/game/cards/13.3-PoS/GuardDuty.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard.js');
+const GameActions = require('../../GameActions');
+
+class GuardDuty extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ faction: 'thenightswatch' });
+        this.reaction({
+            when: {
+                onDeclaredAsDefender: event => this.parent && event.card === this.parent && this.parent.kneeled && this.parent.allowGameAction('stand')
+            },
+            cost: ability.costs.kneelSelf(),
+            message: {
+                format: '{player} uses {source} to stand {parent}',
+                args: { parent: () => this.parent }
+            },
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.standCard(() => ({ card: this.parent })),
+                    context
+                );
+            }
+        });
+    }
+}
+
+GuardDuty.code = '13046';
+
+module.exports = GuardDuty;
