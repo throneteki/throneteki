5fc63e8e4a17461898d8b9cba748dc32efd5725d
Brian Upton <ystros@gmail.com>
2019-06-22 02:19:40 -0700

    Implement A Pit of Snakes (#2631)

---
+++ server/game/cards/13.3-PoS/APitOfSnakes.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard');
+
+class APitOfSnakes extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Place poison token',
+            phase: 'challenge',
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.getNumberOfIcons() === 0
+            },
+            handler: context => {
+                this.atEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.poison
+                }));
+            },
+            max: ability.limit.perPhase(1)
+        });
+    }
+}
+
+APitOfSnakes.code = '13056';
+
+module.exports = APitOfSnakes;
