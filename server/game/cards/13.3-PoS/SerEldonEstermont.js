df3ab0d110f0d499e08b12663ed6394ab4a8ab3c
Brian Upton <ystros@gmail.com>
2019-06-22 02:29:36 -0700

    Implement Ser Eldon Estermont (#2638)

---
+++ server/game/cards/13.3-PoS/SerEldonEstermont.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class SerEldonEstermont extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Stand location',
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'location' && card.isFaction('baratheon') && card.kneeled,
+                gameAction: 'stand'
+            },
+            message: '{player} uses {source} to stand {target}',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.standCard(context => ({ card: context.target })),
+                    context
+                );
+            },
+            limit: ability.limit.perRound(1)
+        });
+    }
+}
+
+SerEldonEstermont.code = '13047';
+
+module.exports = SerEldonEstermont;
