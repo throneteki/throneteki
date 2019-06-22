f506ad98af0c954b4ba1f3c10503312615b1f14b
Stuart Walsh <stu@ipng.org.uk>
2019-06-22 17:47:23 +0100

    implement master mullin (#2635)

---
+++ server/game/cards/13.3-PoS/MaesterMullin.js
@@ -0,0 +1,44 @@
+const DrawCard = require('../../drawcard.js');
+const ChallengeTypes = require('../../ChallengeTypes');
+
+class MaesterMullin extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Kneel to choose a character',
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character'
+            },
+            handler: (context) => {
+                this.selectedCharacter = context.target;
+
+                this.game.promptWithMenu(this.controller, this, {
+                    activePrompt: {
+                        menuTitle: 'Select a challenge type',
+                        buttons: ChallengeTypes.asButtons({ method: 'selectChallengeType' })
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    selectChallengeType(player, challengeType) {
+        this.untilEndOfPhase(ability => ({
+            condition: () => this.game.isDuringChallenge({ challengeType }),
+            match: this.selectedCharacter,
+            effect: ability.effects.cannotBeDeclaredAsAttacker()
+
+        }));
+
+        this.game.addMessage('{0} uses {1} to make {2} unable to be declared as an attacker in {3} challenges this phase',
+            player, this, this.selectedCharacter, challengeType);
+
+        this.selectedCharacter = null;
+
+        return true;
+    }
+}
+
+MaesterMullin.code = '13045';
+
+module.exports = MaesterMullin;
