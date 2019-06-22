7340b71982226e8514d570c7112f3311fe6bd38f
Brian Upton <ystros@gmail.com>
2019-06-22 02:28:36 -0700

    Implement Statue of Baelor (#2637)

---
+++ server/game/cards/13.3-PoS/StatueOfBaelor.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class StatueOfBaelor extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Stand card',
+            cost: [
+                ability.costs.kneelSelf(),
+                ability.costs.discardPower(1, card => card !== this && ['character', 'location'].includes(card.getType()) && card.kneeled && card.allowGameAction('stand'))
+            ],
+            message: {
+                format: '{player} kneels {source} and discards 1 power from {discardPower} to stand {discardPower}',
+                args: { discardPower: context => context.costs.discardPower }
+            },
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.standCard(context => ({ card: context.costs.discardPower })),
+                    context
+                );
+            }
+        });
+    }
+}
+
+StatueOfBaelor.code = '13058';
+
+module.exports = StatueOfBaelor;
