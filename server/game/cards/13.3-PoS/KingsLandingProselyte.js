8f404cf602ad64398a89b39e64c8504c8eb9bf38
Brian Upton <ystros@gmail.com>
2019-06-22 02:24:53 -0700

    Implement King's Landing Proselyte (#2633)

---
+++ server/game/cards/13.3-PoS/KingsLandingProselyte.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard');
+
+class KingsLandingProselyte extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Put into play',
+            location: 'shadows',
+            cost: ability.costs.kill(card => !card.kneeled && card.isFaction('greyjoy')),
+            message: {
+                format: '{player} uses {source} and kills {killed} to put {source} into play from shadows',
+                args: { killed: context => context.costs.kill }
+            },
+            handler: context => {
+                context.player.putIntoPlay(this);
+            }
+        });
+    }
+}
+
+KingsLandingProselyte.code = '13051';
+
+module.exports = KingsLandingProselyte;
