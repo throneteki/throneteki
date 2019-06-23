ffd5c59c7bd8e76c18a358a99bbf0664d7270a7d
EvadLhorg <lukas.a-v@web.de>
2019-06-23 07:39:15 +0200

    implemented Tower Of The Hand (BtRK) (#2642)

---
+++ server/game/cards/13.4-BtRK/TowerOfTheHand.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class TowerOfTheHand extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardOutOfShadows: event => 
+                    event.card.controller === this.controller &&
+                    this.game.anyPlotHasTrait('Winter')
+            },
+            limit: ability.limit.perPhase(1),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.isFaction('stark') && card.kneeled,
+                gameAction: 'stand'
+            },
+            message: '{player} kneels {source} to stand {target}',
+            handler: context => {
+                context.player.standCard(context.target);
+            }
+        });
+    }
+}
+
+TowerOfTheHand.code = '13062';
+
+module.exports = TowerOfTheHand;
