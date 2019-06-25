947ba7f13a0b052ff776fe869a96390a91111a3b
Brian Upton <ystros@gmail.com>
2019-06-24 20:29:32 -0700

    Add test around removing R'hllor for Azor Ahai

--- test/server/cards/14-FotS/AzorAhaiReborn.spec.js
+++ test/server/cards/14-FotS/AzorAhaiReborn.spec.js
@@ -1,5 +1,52 @@
 describe('Azor Ahai Reborn', function() {
     integration(function() {
+        describe('when the only other R\'hllor character is remove from the challenge', function() {
+            beforeEach(function() {
+                const deck = this.buildDeck('baratheon', [
+                    'A Noble Cause',
+                    'Fiery Followers', 'Maester Wendamyr', 'Azor Ahai Reborn', 'Highgarden'
+                ]);
+                this.player1.selectDeck(deck);
+                this.player2.selectDeck(deck);
+                this.startGame();
+                this.keepStartingHands();
+
+                this.rhllor = this.player1.findCardByName('Fiery Followers');
+                this.character = this.player1.findCardByName('Maester Wendamyr');
+                this.attachment = this.player1.findCardByName('Azor Ahai Reborn');
+                this.highgarden = this.player2.findCardByName('Highgarden');
+
+                this.player1.clickCard(this.character);
+                this.player1.clickCard(this.attachment);
+                this.player1.clickCard(this.rhllor);
+                this.player2.clickCard(this.highgarden);
+
+                this.completeSetup();
+
+                // Place attachments
+                this.player1.clickCard(this.attachment);
+                this.player1.clickCard(this.character);
+
+                this.selectFirstPlayer(this.player1);
+
+                this.completeMarshalPhase();
+
+                this.player1.clickPrompt('Intrigue');
+                this.player1.clickCard('Fiery Followers', 'play area');
+                this.player1.clickPrompt('Done');
+
+                this.player1.clickPrompt('Pass');
+            });
+
+            it('removes the attached character from the challenge', function() {
+                // Remove the declared attacker from the challenge
+                this.player2.clickMenu(this.highgarden, 'Remove character from challenge');
+                this.player2.clickCard(this.rhllor);
+
+                expect(this.game.currentChallenge.attackers).toEqual([]);
+            });
+        });
+
         describe('when the character has stealth', function() {
             beforeEach(function() {
                 const deck = this.buildDeck('baratheon', [
