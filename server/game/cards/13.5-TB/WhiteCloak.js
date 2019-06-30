6e65d45834dd1dcfc20d252f5b3f8897fc005b97
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-06-30 22:18:59 +0200

    implement White Cloak

---
+++ server/game/cards/13.5-TB/WhiteCloak.js
@@ -0,0 +1,32 @@
+const DrawCard = require('../../drawcard.js');
+
+class WhiteCloak extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ controller: 'current', trait: ['Knight'] });
+        this.whileAttached({
+            effect: [
+                ability.effects.addTrait('Kingsguard')
+            ]
+        });
+        this.interrupt({
+            canCancel: true,
+            when: {
+                onCharacterKilled: event => event.allowSave && event.card.canBeSaved() && this.isKingOrQueen(event.card)
+            },
+            cost: ability.costs.kneelParent(),
+            handler: context => {
+                let parent = context.cardStateWhenInitiated.parent;
+                context.event.saveCard();
+                this.game.addMessage('{0} kneels {1} to save {2}', this.controller, this, parent);
+            }
+        });
+    }
+
+    isKingOrQueen(card) {
+        return (card.hasTrait('King') || card.hasTrait('Queen'));
+    }
+}
+
+WhiteCloak.code = '13098';
+
+module.exports = WhiteCloak;
