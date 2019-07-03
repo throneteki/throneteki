4d40954f852f9af7917478e1060b7eeadcb8d34e
EvadLhorg <lukas.a-v@web.de>
2019-07-03 17:17:23 +0200

    implement The Mountain´s Skull (#2656)

---
+++ server/game/cards/13.5-TB/TheMountainsSkull.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheMountainsSkull extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: [
+                ability.effects.modifyStrength(2),
+                ability.effects.addKeyword('Renown')
+            ]
+        });
+        this.reaction({
+            location: ['hand', 'discard pile'],
+            when: {
+                onCharacterKilled: event => event.card.controller !== this.controller && event.card.getPrintedStrength() >= 6 && this.controller.canPutIntoPlay(this)
+            },
+            handler: context => {
+                context.player.putIntoPlay(this);
+                this.game.addMessage('{0} uses {1} to put {1} into play.', context.player, this);
+            }
+        });
+    }
+}
+
+TheMountainsSkull.code = '13096';
+
+module.exports = TheMountainsSkull;
