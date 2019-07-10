aa69dfeb790bfc8e25ba3d32390484853a75c2aa
Brian Upton <ystros@gmail.com>
2019-07-09 21:26:24 -0700

    Allow event abilities to trigger from draw deck
    Maester Gormon requires that event cards listen to game events even
    while in the draw deck. The ability system should already prevent event
    card abilities from triggering while the card is not in a playable
    location - Maester Gormon simply makes the top of the deck a playable
    location.

---
+++ test/server/cards/13.3-PoS/MaesterGormon.spec.js
@@ -0,0 +1,41 @@
+describe('Maester Gormon', function() {
+    integration(function() {
+        describe('playing events from top of deck', function() {
+            beforeEach(function() {
+                const deck = this.buildDeck('tyrell', [
+                    'A Noble Cause',
+                    'Maester Gormon', 'The Hand\'s Judgment',
+                    'Margaery Tyrell (Core)', 'Growing Strong'
+                ]);
+
+                this.player1.selectDeck(deck);
+                this.player2.selectDeck(deck);
+                this.startGame();
+                this.keepStartingHands();
+
+                this.topDeckEvent = this.player1.findCardByName('The Hand\'s Judgment');
+                this.opponentChar = this.player2.findCardByName('Margaery Tyrell');
+                this.opponentEvent = this.player2.findCardByName('Growing Strong');
+
+                this.player1.clickCard('Maester Gormon', 'hand');
+                this.player2.clickCard(this.opponentChar);
+
+                this.completeSetup();
+
+                this.selectFirstPlayer(this.player2);
+                this.completeMarshalPhase();
+
+                // Drag the event to the top of the deck
+                this.player1.dragCard(this.topDeckEvent, 'draw deck');
+            });
+
+            it('allows the event to be played', function() {
+                this.player2.clickCard(this.opponentEvent);
+                this.player2.clickCard(this.opponentChar);
+                this.player2.clickPrompt('Done');
+
+                expect(this.player1).toAllowAbilityTrigger(this.topDeckEvent);
+            });
+        });
+    });
+});
