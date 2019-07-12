b34d5f25c0be41c7279ba1faf077bcbdce26e658
weber-knecht <16719940+weber-knecht@users.noreply.github.com>
2019-07-12 22:28:35 +0200

    Implement Yoren (#2662)

---
+++ server/game/cards/13.5-TB/Yoren.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard.js');
+
+class Yoren extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card === this
+            },
+            target: {
+                cardCondition: card => card.getType() === 'character' && card.owner !== this.controller && card.getPrintedCost() <= 5 && card.location === 'play area'
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to take control of {2}', context.player, this, context.target);
+                this.lastingEffect(ability => ({
+                    until: {
+                        onCardLeftPlay: event => event.card === this || event.card === context.target 
+                    },
+                    match: context.target,
+                    effect: ability.effects.takeControl(this.controller)
+                }));            
+            }
+        });
+    }
+}
+
+Yoren.code = '13085';
+
+module.exports = Yoren;
