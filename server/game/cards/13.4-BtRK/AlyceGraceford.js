9f4f055bcf28f0bedae82ed8c400cf71f79e3b88
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-07-13 16:22:04 +0200

    implement Alyce Graceford, card code needs to be fixed

---
+++ server/game/cards/13.4-BtRK/AlyceGraceford.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class AlyceGraceford extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Kneel to stand character',
+            cost: ability.costs.kneelSelf(),
+            target: {
+                activePromptTitle: 'Select another The Seven character',
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.hasTrait('The Seven')
+            },
+            handler: context => {
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.addKeyword('renown')
+                }));
+
+                this.game.addMessage('{0} kneels {1} to give {2} renown until the end of the phase', this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+AlyceGraceford.code = 'TODO';
+
+module.exports = AlyceGraceford;
