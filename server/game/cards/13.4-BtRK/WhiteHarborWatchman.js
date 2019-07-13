8f343d8c9ea8b4315b2e21e3073f911fd58e26da
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-07-13 16:13:22 +0200

    implement White Harbor Watchman, card code needs to be fixed

---
+++ server/game/cards/13.4-BtRK/WhiteHarborWatchman.js
@@ -0,0 +1,20 @@
+const DrawCard = require('../../drawcard');
+
+class WhiteHarborWatchman extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardOutOfShadows: event => event.card.getType() === 'character'
+            },
+            cost: ability.costs.sacrificeSelf(),
+            handler: context => {
+                this.game.killCharacter(context.event.card);
+                this.game.addMessage('{0} sacrifices {1} to kill {2}', this.controller, this, context.event.card);
+            }
+        });
+    }
+}
+
+WhiteHarborWatchman.code = 'TODO';
+
+module.exports = WhiteHarborWatchman;
