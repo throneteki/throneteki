491d1d97fb7e61134234c7bbe0313392b06f6145
EvadLhorg <lukas.a-v@web.de>
2019-07-14 01:44:38 +0200

    implement dragon skull (#2674)

---
+++ server/game/cards/13.4-BtRK/DragonSkull.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard.js');
+
+class DragonSkull extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ faction: 'targaryen' });
+        this.whileAttached({
+            condition: () => this.areNoDragonsInPlay(),
+            effect: [
+                ability.effects.addKeyword('Intimidate'),
+                ability.effects.addKeyword('Renown')
+            ]
+        });   
+    }
+
+    areNoDragonsInPlay() {
+        return !this.game.getPlayers().some(player => {
+            return player.anyCardsInPlay(card => card.getType() === 'character' && card.hasTrait('Dragon'));
+        });
+    }
+}
+
+DragonSkull.code = '13074';
+
+module.exports = DragonSkull;
