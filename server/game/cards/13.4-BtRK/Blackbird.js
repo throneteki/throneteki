7ed56eb59e1d4ff5980d31037db85325ab2b2d6a
EvadLhorg <lukas.a-v@web.de>
2019-07-14 01:35:40 +0200

    implement Blackbird (#2671)

---
+++ server/game/cards/13.4-BtRK/Blackbird.js
@@ -0,0 +1,30 @@
+const DrawCard = require('../../drawcard.js');
+const GameActions = require('../../GameActions');
+
+class Blackbird extends DrawCard {
+    setupCardAbilities() {
+        this.plotModifiers({
+            reserve: 1
+        });
+        this.reaction({
+            when: {
+                onCardOutOfShadows: event => event.card === this
+            },
+            target: {
+                activePromptTitle: 'Select a location',
+                cardCondition: (card) => card.location === 'play area' && card !== this && card.getType() === 'location' && card.isFaction('thenightswatch'),
+                gameAction: 'stand'
+            },
+            message: '{player} uses {source} to stand {target}',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.standCard({ card: context.target })
+                );
+            }
+        });
+    }
+}
+
+Blackbird.code = '13066';
+
+module.exports = Blackbird;
