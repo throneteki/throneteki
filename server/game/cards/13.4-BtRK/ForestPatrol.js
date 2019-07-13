fa490817b116778f17eefbc99ed696ce66e67512
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-07-13 22:10:06 +0200

    implement forest patrol, card code needs to be fixed

---
+++ server/game/cards/13.4-BtRK/ForestPatrol.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard');
+
+class ForestPatrol extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Put into play',
+            location: 'shadows',
+            cost: ability.costs.kneel(card => card.isFaction('thenightswatch') && card.getType() === 'location'),
+            message: {
+                format: '{player} uses {source} and kneels {location} to put {source} into play from shadows',
+                args: { location: context => context.costs.kneel }
+            },
+            handler: context => {
+                context.player.putIntoPlay(this);
+            }
+        });
+    }
+}
+
+ForestPatrol.code = 'TODO';
+
+module.exports = ForestPatrol;
