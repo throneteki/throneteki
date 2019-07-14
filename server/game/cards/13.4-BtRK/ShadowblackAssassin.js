f54fd11f7295a27ed4ca42e736bff7d6e91891dc
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-07-14 12:30:13 +0200

    implement changes after review

--- server/game/cards/13.4-BtRK/ShadowblackAssassin.js
+++ server/game/cards/13.4-BtRK/ShadowblackAssassin.js
@@ -7,9 +7,7 @@ class ShadowblackAssassin extends DrawCard {
                 onCardOutOfShadows: event => event.card === this
             },
             target: {
-                type: 'select',
-                activePromptTitle: 'Select a character with STR 1',
-                cardCondition: card => card.location === 'play area' && card.controller !== this.controller && card.getType() === 'character' && card.getStrength() === 1
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.getStrength() <= 1
             },
             handler: context => {
                 this.game.addMessage('{0} uses {1} to kill {2}', context.player, this, context.target);
