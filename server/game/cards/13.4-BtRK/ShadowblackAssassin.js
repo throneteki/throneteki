d14fbfa281d4510ba9addc136e8dc4ba6e759f26
EvadLhorg <lukas.a-v@web.de>
2019-07-14 20:20:55 +0200

    implement shadowblack assassin (#2673)

---
+++ server/game/cards/13.4-BtRK/ShadowblackAssassin.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard');
+
+class ShadowblackAssassin extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardOutOfShadows: event => event.card === this
+            },
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.getStrength() <= 1
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to kill {2}', context.player, this, context.target);
+                this.game.killCharacter(context.target);
+            }
+        });
+    }
+}
+
+ShadowblackAssassin.code = '13073';
+
+module.exports = ShadowblackAssassin;
