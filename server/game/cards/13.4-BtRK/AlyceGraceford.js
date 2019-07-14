5846e879bbc40818bf692b50ac6d5eac262aa014
EvadLhorg <lukas.a-v@web.de>
2019-07-14 20:25:33 +0200

    implement Alyce Graceford (#2670)

---
+++ server/game/cards/13.4-BtRK/AlyceGraceford.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard.js');
+
+class AlyceGraceford extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Give renown',
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: card => card.location === 'play area' && card.getType() === 'character' && card.hasTrait('The Seven')
+            },
+            handler: context => {
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.addKeyword('renown')
+                }));
+
+                this.game.addMessage('{0} kneels {1} to give {2} renown until the end of the phase', this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+AlyceGraceford.code = '13063';
+
+module.exports = AlyceGraceford;
