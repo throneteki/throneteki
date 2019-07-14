25e1d3cf818b7ce57d4a9dd472ff962899ca66ea
Brian Upton <ystros@gmail.com>
2019-07-13 17:53:57 -0700

    Implement place token game action

---
+++ server/game/GameActions/PlaceToken.js
@@ -0,0 +1,19 @@
+const GameAction = require('./GameAction');
+
+class PlaceToken extends GameAction {
+    constructor() {
+        super('placeToken');
+    }
+
+    canChangeGameState({ card, amount = 1 }) {
+        return ['active plot', 'play area', 'shadows', 'title'].includes(card.location) && amount > 0;
+    }
+
+    createEvent({ card, token, amount = 1 }) {
+        return this.event('onTokenPlaced', { card, token, amount }, event => {
+            event.card.modifyToken(event.token, event.amount);
+        });
+    }
+}
+
+module.exports = new PlaceToken();
