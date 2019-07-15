ec89bec893fef5f4fe3fc0a009a7f7404d73ece0
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-07-15 18:54:02 +0200

    implement sarella sand

---
+++ server/game/cards/13.4-BtRK/SarellaSand.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard.js');
+
+class SarellaSand extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.loser === this.controller && this.isParticipating() && event.challenge.attackingPlayer === this.controller
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to be able to initiate an additional {2} challenge this phase', this.controller, this, context.event.challenge.challengeType);
+                this.untilEndOfPhase(ability => ({
+                    targetController: 'current',
+                    effect: ability.effects.mayInitiateAdditionalChallenge(context.event.challenge.challengeType)
+                }));
+            }
+        });
+    }
+}
+
+SarellaSand.code = '13075';
+
+module.exports = SarellaSand;
