7d41d3d764aa582941d893470942ebc570394120
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-07-16 18:24:59 +0200

    add title fix targeting of untilEndOfPhase effect

--- server/game/cards/13.4-BtRK/KnightsOfTheMind.js
+++ server/game/cards/13.4-BtRK/KnightsOfTheMind.js
@@ -3,10 +3,12 @@ const DrawCard = require('../../drawcard.js');
 class KnightsOfTheMind extends DrawCard {
     setupCardAbilities() {
         this.action({
+            title: 'Give each Maester character you control a military icon',
             handler: () => {
                 this.game.addMessage('{0} plays {1} to give each Maester character a military icon until the end of the phase', this.controller, this);
+                let maesters = this.controller.filterCardsInPlay(card => card.getType() === 'character' && card.hasTrait('Maester'));
                 this.untilEndOfPhase(ability => ({
-                    match: card => card.location === 'play area' && card.getType() === 'character' && card.hasTrait('Maester') && card.controller === this.controller,
+                    match: maesters,
                     effect: ability.effects.addIcon('military')
                 }));
             }
