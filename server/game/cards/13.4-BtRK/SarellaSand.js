e2a71f7c51991a449bfc8b7f4ff6b2bc923e6739
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-07-16 18:18:41 +0200

    add limit for reaction

--- server/game/cards/13.4-BtRK/SarellaSand.js
+++ server/game/cards/13.4-BtRK/SarellaSand.js
@@ -1,11 +1,12 @@
 const DrawCard = require('../../drawcard.js');
 
 class SarellaSand extends DrawCard {
-    setupCardAbilities() {
+    setupCardAbilities(ability) {
         this.reaction({
             when: {
                 afterChallenge: event => event.challenge.loser === this.controller && this.isParticipating() && event.challenge.attackingPlayer === this.controller
             },
+            limit: ability.limit.perPhase(1),
             handler: context => {
                 this.game.addMessage('{0} uses {1} to be able to initiate an additional {2} challenge this phase', this.controller, this, context.event.challenge.challengeType);
                 this.untilEndOfPhase(ability => ({
