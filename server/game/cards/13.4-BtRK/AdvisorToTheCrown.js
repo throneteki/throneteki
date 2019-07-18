48fab2d28e7a67608ead1fdb6a6cbbd7399f54fb
EvadLhorg <lukas.a-v@web.de>
2019-07-18 03:24:09 +0200

    implement advisor to the crown (#2681)

---
+++ server/game/cards/13.4-BtRK/AdvisorToTheCrown.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+
+class AdvisorToTheCrown extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Kneel to reduce',
+            clickToActivate: true,
+            cost: ability.costs.kneelSelf(),
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to reduce the cost of the next Maester character by 1',
+                    this.controller, this);
+                this.untilEndOfPhase(ability => ({
+                    condition: () => !context.abilityDeactivated,
+                    targetController: 'current',
+                    effect: ability.effects.reduceNextMarshalledCardCost(
+                        1,
+                        card => card.getType() === 'character' && card.hasTrait('Maester')
+                    )
+                }));
+            }
+        });
+    }
+}
+
+AdvisorToTheCrown.code = '13077';
+
+module.exports = AdvisorToTheCrown;
