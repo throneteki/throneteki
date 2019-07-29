5c17a73ae892a098a56cecb7349e0f5e951f2e0c
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-07-29 10:10:40 +0200

    implement tyrion lannister

---
+++ server/game/cards/13.5-TB/TyrionLannister.js
@@ -0,0 +1,37 @@
+const DrawCard = require('../../drawcard');
+const {Tokens} = require('../../Constants');
+
+class TyrionLannister extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardOutOfShadows: event => event.card.controller === this.controller 
+                                          && event.card.getType() !== 'event'
+            },
+            limit: ability.limit.perRound(1),
+            target: {
+                cardCondition: card => card.location === 'play area'
+                                    && card.controller === this.controller
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to put a card into shadow', context.player, this);
+                context.player.putIntoShadows(context.target, false, () => {
+                    context.target.modifyToken(Tokens.shadow, 1);
+
+                    if(!context.target.isShadow()) {
+                        this.lastingEffect(ability => ({
+                            condition: () => context.target.location === 'shadows',
+                            targetLocation: 'any',
+                            match: context.target,
+                            effect: ability.effects.addKeyword(`Shadow (${context.target.getPrintedCost()})`)
+                        }));
+                    }
+                });
+            }
+        });
+    }
+}
+
+TyrionLannister.code = '13089';
+
+module.exports = TyrionLannister;
