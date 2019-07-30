49ac42fd9202101d6e19f9933d43dda1423567d7
EvadLhorg <lukas.a-v@web.de>
2019-07-30 22:02:00 +0200

    implement blackwater rush (#2695)

---
+++ server/game/cards/13.5-TB/BlackwaterRush.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../drawcard');
+
+class BlackwaterRush extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardOutOfShadows: event => event.card.controller === this.controller
+            },
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: card => (
+                    card.location === 'play area' &&
+                    card.getType() === 'character' &&
+                    card.isFaction('baratheon') &&
+                    card.canGainPower())
+            },
+            handler: context => {
+                context.target.modifyPower(1);
+
+                this.game.addMessage('{0} kneels {1} to have {2} gain one power.',
+                    context.player, this, context.target);
+            }
+        });
+    }
+}
+
+BlackwaterRush.code = '13088';
+
+module.exports = BlackwaterRush;
