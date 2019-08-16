2760c9dc6743c15a722bfa6dfd319aa3cd0cc782
Brian Upton <ystros@gmail.com>
2019-08-15 21:20:20 -0700

    Implement Smallfolk Mob

---
+++ server/game/cards/13.5-TB/SmallfolkMob.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard');
+
+class SmallfolkMob extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Put into play',
+            location: 'shadows',
+            condition: context => context.player.canPutIntoPlay(this, 'outOfShadows'),
+            cost: ability.costs.sacrifice(card => card.isMatch({ type: 'character', faction: 'stark', unique: true })),
+            message: {
+                format: '{player} sacrifices {sacrificedCard} to put {source} into play from shadows',
+                args: { sacrificedCard: context => context.costs.sacrifice }
+            },
+            handler: context => {
+                context.player.putIntoPlay(this, 'outOfShadows');
+            }
+        });
+    }
+}
+
+SmallfolkMob.code = '13081';
+
+module.exports = SmallfolkMob;
