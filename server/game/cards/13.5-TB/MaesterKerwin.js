d0f3d7c8780a5dce2e6e4edccb4597ee0ad42111
EvadLhorg <lukas.a-v@web.de>
2019-08-22 22:25:49 +0200

    implement maester kerwin (#2719)

---
+++ server/game/cards/13.5-TB/MaesterKerwin.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+
+class MaesterKerwin extends DrawCard {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            canCancel: true,
+            when: {
+                onCharacterKilled: event => event.allowSave && event.card.canBeSaved() && event.card.controller === this.controller && event.card !== this
+            },
+            cost: ability.costs.killSelf(),
+            handler: context => {
+                context.event.saveCard();
+                this.game.addMessage('{0} kills {1} to save {2}', context.player, this, context.event.card);
+            }
+        });
+    }
+}
+
+MaesterKerwin.code = '13091';
+
+module.exports = MaesterKerwin;
