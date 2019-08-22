4de007037f39b36175eb9969bc75cdb39386a9d8
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-08-22 22:06:32 +0200

    implement Sparrows

---
+++ server/game/cards/13.5-TB/Sparrows.js
@@ -0,0 +1,54 @@
+const DrawCard = require('../../drawcard');
+
+class Sparrows extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardOutOfShadows: event => event.card === this && this.opponentHasPower()
+            },
+            target: {
+                cardCondition: card => (
+                    card.location === 'play area' &&
+                    card !== this &&
+                    card.hasTrait('The Seven'))
+            },
+            handler: context => {
+                let opponents = this.game.getOpponents(this.controller).filter(opponent => opponent.faction.power > 0);
+
+                if(opponents.length === 0) {
+                    return;
+                }
+
+                if(opponents.length === 1) {
+                    this.stealPowerFromOpponent(opponents[0], context.target);
+                    return;
+                }
+
+                let factionCards = opponents.map(opponent => opponent.faction);
+
+                this.game.promptForSelect(this.controller, {
+                    activePromptTitle: 'Select a faction card',
+                    cardCondition: card => factionCards.includes(card),
+                    cardType: 'faction',
+                    onSelect: (player, card) => this.stealPowerFromOpponent(card.owner, context.target)
+                });
+            }
+        });
+    }
+
+    stealPowerFromOpponent(opponent, targetCard) {
+        this.game.addMessage('{0} moves 1 power from {1}\'s faction card to {2}',
+            this.controller, opponent, targetCard);
+        this.game.movePower(opponent.faction, targetCard, 1);
+        return true;
+    }
+
+    opponentHasPower() {
+        let opponents = this.game.getOpponents(this.controller);
+        return opponents.some(opponent => opponent.faction.power > 0);
+    }
+}
+
+Sparrows.code = '13097';
+
+module.exports = Sparrows;
