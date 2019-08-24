82141fd1d62c5d0fc2573e49e11f1c5b465b87ef
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-08-24 23:15:24 +0200

    implement mercernary contract

---
+++ server/game/cards/13.6-LMHR/MercenaryContract.js
@@ -0,0 +1,38 @@
+const DrawCard = require('../../drawcard.js');
+const GameActions = require('../../GameActions');
+
+class MercenaryContract extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.addTrait('Mercenary')
+        });
+        
+        this.reaction({
+            when: {
+                afterChallenge: event => (
+                    event.challenge.winner === this.controller &&
+                    event.challenge.isAttacking(this.parent))
+            },
+            cost: [
+                ability.costs.payGold(1),
+                ability.costs.kneelSelf()
+            ],
+            message: {
+                format: '{player} kneels {source} and pays 1 gold to stand {parent}',
+                args: {
+                    parent: context => context.card.parent
+                }
+            },
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.standCard({ card: this.parent }),
+                    context
+                );
+            }
+        });
+    }
+}
+
+MercenaryContract.code = '13114';
+
+module.exports = MercenaryContract;
