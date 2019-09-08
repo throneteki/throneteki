7dc861bae9c1d0765713e350903cbb27f188d0a7
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-09-08 21:08:53 +0200

    implement ser jorah mormont

---
+++ server/game/cards/13.6-LMHR/SerJorahMormont.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard.js');
+
+class SerJorahMormont extends DrawCard {
+    setupCardAbilities() {
+        this.interrupt({
+            when: {
+                onCardDiscarded: event => event.card === this && event.originalLocation === 'hand'
+            },
+            location: ['hand'],
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to put {1} into shadows instead of placing it in their discard pile', this.controller, this);
+                context.event.replaceHandler(() => {
+                    this.controller.putIntoShadows(this);
+                });
+            }
+        });
+    }
+}
+
+SerJorahMormont.code = '13113';
+
+module.exports = SerJorahMormont;
