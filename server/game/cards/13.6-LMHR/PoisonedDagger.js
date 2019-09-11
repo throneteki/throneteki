cb55c106a4febe2c9115629543735ade72004183
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-09-11 21:48:16 +0200

    implement poisoned dagger

---
+++ server/game/cards/13.6-LMHR/PoisonedDagger.js
@@ -0,0 +1,38 @@
+const DrawCard = require('../../drawcard.js');
+
+class PoisonedDagger extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ controller: 'current' });
+        this.whileAttached({
+            effect: ability.effects.modifyStrength(2)
+        });
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && this.parent.isParticipating() && event.challenge.challengeType === 'intrigue'
+            },
+            cost: ability.costs.sacrificeSelf(), 
+            handler: context => {
+                let otherPlayer = context.event.challenge.loser;
+                this.game.promptForSelect(otherPlayer, {
+                    cardCondition: card => (
+                        card.isParticipating() &&
+                        card.getType() === 'character') &&
+                        card.controller === otherPlayer,
+                    activePromptTitle: 'Select character to kill',
+                    source: this,
+                    gameAction: 'kill',
+                    onSelect: (player, card) => {
+                        card.controller.killCharacter(card);
+                        this.game.addMessage('{0} uses {1} to force {2} to kill {3}', this.controller, this, otherPlayer, card);
+
+                        return true;
+                    }
+                });
+            }
+        });
+    }
+}
+
+PoisonedDagger.code = '13116';
+
+module.exports = PoisonedDagger;
