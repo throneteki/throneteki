a35aa00b24bb1b24e6df5683ba9b33f949ff8e9d
EvadLhorg <lukas.a-v@web.de>
2019-09-11 05:46:42 +0200

    implement long may he reign (#2740)

---
+++ server/game/cards/13.6-LMHR/LongMayHeReign.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard.js');
+
+class LongMayHeReign extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ trait: 'King' });
+        this.whileAttached({
+            effect: ability.effects.cannotBeKilled()
+        });
+        this.forcedReaction({
+            when: {
+                afterChallenge: event => event.challenge.loser === this.controller && event.challenge.challengeType === 'power' && event.challenge.defendingPlayer === this.controller
+            },
+            handler: () => {
+                this.game.addMessage('{0} is forced by {1} to sacrifice {1}', this.controller, this);
+                this.controller.sacrificeCard(this);
+            }
+        });
+    }
+}
+
+LongMayHeReign.code = '13108';
+
+module.exports = LongMayHeReign;
