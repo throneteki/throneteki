9502ad1ecf63043f7b319536ef5462376d76f633
EvadLhorg <lukas.a-v@web.de>
2019-09-11 05:48:51 +0200

    implement rimegate (#2741)

---
+++ server/game/cards/13.6-LMHR/Rimegate.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class RimeGate extends DrawCard {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            canCancel: true,
+            when: {
+                onCardDiscarded: event => (
+                    event.card !== this &&
+                    event.card.controller === this.controller && 
+                    event.card.getType() === 'location' && 
+                    event.card.canBeSaved() && 
+                    event.allowSave)
+            },
+            cost: ability.costs.kneelSelf(),
+            handler: context => {
+                context.event.saveCard();
+                this.game.addMessage('{0} kneels {1} to save {2}', context.player, this, context.event.card);
+            }
+        });
+    }
+}
+
+RimeGate.code = '13106';
+
+module.exports = RimeGate;
