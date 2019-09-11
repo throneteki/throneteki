1118439a284879d50009d753cb245652a05f0224
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-09-11 21:20:01 +0200

    implement alchemists guildhall

---
+++ server/game/cards/13.6-LMHR/AlchemistsGuildhall.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard.js');
+
+class AlchemistsGuildhall extends DrawCard {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            canCancel: true,
+            max: ability.limit.perPhase(1),
+            when: {
+                //Restrict triggering on own character abilities to forced triggered abilities
+                onCardAbilityInitiated: event => event.source.getType() === 'character' && event.ability.isTriggeredAbility() &&
+                                                 (event.ability.isForcedAbility() || event.source.controller !== this.controller) &&
+                                                 !event.source.isShadow()
+            },
+            cost: ability.costs.kneelSelf() && ability.costs.putSelfIntoShadows(),
+            handler: context => {
+                context.event.cancel();
+                this.game.addMessage('{0} kneels and returns {1} to shadows to cancel {2}', this.controller, this, context.event.source);
+            }
+        });
+    }
+}
+
+AlchemistsGuildhall.code = '13110';
+
+module.exports = AlchemistsGuildhall;
