7a7d3e9b92a0481189cef85f772e3c94fab9b138
EvadLhorg <lukas.a-v@web.de>
2019-09-12 03:36:30 +0200

    implement the queens retinue (#2744)

---
+++ server/game/cards/13.6-LMHR/TheQueensRetinue.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard');
+
+class TheQueensRetinue extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Put into play',
+            location: 'shadows',
+            handler: context => {
+                context.player.putIntoPlay(this);
+                for(let opponent of this.game.getOpponents(this.controller)) {
+                    if(opponent.canDraw()) {
+                        opponent.drawCardsToHand(2);
+                    }
+                }
+                this.game.addMessage('{0} uses {1} to put {1} into play from shadows and have each opponent draw 2 cards', this.controller, this);
+            }
+        });
+    }
+}
+
+TheQueensRetinue.code = '13103';
+
+module.exports = TheQueensRetinue;
+
