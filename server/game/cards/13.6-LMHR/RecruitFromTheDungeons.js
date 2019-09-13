6abbb06e5db1f48449ece161c1c64f97ff0c8a32
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-09-13 19:34:49 +0200

    fixed the this.game.once statement

--- server/game/cards/13.6-LMHR/RecruitFromTheDungeons.js
+++ server/game/cards/13.6-LMHR/RecruitFromTheDungeons.js
@@ -10,13 +10,13 @@ class RecruitFromTheDungeons extends DrawCard {
                 this.challengeToParticipateIn = this.game.currentChallenge;
                 this.challengeToParticipateIn.addDefender(this);
                 this.game.addMessage('{0} kneels {1} to have {1} participate in the challenge on their side', context.player, this);
-                this.game.once('afterChallenge', this.afterChallenge.bind(this.challengeToParticipateIn, context));
+                this.game.once('afterChallenge', () => this.afterChallenge(this.challengeToParticipateIn, context));
             }
         });
     }
 
     afterChallenge(challenge, context) {
-        if(challenge.looser === context.player) {
+        if(challenge.loser === context.player) {
             context.player.moveCard(this, 'shadows');
             this.game.addMessage('{0} returns {1} to shadows', context.player, this);
         }
