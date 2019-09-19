b2d8862a654078b1369520ce7ec850b01f00a6ba
Brian Upton <ystros@gmail.com>
2019-09-19 07:15:06 -0700

    Implement The Iron Throne (LMHR) (#2738)

---
+++ server/game/cards/13.6-LMHR/TheIronThrone.js
@@ -0,0 +1,38 @@
+const DrawCard = require('../../drawcard');
+
+class TheIronThrone extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onPhaseStarted: () => true
+            },
+            cost: ability.costs.kneelSelf(),
+            handler: () => {
+                this.game.promptWithMenu(this.controller, this, {
+                    activePrompt: {
+                        menuTitle: 'Name a card',
+                        controls: [
+                            { type: 'card-name', command: 'menuButton', method: 'selectCardName' }
+                        ]
+                    }
+                });
+            }
+        });
+    }
+
+    selectCardName(player, cardName) {
+        this.game.addMessage('{0} kneels {1} to name {2}', player, this, cardName);
+        this.untilEndOfPhase(ability => ({
+            effect: [
+                ability.effects.cannotMarshal(card => card.name.toLowerCase() === cardName.toLowerCase()),
+                ability.effects.cannotPlay(card => card.name.toLowerCase() === cardName.toLowerCase()),
+                ability.effects.cannotPutIntoPlay(card => card.name.toLowerCase() === cardName.toLowerCase())
+            ]
+        }));
+        return true;
+    }
+}
+
+TheIronThrone.code = '13117';
+
+module.exports = TheIronThrone;
