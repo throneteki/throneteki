af3106f7f06543f4fdd2b836011fa3cc2573b3bc
Brian Upton <ystros@gmail.com>
2019-10-24 08:00:11 -0700

    Implement Loan from the Iron Bank

---
+++ server/game/cards/15-DotE/LoanFromTheIronBank.js
@@ -0,0 +1,17 @@
+const PlotCard = require('../../plotcard');
+
+class LoanFromTheIronBank extends PlotCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            location: 'any',
+            condition: () => this.location === 'revealed plots',
+            match: card => card === card.controller.activePlot,
+            targetController: 'current',
+            effect: ability.effects.modifyGold(-1)
+        });
+    }
+}
+
+LoanFromTheIronBank.code = '15051';
+
+module.exports = LoanFromTheIronBank;
