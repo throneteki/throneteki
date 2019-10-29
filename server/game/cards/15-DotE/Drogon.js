d68ea5005335b2b120e5fd6d38ea8d716fe55055
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-10-29 21:44:17 +0100

    implement drogon

---
+++ server/game/cards/15-DotE/Drogon.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+
+class Drogon extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: (card) => card.hasTrait('Stormborn'),
+            effect: ability.effects.addKeyword('Renown')
+        });
+
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && this.isAttacking()
+            },
+            handler: context => {
+                let characters = this.game.currentChallenge.defendingPlayer.filterCardsInPlay(card => card.getType() === 'character' && card.getStrength() <= 1);
+                this.game.killCharacters(characters);
+
+                this.game.addMessage('{0} uses {1} to kill all characters {2} controls with STR 1',
+                    context.player, this, this.game.currentChallenge.defendingPlayer);
+            }
+        });
+    }
+}
+
+Drogon.code = '15002';
+
+module.exports = Drogon;
