0aa4f0134c828aec4e7d77be0897a0614d6f39a6
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-10-29 22:20:38 +0100

    implement jhogo

---
+++ server/game/cards/15-DotE/Jhogo.js
@@ -0,0 +1,32 @@
+const DrawCard = require('../../drawcard.js');
+
+class Jhogo extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => 
+                    event.challenge.winner === this.controller && 
+                    this.getNumberOfAttackingDothrakis() >= 2
+            },
+            handler: context => {
+                let otherPlayer = context.event.challenge.loser;
+                this.game.movePower(otherPlayer.faction, this.controller.faction, 1);
+
+                this.game.addMessage('{0} uses {1} to move 1 power from {2}\'s faction to their own',
+                    this.controller, this, otherPlayer);
+            }
+        });
+    }
+
+    getNumberOfAttackingDothrakis() {
+        let cards = this.controller.filterCardsInPlay(card => {
+            return card.isAttacking() && card.hasTrait('Dothraki') && card.getType() === 'character';
+        });
+
+        return cards.length;
+    }
+}
+
+Jhogo.code = '15007';
+
+module.exports = Jhogo;
