508619a0d91fa9845eeeb5dbd25874e04930b493
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-10-29 21:28:03 +0100

    implement rhaegal

---
+++ server/game/cards/15-DotE/Rhaegal.js
@@ -0,0 +1,34 @@
+const DrawCard = require('../../drawcard.js');
+const GameActions = require('../../GameActions');
+
+class Rhaegal extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => this.controller === event.challenge.winner && this.isAttacking()
+            },
+            target: {
+                cardCondition: card =>
+                    card.location === 'play area' &&
+                    card.getType() === 'character' &&
+                    card.kneeled &&
+                    card.controller === this.controller,
+                gameAction: 'stand'
+            },
+            message: '{player} uses {source} to stand {target}',
+            handler: context => {
+                this.game.resolveGameAction(GameActions.standCard({ card: context.target })
+                ).thenExecute(() => {
+                    if(context.target.hasTrait('Dragon') || context.target.hasTrait('Stormborn')) {
+                        this.game.resolveGameAction(GameActions.standCard({ card: this }));
+                        this.game.addMessage('Then {0} stands {1}', context.player, this);
+                    }
+                });
+            }
+        });
+    }
+}
+
+Rhaegal.code = '15003';
+
+module.exports = Rhaegal;
