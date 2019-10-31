68bb6a3d11b48085657c009a895b204519be7164
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-10-31 22:10:50 +0100

    implement blue graces

---
+++ server/game/cards/15-DotE/BlueGraces.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class BlueGraces extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Shuffle card back into deck',
+            phase: 'challenge',
+            cost: ability.costs.removeSelfFromGame(),
+            target: {
+                activePromptTitle: 'Select a card',
+                cardCondition: card => (card.location === 'discard pile' || card.location === 'dead pile') &&
+                                       card.getPrintedCost() >= 6
+            },
+            handler: context => {
+                context.target.owner.moveCard(context.target, 'draw deck');
+                context.target.owner.shuffleDrawDeck();
+                this.game.addMessage('{0} removes {1} from the game to shuffle {2} back into their deck',
+                    this.controller, this, context.target);
+            }
+        });
+    }
+}
+
+BlueGraces.code = '15013';
+
+module.exports = BlueGraces;
