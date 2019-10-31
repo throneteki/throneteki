23551912507cd48424e8023e6b760358f2bd1f94
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-10-31 21:59:03 +0100

    implement barsena blackhair

---
+++ server/game/cards/15-DotE/BarsenaBlackhair.js
@@ -0,0 +1,15 @@
+const DrawCard = require('../../drawcard.js');
+
+class BarsenaBlackhair extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.controller.anyCardsInPlay(card => card.parent === this && card.getType() === 'attachment' && card.hasTrait('Weapon')),
+            match: this,
+            effect: ability.effects.modifyStrength(3)
+        });
+    }
+}
+
+BarsenaBlackhair.code = '15011';
+
+module.exports = BarsenaBlackhair;
