d24de7d109b9c1a005f214373c7fb51e9633cc04
EvadLhorg <lukas.a-v@web.de>
2019-11-01 04:10:15 +0100

    implement rakharo (#2795)

---
+++ server/game/cards/15-DotE/Rakharo.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../drawcard.js');
+
+class Rakharo extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && event.challenge.challengeType === 'military' &&
+                                         this.getNumberOfAttackingDothrakis() >= 2
+            },
+            handler: () => {
+                let loser = this.game.currentChallenge.loser;
+                loser.discardAtRandom(1);
+
+                this.game.addMessage('{0} uses {1} to discard 1 card at random from {2}\'s hand',
+                    this.controller, this, loser);
+            }
+        });
+    }
+
+    getNumberOfAttackingDothrakis() {
+        let cards = this.controller.filterCardsInPlay(card => {
+            return card.isAttacking() && card.hasTrait('Dothraki') && card.getType() === 'character';
+        });
+
+        return cards.length;
+    }
+}
+
+Rakharo.code = '15009';
+
+module.exports = Rakharo;
