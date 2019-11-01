80acba6a68cbb0ca89ca5bf91468b5afa295be9b
EvadLhorg <lukas.a-v@web.de>
2019-11-01 03:34:07 +0100

    implement the titans bastard (#2796)

---
+++ server/game/cards/15-DotE/TheTitansBastard.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheTitansBastard extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Return a Mercenary',
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: (card, context) => (
+                    card.location === 'discard pile' &&
+                    card.controller === context.player &&
+                    card.hasTrait('Mercenary') &&
+                    card.getType() === 'character')
+            },
+            handler: context => {
+                context.player.moveCard(context.target, 'hand');
+                this.game.addMessage('{0} kneels {1} to move {2} from their discard pile to their hand',
+                    context.player, this, context.target);
+            }
+        });
+    }
+}
+
+TheTitansBastard.code = '15010';
+
+module.exports = TheTitansBastard;
