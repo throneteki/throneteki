f64d805396df04285ad5aac3a56de3ff37afd0ea
EvadLhorg <lukas.a-v@web.de>
2019-11-02 22:59:41 +0100

    implement brown ben plumm (#2801)

---
+++ server/game/cards/15-DotE/BrownBenPlumm.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class BrownBenPlumm extends DrawCard {
+    setupCardAbilities() {
+        this.forcedInterrupt({
+            when: {
+                onPhaseEnded: event => event.phase === 'challenge' && this.controller.gold === 0
+            },
+            message: '{player} is forced to sacrifice {source} because they have no gold left in their gold pool',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.sacrificeCard({ card: this }),
+                    context
+                );
+            }
+        });
+    }
+}
+
+BrownBenPlumm.code = '15016';
+
+module.exports = BrownBenPlumm;
