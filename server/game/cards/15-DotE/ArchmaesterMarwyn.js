a4bf130a906950512197854aa90981979ff2815d
Brian Upton <ystros@gmail.com>
2019-11-03 11:23:04 -0800

    Implement Archmaester Marwyn (#2793)

---
+++ server/game/cards/15-DotE/ArchmaesterMarwyn.js
@@ -0,0 +1,50 @@
+const DrawCard = require('../../drawcard');
+const GenericTracker = require('../../EventTrackers/GenericTracker');
+const CardEntersPlayTracker = require('../../EventTrackers/CardEntersPlayTracker');
+
+class ArchmaesterMarwyn extends DrawCard {
+    setupCardAbilities(ability) {
+        this.enterPlayTracker = CardEntersPlayTracker.forPhase(this.game);
+        this.playedTracker = GenericTracker.forPhase(this.game, 'onCardPlayed');
+
+        this.persistentEffect({
+            condition: () => !this.hasPlayedFromUnderAgenda() && !this.hasMarshalledFromUnderAgenda(),
+            targetController: 'current',
+            effect: [
+                ability.effects.canMarshal(card => card.location === 'conclave'),
+                ability.effects.canPlay(card => card.location === 'conclave')
+            ]
+        });
+
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card === this && this.controller.agenda
+            },
+            message: '{player} uses {source} to put top 2 cards of their deck under their agenda',
+            handler: context => {
+                const topCards = context.player.drawDeck.slice(0, 2);
+                for(const card of topCards) {
+                    context.player.moveCard(card, 'conclave');
+                }
+            }
+        });
+    }
+
+    hasPlayedFromUnderAgenda() {
+        return this.playedTracker.events.some(event => (
+            event.originalLocation === 'conclave' &&
+            event.player === this.controller
+        ));
+    }
+
+    hasMarshalledFromUnderAgenda() {
+        return this.enterPlayTracker.events.some(event => (
+            event.originalLocation === 'conclave' &&
+            event.card.controller === this.controller
+        ));
+    }
+}
+
+ArchmaesterMarwyn.code = '15041';
+
+module.exports = ArchmaesterMarwyn;
