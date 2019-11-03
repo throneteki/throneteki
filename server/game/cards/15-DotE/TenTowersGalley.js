eaa9437604ff87549c03caed39a668d522a1e63a
EvadLhorg <lukas.a-v@web.de>
2019-11-03 21:17:38 +0100

    implement ten towers galley (#2810)

---
+++ server/game/cards/15-DotE/TenTowersGalley.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+
+class TenTowersGalley extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => !this.kneeled,
+            match: (card) => card === card.controller.activePlot,
+            targetController: 'opponent',
+            effect: ability.effects.modifyReserve(-1)
+        });
+        
+        this.action({
+            title: 'Kneel Ten Towers Galley',
+            condition: () => !this.kneeled,
+            anyPlayer: true,
+            cost: ability.costs.kneel(card => card.getType() === 'location' && card.getPrintedCost() >= 2),
+            handler: context => {
+                this.game.addMessage('{0} kneels {1} to kneel {2}', context.player, context.costs.kneel, this);
+                this.controller.kneelCard(this);
+            }
+        });
+    }
+}
+
+TenTowersGalley.code = '15028';
+
+module.exports = TenTowersGalley;
