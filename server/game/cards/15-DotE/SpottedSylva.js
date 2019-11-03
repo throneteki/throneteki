e65a365a395f65960fe2d08ac53986dfc2af53e9
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-11-03 21:05:42 +0100

    implement spotted sylva

---
+++ server/game/cards/15-DotE/SpottedSylva.js
@@ -0,0 +1,33 @@
+const DrawCard = require('../../drawcard.js');
+const GameActions = require('../../GameActions');
+
+class SpottedSylva extends DrawCard {
+    setupCardAbilities() {
+        this.forcedReaction({
+            when: {
+                afterChallenge: event => event.challenge.loser === this.controller && this.isAttacking()
+            },
+            target: {
+                choosingPlayer: 'each',
+                ifAble: true,
+                cardCondition: (card, context) => 
+                    card.location === 'play area' &&
+                    card.controller === context.choosingPlayer &&
+                    card.getType() === 'character' &&
+                    card.isParticipating()
+            },
+            handler: context => {
+                let selections = context.targets.selections.filter(selection => !!selection.value);
+                this.game.resolveGameAction(
+                    GameActions.simultaneously(
+                        selections.map(selection => GameActions.returnCardToHand({ player: selection.choosingPlayer, card: selection.value }))
+                    )
+                );
+            }
+        });
+    }
+}
+
+SpottedSylva.code = '15031';
+
+module.exports = SpottedSylva;
