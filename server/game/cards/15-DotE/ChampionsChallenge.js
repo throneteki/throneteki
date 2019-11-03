9587e39e0b576785ebbd800cfe37e631a964a41c
Brian Upton <ystros@gmail.com>
2019-11-03 11:19:43 -0800

    Implement Champion's Challenge (#2781)
    * Implement Champion's Challenge
    
    * Fix message

---
+++ server/game/cards/15-DotE/ChampionsChallenge.js
@@ -0,0 +1,30 @@
+const PlotCard = require('../../plotcard');
+
+class ChampionsChallenge extends PlotCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onChallengeInitiated: event => event.challenge.defendingPlayer === this.controller
+            },
+            cost: ability.costs.kneel((card, context) => card.getType() === 'character' && card.getStrength() === this.highestStrength(context.player)),
+            message: {
+                format: '{player} uses {source} and kneels {kneeledCard} to end the current challenge',
+                args: { kneeledCard: context => context.costs.kneel }
+            },
+            handler: context => {
+                context.event.challenge.cancelChallenge();
+            }
+        });
+    }
+
+    highestStrength(player) {
+        let charactersInPlay = player.filterCardsInPlay(card => card.getType() === 'character');
+        let strengths = charactersInPlay.map(card => card.getStrength());
+        return Math.max(...strengths);
+    }
+}
+
+ChampionsChallenge.code = '15049';
+
+module.exports = ChampionsChallenge;
+
