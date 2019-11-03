47ac01117e12adcb1deaa502404fdb73d6014ec8
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-11-03 21:24:26 +0100

    implement choosing the spear

---
+++ server/game/cards/15-DotE/ChoosingTheSpear.js
@@ -0,0 +1,45 @@
+const DrawCard = require('../../drawcard');
+
+class ChoosingTheSpear extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onDeclaredAsAttacker: event => 
+                    event.card.controller === this.controller && 
+                    event.card.isFaction('martell') && 
+                    event.challenge.challengeType === 'military' &&
+                    event.card.getType() === 'character'
+            },
+            handler: context => {
+                this.untilEndOfChallenge(ability => ({
+                    match: card => card === this.controller.activePlot,
+                    effect: ability.effects.modifyClaim(1)
+                }));
+                this.game.addMessage('{0} plays {1} to raise the claim value on their revealed plot card by 1 until the end of the challenge',
+                    this.controller, this);
+                this.game.once('afterChallenge', () => this.afterChallenge(this.game.currentChallenge, context));
+            }
+        });
+    }
+
+    afterChallenge(challenge, context) {
+        if(challenge.loser === context.player) {
+            this.game.promptForSelect(context.player, {
+                source: this,
+                cardCondition: card => card.location === 'play area' && card.controller === context.player && card.getType() === 'character',
+                gameAction: 'kill',
+                onSelect: (p, card) => this.onCardSelected(p, card)
+            });
+        }
+    }
+
+    onCardSelected(player, card) {
+        card.owner.killCharacter(card);
+        this.game.addMessage('{0} forces {1} to kill {2}', this, player, card);
+        return true;
+    }
+}
+
+ChoosingTheSpear.code = '15032';
+
+module.exports = ChoosingTheSpear;
