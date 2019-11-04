4432288d3c0a3109a7143d0b9bc764706f49a8aa
Brian Upton <ystros@gmail.com>
2019-11-03 18:17:56 -0800

    Remove unneeded check from when + fix limit

--- server/game/cards/15-DotE/WyllaManderly.js
+++ server/game/cards/15-DotE/WyllaManderly.js
@@ -4,23 +4,18 @@ class WyllaManderly extends DrawCard {
     setupCardAbilities(ability) {
         this.reaction({
             when: {
-                onCharacterKilled: event => event.card.controller === this.controller && this.canChangeGameState(event.card),
-                onSacrificed: event => event.card.controller === this.controller && this.canChangeGameState(event.card)
+                onCharacterKilled: event => event.card.controller === this.controller,
+                onSacrificed: event => event.card.controller === this.controller
             },
-            limit: ability.limit.perRound(1),
             target: {
                 cardCondition: (card, context) => card.location === 'discard pile' && card.controller === this.controller && card !== context.event.card
             },
+            message: '{player} uses {source} to move {target} to the bottom of their deck',
             handler: context => {
                 this.game.placeOnBottomOfDeck(context.target);
-                this.game.addMessage('{0} uses {1} to move {2} to the bottom of their deck', this.controller, this, context.target);
-            }
+            },
+            limit: ability.limit.perPhase(1)
         });
-    }
-
-    canChangeGameState(cardSacrificedOrKilled) {
-        let cards = this.controller.discardPile.filter(card => card !== cardSacrificedOrKilled);
-        return cards.length > 0;
     }
 }
 
