f6f8468ea9e576dfb65cb3ec335b0858607d3ab1
EvadLhorg <lukas.a-v@web.de>
2019-11-04 03:19:15 +0100

    implement clydas (#2815)

---
+++ server/game/cards/15-DotE/Clydas.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard.js');
+
+class Clydas extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Move 1 gold to your gold pool',
+            chooseOpponent: opponent => opponent.gold > 0,
+            message: '{player} uses {source} to move 1 gold from {opponent}\'s gold pool to their own',
+            handler: context => {
+                this.game.transferGold({ from: context.opponent, to: context.player, amount: 1 });
+            },
+            limit: ability.limit.perPhase(1)
+        });
+    }
+}
+
+Clydas.code = '15033';
+
+module.exports = Clydas;
