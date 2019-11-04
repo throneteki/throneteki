014dd37d63e9c44a9cabb8a11978f77e363d756a
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-11-04 15:55:00 +0100

    implement braavos

---
+++ server/game/cards/15-DotE/Braavos.js
@@ -0,0 +1,37 @@
+const DrawCard = require('../../drawcard.js');
+
+class Braavos extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Gain gold',
+            phase: 'marshal',
+            condition: () => this.controller.canGainGold(),
+            cost: ability.costs.kneelSelf(),
+            handler: () => {
+                let gold = this.getNumberOfFactions();
+                this.game.addGold(this.controller, gold);
+                this.game.addMessage('{0} kneels {1} to gain {2} gold', this.controller, this, gold);
+            }
+        });
+    }
+
+    getNumberOfFactions() {
+        let charactersInPlay = this.controller.filterCardsInPlay(card => card.getType() === 'character' && !card.isFaction('neutral'));
+        let factionsInPlay = [];
+
+        for(let card of charactersInPlay) {
+            let factions = card.getFactions();
+            for(let faction of factions) {
+                if(!factionsInPlay.includes(faction)) {
+                    factionsInPlay.push(faction);
+                }
+            }
+        }
+
+        return factionsInPlay.length;
+    }
+}
+
+Braavos.code = '15042';
+
+module.exports = Braavos;
