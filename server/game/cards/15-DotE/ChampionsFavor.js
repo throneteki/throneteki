4cd01970ee9b0195f56a20bcbb3922850b0a769e
EvadLhorg <EvadLhorg@users.noreply.github.com>
2019-11-04 15:05:29 +0100

    implement champions favor

---
+++ server/game/cards/15-DotE/ChampionsFavor.js
@@ -0,0 +1,32 @@
+const DrawCard = require('../../drawcard.js');
+
+class ChampionsFavor extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ trait: 'Lady' });
+        this.action({
+            title: 'Give STR',
+            condition: () => this.isNonLadyParticipating(),
+            cost: ability.costs.kneelParent(),
+            target: {
+                cardCondition: card => card.isParticipating() && !card.hasTrait('Lady') && card.getType() === 'character'
+            },
+            handler: context => {
+                let str = this.parent.getStrength() + 2;
+                this.game.addMessage('{0} uses {1} and kneels {2} to give {3} +{4} STR until the end of the challenge',
+                    this.controller, this, this.parent, context.target, str);
+                this.untilEndOfChallenge(ability => ({
+                    match: context.target,
+                    effect: ability.effects.modifyStrength(str)
+                }));
+            }
+        });
+    }
+
+    isNonLadyParticipating() {
+        return this.game.anyCardsInPlay(card => card.isParticipating() && !card.hasTrait('Lady') && card.getType() === 'character');
+    }
+}
+
+ChampionsFavor.code = '15038';
+
+module.exports = ChampionsFavor;
