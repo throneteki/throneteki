581f9f726c9e510bc950ddde38ca62c40601f9b7
EvadLhorg <lukas.a-v@web.de>
2019-11-07 03:56:54 +0100

    implement dragon egg without search effect (#2804)

---
+++ server/game/cards/15-DotE/DragonEgg.js
@@ -0,0 +1,36 @@
+const DrawCard = require('../../drawcard.js');
+const GameActions = require('../../GameActions');
+
+class DragonEgg extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.addKeyword('Insight')
+        });
+
+        this.reaction({
+            when: {
+                onPhaseStarted: event => event.phase === 'marshal' && this.parent.name === 'Daenerys Targaryen'
+            },
+            cost: ability.costs.sacrificeSelf(),
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.search({
+                        title: 'Select a character',
+                        match: { type: 'character', trait: 'Hatchling' },
+                        message: '{player} uses {source} to search their deck and put {searchTarget} into play',
+                        cancelMessage: '{player} uses {source} to search their deck but does not find a card',
+                        gameAction: GameActions.putIntoPlay(context => ({
+                            player: context.player,
+                            card: context.searchTarget
+                        }))
+                    }),
+                    context
+                );
+            }
+        });
+    }
+}
+
+DragonEgg.code = '15020';
+
+module.exports = DragonEgg;
