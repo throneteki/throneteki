a078d4c1fbd05d1301c845a866d0a4574b3a85d8
EvadLhorg <lukas.a-v@web.de>
2019-11-07 03:47:11 +0100

    implement water dancers sword (#2828)

---
+++ server/game/cards/15-DotE/WaterDancersSword.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class WaterDancersSword extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ printedCostOrLower: 3 });
+        this.whileAttached({
+            effect: ability.effects.modifyStrength(2)
+        });
+        this.forcedInterrupt({
+            when: {
+                onPhaseEnded: event => event.phase === 'challenge'
+            },
+            message: '{player} is forced to return {source} to their hand',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.returnCardToHand(context => ({ card: context.source })),
+                    context
+                );
+            }
+        });
+    }
+}
+
+WaterDancersSword.code = '15043';
+
+module.exports = WaterDancersSword;
