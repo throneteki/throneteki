324d00c17b10ee7d8156cfc934a728bf905e74b8
EvadLhorg <lukas.a-v@web.de>
2019-11-10 20:10:07 +0100

    implement lay waste (#2829)

---
+++ server/game/cards/15-DotE/LayWaste.js
@@ -0,0 +1,59 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class LayWaste extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Discard location or attachment',
+            phase: 'dominance',
+            target: {
+                activePromptTitle: 'Select a non-limited location or attachment',
+                cardCondition: card => card.isMatch({
+                    location: 'play area',
+                    type: ['attachment', 'location'],
+                    limited: false
+                }),
+                gameAction: 'discard'
+            },
+            message: '{player} plays {source} to discard {target} from play',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.discardCard(context => ({
+                        card: context.target
+                    })).then({
+                        handler: thenContext => {
+                            this.game.resolveGameAction(
+                                GameActions.search({
+                                    player: thenContext => thenContext.parentContext.target.owner,
+                                    title: 'Select a card',
+                                    match: {
+                                        type: ['attachment', 'location'],
+                                        printedCostOrLower: context.target.getPrintedCost() - 1
+                                    },
+                                    message: {
+                                        format: 'Then {targetOwner} uses {source} to search their deck and put {searchTarget} into play',
+                                        args: { targetOwner: thenContext => thenContext.parentContext.target.owner }
+                                    },
+                                    cancelMessage: {
+                                        format: 'Then {targetOwner} uses {source} to search their deck but does not find a card',
+                                        args: { targetOwner: thenContext => thenContext.parentContext.target.owner }
+                                    },
+                                    gameAction: GameActions.putIntoPlay(thenContext => ({
+                                        player: thenContext.player,
+                                        card: thenContext.searchTarget
+                                    }))
+                                }),
+                                thenContext
+                            );
+                        }
+                    }),
+                    context
+                );
+            }
+        });
+    }
+}
+
+LayWaste.code = '15044';
+
+module.exports = LayWaste;
