397726740a6983fc100ea4e24413c4fbe72d473b
Brian Upton <ystros@gmail.com>
2019-11-09 17:53:01 -0800

    Add cancellation message + add challenge to event

--- server/game/cards/15-DotE/ChoosingTheSpear.js
+++ server/game/cards/15-DotE/ChoosingTheSpear.js
@@ -5,9 +5,9 @@ class ChoosingTheSpear extends DrawCard {
         this.reaction({
             max: ability.limit.perChallenge(1),
             when: {
-                onDeclaredAsAttacker: event => 
-                    event.card.controller === this.controller && 
-                    event.card.isFaction('martell') && 
+                onDeclaredAsAttacker: event =>
+                    event.card.controller === this.controller &&
+                    event.card.isFaction('martell') &&
                     event.challenge.challengeType === 'military' &&
                     event.card.getType() === 'character'
             },
@@ -29,14 +29,20 @@ class ChoosingTheSpear extends DrawCard {
                 source: this,
                 cardCondition: card => card.location === 'play area' && card.controller === context.player && card.getType() === 'character',
                 gameAction: 'kill',
-                onSelect: (p, card) => this.onCardSelected(p, card)
+                onSelect: (p, card) => this.handleCardSelected(p, card),
+                onCancel: (player) => this.handleCancelled(player)
             });
         }
     }
 
-    onCardSelected(player, card) {
+    handleCardSelected(player, card) {
         card.owner.killCharacter(card);
         this.game.addMessage('{0} forces {1} to kill {2}', this, player, card);
+        return true;
+    }
+
+    handleCancelled(player) {
+        this.game.addAlert('danger', '{0} cancels the resolution of {1}', player, this);
         return true;
     }
 }
