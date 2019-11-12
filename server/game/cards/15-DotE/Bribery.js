15a92740f74e15bbf1b9e2bb24a4d18ef5f7b70c
Brian Upton <ystros@gmail.com>
2019-11-11 21:32:48 -0800

    Add min cost, ensure not kneeled, improve message

--- server/game/cards/15-DotE/Bribery.js
+++ server/game/cards/15-DotE/Bribery.js
@@ -4,20 +4,23 @@ const GameActions = require('../../GameActions');
 class Bribery extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
-            cost: ability.costs.payXGold(() => 0, () => 99),
+            cost: ability.costs.payXGold(() => this.getMinXValue(), () => 99),
             target: {
-                cardCondition: (card, context) => card.location === 'play area' && card.getType() === 'character' && card.getPrintedCost() <= context.xValue,
+                cardCondition: (card, context) => (
+                    card.isMatch({ location: 'play area', type: 'character', kneeled: false }) &&
+                    (!context.xValue || card.getPrintedCost() <= context.xValue)
+                ),
                 gameAction: 'kneel'
             },
             handler: context => {
-                this.targetCharacter = context.target;
-                if(this.targetCharacter.hasTrait('Ally') || this.targetCharacter.hasTrait('Mercenary')) {
+                this.context = context;
+                if(context.target.hasTrait('Ally') || context.target.hasTrait('Mercenary')) {
                     let buttons = [
                         { text: 'Kneel', method: 'kneelCharacter' },
                         { text: 'Take control', method: 'takeControl' }
                     ];
-            
-                    this.game.promptWithMenu(this.controller, this, {
+
+                    this.game.promptWithMenu(context.player, this, {
                         activePrompt: {
                             menuTitle: 'Take control of character?',
                             buttons: buttons
@@ -25,38 +28,34 @@ class Bribery extends DrawCard {
                         source: this
                     });
                 } else {
-                    this.kneelCharacter(this.controller);
+                    this.kneelCharacter(context.player);
                 }
             }
         });
     }
 
-    kneelCharacter(player) {
-        if(!this.targetCharacter) {
-            return false;
-        }
+    getMinXValue() {
+        const characters = this.game.filterCardsInPlay(card => card.isMatch({ type: 'character', kneeled: false }));
+        const costs = characters.map(card => card.getPrintedCost());
+        return Math.min(...costs);
+    }
 
+    kneelCharacter(player) {
         this.game.resolveGameAction(
-            GameActions.kneelCard({ card: this.targetCharacter })
+            GameActions.kneelCard({ card: this.context.target })
         );
 
-        this.game.addMessage('{0} uses {1} to kneel {2}',
-            player, this, this.targetCharacter);
-        this.targetCharacter = null;
+        this.game.addMessage('{0} plays {1} and pays {2} gold to kneel {3}',
+            player, this, this.context.xValue, this.context.target);
 
         return true;
     }
 
     takeControl(player) {
-        if(!this.targetCharacter) {
-            return false;
-        }
+        this.game.takeControl(player, this.context.target);
 
-        this.game.takeControl(player, this.targetCharacter);
-
-        this.game.addMessage('{0} uses {1} to take control of {2}',
-            player, this, this.targetCharacter);
-        this.targetCharacter = null;
+        this.game.addMessage('{0} plays {1} and pays {2} gold to take control of {3}',
+            player, this, this.context.xValue, this.context.target);
 
         return true;
     }
