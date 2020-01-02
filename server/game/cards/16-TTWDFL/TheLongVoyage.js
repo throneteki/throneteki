64f9f39ac22bb3edcfdf1fb53db04db2c7560549
Brian Upton <ystros@gmail.com>
2020-01-01 23:27:34 -0600

    Implement The Long Voyage

---
+++ server/game/cards/16-TTWDFL/TheLongVoyage.js
@@ -0,0 +1,22 @@
+const AgendaCard = require('../../agendacard');
+const GameActions = require('../../GameActions');
+
+class TheLongVoyage extends AgendaCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardsDrawn: event => event.player === this.controller && event.reason === 'drawPhase'
+            },
+            cost: ability.costs.kneelFactionCard(),
+            message: '{player} uses {source} and kneels their faction card to draw 1 card',
+            gameAction: GameActions.drawCards(context => ({
+                amount: 1,
+                player: context.player
+            }))
+        });
+    }
+}
+
+TheLongVoyage.code = '16030';
+
+module.exports = TheLongVoyage;
