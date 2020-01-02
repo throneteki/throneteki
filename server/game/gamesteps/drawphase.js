7357dbdbc7536dd77186160bf91404bb72d62674
Brian Upton <ystros@gmail.com>
2020-01-01 23:25:48 -0600

    Add reason to drawing cards in draw phase
    The Long Voyage only reacts to cards drawn by the draw phase
    framework action. So a reason needs to be specified during that
    draw.

--- server/game/gamesteps/drawphase.js
+++ server/game/gamesteps/drawphase.js
@@ -1,6 +1,7 @@
 const Phase = require('./phase.js');
 const SimpleStep = require('./simplestep.js');
 const ActionWindow = require('./actionwindow.js');
+const GameActions = require('../GameActions');
 
 class DrawPhase extends Phase {
     constructor(game) {
@@ -12,12 +13,22 @@ class DrawPhase extends Phase {
     }
 
     draw() {
-        for(let player of this.game.getPlayers()) {
-            if(player.canDraw()) {
-                let cards = player.drawCardsToHand(player.drawPhaseCards);
-                this.game.addMessage('{0} draws {1} cards', player, cards.length);
-            }
-        }
+        const players = this.game.getPlayers();
+        const actions = players.map(player => this.createAction(player));
+
+        this.game.resolveGameAction(
+            GameActions.simultaneously(actions)
+        );
+    }
+
+    createAction(player) {
+        return GameActions.drawCards({
+            amount: player.drawPhaseCards,
+            player,
+            reason: 'drawPhase'
+        }).thenExecute(event => {
+            this.game.addMessage('{0} draws {1} cards', event.player, event.amount);
+        });
     }
 }
 
