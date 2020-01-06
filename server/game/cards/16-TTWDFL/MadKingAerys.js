f4217de690cdf7f90b9f035a46217fc86294e804
Brian Upton <ystros@gmail.com>
2020-01-06 02:40:07 -0600

    Implement Mad King Aerys (#2884)
    * Implement Mad King Aerys

---
+++ server/game/cards/16-TTWDFL/MadKingAerys.js
@@ -0,0 +1,37 @@
+const DrawCard = require('../../drawcard');
+
+class MadKingAerys extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.opponentControlsKingslayer(),
+            match: this,
+            effect: ability.effects.cannotBeKilled()
+        });
+
+        this.action({
+            title: 'Kill character',
+            phase: 'dominance',
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: card => card.isMatch({ location: 'play area', trait: ['Lord', 'Lady'] }),
+                gameAction: 'kill'
+            },
+            message: '{player} kneels {source} to kill {target}',
+            handler: context => {
+                this.game.killCharacter(context.target);
+            },
+            limit: ability.limit.perRound(1)
+        });
+    }
+
+    opponentControlsKingslayer() {
+        return this.game.anyCardsInPlay(card => (
+            card.controller !== this.controller &&
+            (card.hasTrait('Lord') || card.hasTrait('Lady') || card.name === 'Ser Jaime Lannister')
+        ));
+    }
+}
+
+MadKingAerys.code = '16013';
+
+module.exports = MadKingAerys;
