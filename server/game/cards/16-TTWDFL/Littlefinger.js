8749ecd44445fa30f03d947f636f1f9f29fd5b41
Brian Upton <ystros@gmail.com>
2020-01-06 02:39:15 -0600

    Implement Littlefinger (TTWDFL) (#2887)

---
+++ server/game/cards/16-TTWDFL/Littlefinger.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../drawcard');
+
+class Littlefinger extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            effect: ability.effects.doesNotKneelAsAttacker()
+        });
+
+        this.interrupt({
+            canCancel: true,
+            when: {
+                onCardAbilityInitiated: event => event.source.getType() === 'event' && event.player !== this.controller
+            },
+            cost: ability.costs.kneelSelf(),
+            message: {
+                format: '{player} kneels {source} to cancel {event}',
+                args: { event: context => context.event.source }
+            },
+            handler: context => {
+                context.event.cancel();
+            }
+        });
+    }
+}
+
+Littlefinger.code = '16017';
+
+module.exports = Littlefinger;
