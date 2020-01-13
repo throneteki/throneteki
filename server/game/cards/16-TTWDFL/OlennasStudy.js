a69ee50ddfd53e666854d0a26a3e80f547ef4f52
Brian Upton <ystros@gmail.com>
2020-01-12 22:18:35 -0800

    Implement Olenna's Study

---
+++ server/game/cards/16-TTWDFL/OlennasStudy.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard');
+
+class OlennasStudy extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            targetController: 'any',
+            effect: ability.effects.mayInitiateAdditionalChallenge('intrigue')
+        });
+
+        this.persistentEffect({
+            condition: () => this.game.isDuringChallenge({ challengeType: 'intrigue' }),
+            match: card => card.isMatch({ type: 'character', unique: false }),
+            targetController: 'any',
+            effect: [
+                ability.effects.cannotBeDeclaredAsAttacker(),
+                ability.effects.cannotBeDeclaredAsDefender()
+            ]
+        });
+    }
+}
+
+OlennasStudy.code = '16016';
+
+module.exports = OlennasStudy;
