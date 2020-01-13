e70f4b8b67ad22d2f07952bb1ec00de1b0ca6193
Brian Upton <ystros@gmail.com>
2020-01-12 22:29:49 -0800

    Implement Knights of the Realm

---
+++ server/game/cards/16-TTWDFL/KnightsOfTheRealm.js
@@ -0,0 +1,31 @@
+const AgendaCard = require('../../agendacard');
+
+class KnightsOfTheRealm extends AgendaCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.controlsMoreKnights(),
+            targetController: 'current',
+            effect: ability.effects.modifyDrawPhaseCards(1)
+        });
+
+        this.persistentEffect({
+            condition: () => this.controlsFewerKnights(),
+            targetController: 'current',
+            effect: ability.effects.modifyDrawPhaseCards(-1)
+        });
+    }
+
+    controlsMoreKnights() {
+        const controlledKnights = this.controller.getNumberOfCardsInPlay({ trait: 'Knight', type: 'character' });
+        return this.game.getOpponents(this.controller).every(opponent => controlledKnights > opponent.getNumberOfCardsInPlay({ trait: 'Knight', type: 'character' }));
+    }
+
+    controlsFewerKnights() {
+        const controlledKnights = this.controller.getNumberOfCardsInPlay({ trait: 'Knight', type: 'character' });
+        return this.game.getOpponents(this.controller).every(opponent => controlledKnights < opponent.getNumberOfCardsInPlay({ trait: 'Knight', type: 'character' }));
+    }
+}
+
+KnightsOfTheRealm.code = '16029';
+
+module.exports = KnightsOfTheRealm;
