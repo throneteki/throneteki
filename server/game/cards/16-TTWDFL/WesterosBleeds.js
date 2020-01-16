c0dd3b33c8e0a02f65285579cfbcffb6f6f36f95
Brian Upton <ystros@gmail.com>
2020-01-15 21:04:36 -0800

    Implement Westeros Bleeds

---
+++ server/game/cards/16-TTWDFL/WesterosBleeds.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class WesterosBleeds extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Discard characters',
+            phase: 'dominance',
+            message: {
+                format: '{player} plays {source} to discard {characters}',
+                args: { characters: () => this.getCharactersInPlay() }
+            },
+            gameAction: GameActions.simultaneously(() => this.getCharactersInPlay().map(
+                card => GameActions.discardCard({ card })
+            ))
+        });
+    }
+
+    getCharactersInPlay() {
+        return this.game.filterCardsInPlay(card => card.getType() === 'character');
+    }
+}
+
+WesterosBleeds.code = '16026';
+
+module.exports = WesterosBleeds;
