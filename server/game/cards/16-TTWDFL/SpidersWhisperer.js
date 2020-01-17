fb08b3ebf61db6e6cb38e299355b5910a287a8ae
Brian Upton <ystros@gmail.com>
2020-01-16 21:23:56 -0800

    Implement Spider's Whisperer

---
+++ server/game/cards/16-TTWDFL/SpidersWhisperer.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class SpidersWhisperer extends DrawCard {
+    setupCardAbilities() {
+        this.forcedReaction({
+            when: {
+                onCardEntersPlay: event => event.card === this
+            },
+            target: {
+                cardCondition: { location: 'play area', trait: 'Ally' }
+            },
+            message: '{player} is forced by {source} to discard {target} from play',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.discardCard(context => ({
+                        card: context.target
+                    })),
+                    context
+                );
+            }
+        });
+    }
+}
+
+SpidersWhisperer.code = '16005';
+
+module.exports = SpidersWhisperer;
