6d5f7110b3dcec6aae608f4ad7b62a4d1f3f44d0
Brian Upton <ystros@gmail.com>
2020-01-16 21:44:44 -0800

    Implement Seductive Promise

---
+++ server/game/cards/16-TTWDFL/SeductivePromise.js
@@ -0,0 +1,30 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class SeductivePromise extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.isMatch({ winner: this.controller, challengeType: 'power', by5: true })
+            },
+            target: {
+                cardCondition: (card, context) => card.isMatch({ location: 'play area', type: 'character', unique: false }) && card.controller === context.event.challenge.loser
+            },
+            message: '{player} plays {source} to take control of {target}',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.takeControl(context => ({
+                        player: context.player,
+                        card: context.target
+                    })),
+                    context
+                );
+            },
+            max: ability.limit.perChallenge(1)
+        });
+    }
+}
+
+SeductivePromise.code = '16025';
+
+module.exports = SeductivePromise;
