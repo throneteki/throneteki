fb69f7aeb6f7b61cce423253a23d1d802dae19f6
Brian Upton <ystros@gmail.com>
2020-01-16 20:35:23 -0800

    Implement King's Landing (TTWDFL)

---
+++ server/game/cards/16-TTWDFL/KingsLanding.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class KingsLanding extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => (
+                    event.card.controller === this.controller &&
+                    event.card.getType() === 'location' &&
+                    event.playingType === 'marshal'
+                )
+            },
+            message: '{player} uses {source} to draw 1 card',
+            gameAction: GameActions.drawCards(context => ({
+                player: context.player,
+                amount: 1
+            })),
+            limit: ability.limit.perRound(3)
+        });
+    }
+}
+
+KingsLanding.code = '16020';
+
+module.exports = KingsLanding;
