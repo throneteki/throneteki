0a9fc4a804be540e7d07ad9ffdfed59d65e7c381
Brian Upton <ystros@gmail.com>
2020-01-16 21:00:13 -0800

    Implement Prince's Loyalist

---
+++ server/game/cards/16-TTWDFL/PrincesLoyalist.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class PrincesLoyalist extends DrawCard {
+    setupCardAbilities(ability) {
+        this.interrupt({
+            canCancel: true,
+            when: {
+                //Restrict triggering on own triggered abilities to forced triggered abilities
+                onCardAbilityInitiated: event => event.ability.isTriggeredAbility() &&
+                                                 event.source.getType() === 'characcter' &&
+                                                 (event.ability.isForcedAbility() || event.source.controller !== this.controller)
+            },
+            cost: ability.costs.payGold(2),
+            message: {
+                format: '{player} uses {source} and pays 2 gold to cancel {abilitySource}',
+                args: { abilitySource: context => context.event.source }
+            },
+            gameAction: GameActions.cancelEffects(context => ({
+                event: context.event
+            }))
+        });
+    }
+}
+
+PrincesLoyalist.code = '16007';
+
+module.exports = PrincesLoyalist;
