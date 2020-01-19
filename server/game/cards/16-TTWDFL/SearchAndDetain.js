737b98f35bde0a9ee0f59d27744861ee5cfc2a29
Brian Upton <ystros@gmail.com>
2020-01-19 10:40:30 -0800

    Implement Search and Detain

---
+++ server/game/cards/16-TTWDFL/SearchAndDetain.js
@@ -0,0 +1,24 @@
+const PlotCard = require('../../plotcard');
+const GameActions = require('../../GameActions');
+
+class SearchAndDetain extends PlotCard {
+    setupCardAbilities() {
+        this.whenRevealed({
+            target: {
+                activePromptTitle: 'Select a card',
+                cardCondition: card => card.isMatch({ location: 'play area', hasAttachments: false }) && card.controller.firstPlayer
+            },
+            message: '{player} uses {source} to return {target} to hand',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.returnCardToHand(context => ({ card: context.target })),
+                    context
+                );
+            }
+        });
+    }
+}
+
+SearchAndDetain.code = '16034';
+
+module.exports = SearchAndDetain;
