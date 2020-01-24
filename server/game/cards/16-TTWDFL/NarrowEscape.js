4994769a1e9234ccdaaa6a6787388a4bcb6e276c
Brian Upton <ystros@gmail.com>
2020-01-23 20:16:40 -0800

    Implement Narrow Escape

---
+++ server/game/cards/16-TTWDFL/NarrowEscape.js
@@ -0,0 +1,81 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+const GenericTracker = require('../../EventTrackers/GenericTracker');
+
+class NarrowEscape extends DrawCard {
+    setupCardAbilities(ability) {
+        this.killTracker = GenericTracker.forPhase(this.game, 'onCharacterKilled');
+        this.discardTracker = GenericTracker.forPhase(this.game, 'onCardDiscarded');
+
+        this.action({
+            title: 'Put characters into play',
+            message: {
+                format: '{player} plays {source} to put {characters} into play',
+                args: { characters: () => this.getEligibleCharacters() }
+            },
+            handler: context => {
+                const opponents = this.game.getOpponentsInFirstPlayerOrder(context.player);
+                this.remainingOpponents = opponents.filter(opponent => opponent.hand.length > 1);
+                this.context = context;
+                this.promptForCancel(context);
+            },
+            max: ability.limit.perPhase(1)
+        });
+    }
+
+    promptForCancel() {
+        if(this.remainingOpponents.length === 0) {
+            this.resolvePutIntoPlay();
+            return true;
+        }
+
+        const nextOpponent = this.remainingOpponents.shift();
+        this.game.promptWithMenu(nextOpponent, this, {
+            activePrompt: {
+                menuTitle: 'Discard hand to cancel?',
+                buttons: [
+                    { text: 'Yes', method: 'cancelResolution' },
+                    { text: 'No', method: 'promptForCancel' }
+                ]
+            },
+            source: this
+        });
+
+        return true;
+    }
+
+    cancelResolution(opponent) {
+        this.game.addMessage('{0} discards their hand to cancel the effects of {1}', opponent, this);
+        this.game.resolveGameAction(
+            GameActions.simultaneously(() => opponent.hand.map(
+                card => GameActions.discardCard({ card })
+            )),
+            this.context
+        );
+        return true;
+    }
+
+    resolvePutIntoPlay() {
+        this.game.resolveGameAction(
+            GameActions.simultaneously(() => [
+                ...this.getEligibleCharacters().map(
+                    card => GameActions.putIntoPlay({
+                        card,
+                        player: card.owner
+                    })
+                )
+            ]),
+            this.context
+        );
+    }
+
+    getEligibleCharacters() {
+        const eligibleKilled = this.killTracker.events.map(event => event.card).filter(card => card.location === 'dead pile');
+        const eligibleDiscards = this.discardTracker.events.filter(event => event.originalLocation === 'play area' && event.card.location === 'discard pile').map(event => event.card);
+        return eligibleKilled.concat(eligibleDiscards).filter(card => card.owner.canPutIntoPlay(card));
+    }
+}
+
+NarrowEscape.code = '16024';
+
+module.exports = NarrowEscape;
