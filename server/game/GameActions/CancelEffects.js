30932b03c0ade7c0be76514b239e66b1d84144ac
Brian Upton <ystros@gmail.com>
2020-01-23 19:24:06 -0800

    Implement Harrenhal (TTWDFL) (#2900)

---
+++ server/game/GameActions/CancelEffects.js
@@ -0,0 +1,19 @@
+const GameAction = require('./GameAction');
+
+class CancelEffects extends GameAction {
+    constructor() {
+        super('cancelEffects');
+    }
+
+    canChangeGameState() {
+        return true;
+    }
+
+    createEvent({ event }) {
+        return this.event('onEffectsCanceled', { canceledEvent: event }, event => {
+            event.canceledEvent.cancel();
+        });
+    }
+}
+
+module.exports = new CancelEffects();
