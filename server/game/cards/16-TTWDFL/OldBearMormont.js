a77f395f0faec020a7f77aafa92505ba1331d6a4
Brian Upton <ystros@gmail.com>
2020-01-28 05:01:14 -0800

    Implement Old Bear Mormont (TTWDFL) (#2910)

---
+++ server/game/cards/16-TTWDFL/OldBearMormont.js
@@ -0,0 +1,36 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class OldBearMormont extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Stand character',
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: card => card !== this && card.kneeled && (card.isMatch({ faction: 'thenightswatch' }) || card.isMatch({ trait: 'Lord' })),
+                gameAction: 'stand'
+            },
+            message: '{player} kneels {source} to stand {target}',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.standCard(context => ({
+                        card: context.target
+                    })),
+                    context
+                );
+            }
+        });
+
+        this.reaction({
+            when: {
+                onCharacterKilled: event => event.card.controller === this.controller
+            },
+            message: '{player} uses {source} to stand {source}',
+            gameAction: GameActions.standCard({ card: this })
+        });
+    }
+}
+
+OldBearMormont.code = '16010';
+
+module.exports = OldBearMormont;
