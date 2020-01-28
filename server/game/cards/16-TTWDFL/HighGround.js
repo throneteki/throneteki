36ccbf8248d42e8f6a652290cb0d779eef4f4f4b
Brian Upton <ystros@gmail.com>
2020-01-28 05:02:27 -0800

    Implement High Ground (#2913)

---
+++ server/game/cards/16-TTWDFL/HighGround.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class HighGround extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Stand character',
+            cost: ability.costs.kneelSelf(),
+            target: {
+                cardCondition: { location: 'play area', trait: 'Army', type: 'character', kneeled: true },
+                gameAction: 'stand'
+            },
+            message: '{player} kneels {source} to stand {target}',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.standCard(context => ({
+                        card: context.target
+                    })),
+                    context
+                );
+            }
+        });
+    }
+}
+
+HighGround.code = '16019';
+
+module.exports = HighGround;
