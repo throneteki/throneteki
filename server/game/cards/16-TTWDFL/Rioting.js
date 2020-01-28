a7c590562e8b20afa34f23a8d86220cfb4c293bf
Brian Upton <ystros@gmail.com>
2020-01-28 04:53:40 -0800

    Implement Rioting (#2897)

---
+++ server/game/cards/16-TTWDFL/Rioting.js
@@ -0,0 +1,27 @@
+const PlotCard = require('../../plotcard.js');
+const GameActions = require('../../GameActions');
+
+class Rioting extends PlotCard {
+    setupCardAbilities() {
+        this.forcedReaction({
+            when: {
+                onPhaseStarted: event => event.phase === 'standing'
+            },
+            message: {
+                format: '{player} is forced by {source} to discard {characters} from play',
+                args: { characters: () => this.getKneelingCharacters() }
+            },
+            gameAction: GameActions.simultaneously(() => this.getKneelingCharacters().map(
+                card => GameActions.discardCard({ card, allowSave: false })
+            ))
+        });
+    }
+
+    getKneelingCharacters() {
+        return this.game.filterCardsInPlay(card => card.getType() === 'character' && card.kneeled);
+    }
+}
+
+Rioting.code = '16032';
+
+module.exports = Rioting;
