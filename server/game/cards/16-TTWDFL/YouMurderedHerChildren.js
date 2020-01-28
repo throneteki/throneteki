57f3e97498dfebbcfd18dd7971177ab9e8acbc1b
Brian Upton <ystros@gmail.com>
2020-01-28 05:00:04 -0800

    Implement You Murdered Her Children (TTWDFL) (#2906)
    * Allow searching hand and other player's decks
    
    * Implement You Murdered Her Children (TTWDFL)

---
+++ server/game/cards/16-TTWDFL/YouMurderedHerChildren.js
@@ -0,0 +1,45 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class YouMurderedHerChildren extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.isMatch({ loser: this.controller, defendingPlayer: this.controller })
+            },
+            target: {
+                cardCondition: { location: 'play area', type: 'character', attacking: true },
+                gameAction: 'discard'
+            },
+            message: '{player} plays {source} to discard {target}',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.discardCard(context => ({
+                        card: context.target,
+                        allowSave: false
+                    })).then(context => ({
+                        gameAction: GameActions.search({
+                            title: 'Select characters',
+                            searchedPlayer: () => context.event.challenge.attackingPlayer,
+                            location: ['draw deck', 'hand'],
+                            match: { name: context.target.name },
+                            numToSelect: 2,
+                            message: {
+                                format: 'Then {player} searches {attackingPlayer}\'s hand and deck to discard {searchTarget}',
+                                args: { attackingPlayer: thenContext => thenContext.parentContext.event.challenge.attackingPlayer }
+                            },
+                            gameAction: GameActions.simultaneously(thenContext => thenContext.searchTarget.map(
+                                card => GameActions.discardCard({ card })
+                            ))
+                        })
+                    })),
+                    context
+                );
+            }
+        });
+    }
+}
+
+YouMurderedHerChildren.code = '16008';
+
+module.exports = YouMurderedHerChildren;
