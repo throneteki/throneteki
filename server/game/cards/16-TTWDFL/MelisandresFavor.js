ed28ef7338c2a8d2115c8ea11a5bcacc32a30d2b
Brian Upton <ystros@gmail.com>
2020-01-28 04:58:05 -0800

    Implement Melisandre's Favor (#2902)

---
+++ server/game/cards/16-TTWDFL/MelisandresFavor.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard');
+const GameActions = require('../../GameActions');
+
+class MelisandresFavor extends DrawCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.takeControl(() => this.controller)
+        });
+
+        this.forcedInterrupt({
+            when: {
+                onPhaseEnded: event => event.phase === 'dominance'
+            },
+            message: '{player} is forced to sacrifice {source}',
+            gameAction: GameActions.sacrificeCard({
+                card: this
+            }),
+            cannotBeCanceled: true
+        });
+    }
+}
+
+MelisandresFavor.code = '16002';
+
+module.exports = MelisandresFavor;
