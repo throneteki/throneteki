96d52f66124ebc95f649cfb35a7035d48d504f29
EvadLhorg <lukas.a-v@web.de>
2020-07-31 17:07:58 +0200

    add option to use the default RL for an event (#108)
    * add an option to use the default RL for events that don´t specify their own RL
    
    * use Checkbox instead of Input for displaying a checkbox

--- pages/EventsAdmin/EventEditor.jsx
+++ pages/EventsAdmin/EventEditor.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Input from '../../Components/Form/Input';
+import Checkbox from '../../Components/Form/Checkbox';
 import Typeahead from '../../Components/Form/Typeahead';
 import TextArea from '../../Components/Form/TextArea';
 import ApiStatus from '../../Components/Site/ApiStatus';
@@ -10,10 +11,11 @@ class EventEditor extends React.Component {
     constructor(props) {
         super(props);
 
-        const event = props.event || { restricted: [], banned: [] };
+        const event = props.event || { useDefaultRestrictedList: false, restricted: [], banned: [] };
         this.state = {
             eventId: event._id,
             name: event.name,
+            useDefaultRestrictedList: event.useDefaultRestrictedList,
             restricted: event.restricted,
             banned: event.banned,
             restrictedListText: this.formatListText(props.cards, event.restricted),
@@ -25,6 +27,7 @@ class EventEditor extends React.Component {
         return {
             _id: this.state.eventId,
             name: this.state.name,
+            useDefaultRestrictedList: this.state.useDefaultRestrictedList,
             restricted: this.state.restricted,
             banned: this.state.banned
         };
@@ -52,6 +55,14 @@ class EventEditor extends React.Component {
         let state = this.state;
 
         state[field] = event.target.value;
+
+        this.setState({ state });
+    }
+
+    onCheckboxChange(field, event) {
+        let state = this.state;
+
+        state[field] = event.target.checked;
 
         this.setState({ state });
     }
@@ -159,7 +170,8 @@ class EventEditor extends React.Component {
                 <form className='form form-horizontal'>
                     <Input name='name' label='Event Name' labelClass='col-sm-3' fieldClass='col-sm-9' placeholder='Event Name'
                         type='text' onChange={ this.onChange.bind(this, 'name') } value={ this.state.name } />
-
+                    <Checkbox name='name' label='Use default Restricted List' labelClass='col-sm-4' fieldClass='col-sm-offset-3 col-sm-8'
+                        onChange={ this.onCheckboxChange.bind(this, 'useDefaultRestrictedList') } checked={ this.state.useDefaultRestrictedList } />
                     <Typeahead label='Card' labelClass={ 'col-sm-3 col-xs-2' } fieldClass='col-sm-4 col-xs-5' labelKey={ 'label' } options={ allCards }
                         onChange={ this.addCardChange.bind(this) }>
                         <div className='col-xs-1 no-x-padding'>
