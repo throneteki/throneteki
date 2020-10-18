f91e6dbae9d929b01b01720b303ac02adaba65f9
EvadLhorg <lukas.a-v@web.de>
2020-10-18 15:43:42 +0200

    add test for cersei missandei interaction

--- test/server/cards/05-LoCR/CerseiLannister.spec.js
+++ test/server/cards/05-LoCR/CerseiLannister.spec.js
@@ -7,7 +7,7 @@ describe('Cersei Lannister (LoCR)', function() {
             ]);
             const deck2 = this.buildDeck('lannister', [
                 'Sneak Attack',
-                'Hedge Knight', 'Hedge Knight'
+                'Hedge Knight', 'Hedge Knight', 'Missandei'
             ]);
             this.player1.selectDeck(deck1);
             this.player2.selectDeck(deck2);
@@ -15,6 +15,10 @@ describe('Cersei Lannister (LoCR)', function() {
             this.keepStartingHands();
 
             this.player1.clickCard('Cersei Lannister', 'hand');
+            [this.hedge1, this.hedge2] = this.player2.filterCardsByName('Hedge Knight', 'hand');
+            this.missandei = this.player2.findCardByName('Missandei');
+            //hide missandei in dead pile for later test
+            this.player2.dragCard(this.missandei, 'dead pile');
             this.completeSetup();
         });
 
@@ -69,6 +73,33 @@ describe('Cersei Lannister (LoCR)', function() {
                 });
 
                 it('should allow Cersei to gain power', function() {
+                    this.player1.triggerAbility('Cersei Lannister');
+                    expect(this.cersei.power).toBe(1);
+                });
+            });
+
+            describe('when a card with an interrupt replacement handler is discarded', function() {
+                beforeEach(function() {
+                    this.cersei = this.player1.findCardByName('Cersei Lannister');
+                    this.player2.dragCard(this.hedge1, 'dead pile');
+                    this.player2.dragCard(this.hedge2, 'dead pile');
+                    this.player2.dragCard(this.missandei, 'hand');
+
+                    this.player1.selectPlot('A Clash of Kings');
+                    this.selectFirstPlayer(this.player1);
+
+                    this.completeMarshalPhase();
+
+                    this.unopposedChallenge(this.player1, 'Intrigue', this.cersei);
+
+                    this.player1.clickPrompt('Apply Claim');
+                });
+
+                it('should allow Cersei to gain power AND NOT CRASH THE GAME', function() {
+                    expect(this.player2).toAllowAbilityTrigger('Missandei');
+                    this.player2.triggerAbility('Missandei');
+                    expect(this.missandei.location).toBe('play area');
+                    expect(this.player1).toAllowAbilityTrigger('Cersei Lannister');
                     this.player1.triggerAbility('Cersei Lannister');
                     expect(this.cersei.power).toBe(1);
                 });
