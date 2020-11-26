2d8b9db4edfdd17a628b023a261dd32f8bafa18e
EvadLhorg <lukas.a-v@web.de>
2020-11-26 17:28:58 +0100

    revert bugfix for mag the mighty

--- server/game/cards/08.1-TAK/MagTheMighty.js
+++ server/game/cards/08.1-TAK/MagTheMighty.js
@@ -1,4 +1,5 @@
 const DrawCard = require('../../drawcard.js');
+
 class MagTheMighty extends DrawCard {
     setupCardAbilities() {
         this.forcedReaction({
@@ -6,20 +7,19 @@ class MagTheMighty extends DrawCard {
                 afterChallenge: event => event.challenge.winner === this.controller && this.isParticipating()
             },
             target: {
-                cardCondition: card => card.location === 'play area' && card.controller === this.controller && card.getType() === 'character' && card.canBeKilled()
+                cardCondition: card => card.location === 'play area' && card.controller === this.controller && card.getType() === 'character'
             },
             handler: context => {
                 this.game.killCharacter(context.target);
                 this.game.addMessage('{0} is forced by {1} to kill {2}', context.player, this, context.target);
 
-                if(context.target.location && context.target.location === 'dead pile') {
-                    this.game.promptForSelect(context.event.challenge.loser, {
-                        activePromptTitle: 'Select a character',
-                        cardCondition: card => card.location === 'play area' && card.controller === context.event.challenge.loser && card.getType() === 'character' && card.canBeKilled(),
-                        source: this,
-                        onSelect: (player, card) => this.onCardSelected(player, card)
-                    });
-                }
+                //TODO Technically should only trigger when the first kill was not saved
+                this.game.promptForSelect(context.event.challenge.loser, {
+                    activePromptTitle: 'Select a character (only when first kill was not saved)',
+                    cardCondition: card => card.location === 'play area' && card.controller === context.event.challenge.loser && card.getType() === 'character',
+                    source: this,
+                    onSelect: (player, card) => this.onCardSelected(player, card)
+                });
             }
         });
     }
