bbd4087f3b641d84e91f62330f0d4846cb03a3c8
Brian Upton <ystros@gmail.com>
2021-01-16 15:12:11 -0800

    Fix Castle Guard target condition (#3093)
    Check for the onCardOutOfShadows event instead of whether a card
    was put into play with the 'outOfShadows' meta-action.

--- server/game/cards/13.4-BtRK/CastleGuard.js
+++ server/game/cards/13.4-BtRK/CastleGuard.js
@@ -1,16 +1,16 @@
 const DrawCard = require('../../drawcard');
-const {CardEntersPlayTracker} = require('../../EventTrackers');
+const GenericTracker = require('../../EventTrackers/GenericTracker');
 
 class CastleGuard extends DrawCard {
     setupCardAbilities() {
-        this.tracker = CardEntersPlayTracker.forPhase(this.game);
+        this.tracker = GenericTracker.forPhase(this.game, 'onCardOutOfShadows');
 
         this.reaction({
             when: {
                 onCardOutOfShadows: event => event.card === this
             },
             target: {
-                cardCondition: (card) => card.location === 'play area' && card !== this && card.getType() === 'character' && this.tracker.hasComeOutOfShadows(card)
+                cardCondition: (card) => card.location === 'play area' && card !== this && card.getType() === 'character' && this.tracker.some(event => event.card === card)
             },
             message: '{player} uses {source} to return {target} to shadows',
             handler: context => {
