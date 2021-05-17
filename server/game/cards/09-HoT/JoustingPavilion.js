654688dcb72f2c892fc486420b965c880a10a4d2
EvadLhorg <lukas.a-v@web.de>
2021-05-17 06:16:36 +0200

    fix the jousting pavilion + knighted + randyll tarly interaction  (#3117)
    * fix the jousting pavilion + knighted + randyll tarly interaction so that the str buff reaction of randyll works
    
    * simplify Jousting Pavilions condition
    
    Co-authored-by: Brian Upton <ystros@gmail.com>

--- server/game/cards/09-HoT/JoustingPavilion.js
+++ server/game/cards/09-HoT/JoustingPavilion.js
@@ -3,7 +3,7 @@ const DrawCard = require('../../drawcard.js');
 class JoustingPavilion extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            condition: () => this.game.currentChallenge && this.game.currentChallenge.hasSingleParticipant(this.controller),
+            condition: () => this.game.isDuringChallenge({ initiated: true, match: challenge => challenge.hasSingleParticipant(this.controller) }),
             match: card => card.hasTrait('Knight') && card.getType() === 'character' && card.isParticipating(),
             effect: ability.effects.modifyStrength(1)
         });
