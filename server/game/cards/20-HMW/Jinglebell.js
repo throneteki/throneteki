e08877edd7bb00be79b452364fa12376111dc24d
EvadLhorg <lukas.a-v@web.de>
2021-06-28 15:20:17 +0200

    simplify jinglebell search action

--- server/game/cards/20-HMW/Jinglebell.js
+++ server/game/cards/20-HMW/Jinglebell.js
@@ -7,23 +7,18 @@ class Jinglebell extends DrawCard {
             when: {
                 onCharacterKilled: event => event.card === this
             },
-            handler: context => {
-                this.game.resolveGameAction(
-                    GameActions.search({
-                        title: 'Select a card',
-                        match: {
-                            type: 'character',
-                            trait: this.getTraits()
-                        },
-                        message: '{player} uses {source} to search their deck and add {searchTarget} to their hand',
-                        cancelMessage: '{player} uses {source} to search their deck but does not find a card',
-                        gameAction: GameActions.addToHand(context => ({
-                            card: context.searchTarget
-                        }))
-                    }),
-                    context
-                );
-            }
+            gameAction: GameActions.search({
+                title: 'Select a card',
+                match: {
+                    type: 'character',
+                    trait: this.getTraits()
+                },
+                message: '{player} uses {source} to search their deck and add {searchTarget} to their hand',
+                cancelMessage: '{player} uses {source} to search their deck but does not find a card',
+                gameAction: GameActions.addToHand(context => ({
+                    card: context.searchTarget
+                }))
+            })
         });
     }
 }
