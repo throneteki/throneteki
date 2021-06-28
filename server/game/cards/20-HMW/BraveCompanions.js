a02c202c49a140f57ccf64a1873ad26efe38c185
EvadLhorg <lukas.a-v@web.de>
2021-06-28 15:22:28 +0200

    update BraveCompanions search action

--- server/game/cards/20-HMW/BraveCompanions.js
+++ server/game/cards/20-HMW/BraveCompanions.js
@@ -1,5 +1,6 @@
 const DrawCard = require('../../drawcard.js');
 const {Tokens} = require('../../Constants');
+const GameActions = require('../../GameActions');
 
 class BraveCompanions extends DrawCard {
     setupCardAbilities(ability) {
@@ -12,17 +13,20 @@ class BraveCompanions extends DrawCard {
             when: {
                 onCardEntersPlay: event => event.card === this && event.playingType === 'marshal'
             },
-            handler: () => {
-                this.game.promptForDeckSearch(this.controller, {
-                    activePromptTitle: 'Select cards',
-                    numCards: 10,
-                    numToSelect: 3,
-                    cardCondition: (card, context) => card.getType() === 'character' && this.remainingTraits(context.selectedCards).some(trait => card.hasTrait(trait)),
-                    onSelect: (player, cards) => this.selectCards(player, cards),
-                    onCancel: player => this.cancelSearch(player),
-                    source: this
-                });
-            }
+            gameAction: GameActions.search({
+                title: 'Select cards',
+                topCards: 10,
+                numToSelect: 3,
+                match: {
+                    type: 'character',
+                    condition: (card, context) => this.remainingTraits(context.selectedCards).some(trait => card.hasTrait(trait))
+                },
+                message: '{player} uses {source} to search their deck and add {searchTarget} to their hand',
+                cancelMessage: '{player} uses {source} to search their deck but does not find a card',
+                gameAction: GameActions.simultaneously(context => (
+                    context.searchTarget.map(card => GameActions.addToHand({ card }))
+                ))
+            })
         });
     }
     
@@ -37,19 +41,6 @@ class BraveCompanions extends DrawCard {
     remainingTraits(selectedCards) {
         const traits = ['Army', 'Commander', 'Mercenary'];
         return traits.filter(trait => !selectedCards.some(card => card.hasTrait(trait)));
-    }
-
-    selectCards(player, cards) {
-        for(let card of cards) {
-            player.moveCard(card, 'hand');
-        }
-        this.game.addMessage('{0} uses {1} to search their deck and adds {2} to their hand', player, this, cards);
-        return true;
-    }
-
-    cancelSearch(player) {
-        this.game.addMessage('{0} uses {1} to search their deck, but does not add any cards to their hand', player, this);
-        return true;
     }
 }
 
