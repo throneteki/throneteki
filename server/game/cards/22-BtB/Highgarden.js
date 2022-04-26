63dd48bfd09b7288d1ad70d842f955e532d5373b
Patane97 <patane97@gmail.com>
2022-04-26 23:14:33 +1000

    Highgarden now utilizing Targets & GameActions

--- server/game/cards/22-BtB/Highgarden.js
+++ server/game/cards/22-BtB/Highgarden.js
@@ -7,18 +7,58 @@ class Highgarden extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
             title: 'Give characters +STR',
-            condition: () => this.controller.hand.length > 0,
             cost: ability.costs.kneelSelf(),
-            handler: () => {
-                this.game.promptForSelect(this.controller, {
-                    activePromptTitle: 'Reveal up to 3 cards',
-                    source: this,
-                    numCards: 3,
-                    mode: 'upTo',
-                    cardCondition: card => card.controller === this.controller && card.location === 'hand' && card.isFaction('tyrell') && card.getType() === 'character',
-                    onSelect: (player, cards) => this.revealSelect(player, cards),
-                    onCancel: (player) => this.revealCancel(player)
-                });
+            target: {
+                cardCondition: card => card.controller === this.controller && card.location === 'hand' && card.isFaction('tyrell') && card.getType() === 'character',
+                activePromptTitle: 'Select up to 3 cards',
+                type: 'select',
+                mode: 'upTo',
+                numCards: 3,
+                gameAction: 'reveal'
+            },
+            message: '{player} kneels {source} to reveal {target} from their hand',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.simultaneously(
+                        context.target.map(card => GameActions.revealCard({ card }))
+                    ).then(preThenContext => ({
+                        target: {
+                            cardCondition: card => card.getType() === 'character' && card.location === 'play area',
+                            activePromptTitle: `Select up to ${preThenContext.target.length} characters`,
+                            type: 'select',
+                            mode: 'upTo',
+                            numCards: preThenContext.target.length,
+                            gameAction: 'increaseStrength'
+                        },
+                        handler: thenContext => {
+                            this.groups = {};
+                            this.remainingCards = thenContext.target;
+                            this.remainingStr = thenContext.parentContext.target.length * 2;
+                            this.game.queueSimpleStep(() => this.calculateNextCharacter(thenContext.player));
+                            
+                            this.game.queueSimpleStep(() => {
+                                let strMessages = [];
+                                for(let group in this.groups) {
+                                    let strength = parseInt(group);
+                                    let characters = this.groups[group];
+                                    for(let character of characters) {
+                                        this.untilEndOfPhase(ability => ({
+                                            match: character,
+                                            effect: ability.effects.modifyStrength(strength)
+                                        }));
+                                    }
+                                    // Groups chat messages by STR's given. eg. "give A, B and C +2 STR", or "give A +4 STR and B +2 STR"
+                                    strMessages.push(Message.fragment('{characters} +{strength} STR', { characters, strength }));
+                                }
+
+                                this.game.addMessage('{0} then uses {1} to give {2} until the end of the phase', thenContext.player, this, strMessages);
+
+                                return true;
+                            });
+                        }
+                    })),
+                    context
+                );
             }
         });
         this.action({
@@ -36,72 +76,19 @@ class Highgarden extends DrawCard {
         });
     }
 
-    revealSelect(player, revealed) {
-        this.game.addMessage('{0} kneels {1} to reveal {2} from their hand', player, this, revealed);
-
-        this.game.resolveGameAction(GameActions.simultaneously(
-            revealed.map(card => GameActions.revealCard({ card }))
-        ));
-
-        let numRevealed = revealed.length;
-
-        this.game.promptForSelect(this.controller, {
-            activePromptTitle: `Select up to ${numRevealed} characters`,
-            source: this,
-            numCards: numRevealed,
-            mode: 'upTo',
-            cardCondition: card => card.getType() === 'character' && card.location === 'play area',
-            onSelect: (player, cards) => this.charactersSelect(player, cards, numRevealed),
-            onCancel: (player) => this.charactersCancel(player)
-        });
-        return true;
-    }
-
-    revealCancel(player) {
-        this.game.addAlert('danger', '{0} did not reveal any cards for {1}', player, this);
-
-        return true;
-    }
-
-    charactersSelect(player, cards, numRevealed) {
-        this.strCharacterMap = new Map();
-        this.remainingCards = cards;
-        this.remainingStr = numRevealed * 2;
-        
-        this.game.queueSimpleStep(() => this.calculateNextCharacter(player));
-        
-        this.game.queueSimpleStep(() => {
-            for(let [str, cards] of this.strCharacterMap) {
-                for(let card of cards) {
-                    this.untilEndOfPhase(ability => ({
-                        match: card,
-                        effect: ability.effects.modifyStrength(str)
-                    }));
-                }
-            }
-            // Groups chat messages by STR's given. eg. "give A, B and C +2 STR", or "give A +4 STR and B +2 STR"
-            let strMessages = Array.from(this.strCharacterMap, ([str, cards]) => Message.fragment('{cards} +{str} STR', { cards, str }));
-
-            this.game.addMessage('{0} then uses {1} to give {2} until the end of the phase', player, this, strMessages);
-
-            return true;
-        });
-        return true;
-    }
-
     calculateNextCharacter(player) {
         let difference = (this.remainingStr / 2) - this.remainingCards.length;
         if(this.remainingCards.length === 1 && this.remainingStr > 0) {
             // If one character remaining, give this character the remaining STR
-            this.addCharacterToStrMap(player, this.remainingStr);
+            this.addCharacterToGroup(player, this.remainingStr);
         } else if(difference === 0) {
             // If STR can only be spread evenly, give this character +2 STR. This will be hit for remaining characters
-            this.addCharacterToStrMap(player, 2);
+            this.addCharacterToGroup(player, 2);
         } else {
             // Otherwise, ask how much STR to give this character
             let buttons = range(1, 2 + difference).reverse().map(amount => {
-                let str = amount * 2;
-                return { text: str.toString(), method: 'addCharacterToStrMap', arg: str };
+                let strength = amount * 2;
+                return { text: strength.toString(), method: 'addCharacterToGroup', arg: strength };
             });
             this.game.promptWithMenu(player, this, {
                 activePrompt: {
@@ -114,22 +101,16 @@ class Highgarden extends DrawCard {
         return true;
     }
 
-    addCharacterToStrMap(player, str) {
-        let characters = this.strCharacterMap.get(str) || [];
+    addCharacterToGroup(player, strength) {
+        let characters = this.groups[strength] || [];
         characters.push(this.remainingCards.shift());
-        this.strCharacterMap.set(str, characters);
+        this.groups[strength] = characters;
 
-        this.remainingStr -= str;
+        this.remainingStr -= strength;
 
         if(this.remainingCards.length > 0) {
             this.calculateNextCharacter(player);
         }
-
-        return true;
-    }
-
-    charactersCancel(player) {
-        this.game.addAlert('danger', '{0} did not select any characters for {1}', player, this);
 
         return true;
     }
