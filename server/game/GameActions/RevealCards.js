72e202034ddff91e658d38b905d4ea6fba2b3077
Brian Upton <ystros@gmail.com>
2022-05-14 22:12:25 -0700

    Fix linting issues in RevealCards

--- server/game/GameActions/RevealCards.js
+++ server/game/GameActions/RevealCards.js
@@ -5,10 +5,11 @@ const AcknowledgeRevealCardsPrompt = require('../gamesteps/AcknowledgeRevealCard
 class RevealCards extends GameAction {
     constructor() {
         super('revealCards');
-        this.defaultWhileRevealed = new HandlerGameActionWrapper({ handler: context => {
+        this.defaultWhileRevealed = new HandlerGameActionWrapper({
+            handler: context => {
                 if(context.revealed.length > 0) {
                     // TODO: Replace the below with a separate Card Reveal UI (and don't show the cards in their respective locations). This would clean up effects which simply reveal a single card.
-                    context.game.queueStep(new AcknowledgeRevealCardsPrompt(context.game, context.revealed, context.player))
+                    context.game.queueStep(new AcknowledgeRevealCardsPrompt(context.game, context.revealed, context.player));
                 }
             }
         });
@@ -18,43 +19,43 @@ class RevealCards extends GameAction {
         return cards.length > 0 && cards.some(card => !this.isImmune({ card, context }))
     }
 
-    createEvent({ cards, player, whileRevealed, context,  }) {
+    createEvent({ cards, player, whileRevealed, context }) {
         context.player = player;
         const allPlayers = context.game.getPlayers();
         const eventParams = {
             player,
             cards,
             source: context.source
-        }
+        };
         return this.event('onCardsRevealed', eventParams, event => {
             const whileRevealedGameAction = whileRevealed || this.defaultWhileRevealed;
             const revealFunc = card => context.revealed.includes(card) && !this.isImmune({ card, context });
 
             context.revealed = event.cards;
-            
+
             // Make cards visible & print reveal message before 'onCardRevealed' to account for any reveal interrupts (eg. Alla Tyrell)
             context.game.cardVisibility.addRule(revealFunc);
             this.highlightRevealedCards(event, context.revealed, allPlayers);
-            context.game.addMessage("{0} reveals {1} from their {2}", event.player, event.cards, [...new Set(event.cards.map(card => card.location))]);
-            
+            context.game.addMessage('{0} reveals {1} from their {2}', event.player, event.cards, [...new Set(event.cards.map(card => card.location))]);
+
             for(let card of event.cards) {
                 const revealEventParams = {
                     card,
                     cardStateWhenRevealed: card.createSnapshot(),
                     player: event.player,
                     source: event.source
-                }
-                
+                };
+
                 event.thenAttachEvent(this.event('onCardRevealed', revealEventParams, revealEvent => {
                     if(revealEvent.card.location !== revealEvent.cardStateWhenRevealed.location) {
                         this.removeInvalidReveal(context, revealEvent.card, allPlayers);
                     }
                 }));
             }
-            
+
             let whileRevealedEvent = whileRevealedGameAction.createEvent(context);
             event.thenAttachEvent(whileRevealedEvent);
-            
+
             whileRevealedEvent.thenExecute(() => {
                 this.hideRevealedCards(event, allPlayers);
                 context.game.cardVisibility.removeRule(revealFunc);
@@ -69,7 +70,7 @@ class RevealCards extends GameAction {
             event.preRevealSelections[player.id] = {
                 selectedCards: player.getSelectedCards(),
                 selectableCards: player.getSelectableCards()
-            }
+            };
 
             player.clearSelectedCards();
             player.clearSelectableCards();
