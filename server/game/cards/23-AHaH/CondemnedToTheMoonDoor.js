d93166dbfb1986f115b2d533ff9be2a22753108a
EvadLhorg <lukas.a-v@web.de>
2022-06-30 20:14:06 +0200

    implement condemned to the moon door

---
+++ server/game/cards/23-AHaH/CondemnedToTheMoonDoor.js
@@ -0,0 +1,31 @@
+const PlotCard = require('../../plotcard.js');
+
+class CondemnedToTheMoonDoor extends PlotCard {
+    setupCardAbilities() {
+        this.whenRevealed({
+            handler: context => {
+                this.game.promptForSelect(context.player, {
+                    cardCondition: card => card.getType() === 'character' && card.location === 'play area' && !card.hasTrait('Army'),
+                    source: this,
+                    onSelect: (player, card) => this.onCardSelected(player, card)
+                });
+            }
+        });
+    }
+
+    onCardSelected(player, card) {
+        this.atEndOfRound(ability => ({
+            match: card,
+            condition: () => card.location === 'play area',
+            targetLocation: 'any',
+            effect: ability.effects.killIfStillInPlayAndKneelFactionCard(player, false)
+        }));
+
+        this.game.addMessage('{0} chooses {1} for {2}',
+            player, card, this);
+    }
+}
+
+CondemnedToTheMoonDoor.code = '23038';
+
+module.exports = CondemnedToTheMoonDoor;
