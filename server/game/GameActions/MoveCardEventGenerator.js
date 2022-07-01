0ebdef575ad9b74f5ecd82f996ca3564f6defffc
Brian Upton <ystros@gmail.com>
2022-07-01 07:22:44 -0700

    Fix interaction between Arianne and Ramsay Snow (#3284)
    The `PutIntoPlay` game action now generates the onCardEntersPlay event
    directly, rather than wrapping it in a placeholder event. By returning
    it directly, the reaction for onCardEntersPlay when Ramsay Snow is put
    into play by Arianne Martel now properly happens after Arianne has
    returned to hand.

--- server/game/GameActions/MoveCardEventGenerator.js
+++ server/game/GameActions/MoveCardEventGenerator.js
@@ -1,4 +1,5 @@
 const AtomicEvent = require('../AtomicEvent');
+const BestowPrompt = require('../gamesteps/bestowprompt');
 const Event = require('../event');
 
 class MoveCardEventGenerator {
@@ -76,6 +77,36 @@ class MoveCardEventGenerator {
         return this.event('onCardPlaced', { card, location, player, bottom }, event => {
             const actualPlayer = event.location !== 'play area' ? event.card.owner : event.player;
             actualPlayer.placeCardInPile({ card, location, bottom });
+        });
+    }
+
+    createEntersPlayEvent({ card, player, playingType = 'play', kneeled = false, isDupe = false }) {
+        const originalLocation = card.location;
+
+        player = player || card.controller;
+        const game = player.game;
+
+        return this.event('onCardEntersPlay', { card, playingType, originalLocation }, event => {
+            // Attachments placed in setup should not be considered to be 'played',
+            // as it will cause then to double their effects when attached later.
+            let isSetupAttachment = playingType === 'setup' && event.card.getPrintedType() === 'attachment';
+
+            let originalFacedownState = event.card.facedown;
+            event.card.facedown = game.currentPhase === 'setup';
+            event.card.new = true;
+            player.placeCardInPile({ card: event.card, location: 'play area', wasFacedown: originalFacedownState });
+            event.card.takeControl(player);
+            event.card.kneeled = playingType !== 'setup' && !!event.card.entersPlayKneeled || !!kneeled;
+
+            if(!isDupe && !isSetupAttachment) {
+                event.card.applyPersistentEffects();
+            }
+
+            game.queueSimpleStep(() => {
+                if(game.currentPhase !== 'setup' && event.card.isBestow()) {
+                    game.queueStep(new BestowPrompt(game, player, event.card));
+                }
+            });
         });
     }
 
