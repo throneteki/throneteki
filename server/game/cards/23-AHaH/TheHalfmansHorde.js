abadd44d2dbbf325d1cdf988d5e927f9d54ebe0b
Patane97 <patane97@gmail.com>
2022-07-08 09:47:03 +1000

    Implement The Halfman's Horde ver. 1

--- server/game/cards/23-AHaH/TheHalfmansHorde.js
+++ server/game/cards/23-AHaH/TheHalfmansHorde.js
@@ -2,7 +2,13 @@ const DrawCard = require('../../drawcard.js');
 const GameActions = require('../../GameActions');
 
 class TheHalfmansHorde extends DrawCard {
-    setupCardAbilities() {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.game.currentChallenge && this.game.currentChallenge.defendingPlayer.getNumberOfCardsInPlay(card => card.getType() === 'character') > this.controller.getNumberOfCardsInPlay(card => card.getType() === 'character'),
+            match: this,
+            effect: ability.effects.doesNotKneelAsAttacker()
+        });
+
         this.reaction({
             when: {
                 afterChallenge: event => event.challenge.winner === this.controller && this.isAttacking()
@@ -10,16 +16,16 @@ class TheHalfmansHorde extends DrawCard {
             message: {
                 format: '{player} uses {source} to discard {number} cards at random from {loser}\'s hand',
                 args: { 
-                    number: context => this.getNumberToDiscard(context.event.challenge.winner),
+                    number: context => this.getNumberOfUniqueAttackingClansman(context.event.challenge.winner),
                     loser: context => context.event.challenge.loser
                 }
             },
-            gameAction: GameActions.discardAtRandom(context => ({ player: context.event.challenge.loser, amount: this.getNumberToDiscard(context.event.challenge.winner) }))
+            gameAction: GameActions.discardAtRandom(context => ({ player: context.event.challenge.loser, amount: this.getNumberOfUniqueAttackingClansman(context.event.challenge.winner) }))
         });
     }
 
-    getNumberToDiscard(player) {
-        return player.getNumberOfCardsInPlay(card => card !== this && card.isUnique() && card.isAttacking() && card.hasTrait('Clansman') && card.getType() === 'character');
+    getNumberOfUniqueAttackingClansman(player) {
+        return player.getNumberOfCardsInPlay(card => card.isUnique() && card.isAttacking() && card.hasTrait('Clansman') && card.getType() === 'character');
     }
 }
 
