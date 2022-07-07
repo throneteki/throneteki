383c0ec2658dfd4d0487d9b8da93bc07f3b4859f
Patane97 <patane97@gmail.com>
2022-07-07 23:38:24 +1000

    Implement The Reader's Septon ver. 1

---
+++ server/game/cards/23-AHaH/TheReadersSepton.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheReadersSepton extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Swap top and bottom cards',
+            cost: ability.costs.kneelSelf(),
+            choosePlayer: () => true,
+            message: '{player} uses {source} to swap the top and bottom card of {chosenPlayer}\'s deck.',
+            handler: context => {
+                let chosenPlayer = context.chosenPlayer;
+                let topCard = chosenPlayer.drawDeck[0];
+                let bottomCard = chosenPlayer.drawDeck.slice(-1)[0];
+                chosenPlayer.moveCard(topCard, 'draw deck', { bottom: true });
+                chosenPlayer.moveCard(bottomCard, 'draw deck');
+            } 
+        });
+    }
+}
+
+TheReadersSepton.code = '23003';
+
+module.exports = TheReadersSepton;
