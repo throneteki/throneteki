0ecbaa83605068da6a3aa253c76cf5f38109b299
Patane97 <patane97@gmail.com>
2022-07-07 23:58:02 +1000

    Implement Torrhen's Square ver. 1

--- server/game/cards/23-AHaH/TorrhensSquare.js
+++ server/game/cards/23-AHaH/TorrhensSquare.js
@@ -1,33 +1,36 @@
 const DrawCard = require('../../drawcard.js');
-const GameActions = require('../../GameActions');
+const GameActions = require('../../GameActions')
 
 class TorrhensSquare extends DrawCard {
     setupCardAbilities(ability) {
         this.plotModifiers({
             initiative: -2
         });
+
+        this.persistentEffect({
+            match: card => card.name === 'Dagmer Cleftjaw' && card.controller === this.controller,
+            effect: [
+                ability.effects.modifyStrength(2),
+                ability.effects.doesNotKneelAsDefender()
+            ]
+        });
+
         this.reaction({
             when: {
-                onCardOutOfShadows: event => event.card.controller === this.controller && event.card.isFaction('greyjoy')
+                onInitiativeDetermined: event => event.winner !== this.controller
             },
-            cost: ability.costs.kneelSelf(),
             message: {
-                format: '{player} kneels {source} to discard the top {amount} cards from {opponents} deck',
-                args: { 
-                    amount: context => this.getNumberOfRaiders(context.player),
-                    opponents: context => this.getOpponentsToDiscard(context)
-                }
+                format: '{player} uses {source} to discard the top {amount} cards from each opponents deck',
+                args: { amount: context => this.getNumberOfRaiders(context.player) }
             },
             gameAction: GameActions.simultaneously(context => 
-                this.getOpponentsToDiscard(context).map(player => GameActions.discardTopCards({ player: player, amount: this.getNumberOfRaiders(context.player) }))
+                context.game.getOpponentsInFirstPlayerOrder(context.player).map(player => GameActions.discardTopCards({ player: player, amount: this.getNumberOfRaiders(context.player) }))
             )
         });
     }
-    getOpponentsToDiscard(context) {
-        return context.game.getOpponentsInFirstPlayerOrder(context.player).filter(player => player.getTotalInitiative() >= context.player.getTotalInitiative());
-    }
+
     getNumberOfRaiders(player) {
-        return Math.min(player.getNumberOfCardsInPlay({ type: 'character', trait: 'Raider' }), 3);
+        return player.filterCardsInPlay(card => card.hasTrait('Raider')).length;
     }
 }
 
