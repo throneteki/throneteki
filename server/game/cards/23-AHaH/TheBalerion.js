fce8f1d7332898c50db3b0d3c310f7159c6ce575
Patane97 <patane97@gmail.com>
2022-07-08 18:07:56 +1000

    Implemented The Balerion ver. 1

--- server/game/cards/23-AHaH/TheBalerion.js
+++ server/game/cards/23-AHaH/TheBalerion.js
@@ -3,34 +3,49 @@ const DrawCard = require('../../drawcard.js');
 class TheBalerion extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            match: card => card.name === 'Captain Groleo' && card.controller === this.controller,
+            match: card => !this.isConverted && card.name === 'Captain Groleo' && card.controller === this.controller,
             effect: [
-                ability.effects.addIcon('power'),
-                ability.effects.addKeyword('stealth')
+                ability.effects.addTrait('Commander'),
+                ability.effects.addIcon('power')
+            ]
+        });
+        this.persistentEffect({
+            condition: () => this.isConverted && this.game.isDuringChallenge({ challengeType: 'power' }),
+            match: card => card === this.parent,
+            effect: [
+                ability.effects.addKeyword('assault')
             ]
         });
         this.action({
-            title: 'Kneel and shuffle into deck',
-            cost: [
-                ability.costs.kneelSelf(),
-                ability.costs.shuffleSelfIntoDeck()
-            ],
+            title: 'Kneel and attach to a character',
+            cost: ability.costs.kneelSelf(),
             target: {
                 title: 'Select a character',
-                cardCondition: { faction: 'targaryen', type: 'character', location: 'play area' }
+                cardCondition: card => card.isMatch({ trait: ['Army', 'Stormborn'], controller: this.controller } && this.isCardEligibleToHaveWeaponAttachments(card))
             },
             phase: 'challenge',
-            message: '{player} kneels {source} and shuffles it into their deck to give {target} +2 STR and assault until the end of the phase',
+            message: '{player} kneels {source} to attach it to {target} as a Weapon attachment, with it gaining assault during power challenges',
             handler: context => {
-                this.untilEndOfPhase(ability => ({
-                    match: context.target,
+                context.player.attach(context.player, this, context.target, 'play', false);
+
+                this.lastingEffect(ability => ({
+                    targetLocation: 'any',
+                    match: this,
                     effect: [
-                        ability.effects.modifyStrength(2),
-                        ability.effects.addKeyword('assault')
+                        ability.effects.setCardType('attachment'),
+                        ability.effects.addKeyword('Terminal'),
+                        ability.effects.addTrait('Weapon')
                     ]
                 }));
+
+                this.isConverted = true;
             } 
         });
+    }
+    isCardEligibleToHaveWeaponAttachments(card) {
+        let noAttachmentKeywords = card.getKeywords().filter(keyword => keyword.startsWith('no attachments'));
+        return noAttachmentKeywords.length === 0 || 
+            (noAttachmentKeywords.length === 1 && noAttachmentKeywords[0] === 'no attachments except <i>weapon</i>');
     }
 }
 
