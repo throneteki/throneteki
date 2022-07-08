629a4303b0ca80fcccf2bf41bc536f4330dad1ee
Patane97 <patane97@gmail.com>
2022-07-08 11:01:13 +1000

    Implement The Warden of the West ver. 1

---
+++ server/game/cards/23-AHaH/TheWardenOfTheWest.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../drawcard.js');
+const GameActions = require('../../GameActions');
+
+class TheWardenOfTheWest extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ faction: 'lannister', trait: 'Lord' });
+        this.reaction({
+            when: {
+                // TODO: Implement player-aggregate so it only looks at cards being discarded from an individuals hands rather than aggregating all cards being discarded at once
+                'onCardDiscarded:aggregate': event => this.getNumberToDraw(event) > 0 && this.allowGameAction('drawCards') && this.game.currentPhase === 'challenge'
+            },
+            limit: ability.limit.perRound(1),
+            message: {
+                format: '{player} uses {source} to draw {amount} cards',
+                args: { amount: context => this.getNumberToDraw(context.event) }
+            },
+            gameAction: GameActions.drawCards(context => ({ player: context.player, amount: this.getNumberToDraw(context.event) }))
+        });
+    }
+
+    getNumberToDraw(event) {
+        return event.events.filter(discardEvent => (
+            discardEvent.cardStateWhenDiscarded.controller !== this.controller &&
+            discardEvent.cardStateWhenDiscarded.location === 'hand'
+        )).length;
+    }
+}
+
+TheWardenOfTheWest.code = '23006';
+
+module.exports = TheWardenOfTheWest;
