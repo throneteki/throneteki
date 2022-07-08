78b632272b976caa84a865c4749bfdadaccd7f4b
Patane97 <patane97@gmail.com>
2022-07-08 11:13:02 +1000

    Implement Mutineer from Craster's Keep ver. 1

---
+++ server/game/cards/23-AHaH/MutineerFromCrastersKeep.js
@@ -0,0 +1,28 @@
+const { context } = require('raven');
+const DrawCard = require('../../drawcard.js');
+const GameActions = require('../../GameActions');
+
+class MutineerFromCrastersKeep extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardEntersPlay: event => event.card === this
+            },
+            target: {
+                title: 'Select a duplicate',
+                cardCondition: card => card.location === 'duplicate'
+            },
+            message: {
+                format: '{player} uses {source} to discard a duplicate on {duplicateParent}',
+                args: { duplicateParent: context => context.target.parent }
+            },
+            handler: context => {
+                this.game.resolveGameAction(GameActions.discardCard({ card: context.target }), context);
+            }
+        })
+    }
+}
+
+MutineerFromCrastersKeep.code = '23009';
+
+module.exports = MutineerFromCrastersKeep;
