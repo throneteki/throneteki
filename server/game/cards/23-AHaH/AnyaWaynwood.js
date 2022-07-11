d096ca228a39c453d2f2e56bb4bf6b43c8229a92
Patane97 <patane97@gmail.com>
2022-07-11 23:52:25 +1000

    Implement Anya Waynwood ver. 1

--- server/game/cards/23-AHaH/AnyaWaynwood.js
+++ server/game/cards/23-AHaH/AnyaWaynwood.js
@@ -3,42 +3,25 @@ const GameActions = require('../../GameActions/index.js');
 
 class AnyaWaynwood extends DrawCard {
     setupCardAbilities(ability) {
-        this.persistentEffect({
-            match: card => card.name === 'Harry the Heir' && card.controller === this.controller,
-            effect: ability.effects.addKeyword('Renown')
-        });
-
-        this.action({
-            title: 'Contribute STR and kneel',
-            phase: 'challenge',
+        this.reaction({
+            when: {
+                onChallengeInitiated: event => event.challenge.initiatedAgainstPlayer === this.controller
+            },
             cost: ability.costs.kneel(card => card.getType() === 'location' && card.isFaction('neutral')),
-            condition: () => this.game.isDuringChallenge({ match: challenge => [challenge.attackingPlayer, challenge.defendingPlayer].includes(this.controller) }),
             target: {
                 title: 'Select a character',
-                cardCondition: {
-                    location: 'play area',
-                    type: 'character',
-                    participating: false,
-                    condition: (card, context) => !context.costs.kneel || card.getPrintedCost() <= context.costs.kneel.getPrintedCost()
-                }
+                cardCondition: { attacking: true, condition: (card, context) => !context.costs.kneel || card.getPrintedCost() <= context.costs.kneel.getPrintedCost() }
             },
             message: {
-                format: '{player} uses {source} and kneels {kneel} to have {target} contribute its STR (currently {STR}) to {player}\'s side until the end of the challenge',
-                args: {
-                    kneel: context => context.costs.kneel,
-                    STR: context => context.target.getStrength()
-                }
+                format: '{player} uses {source} and kneels {kneel} to kneel and take control of {target} until the end of the challenge',
+                args: { kneel: context => context.costs.kneel }
             },
             handler: context => {
-                this.untilEndOfChallenge(ability => ({
-                    // Force the effect to recalculate mid-challenge in case the character STR changes
-                    condition: () => true,
-                    targetController: 'current',
-                    effect: ability.effects.contributeChallengeStrength(() => context.target.getStrength())
-                }));
-
-                this.game.addMessage('Then, {0} kneels {1}', context.player, context.target);
                 this.game.resolveGameAction(GameActions.kneelCard({ card: context.target }), context);
+                this.untilEndOfChallenge(ability => ({
+                    match: context.target,
+                    effect: ability.effects.takeControl(this.controller)
+                }));
             }
         });
     }
