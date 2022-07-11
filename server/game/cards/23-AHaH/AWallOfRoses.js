f8a97e13f3c37878cf85915d77cc6c8e74534a70
Patane97 <patane97@gmail.com>
2022-07-11 23:51:37 +1000

    Fix for A Wall of Roses

--- server/game/cards/23-AHaH/AWallOfRoses.js
+++ server/game/cards/23-AHaH/AWallOfRoses.js
@@ -8,7 +8,7 @@ class AWallOfRoses extends DrawCard {
             when: {
                 onRemovedFromChallenge: event => event.isAttacking
             },
-            cost: ability.costs.payXGold(() => 0, context => context.event.challenge.defenders.length),
+            cost: ability.costs.payXGold(() => 1, context => context.event.challenge.defenders.length),
             message: {
                 format: '{player} uses {source} to choose {xValue} characters',
                 args: { xValue: context => context.xValue }
@@ -21,30 +21,30 @@ class AWallOfRoses extends DrawCard {
                     activePromptTitle: `Select ${TextHelper.count(xValue, 'character')}`,
                     source: this,
                     cardCondition: { defending: true },
-                    onSelect: (player, cards) => this.targetsSelected(player, cards)
+                    onSelect: (player, cards) => this.targetsSelected(context, player, cards)
                 });
             } 
         });
     }
 
-    targetsSelected(player, cards) {
+    targetsSelected(context, player, cards) {
         this.game.addMessage('{0} chooses {1} for {2}', player, cards, this);
 
-        this.game.once('afterChallenge:interrupt', event => this.resolveIfWin(player, event.challenge, cards));
+        this.game.once('afterChallenge:interrupt', event => this.resolveIfWin(context, player, event.challenge, cards));
 
         return true;
     }
 
-    resolveIfWin(player, challenge, cards) {
+    resolveIfWin(context, player, challenge, cards) {
         if(challenge.winner !== player) {
             return;
         }
 
-        let actions = [GameActions.gainPower({ card: player.faction, amount: 2 })];
-        actions.concat(cards.map(card => GameActions.standCard({ card: card })));
+        let gameActions = [GameActions.gainPower({ card: player.faction, amount: 2 })];
+        gameActions.concat(cards.map(card => GameActions.standCard({ card: card })));
         
-        this.game.addMessage('{0} uses {1} to gain 2 power and stand {2}', player, this, cards);
-        this.game.resolveGameAction(GameActions.simultaneously(actions));
+        this.game.addMessage('{0} then uses {1} to gain 2 power and stand {2}', player, this, cards);
+        this.game.resolveGameAction(GameActions.simultaneously(gameActions), context);
     }
 }
 
