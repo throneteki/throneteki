3812edbe97f3c0cb222a0ae0ebb47a6d8403a5dd
Patane97 <patane97@gmail.com>
2022-07-15 11:48:56 +1000

    Implement Sweetrobin ver. 1

--- server/game/cards/23-AHaH/Sweetrobin.js
+++ server/game/cards/23-AHaH/Sweetrobin.js
@@ -1,20 +1,45 @@
 const DrawCard = require('../../drawcard.js');
-const GameActions = require('../../GameActions');
 
 class Sweetrobin extends DrawCard {
     setupCardAbilities(ability) {
-        this.interrupt({
-            when: {
-                // TODO: As this currently isn't properly implemented, confirm this works after Alla Implementation is added
-                onCardRevealed: event => event.card.hasPrintedCost() && event.card.getPrintedCost() <= event.card.owner.getTotalInitiative()
-            },
-            limit: ability.limit.perPhase(1),
-            message: {
-                format: '{player} uses {source} to remove {revealed} from the game',
-                args: { revealed: context => context.event.card }
-            },
-            gameAction: GameActions.removeFromGame(context => ({ card: context.event.card }))
+        this.persistentEffect({
+            match: card => card === this,
+            effect: ability.effects.immuneTo(card => card.getType() === 'plot')
         });
+
+        this.forcedReaction({
+            when: {
+                onChallengeInitiated: event => event.challenge.challengeType === 'military'
+            },
+            message: '{player} is forced by {source} to either reveal their hand/shadows, or reduce the claim on their revealed plot card by 1 until the end of the challenge',
+            handler: context => {
+                this.game.promptWithMenu(context.player, this, {
+                    activePrompt: {
+                        menuTitle: `Choose one for ${this.name}`,
+                        buttons: [
+                            { text: 'Reveal hand and shadows', method: 'reveal' },
+                            { text: 'Reduce claim', method: 'reduce' }
+                        ]
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    reveal(player) {
+        let revealing = this.game.allCards.filter(card => card.controller === player && ['hand', 'shadows'].includes(card.location));
+        // this.resolveGameAction(GameActions.simultaneously(revealing.map(reveal => GameActions.revealCard({ card: reveal })))); // TODO: Update to this with Alla update
+        this.game.addMessage('{0} chooses to reveal {1} from their hand and shadows area', player, revealing);
+        return true;
+    }
+    reduce(player) {
+        this.untilEndOfChallenge(ability => ({
+            match: card => card === player.activePlot,
+            effect: ability.effects.modifyClaim(-1)
+        }));
+        this.game.addMessage('{0} chooses to reduce the claim on their revealed plot card by 1 until the end of the challenge', player);
+        return true;
     }
 }
 
