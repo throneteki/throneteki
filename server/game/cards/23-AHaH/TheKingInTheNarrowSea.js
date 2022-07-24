67542531c1e6bfa5e50ba5c84abf2d5e69b7131a
Patane97 <patane97@gmail.com>
2022-07-24 22:07:57 +1000

    Updating The King in the Narrow Sea to v1.1

--- server/game/cards/23-AHaH/TheKingInTheNarrowSea.js
+++ server/game/cards/23-AHaH/TheKingInTheNarrowSea.js
@@ -3,27 +3,40 @@ const GameActions = require('../../GameActions');
 
 class TheKingInTheNarrowSea extends DrawCard {
     setupCardAbilities(ability) {
-        this.persistentEffect({
-            targetLocation: 'hand',
-            match: card => card.hasTrait('Smuggler') && card.getType() === 'character',
-            effect: ability.effects.gainAmbush()
+        this.attachmentRestriction(
+            { faction: 'baratheon', unique: true },
+        );
+        this.plotModifiers({
+            initiative: -1
         });
         this.reaction({
             when: {
                 onInitiativeDetermined: event => event.winner !== this.controller
             },
             cost: ability.costs.returnToHand(card => card.isMatch({ trait: ['Mercenary', 'Smuggler'], type: 'character' })),
-            target: {
-                activePromptTitle: 'Select a location',
-                cardCondition: { type: 'location', location: 'play area', condition: (card, context) => !context.costs.returnToHand || card.getPrintedCost() < context.costs.returnToHand.getPrintedCost() },
-                gameAction: 'kneel'
+            targets: {
+                character: {
+                    activePromptTitle: 'Select a character',
+                    cardCondition: { type: 'character', location: 'play area', condition: (card, context) => !context.costs.returnToHand || card.getPrintedCost() < context.costs.returnToHand.getPrintedCost() }
+                },
+                location: {
+                    activePromptTitle: 'Select a location',
+                    cardCondition: { type: 'location', location: 'play area', condition: (card, context) => !context.costs.returnToHand || card.getPrintedCost() < context.costs.returnToHand.getPrintedCost() }
+                }
             },
             message: {
-                format: '{player} uses {source} and returns {returnedToHand} to its owners hand to kneel {target}',
-                args: { returnedToHand: context => context.costs.returnToHand }
+                format: '{player} uses {source} and returns {returnedToHand} to its owners hand to kneel {knelt}',
+                args: { 
+                    returnedToHand: context => context.costs.returnToHand,
+                    knelt: context => context.targets.getTargets()
+                }
             },
             handler: context => {
-                this.game.resolveGameAction(GameActions.kneelCard({ card: context.target }), context);
+                this.game.resolveGameAction(
+                    GameActions.simultaneously(
+                        context.targets.getTargets().map(card => GameActions.kneelCard({ card: card }), context)
+                    ), context
+                );
             }
         });
     }
