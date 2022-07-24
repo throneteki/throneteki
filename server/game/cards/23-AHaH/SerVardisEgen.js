8b6ee0b7441ee81ee495aea8c6bfd5a972b759f0
Patane97 <patane97@gmail.com>
2022-07-24 21:41:20 +1000

    Implement Ser Vardis Egen ver. 1

---
+++ server/game/cards/23-AHaH/SerVardisEgen.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard.js');
+
+class SerVardisEgen extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {                
+                afterChallenge: event => this.isDefending() && event.challenge.isMatch({ winner: this.controller, challengeType: 'military' })
+            },
+            cost: ability.costs.sacrificeSelf(),
+            message: '{player} sacrifices {source} to be able to initiate an additional power challenge this phase',
+            handler: () => {
+                this.untilEndOfPhase(ability => ({
+                    targetController: 'current',
+                    effect: ability.effects.mayInitiateAdditionalChallenge('power')
+                }));
+            }
+        })
+    }
+}
+
+SerVardisEgen.code = '23026';
+
+module.exports = SerVardisEgen;
