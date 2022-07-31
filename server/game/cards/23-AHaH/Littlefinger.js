ea8819a2cde9d8967564b47d79df43e48964115d
EvadLhorg <lukas.a-v@web.de>
2022-07-31 13:02:50 +0200

    update littlefinger to v1.1

--- server/game/cards/23-AHaH/Littlefinger.js
+++ server/game/cards/23-AHaH/Littlefinger.js
@@ -1,28 +1,36 @@
 const DrawCard = require('../../drawcard.js');
 const GameActions = require('../../GameActions/index.js');
+const Messages = require('../../Messages');
 
 class Littlefinger extends DrawCard {
     setupCardAbilities() {
         this.reaction({
             when: {
-                onDeclaredAsDefender: event => event.card.controller === this.controller && event.card !== this && event.card.isUnique() && event.card.hasTrait('House Arryn')
-            },
-            target: {
-                title: 'Select a character',
-                choosingPlayer: 'each',
-                ifAble: true,
-                cardCondition: { participating: true, condition: (card, context) => card.hasPrintedCost() && card.getPrintedCost() <= context.choosingPlayer.getTotalInitiative() }
+                afterChallenge: event => event.challenge.winner === this.controller
             },
             message: {
-                format: '{player} uses {source} to have each player choose and return {cards} to its owners hands',
-                args: { cards: context => context.targets.selections.filter(selection => selection.value).map(selection => selection.value) }
+                format: '{player} uses {source} to have each player choose and reveal 1 card from their hand or shadows area'
+            },
+            target: {
+                choosingPlayer: 'each',
+                ifAble: true,
+                activePromptTitle: 'Select a card',
+                cardCondition: (card, context) => ['hand', 'shadows'].includes(card.location) && card.controller === context.choosingPlayer,
+                gameAction: 'reveal',
+                messages: Messages.eachPlayerSecretTargetingForCardType('cards in hand/shadows')
             },
             handler: context => {
-                let cards = context.targets.selections.filter(selection => selection.value).map(selection => selection.value);
+                let cards = context.targets.selections.map(selection => selection.value).filter(card => !!card);
                 this.game.resolveGameAction(
-                    GameActions.simultaneously(cards.map(card => GameActions.returnCardToHand({ card }))),
-                    context
+                    GameActions.simultaneously(cards.map(card => GameActions.revealCard({ card })))
                 );
+                let playerCardType = cards.filter(card => card.owner === context.player)[0].getPrintedType();
+                let otherCardTypes = cards.filter(card => card.owner !== context.player).map(card => card.getPrintedType());
+                if(!otherCardTypes.includes(playerCardType)) {
+                    this.game.addMessage('{0} uses {1} to stand {1} and have him gain 1 power', context.player, this);
+                    context.player.standCard(this);
+                    this.modifyPower(1);
+                }
             }
         });
     }
