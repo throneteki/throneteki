687e4bdbe7bc63bfafac02a5a8c933c8d5351631
EvadLhorg <lukas.a-v@web.de>
2022-08-06 11:00:33 +0200

    update banner of the falcon to ver 1.1

--- server/game/cards/23-AHaH/BannerOfTheFalcon.js
+++ server/game/cards/23-AHaH/BannerOfTheFalcon.js
@@ -1,67 +1,31 @@
 const AgendaCard = require('../../agendacard');
-const GameActions = require('../../GameActions');
+const AbilityLimit = require('../../abilitylimit.js');
+const EventPlayedTracker = require('../../EventTrackers/EventPlayedTracker');
+const CardEntersPlayTracker = require('../../EventTrackers/CardEntersPlayTracker');
 
 class BannerOfTheFalcon extends AgendaCard {
     setupCardAbilities(ability) {
+        this.eventPlayedTracker = EventPlayedTracker.forRound(this.game);
+        this.enteredPlayTracker = CardEntersPlayTracker.forRound(this.game);
+        const inFactionMatcher = card => card.isFaction(card.owner.faction.getPrintedFaction());
+
         this.persistentEffect({
-            condition: () => !this.game.isDuringChallenge({
-                attackingPlayer: this.controller,
-                match: challenge => challenge.attackers.some(attacker => attacker.hasTrait('House Arryn'))
-            }),
-            match: card => card === this.controller.activePlot,
-            effect: ability.effects.modifyClaim(-1)
-        });
-
-        // TODO: If a limit is introduced on this card, it will need to be combined into a proper, single reaction
-        // instead of 3 "separate" reactions.
-        this.reaction({
-            when: {
-                afterChallenge: event => event.challenge.isMatch({ winner: this.controller, by5: true, challengeType: 'military' })
-            },
-            target: {
-                cardCondition: card => card.isParticipating(),
-                gameAction: 'gainPower'
-            },
-            message: '{player} uses {source} to have {target} gain 1 power',
-            handler: context => {
-                this.game.resolveGameAction(
-                    GameActions.gainPower(context => ({
-                        card: context.target,
-                        amount: 1
-                    })),
-                    context
-                );
-            }
-        });
-
-        this.reaction({
-            when: {
-                afterChallenge: event => event.challenge.isMatch({ winner: this.controller, by5: true, challengeType: 'intrigue' })
-            },
-            message: '{player} uses {source} to draw 1 card',
-            gameAction: GameActions.drawCards(context => ({
-                player: context.player,
+            targetController: 'any',
+            match: player => !this.eventPlayedTracker.hasPlayedEvent(player, inFactionMatcher)
+                                && !this.enteredPlayTracker.hasPlayerAmbushedAnyCardWithPredicate(player, inFactionMatcher)
+                                && !this.enteredPlayTracker.hasPlayerBroughtOutOfShadowsAnyCardWithPredicate(player, inFactionMatcher),
+            effect: ability.effects.increaseCost({
+                playingTypes: ['play', 'ambush', 'outOfShadows'],
+                limit: AbilityLimit.perRound(1),
+                match: inFactionMatcher,
                 amount: 1
-            }))
+            })
         });
 
-        this.reaction({
-            when: {
-                afterChallenge: event => event.challenge.isMatch({ winner: this.controller, by5: true, challengeType: 'power' })
-            },
-            target: {
-                cardCondition: card => card.isParticipating() && card.kneeled,
-                gameAction: 'stand'
-            },
-            message: '{player} uses {source} to stand {target}',
-            handler: context => {
-                this.game.resolveGameAction(
-                    GameActions.standCard(context => ({
-                        card: context.target
-                    })),
-                    context
-                );
-            }
+        this.persistentEffect({
+            condition: () => this.game.getOpponents(this.controller).every(player => player.getTotalInitiative() > this.controller.getTotalInitiative()),
+            match: card => card.hasTrait('House Arryn') && card.getType() === 'character' && card.controller === this.controller,
+            effect: ability.effects.modifyStrength(1)
         });
     }
 }
