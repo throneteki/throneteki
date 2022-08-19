adf5fd8fbbf0933bf178d8c46229c958f779f9c5
Brian Upton <ystros@gmail.com>
2022-08-18 18:03:46 -0700

    Implement The Eyrie v1.2 (AHaH)

--- server/game/cards/23-AHaH/TheEyrie.js
+++ server/game/cards/23-AHaH/TheEyrie.js
@@ -1,17 +1,23 @@
 const DrawCard = require('../../drawcard');
+const {ChallengeTracker} = require('../../EventTrackers')
 
 class TheEyrie extends DrawCard {
     setupCardAbilities(ability) {
+        this.challengeTracker = ChallengeTracker.forPhase(this.game);
+
         this.plotModifiers({
             initiative: -2,
-            reserve: 2
+            reserve: 1
         });
 
         this.persistentEffect({
             condition: () => !this.kneeled,
-            targetController: 'current',
-            match: card => card.controller === this.controller && card.hasTrait('House Arryn') && card.isUnique(),
-            effect: ability.effects.dynamicStrength(() => this.power)
+            targetController: 'opponent',
+            match: opponent => (
+                opponent.getTotalPower() > this.controller.getTotalPower() &&
+                this.challengeTracker.count({ initiatingPlayer: opponent, initiatedAgainstPlayer: this.controller }) >= 2
+            ),
+            effect: ability.effects.cannotInitiateChallengeType('any', opponent => opponent === this.controller)
         });
 
         this.reaction({
@@ -19,31 +25,10 @@ class TheEyrie extends DrawCard {
                 onPhaseStarted: event => event.phase === 'challenge'
             },
             cost: ability.costs.kneelSelf(),
-            choosePlayer: true,
+            chooseOpponent: opponent => !opponent.firstPlayer,
+            message: '{player} kneels {source} to have {opponent} become first player',
             handler: context => {
-                this.game.setFirstPlayer(context.chosenPlayer);
-                this.game.addMessage('{0} kneels {1} to have {2} become first player', context.player, this, context.chosenPlayer);
-            }
-        });
-
-        this.interrupt({
-            //limit once per round so you can´t cancel the cancel with another power
-            limit: ability.limit.perRound(1),
-            //TODO this only works in joust, not in meelee
-            player: () => this.game.getOpponents(this.controller)[0],
-            when: {
-                onCardAbilityInitiated: event => event.ability.isTriggeredAbility() &&
-                                                 event.source === this &&
-                                                 event.source.controller === this.controller
-            },
-            cost: ability.costs.movePowerFromCardToFixedTarget({
-                target: this,
-                amount: 1,
-                condition: (card, context) => ((card.controller === context.player && card.location === 'play area') || card === context.player.faction) && card.power > 0
-            }),
-            handler: context => {
-                context.event.cancel();
-                this.game.addMessage('{0} moves 1 power from {1} to {2} to cancel {2}', context.player, context.costs.movePowerFromCard, this, context.event.source);
+                this.game.setFirstPlayer(context.opponent);
             }
         });
     }
