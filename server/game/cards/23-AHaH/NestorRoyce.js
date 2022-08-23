ba3e1ad81d2f15e9de396420fa0ee9eaf99ef1ee
Brian Upton <ystros@gmail.com>
2022-08-22 19:44:17 -0700

    Implement Nestor Royce v1.2

--- server/game/cards/23-AHaH/NestorRoyce.js
+++ server/game/cards/23-AHaH/NestorRoyce.js
@@ -1,25 +1,38 @@
 const DrawCard = require('../../drawcard');
-const GameActions = require('../../GameActions');
 
 class NestorRoyce extends DrawCard {
     setupCardAbilities(ability) {
-        this.persistentEffect({
-            condition: () => this.game.getOpponents(this.controller).every(player => player.getTotalInitiative() > this.controller.getTotalInitiative()),
-            match: this,
-            effect: ability.effects.addKeyword('Renown')
-        });
-
         this.reaction({
             when: {
                 afterChallenge: event => this.isAttacking() && event.challenge.isMatch({ winner: this.controller })
             },
             cost: ability.costs.kneel({ type: 'location', faction: 'neutral', printedCostOrHigher: 1 }),
             message: {
-                format: '{player} uses {source} and kneels {kneel} to stand {source}',
+                format: '{player} uses {source} and kneels {kneel} to give {source} a keyword',
                 args: { kneel: context => context.costs.kneel }
             },
-            gameAction: GameActions.standCard({ card: this })
+            handler: context => {
+                this.game.promptWithMenu(context.player, this, {
+                    activePrompt: {
+                        menuTitle: `Gain keyword for ${context.source.name}`,
+                        buttons: [
+                            { text: 'Renown', arg: 'renown', method: 'gainKeyword' },
+                            { text: 'Intimidate', arg: 'intimidate', method: 'gainKeyword' }
+                        ]
+                    },
+                    source: context.source
+                });
+            }
         });
+    }
+
+    gainKeyword(player, keyword) {
+        this.game.addMessage('{0} gains {1}', this, keyword);
+        this.untilEndOfChallenge(ability => ({
+            match: this,
+            effect: ability.effects.addKeyword(keyword)
+        }));
+        return true;
     }
 }
 
