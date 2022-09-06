29ffff7c6febdd21f194697f14cb481c5d1d9766
Patane97 <patane97@gmail.com>
2022-09-06 11:57:48 +1000

    eslint test fix

--- server/game/cards/23-AHaH/Littlefinger.js
+++ server/game/cards/23-AHaH/Littlefinger.js
@@ -39,7 +39,7 @@ class Littlefinger extends DrawCard {
                         })
                     }),
                     context
-                )
+                );
             }
         });
     }
