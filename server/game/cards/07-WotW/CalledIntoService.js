1b7085f890060958a3dc8cde69be6d8389e03acf
Patane97 <patane97@gmail.com>
2022-09-17 17:32:12 +1000

    Minor tweaks to CalledIntoService & Pentos

--- server/game/cards/07-WotW/CalledIntoService.js
+++ server/game/cards/07-WotW/CalledIntoService.js
@@ -1,5 +1,6 @@
 const PlotCard = require('../../plotcard.js');
 const GameActions = require('../../GameActions');
+const { context } = require('raven');
 
 class CalledIntoService extends PlotCard {
     setupCardAbilities() {
@@ -11,7 +12,10 @@ class CalledIntoService extends PlotCard {
                 message: '{player} {gameAction}',
                 gameAction: GameActions.ifCondition({
                     condition: context => context.event.cards[0].getType() === 'character',
-                    thenAction: GameActions.simultaneously(context => context.event.revealed.map(card => GameActions.putIntoPlay({ card }))),
+                    thenAction: GameActions.ifCondition({
+                        condition: context => context.event.revealed.length > 0,
+                        thenAction: GameActions.putIntoPlay({ card: context.event.revealed[0] })
+                    }),
                     elseAction: GameActions.simultaneously(context => [
                         GameActions.drawSpecific(context => ({
                             player: context.player,
--- server/game/cards/15-DotE/Pentos.js
+++ server/game/cards/15-DotE/Pentos.js
@@ -23,9 +23,9 @@ class Pentos extends DrawCard {
                 gameAction: GameActions.putIntoPlay(context => ({
                     card: context.event.revealed[0]
                 })).then({
-                    message: 'Then {player} draws 1 card',
-                    gameAction: GameActions.drawCards(thenContext => ({
-                        player: thenContext.player,
+                    message: 'Then, {player} draws 1 card',
+                    gameAction: GameActions.drawCards(context => ({
+                        player: context.player,
                         amount: 1
                     }))
                 })
