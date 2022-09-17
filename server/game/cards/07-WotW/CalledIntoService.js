281053956acf6823d8f07454654715cb51269877
Patane97 <patane97@gmail.com>
2022-09-17 17:38:15 +1000

    Correcting CalledIntoService

--- server/game/cards/07-WotW/CalledIntoService.js
+++ server/game/cards/07-WotW/CalledIntoService.js
@@ -1,6 +1,5 @@
 const PlotCard = require('../../plotcard.js');
 const GameActions = require('../../GameActions');
-const { context } = require('raven');
 
 class CalledIntoService extends PlotCard {
     setupCardAbilities() {
@@ -14,7 +13,7 @@ class CalledIntoService extends PlotCard {
                     condition: context => context.event.cards[0].getType() === 'character',
                     thenAction: GameActions.ifCondition({
                         condition: context => context.event.revealed.length > 0,
-                        thenAction: GameActions.putIntoPlay({ card: context.event.revealed[0] })
+                        thenAction: GameActions.putIntoPlay(context => ({ card: context.event.revealed[0] }))
                     }),
                     elseAction: GameActions.simultaneously(context => [
                         GameActions.drawSpecific(context => ({
