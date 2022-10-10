3cfae0f2aafccea13a749e0aae975b343b2e79d5
Stephen Patane <patane97@gmail.com>
2022-10-10 13:22:07 +1100

    Updated Bannerofthefalcon to ver. 1.2

--- server/game/cards/23-AHaH/BannerOfTheFalcon.js
+++ server/game/cards/23-AHaH/BannerOfTheFalcon.js
@@ -1,27 +1,7 @@
 const AgendaCard = require('../../agendacard');
-const AbilityLimit = require('../../abilitylimit.js');
-const EventPlayedTracker = require('../../EventTrackers/EventPlayedTracker');
-const CardEntersPlayTracker = require('../../EventTrackers/CardEntersPlayTracker');
 
 class BannerOfTheFalcon extends AgendaCard {
     setupCardAbilities(ability) {
-        this.eventPlayedTracker = EventPlayedTracker.forRound(this.game);
-        this.enteredPlayTracker = CardEntersPlayTracker.forRound(this.game);
-        const inFactionMatcher = card => card.isFaction(card.owner.faction.getPrintedFaction());
-
-        this.persistentEffect({
-            targetController: 'any',
-            match: player => !this.eventPlayedTracker.hasPlayedEvent(player, inFactionMatcher)
-                                && !this.enteredPlayTracker.hasPlayerAmbushedAnyCardWithPredicate(player, inFactionMatcher)
-                                && !this.enteredPlayTracker.hasPlayerBroughtOutOfShadowsAnyCardWithPredicate(player, inFactionMatcher),
-            effect: ability.effects.increaseCost({
-                playingTypes: ['play', 'ambush', 'outOfShadows'],
-                limit: AbilityLimit.perRound(1),
-                match: inFactionMatcher,
-                amount: 1
-            })
-        });
-
         this.persistentEffect({
             condition: () => this.game.getOpponents(this.controller).every(player => player.getTotalInitiative() > this.controller.getTotalInitiative()),
             match: card => card.hasTrait('House Arryn') && card.getType() === 'character' && card.controller === this.controller,
