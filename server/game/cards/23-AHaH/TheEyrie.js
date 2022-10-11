8501d3d4479625ae131abdb02ec6cd64d2a60f6d
Stephen Patane <patane97@gmail.com>
2022-10-11 23:19:58 +1100

    Updating The Eyrie to ver. 1.4

--- server/game/cards/23-AHaH/TheEyrie.js
+++ server/game/cards/23-AHaH/TheEyrie.js
@@ -1,45 +1,30 @@
 const DrawCard = require('../../drawcard');
-const {ChallengeTracker} = require('../../EventTrackers');
 
 class TheEyrie extends DrawCard {
     setupCardAbilities(ability) {
-        this.challengeTracker = ChallengeTracker.forPhase(this.game);
-
         this.plotModifiers({
             initiative: -2,
             reserve: 1
         });
 
         this.persistentEffect({
-            condition: () => this.controller.getTotalInitiative() === 0 && this.game.isDuringChallenge({ initiated: true }),
-            targetController: 'any',
-            match: card => card.getType() === 'character' && !card.isParticipating(),
-            effect: ability.effects.modifyStrength(2)
+            condition: () => this.controller.getTotalInitiative() === 0,
+            match: this,
+            effect: ability.effects.immuneTo(() => true)
         });
 
         this.action({
-            title: 'Contribute STR to challenge',
+            title: 'Contribute 3 STR',
             phase: 'challenge',
-            cost: ability.costs.kneelFactionCard(),
-            condition: () => this.game.isDuringChallenge(),
-            target: {
-                cardCondition: {
-                    location: 'play area',
-                    type: 'character',
-                    participating: false, // TODO: Remove this once contributeSTR properly prevents a participating character also contributing (adding STR twice)
-                    controller: 'current'
-                }
-            },
-            message: {
-                format: '{player} uses {source} and kneels their faction card to have {target} contribute its STR (currently {STR}) to {player}\'s this challenge',
-                args: { STR: context => context.target.getStrength() }
-            },
-            handler: context => {
+            cost: ability.costs.kneelSelf(),
+            condition: () => this.game.isDuringChallenge() && this.game.currentChallenge.anyParticipants(card => card.controller === this.controller),
+            message: '{player} kneels {source} to have it contribute 3 STR to {player}\'s side of the challenge',
+            handler: () => {
+                // TODO: Update this (contribute strength) to a GameAction
                 this.untilEndOfChallenge(ability => ({
-                    // Force the effect to recalculate mid-challenge in case the character STR changes
                     condition: () => true,
                     targetController: 'current',
-                    effect: ability.effects.contributeChallengeStrength(() => context.target.getStrength())
+                    effect: ability.effects.contributeChallengeStrength(3)
                 }));
             }
         });
