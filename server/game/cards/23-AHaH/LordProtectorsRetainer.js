984029df2d6bbcc12b6a44bf3200f10230b9a5ce
Stephen Patane <patane97@gmail.com>
2022-10-27 17:18:43 +1100

    Applying fix to LPR + updating similar card FCDTS

--- server/game/cards/23-AHaH/LordProtectorsRetainer.js
+++ server/game/cards/23-AHaH/LordProtectorsRetainer.js
@@ -7,12 +7,16 @@ class LordProtectorsRetainer extends DrawCard {
         this.interrupt({
             canCancel: true,
             when: {
-                onCardAbilityInitiated: event => event.targets.hasSingleTarget() &&
-                                                event.targets.anySelection(selection => (
-                                                    selection.value.controller === this.controller &&
-                                                    selection.value.isMatch({ trait: ['Lord', 'Lady'], type: 'character' })
-                                                ))
+                onCardAbilityInitiated: event => event.ability.targets.some(target => target.type === 'choose') &&
+                                                event.targets.length === 1 && 
+                                                event.targets[0].controller === this.controller &&
+                                                event.targets[0].isMatch({ trait: ['Lord', 'Lady'], type: 'character' })
             },
+            message: {
+                format: '{player} returns {source} to their hand to cancel {event}',
+                args: { event: context => context.event.source }
+            },
+            cost: ability.costs.returnSelfToHand(),
             max: ability.limit.perPhase(1),
             gameAction: GameActions.cancelEffects(context => ({ event: context.event }))
         });
