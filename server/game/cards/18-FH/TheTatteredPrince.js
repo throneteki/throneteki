5675b58915fe0eb1c0f5111c3008ad3cff785b8d
Stephen Patane <patane97@gmail.com>
2023-02-15 14:15:57 +1100

    Updating TTP to GameAction & allowing save

--- server/game/cards/18-FH/TheTatteredPrince.js
+++ server/game/cards/18-FH/TheTatteredPrince.js
@@ -1,48 +1,34 @@
 const DrawCard = require('../../drawcard.js');
+const GameActions = require('../../GameActions/index.js');
 const {Tokens} = require('../../Constants');
 
 class TheTatteredPrince extends DrawCard {
     setupCardAbilities() {
         this.forcedReaction({
             when: {
-                onCardPlaced: event => event.location === 'revealed plots' &&
-                        event.player === this.controller
+                onCardPlaced: event => event.location === 'revealed plots' && event.player === this.controller
             },
-            handler: context => {
-                if(!this.hasToken(Tokens.gold)) {
-                    context.player.putIntoShadows(this, false);
-                    this.game.addMessage('{0} is forced by {1} to return {1} to shadows', this.controller, this);
-                    return;
+            gameAction: GameActions.ifCondition({
+                condition: context => context.source.hasToken(Tokens.gold),
+                thenAction: GameActions.choose({
+                    title: context => `Discard 1 gold for ${context.source.name}?`,
+                    choices: {
+                        'Yes': {
+                            message: '{player} chooses to discard a gold from {source}',
+                            gameAction: GameActions.discardToken(context => ({ card: context.source, token: Tokens.gold }))
+                        },
+                        'No': {
+                            message: '{player} chooses to return {source} to shadows',
+                            gameAction: GameActions.putIntoShadows(context => ({ card: context.source }))
+                        }
+                    }
+                }),
+                elseAction: {
+                    message: '{player} is forced to return {source} to shadows',
+                    gameAction: GameActions.putIntoShadows(context => ({ card: context.source }))
                 }
-
-                this.context = context;
-
-                this.game.promptWithMenu(this.controller, this, {
-                    activePrompt: {
-                        menuTitle: 'Discard a gold from ' + this.name + '?',
-                        buttons: [
-                            { text: 'Yes', method: 'discardGold' },
-                            { text: 'No', method: 'returnToShadows' }
-                        ]
-                    },
-                    source: this
-                });
-            }
+            })
         });
-    }
-
-    discardGold() {
-        this.modifyToken(Tokens.gold, -1);
-        this.game.addMessage('{0} is forced to discard a gold from {1}', this.controller, this);
-
-        return true;
-    }
-
-    returnToShadows() {
-        this.context.player.putIntoShadows(this, false);
-        this.game.addMessage('{0} uses {1} to return {1} to shadows', this.context.player, this);
-
-        return true;
     }
 }
 
