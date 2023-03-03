d42c925644d8601d4a4c5e23b0f523bf21ad0226
Stephen Patane <patane97@gmail.com>
2023-03-03 23:29:46 +1100

    Fixing Oxcross Survivers match

--- server/game/cards/24-TSoW/OxcrossSurvivors.js
+++ server/game/cards/24-TSoW/OxcrossSurvivors.js
@@ -4,6 +4,7 @@ class OxcrossSurvivors extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
             condition: () => this.controller.anyCardsInPlay({ faction: 'lannister', type: 'character', trait: ['Lord', 'Commander'] }),
+            match: this,
             effect: [
                 ability.effects.modifyStrength(3),
                 ability.effects.addKeyword('Assault')
