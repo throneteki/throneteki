97e444224d27708136456ce8aec0f2a361ff6c3d
Stephen Patane <patane97@gmail.com>
2023-04-06 09:33:34 +1000

    Updating Salladhor Saan's Lyseni to ver. 2.2

--- server/game/cards/24-TSoW/SalladhorSaansLyseni.js
+++ server/game/cards/24-TSoW/SalladhorSaansLyseni.js
@@ -5,20 +5,23 @@ class SalladhorSaansLyseni extends DrawCard {
     setupCardAbilities(ability) {
         this.interrupt({
             when: {
-                onCardKneeled: event => event.card.getType() === 'location' && event.card.controller !== this.controller && event.card.controller.faction.power > 0 // TODO: Clean up kneeling to always identify if it is a card effect or not (eg. setting a cause where appropriate)
+                afterChallenge: event => event.challenge.winner === this.controller && this.isAttacking()
             },
-            target: {
-                cardCondition: { type: 'character', location: 'play area' }
-            },
-            limit: ability.limit.perRound(1),
+            limit: ability.limit.perPhase(1),
             message: {
-                format: '{player} uses {source} to move 1 power from {controller}\'s faction card to {target}',
-                args: { controller: context => context.event.card.controller }
+                format: '{player} uses {source} to have it gain {amount} power',
+                args: { amount: context => this.getAmount(context) }
             },
-            handler: context => {
-                this.game.resolveGameAction(GameActions.movePower(context => ({ from: context.event.card.controller.faction, to: context.target, amount: 1 })), context);
-            }
+            gameAction: GameActions.gainPower(context => ({
+                card: this,
+                amount: this.getAmount(context)
+            }))
         });
+    }
+
+    getAmount(context) {
+        let numCards = context.challenge.loser.getNumberOfCardsInPlay({ type: 'location', location: 'play area', kneeled: true });
+        return Math.floor(numCards / 2);
     }
 }
 
