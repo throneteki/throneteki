1a47db930b2b4c5711d4c7cd159ef39a72698603
Brian Upton <ystros@gmail.com>
2023-04-22 08:57:26 -0700

    Fix Shagwell / Patchface loop (#3358)
    * Fix feedback loop between Shagwell and Patchface
    
    Previously, Shagwell and Patchface had independent effects that applied
    dynamic keywords. However, if Patchface had a gold placed on it, it
    became a valid source for Shagwell's effect. Because each effect was
    independent, this led to Patchface keeping keywords even after the
    original source left place or was no longer a source via Shagwell.
    
    Example:
    * Shagwell, Patchface, and Fickel Bannerman are in play, each with gold
      on them.
    * Both Shagwell and Patchface receive Renown from Fickel Bannerman.
    * Then Fickel Bannerman leaves play or has its gold removed.  Shagwell
      and Patchface inappropriately kept Renown - Shagwell was keeping it
      because Patchface still had it. Patchface was keeping it because
      Shagwell still had it.
    
    Now, both these cards effect is to add a "keyword source" to the
    receiving card. This keyword source is a function that returns true for
    cards that match the effect criteria to provide the keywords (gold
    tokens for Shagwell, Fool trait for Patchface).
    
    These keyword source functions are then used in separate, global effect
    that constructs a directed graph of cards that provide each other
    keywords. The effect uses the graph to get the final list of connected
    cards, get their keywords, and apply them to the card. This prevents
    cycles between Shagwell and Patchface from inappropriately keeping
    keywords, since the combined keywords for both of them is calculated and
    added / removed at the same time.
    
    * Updating Shagwell test
    
    * Fix linting
    
    ---------
    
    Co-authored-by: Stephen Patane <patane97@gmail.com>

---
+++ server/game/DynamicKeywordsEffect.js
@@ -0,0 +1,97 @@
+const { flatMap } = require('../Array');
+
+class DynamicKeywordsEffect {
+    constructor({ game }) {
+        this.game = game;
+        this.duration = 'persistent';
+        this.order = 999; // Apply this effect after all other keyword effects
+        this.isStateDependent = true;
+
+        this.graph = this.buildGraph();
+        this.appliedKeywords = new Map();
+    }
+
+    isInActiveLocation() {
+        return true;
+    }
+
+    addTargets() {
+    }
+
+    clearInvalidTargets() {
+    }
+
+    updateAppliedTargets() {
+    }
+
+    hasEnded() {
+        return false;
+    }
+
+    cancel() {
+    }
+
+    setActive() {
+    }
+
+    reapply() {
+        for(const [card, keywords] of this.appliedKeywords.entries()) {
+            for(const keyword of keywords) {
+                card.removeKeyword(keyword);
+            }
+        }
+
+        this.graph = this.buildGraph();
+
+        const cardToKeywordsMap = new Map();
+        for(const card of this.graph.keys()) {
+            const connectedSources = this.bfs(card);
+            const keywords = new Set(flatMap(connectedSources, source => source.getKeywords()));
+            cardToKeywordsMap.set(card, keywords);
+        }
+
+        for(const [card, keywords] of cardToKeywordsMap.entries()) {
+            for(const keyword of keywords) {
+                card.addKeyword(keyword);
+            }
+        }
+
+        this.appliedKeywords = cardToKeywordsMap;
+    }
+
+    buildGraph() {
+        const graph = new Map();
+        const cardsWithDynamicKeywords = this.game.filterCardsInPlay(card => card.keywordSources.length > 0);
+        const inPlayCards = this.game.filterCardsInPlay(() => true);
+        for(const card of cardsWithDynamicKeywords) {
+            const sources = inPlayCards.filter(inPlayCard => card.keywordSources.some(sourceFunc => sourceFunc(inPlayCard)));
+            graph.set(card, sources);
+        }
+        return graph;
+    }
+
+    bfs(startingNode) {
+        const visited = new Map();
+        const nodeQueue = [];
+
+        visited.set(startingNode, true);
+        nodeQueue.push(startingNode);
+
+        while(nodeQueue.length > 0) {
+            let currentNode = nodeQueue.shift();
+
+            const sources = this.graph.get(currentNode) || [];
+
+            for(const source of sources) {
+                if(!visited.get(source)) {
+                    visited.set(source, true);
+                    nodeQueue.push(source);
+                }
+            }
+        }
+
+        return [...visited.keys()];
+    }
+}
+
+module.exports = DynamicKeywordsEffect;
--- server/game/cards/20-HMW/Shagwell.js
+++ server/game/cards/20-HMW/Shagwell.js
@@ -1,31 +1,13 @@
 const DrawCard = require('../../drawcard.js');
 const {Tokens} = require('../../Constants');
-const Array = require('../../../Array');
 
 class Shagwell extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
             match: card => card.hasTrait('Fool') && card.hasToken(Tokens.gold),
             targetController: 'any',
-            effect: ability.effects.dynamicKeywords((card, context) => this.getFoolKeywords(card, context))
+            effect: ability.effects.dynamicKeywordSources(card => card.getType() === 'character' && card.hasToken(Tokens.gold))
         });
-    }
-
-    getFoolKeywords(card, context) {
-        let cardsWithGold = this.game.filterCardsInPlay(card => card.getType() === 'character' && card.hasToken(Tokens.gold));
-        let keywords = [...new Set(Array.flatten(cardsWithGold.map(cwg => this.getKeywordsNotAddedByShagwell(cwg, context))))];
-
-        return keywords;
-    }
-
-    getKeywordsNotAddedByShagwell(card, context) {
-        let dynamicKeywords = context.dynamicKeywords[card.uuid] || [];
-        let effectCount = Array.countAsMap(dynamicKeywords);
-        let currentCount = card.keywords.getValues();
-
-        let keywordsNotAddedByShagwell = currentCount.filter(keyword => card.keywords.getCount(keyword) - (effectCount.get(keyword) || 0) > 0);
-
-        return keywordsNotAddedByShagwell;
     }
 }
 
