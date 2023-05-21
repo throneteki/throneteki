25974a9ddf2909642fe957fd0acf29951987f759
Stephen Patane <patane97@gmail.com>
2023-05-21 23:08:30 +1000

    Updating Harlaw Scout to ver. 1.1

--- server/game/cards/24-TSoW/HarlawScout.js
+++ server/game/cards/24-TSoW/HarlawScout.js
@@ -2,11 +2,12 @@ const DrawCard = require('../../drawcard.js');
 const GameActions = require('../../GameActions/index.js');
 
 class HarlawScout extends DrawCard {
-    setupCardAbilities() {
+    setupCardAbilities(ability) {
         this.interrupt({
             when: {
                 'onCardDiscarded:aggregate': event => event.events.some(discardEvent => (discardEvent.source === 'reserve'))
             },
+            limit: ability.limit.perRound(2),
             message: '{player} uses {source} to gain 1 gold',
             gameAction: GameActions.gainGold(context => ({ player: context.player, amount: 1 }))
         });
