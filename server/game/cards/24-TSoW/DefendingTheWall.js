c730ed7d9d426f80a809cf33635cda3d6485c8fc
Stephen Patane <patane97@gmail.com>
2023-05-27 13:37:31 +1000

    Updating DefendingTheWall to ver. 1.3

--- server/game/cards/24-TSoW/DefendingTheWall.js
+++ server/game/cards/24-TSoW/DefendingTheWall.js
@@ -6,25 +6,24 @@ class DefendingTheWall extends DrawCard {
         this.action({
             title: 'Remove character from challenge',
             phase: 'challenge',
-            condition: context => context.player.anyCardsInPlay({ name: ['The Wall', 'Castle Black'] }),
+            condition: context => context.player.anyCardsInPlay({ name: ['Castle Black', 'The Wall'] }),
             target: {
                 cardCondition: card => card.isAttacking() && card.getType() === 'character' && card.getNumberOfIcons() > 1 
             },
             message: '{player} plays {source} to remove {target} from the challenge',
             handler: context => {
                 this.resolveGameAction(
-                    GameActions.removeFromChallenge(context => ({
-                        card: context.target
-                    })).then({
-                        condition: context => !(context.parentContext.target.hasTrait('Army') || context.parentContext.target.hasTrait('Wildling')),
-                        message: {
-                            format: 'Then {player} stands {originalTarget}',
-                            args: { originalTarget: context => context.parentContext.target }
-                        },
-                        gameAction: GameActions.standCard(context => ({
-                            card: context.parentContext.target
-                        }))
-                    }),
+                    GameActions.simultaneously([
+                        GameActions.removeFromChallenge(context => ({
+                            card: context.target
+                        })),
+                        GameActions.ifCondition({
+                            condition: context => !(context.target.hasTrait('Army') || context.target.hasTrait('Wildling')),
+                            thenAction: GameActions.standCard(context => ({
+                                card: context.target
+                            }))
+                        })
+                    ]),
                     context
                 );
             }
