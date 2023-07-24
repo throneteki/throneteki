8501c5424fb3a03177a4605eaafc6a877db55c0c
Stephen Patane <patane97@gmail.com>
2023-07-24 23:28:24 +1000

    Implement TheLaughingStorm v1.0

---
+++ server/game/cards/25.7-MoM/TheLaughingStorm.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+
+class TheLaughingStorm extends DrawCard {
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ type: 'location', unique: true });
+        this.persistentEffect({
+            condition: () => this.eachOpponentHasFewerStandingCharacters(),
+            targetLocation: 'hand',
+            effect: ability.effects.cannotBeDiscarded()
+        });
+    }
+
+    eachOpponentHasFewerStandingCharacters() {
+        return this.game.getOpponents(this.controller).every(opponent => opponent.getNumberOfCardsInPlay({ type: 'character', kneeled: false }) < this.controller.getNumberOfCardsInPlay({ type: 'character', kneeled: false }));
+    }
+}
+
+TheLaughingStorm.code = '25509';
+TheLaughingStorm.version = '1.0';
+
+module.exports = TheLaughingStorm;
