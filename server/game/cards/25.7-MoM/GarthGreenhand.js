fe807dcaff56bf58484afa4186ed9297b5611a32
Stephen Patane <patane97@gmail.com>
2023-07-25 20:33:05 +1000

    Implement GarthGreenhand v1.0

---
+++ server/game/cards/25.7-MoM/GarthGreenhand.js
@@ -0,0 +1,20 @@
+const DrawCard = require('../../drawcard.js');
+
+class GarthGreenhand extends DrawCard {
+    setupCardAbilities() {
+        this.attachmentRestriction({ type: 'location', unique: true });
+        this.plotModifiers({
+            gold: 1
+        });
+        this.persistentEffect({
+            condition: () => this.game.isDuringChallenge(),
+            match: card => card.controller === this.controller && card.isParticipating() && card.isFaction('tyrell'),
+            effect: ability.effects.modifyStrength(1)
+        });
+    }
+}
+
+GarthGreenhand.code = '25593';
+GarthGreenhand.version = '1.0';
+
+module.exports = GarthGreenhand;
