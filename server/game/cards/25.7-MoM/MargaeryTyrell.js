5924ff004f4e83daf888a386483e2a5f246583f8
Stephen Patane <patane97@gmail.com>
2023-07-30 20:32:48 +1000

    Implement MargaeryTyrell v1.0

---
+++ server/game/cards/25.7-MoM/MargaeryTyrell.js
@@ -0,0 +1,23 @@
+const GameActions = require('../../GameActions/index.js');
+const DrawCard = require('../../drawcard.js');
+
+class MargaeryTyrell extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            when: {
+                onCardRevealed: event => event.card.isFaction('tyrell') && event.card.controller === this.controller && ['hand', 'draw deck'].includes(event.card.location)
+            },
+            limit: ability.limit.perRound(2),
+            message: {
+                format: '{player} uses {source} to place {card} in shadows',
+                args: { card: context => context.event.card }
+            },
+            gameAction: GameActions.placeCard(context => ({ card: context.event.card, location: 'shadows' }))
+        });
+    }
+}
+
+MargaeryTyrell.code = '25585';
+MargaeryTyrell.version = '1.0';
+
+module.exports = MargaeryTyrell;
