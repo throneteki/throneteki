84fdbc202922a3f0d2eeab322403fe66fc88ff3c
Stephen Patane <patane97@gmail.com>
2023-08-16 12:20:11 +1000

    Implement & Update GreatKrakens Crew to v1.1

---
+++ server/game/cards/25.7-MoM/GreatKrakensCrew.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+
+class GreatKrakensCrew extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            when: {
+                onCardOutOfShadows: event => event.card === this
+            },
+            target: {
+                cardCondition: card => card.isDefending() && card.getType() === 'character' && card.hasPrintedCost() && card.getPrintedCost() <= 3
+            },
+            message: '{player} uses {source} to have {target} not contribute it\'s STR to this challenge',
+            handler: context => {
+                this.untilEndOfChallenge(ability => ({
+                    match: context.target,
+                    effect: ability.effects.doesNotContributeStrength()
+                }));
+            }
+        });
+    }
+}
+
+GreatKrakensCrew.code = '25518';
+GreatKrakensCrew.version = '1.1';
+
+module.exports = GreatKrakensCrew;
