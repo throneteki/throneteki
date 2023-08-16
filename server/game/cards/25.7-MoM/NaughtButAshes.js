5c6f10071e91084f9dc7d63ba7a4e00198c4a9ab
Stephen Patane <patane97@gmail.com>
2023-08-16 12:20:42 +1000

    Implement NaughtButAshes v1.0

---
+++ server/game/cards/25.7-MoM/NaughtButAshes.js
@@ -0,0 +1,30 @@
+const DrawCard = require('../../drawcard.js');
+
+class NaughtButAshes extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Give character -X STR',
+            target: { location: 'play area', type: 'character' },
+            max: ability.limit.perRound(1),
+            message: {
+                format: '{player} plays {source} to give {target} {amount} STR until the end of the phase',
+                args: { amount: () => this.getAmount() }
+            },
+            handler: context => {
+                this.untilEndOfPhase(ability => ({
+                    match: context.target,
+                    effect: ability.effects.modifyStrength(this.amount())
+                }));
+            }
+        });
+    }
+
+    getAmount() {
+        return this.game.getPlayers().reduce((acc, player) => acc + player.shadows.filter(card => card !== this).length, 0) * -1;
+    }
+}
+
+NaughtButAshes.code = '25583';
+NaughtButAshes.version = '1.0';
+
+module.exports = NaughtButAshes;
