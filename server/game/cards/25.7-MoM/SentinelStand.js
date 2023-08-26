de0f6596b3ff1410bdfcd67684b8a0c28409bd8c
Stephen Patane <patane97@gmail.com>
2023-08-26 14:02:04 +1000

    Fixing bug with SentinelStand

--- server/game/cards/25.7-MoM/SentinelStand.js
+++ server/game/cards/25.7-MoM/SentinelStand.js
@@ -4,7 +4,7 @@ class SentinelStand extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
             condition: () => !this.kneeled,
-            match: card => card.getNumberOfIcons() <= 1,
+            match: card => card.getType() === 'character' && card.getNumberOfIcons() <= 1,
             effect: ability.effects.modifyStrength(2)
         });
     }
