3f445ccf63feafb4669aef13740117b0b96fb3ee
Stephen Patane <patane97@gmail.com>
2023-08-30 21:41:26 +1000

    Updating VictarionGreyjoy to v1.1

--- server/game/cards/25.7-MoM/VictarionGreyjoy.js
+++ server/game/cards/25.7-MoM/VictarionGreyjoy.js
@@ -13,30 +13,29 @@ class VictarionGreyjoy extends DrawCard {
 
                 if(context.costs.kneel.hasTrait('R\'hllor')) {
                     effects.push(ability.effects.addKeyword('insight'));
-                    effects.push(ability.effects.cannotBeKneeled(context => context.resolutionStage === 'effect'));
-                    bonusMessage.push('insight and cannot be knelt by card effects');
+                    effects.push(ability.effects.immuneTo(card => card.controller !== this.controller && card.getType() === 'character'));
+                    bonusMessage.push('insight');
+                    bonusMessage.push('immunity to opponent\'s character abilities');
                 }
                 
                 if(context.costs.kneel.hasTrait('Drowned God')) {
                     effects.push(ability.effects.addKeyword('renown'));
                     effects.push(ability.effects.cannotBeKilled());
-                    if(bonusMessage.length === 1) {
-                        bonusMessage.push(' and ');
-                    }
-                    bonusMessage.push('renown and cannot be killed');
+                    bonusMessage.push('renown');
+                    bonusMessage.push('cannot be killed');
                 }
 
                 this.untilEndOfPhase(() => ({
                     match: this,
                     effect: effects
                 }));
-                this.game.addMessage('{0} uses {1} to have {1} gain {2}', this.controller, this, bonusMessage);
+                this.game.addMessage('{0} uses {1} to have {1} gain {2} until the end of the phase', this.controller, this, bonusMessage);
             }
         });
     }
 }
 
 VictarionGreyjoy.code = '25515';
-VictarionGreyjoy.version = '1.0';
+VictarionGreyjoy.version = '1.1';
 
 module.exports = VictarionGreyjoy;
