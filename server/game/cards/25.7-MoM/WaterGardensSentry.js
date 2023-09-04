fe02a7aa7c4c35fa1506c38e0092af79d2c4e5bc
Stephen Patane <patane97@gmail.com>
2023-09-04 12:29:20 +1000

    Updating WaterGardensSentry to v1.1

--- server/game/cards/25.7-MoM/WaterGardensSentry.js
+++ server/game/cards/25.7-MoM/WaterGardensSentry.js
@@ -3,7 +3,7 @@ const DrawCard = require('../../drawcard.js');
 class WaterGardensSentry extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            condition: () => this.controller.anyCardsInPlay({ type: 'character', faction: 'martell', trait: ['Lord', 'Lady'] }) && this.game.isDuringChallenge({ challengeType: ['intrigue', 'power'], defendingPlayer: this.controller }),
+            condition: () => this.controller.anyCardsInPlay({ type: 'character', faction: 'martell', trait: ['Lord', 'Lady'] }) && this.game.isDuringChallenge({ challengeType: 'intrigue', defendingPlayer: this.controller }),
             match: this,
             effect: [
                 ability.effects.canBeDeclaredWithoutIcon()
@@ -13,6 +13,6 @@ class WaterGardensSentry extends DrawCard {
 }
 
 WaterGardensSentry.code = '25542';
-WaterGardensSentry.version = '1.0';
+WaterGardensSentry.version = '1.1';
 
 module.exports = WaterGardensSentry;
