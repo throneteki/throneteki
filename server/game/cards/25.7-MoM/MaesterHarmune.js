5a106c80a151ee6c03d466f567d4789ce8fe76c5
Stephen Patane <patane97@gmail.com>
2023-09-08 13:17:50 +1000

    Fixing MaesterHarmune

--- server/game/cards/25.7-MoM/MaesterHarmune.js
+++ server/game/cards/25.7-MoM/MaesterHarmune.js
@@ -5,19 +5,19 @@ class MaesterHarmune extends DrawCard {
     setupCardAbilities(ability) {
         this.reaction({
             when: {
-                onGoldTransferred: context => context.event.from.getGameElementType() === 'player' && context.event.from !== this.controller && context.event.to === this.controller
+                onGoldTransferred: event => event.source.getGameElementType() === 'player' && event.source !== this.controller && event.target === this.controller
             },
             limit: ability.limit.perPhase(1),
             message: '{player} uses {source} to draw 1 card',
             gameAction: GameActions.drawCards(context => ({ player: context.player, amount: 1, source: this }))
                 .then({
                     message: {
-                        format: 'Then, {player} and {opponent} each place a card from their hand on top of their deck',
-                        args: { opponent: context => context.parentContext.event.from }
+                        format: 'Then, {player} and {other} each place a card from their hand on top of their deck',
+                        args: { other: context => context.parentContext.event.source }
                     },
                     target: {
                         activePromptTitle: 'Select a card',
-                        choosingPlayer: 'each',
+                        choosingPlayer: (player, context) => player === context.player || player === context.parentContext.event.source,
                         cardCondition: { location: 'hand', controller: 'choosingPlayer', condition: card => GameActions.placeCard({ card, location: 'draw deck', bottom: false }).allow() }
                     },
                     handler: context => {
