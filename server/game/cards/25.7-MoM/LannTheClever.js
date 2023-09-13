29a2736a6a063f4ca4ffd68937a73f7fb2c4a89a
Stephen Patane <patane97@gmail.com>
2023-09-13 10:44:51 +1000

    Updating LannTheClever to v1.2

--- server/game/cards/25.7-MoM/LannTheClever.js
+++ server/game/cards/25.7-MoM/LannTheClever.js
@@ -1,17 +1,25 @@
+const GameActions = require('../../GameActions/index.js');
 const DrawCard = require('../../drawcard.js');
 
 class LannTheClever extends DrawCard {
     setupCardAbilities(ability) {
         this.attachmentRestriction({ type: 'location', faction: 'lannister', controller: 'current', unique: true });
-        this.persistentEffect({
-            condition: () => this.game.isDuringChallenge({ attackingPlayer: this.controller, match: challenge => this.controller.hand.length > challenge.defendingPlayer.hand.length }),
-            targetController: 'current',
-            effect: ability.effects.choosesIntrigueClaim()
+        this.reaction({
+            cannotBeCanceled: true,
+            when: {
+                afterChallenge: event => event.challenge.challengeType === 'intrigue' && event.challenge.winner === this.controller
+            },
+            cost: [
+                ability.costs.kneelSelf(),
+                ability.costs.returnToHand(card => card.isMatch({ type: 'character', faction: 'lannister', participating: true }))
+            ],
+            message: '{player} kneels {costs.kneel} and returns {costs.returnToHand} to their hand to gain 3 gold',
+            gameAction: GameActions.gainGold(context => ({ player: context.player, amount: 3 }))
         });
     }
 }
 
 LannTheClever.code = '25533';
-LannTheClever.version = '1.1';
+LannTheClever.version = '1.2';
 
 module.exports = LannTheClever;
