b56aa7f946648d305deb29d93677398995d60460
Stephen Patane <patane97@gmail.com>
2023-09-13 10:45:12 +1000

    Updating RooseBolton to v1.1

--- server/game/cards/25.7-MoM/RooseBolton.js
+++ server/game/cards/25.7-MoM/RooseBolton.js
@@ -7,14 +7,13 @@ class RooseBolton extends DrawCard {
             when: {
                 onCardEntersPlay: event => event.card === this && event.playingType === 'marshal'
             },
-            message: {
-                format: '{player} is forced to kill {characters} for {source}',
-                args: { characters: context => this.getEachOtherLoyalCharacter(context) }
-            },
-            gameAction: GameActions.simultaneously(context =>
-                this.getEachOtherLoyalCharacter(context)
-                    .map(card => GameActions.kill(context => ({ card, player: context.player })))
-            )
+            message: '{player} is forced by {source} to kneel their faction card',
+            gameAction: GameActions.kneelCard(context => ({ card: context.player.faction, source: this })).then({
+                message: 'Then, each other loyal character is killed',
+                gameAction: GameActions.simultaneously(context =>
+                    this.getEachOtherLoyalCharacter(context).map(card => GameActions.kill(context => ({ card, player: context.player })))
+                )
+            })
         });
     }
 
@@ -24,6 +23,6 @@ class RooseBolton extends DrawCard {
 }
 
 RooseBolton.code = '25561';
-RooseBolton.version = '1.0';
+RooseBolton.version = '1.1';
 
 module.exports = RooseBolton;
