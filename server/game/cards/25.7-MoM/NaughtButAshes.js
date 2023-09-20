a5fd3f328971666031a2d190585d2183f95a7882
Stephen Patane <patane97@gmail.com>
2023-09-20 11:17:51 +1000

    Fixing target on NaughtButAshes

--- server/game/cards/25.7-MoM/NaughtButAshes.js
+++ server/game/cards/25.7-MoM/NaughtButAshes.js
@@ -4,7 +4,9 @@ class NaughtButAshes extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
             title: 'Give character -X STR',
-            target: { location: 'play area', type: 'character', participating: true, hasAttachments: true },
+            target: {
+                cardCondition: { location: 'play area', type: 'character', participating: true, hasAttachments: true }
+            },
             max: ability.limit.perRound(1),
             message: {
                 format: '{player} plays {source} to give {target} {amount} STR until the end of the phase',
