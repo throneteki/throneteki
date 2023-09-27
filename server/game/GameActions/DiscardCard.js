94a84bebb9cef317cf06bbbf4cd272abf028e67f
Stephen Patane <patane97@gmail.com>
2023-09-27 13:31:49 +1000

    Update MaesterHarmune to v1.1

--- server/game/GameActions/DiscardCard.js
+++ server/game/GameActions/DiscardCard.js
@@ -1,10 +1,25 @@
 const GameAction = require('./GameAction');
 const LeavePlay = require('./LeavePlay');
+const Message = require('../Message');
 const MoveCardEventGenerator = require('./MoveCardEventGenerator');
 
 class DiscardCard extends GameAction {
     constructor() {
         super('discard');
+    }
+
+    message({ card }) {
+        if(card.location === 'play area') {
+            return Message.fragment('discards {card} from play', { card });
+        }
+        if(card.location === 'duplicate') {
+            return Message.fragment('discards a duplicate from {parent}', { parent: card.parent });
+        }
+        if(card.location === 'underneath') {
+            return Message.fragment('discards {card} from underneath {parent}', { card, parent: card.parent });
+        }
+        
+        return Message.fragment('discards {card} from {controller}\'s {location}', { card, controller: card.controller, location: card.location });
     }
 
     canChangeGameState({ card }) {
