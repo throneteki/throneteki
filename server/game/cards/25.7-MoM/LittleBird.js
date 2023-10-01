a428ade21b23b36be64048b31f952b5d73c49f34
EvadLhorg <lukas.a-v@web.de>
2023-10-01 21:08:22 +0200

    update little bird to v1.2

--- server/game/cards/25.7-MoM/LittleBird.js
+++ server/game/cards/25.7-MoM/LittleBird.js
@@ -3,25 +3,36 @@ const DrawCard = require('../../drawcard.js');
 
 class LittleBird extends DrawCard {
     setupCardAbilities() {
-        this.forcedReaction({
+        this.reaction({
             when: {
                 onCardOutOfShadows: event => event.card === this
             },
-            message: '{player} uses {source} to draw 1 card',
-            gameAction: GameActions.simultaneously(context =>
-                // TODO: Add a 'decline message' to GameActions.may
-                context.game.getPlayersInFirstPlayerOrder().map(player => GameActions.may({
-                    player,
-                    title: context => 'Draw 1 card from ' + context.source.name + '?',
-                    message: '{choosingPlayer} draws 1 card',
-                    gameAction: GameActions.drawCards(context => ({ player: context.choosingPlayer, amount: 1 }))
-                }))
-            )
+            chooseOpponent: opponent => opponent.drawDeck.length > 0,
+            message: '{player} uses {source} to look at the top card of an opponent\'s deck',
+            handler: context => {
+                this.game.resolveGameAction(
+                    GameActions.lookAtDeck(context => ({
+                        player: context.player,
+                        lookingAt: context.opponent,
+                        amount: 1
+                    })),
+                    context
+                ).thenExecute(() => {
+                    GameActions.simultaneously(context =>
+                        // TODO: Add a 'decline message' to GameActions.may
+                        context.game.getPlayersInFirstPlayerOrder().map(player => GameActions.may({
+                            player,
+                            title: context => 'Draw 1 card from ' + context.source.name + '?',
+                            message: '{choosingPlayer} draws 1 card',
+                            gameAction: GameActions.drawCards(context => ({ player: context.choosingPlayer, amount: 1 }))
+                        })));
+                });
+            }
         });
     }
 }
 
 LittleBird.code = '25602';
-LittleBird.version = '1.1';
+LittleBird.version = '1.2';
 
 module.exports = LittleBird;
