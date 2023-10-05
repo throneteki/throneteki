dbb00da75c3e3b39db544c22c9cc53d7a870103a
Stephen Patane <patane97@gmail.com>
2023-10-05 21:51:35 +1100

    Fixing drownedgodwakes

--- server/game/cards/25.7-MoM/TheDrownedGodWakes.js
+++ server/game/cards/25.7-MoM/TheDrownedGodWakes.js
@@ -1,29 +1,23 @@
 const DrawCard = require('../../drawcard.js');
 
 class TheDrownedGodWakes extends DrawCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.reaction({
             when: {
                 onDominanceDetermined: event => this.controller === event.winner && event.difference >= 10
             },
-            cost: ability.costs.kneelFactionCard(),
-            message: '{player} uses {source} to have each player chose a character they control and kill each character not chosen',
+            message: '{player} plays {source} to have each player choose a character they control, and kill the others',
             target: {
                 choosingPlayer: 'each',
                 ifAble: true,
                 cardCondition: (card, context) => card.location === 'play area' && card.getType() === 'character' && card.controller === context.choosingPlayer
             },
             handler: context => {
-                const uniqueCards = this.getChosenCards(context);
-                let characters = this.game.filterCardsInPlay(card => !uniqueCards.includes(card) && card.getType() === 'character');
+                const targets = context.targets.getTargets();
+                let characters = this.game.filterCardsInPlay(card => !targets.includes(card) && card.getType() === 'character');
                 this.game.killCharacters(characters);
             }
         });
-    }
-
-    getChosenCards(context) {
-        const cards = context.targets.selections.map(selection => selection.value).filter(card => !!card);
-        return [...new Set(cards)];
     }
 }
 
