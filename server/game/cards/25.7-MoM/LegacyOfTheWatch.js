079af26b6cb43776c58cd6012d434914d3e3cf69
Stephen Patane <patane97@gmail.com>
2023-10-05 22:40:31 +1100

    Update LegacyOfTheWatch to v1.2

--- server/game/cards/25.7-MoM/LegacyOfTheWatch.js
+++ server/game/cards/25.7-MoM/LegacyOfTheWatch.js
@@ -1,32 +1,78 @@
+const GameActions = require('../../GameActions/index.js');
 const DrawCard = require('../../drawcard.js');
+const Message = require('../../Message');
 
 class LegacyOfTheWatch extends DrawCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.attachmentRestriction({ type: 'location', faction: 'thenightswatch', controller: 'current', unique: true });
-        this.persistentEffect({
-            targetController: 'current',
-            effect: ability.effects.lookAtBottomCard(() => this.game.getOpponents(this.controller))
-        });
-        this.persistentEffect({
-            targetController: 'current',
-            effect: [
-                ability.effects.canMarshal(card => this.isBottomCard(card)),
-                ability.effects.canPlay(card => this.isBottomCard(card))
-            ]
+
+        this.reaction({
+            when: {
+                afterChallenge: event => event.challenge.winner === this.controller && this.satisfiableBonuses(event.challenge).length > 0
+            },
+            handler: context => {
+                let bonuses = this.satisfiableBonuses(context.event.challenge);
+                if(bonuses.includes('stand')) {
+                    this.game.promptForSelect(this.controller, {
+                        activePromptTitle: 'Select a location',
+                        cardCondition: card => this.canStandLocation(card),
+                        onSelect: (player, card) => this.activateBonuses(bonuses, card),
+                        onCancel: () => this.activateBonuses(bonuses, null),
+                        source: this
+                    });
+                    return;
+                }
+
+                this.activateBonuses(bonuses, null);
+            }
         });
     }
 
-    isBottomCard(card) {
-        return (
-            card.controller !== this.controller &&
-            card.location === 'draw deck' &&
-            card === card.controller.drawDeck[card.controller.drawDeck.length - 1] &&
-            card.getType() === 'character'
-        );
+    canStandLocation(card) {
+        return card.location === 'play area' && card.getType() === 'location' && card.isFaction('thenightswatch') && card.kneeled;
+    }
+
+    satisfiableBonuses(challenge) {
+        let satisfiable = [];
+        let participants = challenge.getParticipants().filter(card => card.controller === this.controller);
+        if(participants.filter(card => card.hasTrait('Ranger')).length >= 2 && this.controller.canGainFactionPower()) {
+            satisfiable.push('power');
+        }
+        if(participants.filter(card => card.hasTrait('Steward')).length >= 2 && this.controller.canDraw()) {
+            satisfiable.push('draw');
+        }
+        if(participants.filter(card => card.hasTrait('Builder')).length >= 2 && this.game.anyCardsInPlay(card => this.canStandLocation(card))) {
+            satisfiable.push('stand');
+        }
+        return satisfiable;
+    }
+
+    activateBonuses(bonuses, location) {
+        let bonusMessages = [];
+        let gameActions = [];
+
+        if(bonuses.includes('power')) {
+            gameActions.push(GameActions.gainPower({ card: this.controller.faction, amount: 1 }));
+            bonusMessages.push('gain 1 power for their faction');
+        }
+
+        if(bonuses.includes('draw')) {
+            gameActions.push(GameActions.drawCards({ player: this.controller, amount: 2 }));
+            bonusMessages.push('draw 2 card');
+        }
+
+        if(bonuses.includes('stand') && location) {
+            gameActions.push(GameActions.standCard({ card: location }));
+            bonusMessages.push(Message.fragment('stand {card}', { card: location }));
+        }
+
+        this.game.addMessage('{0} uses {1} to {2}', this.controller, this, bonusMessages);
+        this.game.resolveGameAction(GameActions.simultaneously(gameActions));
+        return true;
     }
 }
 
 LegacyOfTheWatch.code = '25557';
-LegacyOfTheWatch.version = '1.1';
+LegacyOfTheWatch.version = '1.2';
 
 module.exports = LegacyOfTheWatch;
