f243b1fcabc156d431be035add51c4756511d7dc
Stephen Patane <patane97@gmail.com>
2023-10-25 11:09:30 +1100

    Updating VultureKing to v1.2

--- server/game/cards/25.7-MoM/VultureKing.js
+++ server/game/cards/25.7-MoM/VultureKing.js
@@ -7,23 +7,29 @@ class VultureKing extends DrawCard {
         });
         this.reaction({
             when: {
-                onCardDiscarded: event => event.isPillage && event.source === this.parent && ['attachment', 'location'].includes(event.card.getType())
+                afterChallenge: event => event.challenge.winner === this.controller && this.parent && this.parent.isParticipating()
             },
+            cost: ability.costs.discardFromDeck(),
             target: {
-                cardCondition: card => card.location === 'play area' && card.getType() === 'character'
+                cardCondition: (card, context) => card.location === 'play area' && card.getType() === 'character' && card.controller === context.event.challenge.loser
             },
-            message: '{player} uses {source} to have {target} lose a power icon until the end of the phase',
+            message: '{player} uses {source} and discards {costs.discardFromDeck} from their deck to have {target} lose a challenge icon until the end of the phase',
             handler: context => {
-                this.untilEndOfPhase(ability => ({
-                    match: context.target,
-                    effect: ability.effects.removeIcon('power')
-                }));
+                this.game.promptForIcon(context.player, this, icon => {
+                    this.untilEndOfPhase(ability => ({
+                        match: context.target,
+                        effect: ability.effects.removeIcon(icon)
+                    }));
+                    
+                    this.game.addMessage('{0} uses {1} to have {2} lose {3} {4} icon until the end of the phase',
+                        context.player, context.source, context.target, icon === 'intrigue' ? 'an' : 'a', icon);
+                });
             }
         });
     }
 }
 
 VultureKing.code = '25546';
-VultureKing.version = '1.1';
+VultureKing.version = '1.2';
 
 module.exports = VultureKing;
