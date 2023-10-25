615627b89405fb9c38c8b1509927964523a56e1a
Stephen Patane <patane97@gmail.com>
2023-10-25 11:08:48 +1100

    Updating RalfKenning to v1.2

--- server/game/cards/25.7-MoM/RalfKenning.js
+++ server/game/cards/25.7-MoM/RalfKenning.js
@@ -3,6 +3,7 @@ const DrawCard = require('../../drawcard.js');
 class RalfKenning extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
+            condition: () => this.game.getPlayers().some(player => player.activePlot && player.activePlot.hasTrait('Winter')),
             match: (card) => card === card.controller.activePlot,
             targetController: 'opponent',
             effect: [
@@ -14,6 +15,6 @@ class RalfKenning extends DrawCard {
 }
 
 RalfKenning.code = '25516';
-RalfKenning.version = '1.1';
+RalfKenning.version = '1.2';
 
 module.exports = RalfKenning;
