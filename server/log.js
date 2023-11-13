3771f3ccee06385c096c96308ac0e007923dedbd
Stuart Walsh <stu@ipng.org.uk>
2023-11-13 16:45:14 +0000

    fix linting

--- server/log.js
+++ server/log.js
@@ -1,24 +1,24 @@
-const winston = require("winston");
-require("winston-daily-rotate-file");
-const fs = require("fs");
+const winston = require('winston');
+require('winston-daily-rotate-file');
+const fs = require('fs');
 
-if (!fs.existsSync(__dirname + "/logs/")) {
-  fs.mkdirSync(__dirname + "/logs/");
+if(!fs.existsSync(__dirname + '/logs/')) {
+    fs.mkdirSync(__dirname + '/logs/');
 }
 
-let rotate = new winston.transports.DailyRotateFile({
-  filename: __dirname + "/logs/throneteki",
-  datePattern: "-yyyy-MM-dd.log",
-  timestamp: true,
-  json: false,
-  zippedArchive: true,
+let rotate = new (winston.transports.DailyRotateFile)({
+    filename: __dirname + '/logs/throneteki',
+    datePattern: '-yyyy-MM-dd.log',
+    timestamp: true,
+    json: false,
+    zippedArchive: true
 });
 
 const logger = new winston.Logger({
-  transports: [
-    new winston.transports.Console({ json: false, timestamp: true }),
-    rotate,
-  ],
+    transports: [
+        new winston.transports.Console({ json: false, timestamp: true }),
+        rotate
+    ]
 });
 
 module.exports = logger;
