51972135957fc86a5b4bddb6852b435792d032c4
Stuart Walsh <stu@ipng.org.uk>
2023-11-13 16:42:03 +0000

    create the log folder if it doesn't exist

--- server/log.js
+++ server/log.js
@@ -1,19 +1,24 @@
-const winston = require('winston');
-require('winston-daily-rotate-file');
+const winston = require("winston");
+require("winston-daily-rotate-file");
+const fs = require("fs");
 
-let rotate = new (winston.transports.DailyRotateFile)({
-    filename: __dirname + '/logs/throneteki',
-    datePattern: '-yyyy-MM-dd.log',
-    timestamp: true,
-    json: false,
-    zippedArchive: true
+if (!fs.existsSync(__dirname + "/logs/")) {
+  fs.mkdirSync(__dirname + "/logs/");
+}
+
+let rotate = new winston.transports.DailyRotateFile({
+  filename: __dirname + "/logs/throneteki",
+  datePattern: "-yyyy-MM-dd.log",
+  timestamp: true,
+  json: false,
+  zippedArchive: true,
 });
 
 const logger = new winston.Logger({
-    transports: [
-        new winston.transports.Console({ json: false, timestamp: true }),
-        rotate
-    ]
+  transports: [
+    new winston.transports.Console({ json: false, timestamp: true }),
+    rotate,
+  ],
 });
 
 module.exports = logger;
