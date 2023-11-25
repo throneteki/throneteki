e1ec4b0a334cb5983cf3f602010b46faa72bcfd9
Stephen Patane <patane97@gmail.com>
2023-11-25 11:25:13 +1100

    Update MartialLaw to v2.1

--- server/game/cards/25.7-MoM/MartialLaw.js
+++ server/game/cards/25.7-MoM/MartialLaw.js
@@ -1,15 +1,19 @@
+const ChallengeTracker = require('../../EventTrackers/ChallengeTracker.js');
 const DrawCard = require('../../drawcard.js');
 
 class MartialLaw extends DrawCard {
     setupCardAbilities(ability) {
+        this.tracker = ChallengeTracker.forRound(this.game);
+
         this.attachmentRestriction({ type: 'location', limited: false });
         this.whileAttached({
+            condition: () => !this.tracker.some({ winner: this.parent.controller, challengeType: 'power' }),
             effect: ability.effects.cannotBeStood()
         });
     }
 }
 
 MartialLaw.code = '25510';
-MartialLaw.version = '2.0';
+MartialLaw.version = '2.1';
 
 module.exports = MartialLaw;
