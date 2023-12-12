dfc2192200cbe1407234ae2c473992daf890cef2
Stephen Patane <patane97@gmail.com>
2023-12-12 15:29:39 +1100

    Update LannTheClever to v1.3

--- server/game/cards/25.7-MoM/LannTheClever.js
+++ server/game/cards/25.7-MoM/LannTheClever.js
@@ -13,13 +13,22 @@ class LannTheClever extends DrawCard {
                 ability.costs.kneelSelf(),
                 ability.costs.returnToHand(card => card.isMatch({ type: 'character', faction: 'lannister', participating: true }))
             ],
-            message: '{player} kneels {costs.kneel} and returns {costs.returnToHand} to their hand to gain 3 gold',
-            gameAction: GameActions.gainGold(context => ({ player: context.player, amount: 3 }))
+            message: '{player} kneels {costs.kneel} and returns {costs.returnToHand} to their hand to either gain 3 gold or 1 power for their faction',
+            choices: {
+                'Gain 3 gold': {
+                    message: '{player} chooses to gain 3 gold',
+                    gameAction: GameActions.gainGold(context => ({ player: context.player, amount: 3 }))
+                },
+                'Gain 1 power': {
+                    message: '{player} chooses to gain 1 power for their faction',
+                    gameAction: GameActions.gainPower(context => ({ card: context.player.faction, amount: 1 }))
+                }
+            }
         });
     }
 }
 
 LannTheClever.code = '25533';
-LannTheClever.version = '1.2';
+LannTheClever.version = '1.3';
 
 module.exports = LannTheClever;
