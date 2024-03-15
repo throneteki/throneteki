7ddc98c3c70057fbdc2c1c72b56d5535976c628e
Stuart Walsh <stu@ipng.org.uk>
2024-03-15 09:48:30 +0000

    A file didn't get saved

--- configureStore.prod.js
+++ configureStore.prod.js
@@ -1,27 +1,23 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers';
-import Raven from 'raven-js';
-import createRavenMiddleware from 'raven-for-redux';
+import { addBreadcrumb } from '@sentry/browser';
 
 import callAPIMiddleware from './middleware/api-middleware.js';
 
-const enhancer = compose(
-    applyMiddleware(thunkMiddleware, callAPIMiddleware, createRavenMiddleware(Raven, {
-        stateTransformer: state => {
-            let ret = Object.assign({}, state);
-            delete ret.auth;
-
-            delete ret.cards.agendas;
-            delete ret.cards.banners;
-            delete ret.cards.cards;
-            delete ret.cards.factions;
-            delete ret.cards.packs;
-            delete ret.account.user.email;
-
-            return ret;
+const sentryReporter = () => (next) => (action) => {
+    addBreadcrumb({
+        message: action.type,
+        category: 'redux action',
+        level: 'info',
+        data: {
+            payload: action.payload
         }
-    })));
+    });
+    return next(action);
+};
+
+const enhancer = compose(applyMiddleware(thunkMiddleware, callAPIMiddleware, sentryReporter));
 
 export default function configureStore(initialState) {
     const store = createStore(rootReducer, initialState, enhancer);
