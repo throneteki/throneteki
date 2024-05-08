89669be530838361f49cb03b892b6dc9ac91f46a
Stephen Patane <patane97@gmail.com>
2024-05-08 14:25:43 +1000

    Improved DiscardCard

--- server/game/GameActions/DiscardCard.js
+++ server/game/GameActions/DiscardCard.js
@@ -1,5 +1,6 @@
 const GameAction = require('./GameAction');
 const LeavePlay = require('./LeavePlay');
+const Message = require('../Message');
 const MoveCardEventGenerator = require('./MoveCardEventGenerator');
 
 class DiscardCard extends GameAction {
@@ -7,10 +8,25 @@ class DiscardCard extends GameAction {
         super('discard');
     }
 
+    message({ card }) {
+        if(card.location === 'play area') {
+            return Message.fragment('discards {card} from play', { card });
+        }
+        if(card.location === 'duplicate') {
+            return Message.fragment('discards a duplicate from {parent}', { parent: card.parent });
+        }
+        if(card.location === 'underneath') {
+            return Message.fragment('discards {card} from underneath {parent}', { card, parent: card.parent });
+        }
+
+        return Message.fragment('discards {card} from {controller}\'s {location}', { card, controller: card.controller, location: card.location });
+    }
+
     canChangeGameState({ card, isRandom = false, context }) {
         if(card.location === 'play area' && !LeavePlay.allow({ card })) {
             return false;
         }
+        // TODO: Change all random discards to use GameActions.discardAtRandom rather than this
         if(isRandom && !card.allowGameAction('discardAtRandom', { card, context })) {
             return false;
         }
