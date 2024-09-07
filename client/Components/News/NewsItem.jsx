060be0226046671adfd2473f4767ac6532be4a2c
Stuart Walsh <stu@ipng.org.uk>
2024-09-07 20:28:40 +0100

    Fix news styling

--- client/Components/News/NewsItem.jsx
+++ client/Components/News/NewsItem.jsx
@@ -37,13 +37,12 @@ const NewsItem = ({ newsItem, icon }) => {
     const parts = getMessageWithLinks(newsItem.text);
 
     return (
-        <div className='flex border-b-1 border-dotted border-primary pb-1 pt-1 text-secondary first:border-t-1 first:bg-primary first:bg-opacity-20 first:text-white'>
-            <div className={`news-icon font-[thronesdb] ${icon} w-5 text-center`}>
-                {icons[icon]}
+        <div className='flex gap-2 border-b-1 border-dotted border-primary pb-1 pt-1 text-secondary first:border-t-1 first:bg-primary first:bg-opacity-20 first:text-white'>
+            <div className={`font-[thronesdb] ${icon} w-5 text-center`}>{icons[icon]}</div>
+            <div>
+                {`${moment(newsItem.datePublished).format('YYYY-MM-DD')} - ${newsItem.poster} - `}
+                {parts}
             </div>
-            &nbsp;
-            {`${moment(newsItem.datePublished).format('YYYY-MM-DD')} - ${newsItem.poster} - `}
-            {parts}
         </div>
     );
 };
