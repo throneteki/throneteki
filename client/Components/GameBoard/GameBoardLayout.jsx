42ea11d44af3a5f65a65b53f9fc6b56c7f8d3b02
Stuart Walsh <stu@ipng.org.uk>
2024-10-18 20:55:16 +0100

    fix board spacing

--- client/Components/GameBoard/GameBoardLayout.jsx
+++ client/Components/GameBoard/GameBoardLayout.jsx
@@ -115,7 +115,7 @@ const GameBoardLayout = ({ thisPlayer, otherPlayer, onCardClick, onMouseOver, on
                             />
                         </div>
                     </div>
-                    <div className='flex flex-1 flex-col justify-between m-2'>
+                    <div className='flex flex-1 flex-col  m-2'>
                         <PlayerBoard
                             cardsInPlay={otherPlayer.cardPiles.cardsInPlay}
                             onCardClick={onCardClick}
