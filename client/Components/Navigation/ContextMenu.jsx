ee9beca9d227a890f18a67a3cb1cdf9950f158b9
Patane97 <patane97@gmail.com>
2024-10-29 00:03:59 +1100

    Fixed spectators & added MouseOverPopover (#3565)
    * Implementing a MouseOverPopover for spectators and deck status
    
    * Fixed/improved/updated the ContextMenu

--- client/Components/Navigation/ContextMenu.jsx
+++ client/Components/Navigation/ContextMenu.jsx
@@ -2,29 +2,20 @@ import React, { useCallback, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toastr } from 'react-redux-toastr';
 import { sendConcedeMessage, sendLeaveGameMessage } from '../../redux/reducers/game';
+import { PopoverContent, PopoverTrigger, Link, NavbarMenuItem } from '@nextui-org/react';
+import MouseOverPopover from '../Site/MouseOverPopover';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWarning } from '@fortawesome/free-solid-svg-icons';
 
 const ContextMenu = () => {
     const dispatch = useDispatch();
     const { currentGame } = useSelector((state) => state.lobby);
     const { user } = useSelector((state) => state.auth);
 
-    const [showPopup, setShowPopup] = useState(undefined);
     const [lastSpectatorCount, setLastSpectatorCount] = useState(0);
     const [showSpectatorWarning, setShowSpectatorWarning] = useState(false);
 
     let spectating = currentGame && !currentGame.players[user.username];
-
-    const onMenuItemMouseOver = useCallback((menuItem) => {
-        setShowPopup(menuItem);
-
-        if (menuItem.onMouseOver) {
-            menuItem.onMouseOver();
-        }
-    }, []);
-
-    const onMenuItemMouseOut = useCallback(() => {
-        setShowPopup(undefined);
-    }, []);
 
     const isGameActive = useMemo(() => {
         if (!currentGame || !user) {
@@ -69,25 +60,37 @@ const ContextMenu = () => {
         dispatch(sendLeaveGameMessage());
     }, [dispatch, isGameActive, spectating]);
 
-    let contextMenu = useMemo(() => {
-        let menuOptions = [];
-
+    const contextMenu = useMemo(() => {
+        const menuOptions = [];
+        const menuItemClass =
+            'cursor-pointer text-medium font-[PoppinsMedium] text-white transition-colors duration-500 ease-in-out hover:text-gray-500';
         if (currentGame?.started) {
-            menuOptions.push({ text: 'Leave Game', onClick: onLeaveClick });
+            menuOptions.push(
+                <Link onPress={onLeaveClick}>
+                    <a className={menuItemClass}>Leave Game</a>
+                </Link>
+            );
             if (currentGame.players[user.username]) {
-                menuOptions.unshift({
-                    text: 'Concede',
-                    onClick: () => dispatch(sendConcedeMessage())
-                });
+                menuOptions.unshift(
+                    <Link onPress={() => dispatch(sendConcedeMessage())}>
+                        <a className={menuItemClass}>Concede</a>
+                    </Link>
+                );
             }
+            const spectatorPopover =
+                currentGame.spectators.length > 0 ? (
+                    <ul>
+                        {currentGame.spectators.map((spectator) => (
+                            <li key={spectator.id}>{spectator.name}</li>
+                        ))}
+                    </ul>
+                ) : (
+                    <ul>
+                        <i>None</i>
+                    </ul>
+                );
 
-            let spectators = currentGame.spectators.map((spectator) => {
-                return <li key={spectator.id}>{spectator.name}</li>;
-            });
-
-            let spectatorPopup = <ul className='spectators-popup absolute-panel'>{spectators}</ul>;
-
-            //if the current user is a player and the number of spectators changed, then display a warning next to the Spectators popup in the navbar
+            // If the current user is a player and the number of spectators changed, then display a warning next to the Spectators popup in the navbar
             if (
                 currentGame.players[user.username] &&
                 currentGame.spectators.length !== lastSpectatorCount
@@ -95,39 +98,26 @@ const ContextMenu = () => {
                 setShowSpectatorWarning(true);
             }
 
-            menuOptions.unshift({
-                text: 'Spectators: ' + currentGame.spectators.length,
-                popup: spectatorPopup,
-                displayWarning: showSpectatorWarning,
-                onMouseOver: () => setShowSpectatorWarning(false)
-            });
+            menuOptions.unshift(
+                <MouseOverPopover triggerScaleOnOpen={false}>
+                    <PopoverTrigger>
+                        <a
+                            className={menuItemClass}
+                            onMouseOver={() => setShowSpectatorWarning(false)}
+                        >
+                            {showSpectatorWarning ? <FontAwesomeIcon icon={faWarning} /> : null}{' '}
+                            {'Spectators: ' + currentGame.spectators.length}
+                        </a>
+                    </PopoverTrigger>
+                    <PopoverContent>{spectatorPopover}</PopoverContent>
+                </MouseOverPopover>
+            );
 
             setLastSpectatorCount(currentGame.spectators.length);
         }
 
-        return menuOptions.map((menuItem) => {
-            return (
-                <li key={menuItem.text}>
-                    <a
-                        className='cursor-pointer font-[PoppinsMedium]'
-                        onMouseOver={() => onMenuItemMouseOver(menuItem)}
-                        onMouseOut={() => onMenuItemMouseOut()}
-                        onClick={
-                            menuItem.onClick
-                                ? (event) => {
-                                      event.preventDefault();
-                                      menuItem.onClick();
-                                  }
-                                : null
-                        }
-                    >
-                        {' '}
-                        {menuItem.displayWarning ? <span className='warning-icon' /> : null}{' '}
-                        {menuItem.text}
-                    </a>
-                    {showPopup?.text === menuItem.text ? showPopup.popup : null}
-                </li>
-            );
+        return menuOptions.map((menuItem, index) => {
+            return <NavbarMenuItem key={index}>{menuItem}</NavbarMenuItem>;
         });
     }, [
         currentGame?.players,
@@ -136,9 +126,6 @@ const ContextMenu = () => {
         dispatch,
         lastSpectatorCount,
         onLeaveClick,
-        onMenuItemMouseOut,
-        onMenuItemMouseOver,
-        showPopup,
         showSpectatorWarning,
         user?.username
     ]);
