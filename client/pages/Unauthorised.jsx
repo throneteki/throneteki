f1bcef9591c434b05287f083f1345962ed61a295
Stephen Patane <patane97@gmail.com>
2024-10-30 21:56:17 +1100

    Nicer looking Unauthorized page

--- client/pages/Unauthorised.jsx
+++ client/pages/Unauthorised.jsx
@@ -1,7 +1,14 @@
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const Unauthorised = () => {
-    return <div>Sorry, you are not authorised to view that page.</div>;
+    return (
+        <div className='w-full h-full text-center p-5'>
+            <FontAwesomeIcon size='xl' icon={faTriangleExclamation} />
+            <div>Sorry, you are not authorised to view that page.</div>
+        </div>
+    );
 };
 
 export default Unauthorised;
