66844a03abf232507d821a2dafe3da22077cd5d4
Stephen Patane <patane97@gmail.com>
2025-01-30 21:35:17 +1100

    Fixing cardpiles to more appropriately show open/close popup menu item + visual tweaks

--- client/Components/GameBoard/MovablePanel.jsx
+++ client/Components/GameBoard/MovablePanel.jsx
@@ -3,7 +3,7 @@ import $ from 'jquery';
 
 import { ItemTypes } from '../../constants';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { PopupDefaults } from './PopupDefaults';
 import { useDndMonitor, useDraggable } from '@dnd-kit/core';
 
@@ -103,7 +103,7 @@ const MovablePanel = ({ name, side, title, onCloseClick, children, size }) => {
                 <span className='flex-1 text-center'>{title}</span>
                 <span className='cursor-pointer'>
                     <a onClick={onCloseClick}>
-                        <FontAwesomeIcon icon={faTimes} />
+                        <FontAwesomeIcon icon={faXmark} />
                     </a>
                 </span>
             </div>
