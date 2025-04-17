4af17e7a1fdeb2e58a33138db6c7fdac35feac44
Fanganga <shanks.andy@gmail.com>
2025-04-17 18:02:16 +0000

    Responding to PR comment. Preventing double-trigger with more precise when clause rather than split reaction

--- server/game/cards/25.6-WK/MaesterCressen.js
+++ server/game/cards/25.6-WK/MaesterCressen.js
@@ -5,26 +5,11 @@ class MaesterCressen extends DrawCard {
     setupCardAbilities() {
         this.reaction({
             when: {
-                onCardOutOfShadows: (event) => event.card === this
-            },
-            message:
-                '{player} uses {source} to place the top 2 cards of their deck under their agenda',
-            gameAction: GameActions.simultaneously((context) =>
-                context.player.drawDeck.slice(0, 2).map((card) =>
-                    GameActions.placeCardUnderneath({
-                        card,
-                        parentCard: context.player.agenda,
-                        facedown: false
-                    })
-                )
-            )
-        });
-        //reaction has to be split to avoid double trigger in scenario where Cressen dies immediately after coming out of shadows
-        this.reaction({
-            when: {
+                onCardOutOfShadows: (event) =>
+                    event.card === this && event.card.location === 'play area',
                 onCardPlaced: (event) => event.card === this && event.card.location === 'dead pile'
             },
-            location: ['dead pile'],
+            location: ['dead pile', 'play area'],
             message:
                 '{player} uses {source} to place the top 2 cards of their deck under their agenda',
             gameAction: GameActions.simultaneously((context) =>
