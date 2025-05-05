1b7726b63368d64d2ed7e3cf8191fafec9eb5c9d
Stephen Patane <patane97@gmail.com>
2025-05-05 23:19:16 +1000

    Fixing active prompt

--- client/Components/GameBoard/SideBoardPanel.jsx
+++ client/Components/GameBoard/SideBoardPanel.jsx
@@ -3,8 +3,11 @@ import GameTimer from './GameTimer';
 import ActivePlayerPrompt from './ActivePlayerPrompt';
 import { sendButtonClickedMessage } from '../../redux/reducers/game';
 import classNames from 'classnames';
+import { useDispatch } from 'react-redux';
 
 const SideBoardPanel = ({ collapsable, player, thisPlayer, isMe, side }) => {
+    const dispatch = useDispatch();
+
     // Side panel must be treated differently for the 2 left-most boards, and for top/bottom
     const sidePanelClassName = classNames(
         'sticky left-0 z-[240] flex flex-col p-1 pointer-events-none',
@@ -26,7 +29,7 @@ const SideBoardPanel = ({ collapsable, player, thisPlayer, isMe, side }) => {
                     promptText={thisPlayer.menuTitle}
                     promptTitle={thisPlayer.promptTitle}
                     onButtonClick={(button) =>
-                        dispatchEvent(
+                        dispatch(
                             sendButtonClickedMessage(
                                 button.promptId,
                                 button.command,
