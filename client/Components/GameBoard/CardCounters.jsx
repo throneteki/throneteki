00ffde841767b5388377f9621be9c88aecab5f29
Stuart Walsh <stu@ipng.org.uk>
2025-05-28 20:10:11 +0100

    FIx token rotation when card is rotated

--- client/Components/GameBoard/CardCounters.jsx
+++ client/Components/GameBoard/CardCounters.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Counter from './Counter';
+import classNames from 'classnames';
 
-const CardCounters = ({ counters }) => {
+const CardCounters = ({ counters, isParentKneeled }) => {
     if (counters.length === 0) {
         return null;
     }
@@ -22,8 +23,13 @@ const CardCounters = ({ counters }) => {
         );
     }
 
+    const classes = classNames(
+        'w-full h-full flex items-center z-[200] justify-center absolute left-0 top-0 pointer-events-none',
+        { '-rotate-90': isParentKneeled }
+    );
+
     return (
-        <div className='w-full h-full flex items-center z-[200] justify-center absolute left-0 top-0 pointer-events-none'>
+        <div className={classes}>
             <div className='text-white justify-center flex flex-wrap items-center gap-1 h-auto w-full flex-row'>
                 {counterDivs}
             </div>
