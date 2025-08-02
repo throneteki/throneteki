4c892efa5a474d2a1ecadcad7250e3dd25f6d68e
Brian Upton <ystros@gmail.com>
2025-08-01 21:59:38 -0700

    Use symbols for flags instead of strings

--- server/game/Constants/Flags.js
+++ server/game/Constants/Flags.js
@@ -1,50 +1,50 @@
 export const Flags = Object.freeze({
     blanks: Object.freeze({
-        excludingTraits: 'blanks.excludingTraits',
-        full: 'blanks.full'
+        excludingTraits: Symbol('blanks.excludingTraits'),
+        full: Symbol('blanks.full')
     }),
     challengeOptions: Object.freeze({
-        canBeDeclaredWhileKneeling: 'challengeOptions.canBeDeclaredWhileKneeling',
-        canBeDeclaredWithoutIcon: 'challengeOptions.canBeDeclaredWithoutIcon',
-        doesNotContributeStrength: 'challengeOptions.doesNotContributeStrength',
+        canBeDeclaredWhileKneeling: Symbol('challengeOptions.canBeDeclaredWhileKneeling'),
+        canBeDeclaredWithoutIcon: Symbol('challengeOptions.canBeDeclaredWithoutIcon'),
+        doesNotContributeStrength: Symbol('challengeOptions.doesNotContributeStrength'),
         doesNotKneelAsAttacker: function (challengeType) {
-            return `challengeOptions.doesNotKneelAsAttacker.${challengeType}`;
+            return Symbol.for(`challengeOptions.doesNotKneelAsAttacker.${challengeType}`);
         },
         doesNotKneelAsDefender: function (challengeType) {
-            return `challengeOptions.doesNotKneelAsDefender.${challengeType}`;
+            return Symbol.for(`challengeOptions.doesNotKneelAsDefender.${challengeType}`);
         },
-        ignoresAssaultLocationCost: 'challengeOptions.ignoresAssaultLocationCost',
-        mustBeDeclaredAsAttacker: 'challengeOptions.mustBeDeclaredAsAttacker',
-        mustBeDeclaredAsDefender: 'challengeOptions.mustBeDeclaredAsDefender'
+        ignoresAssaultLocationCost: Symbol('challengeOptions.ignoresAssaultLocationCost'),
+        mustBeDeclaredAsAttacker: Symbol('challengeOptions.mustBeDeclaredAsAttacker'),
+        mustBeDeclaredAsDefender: Symbol('challengeOptions.mustBeDeclaredAsDefender'),
     }),
     dominanceOptions: Object.freeze({
-        contributesWhileKneeling: 'dominanceOptions.contributesWhileKneeling',
-        doesNotContribute: 'dominanceOptions.doesNotContribute'
+        contributesWhileKneeling: Symbol('dominanceOptions.contributesWhileKneeling'),
+        doesNotContribute: Symbol('dominanceOptions.doesNotContribute'),
     }),
     losesAspect: Object.freeze({
-        allFactions: 'losesAspect.allFactions',
+        allFactions: Symbol('losesAspect.allFactions'),
         faction: function (factionName) {
-            return `losesAspect.factions.${factionName}`;
+            return Symbol.for(`losesAspect.factions.${factionName}`);
         },
-        keywords: 'losesAspect.keywords',
-        immunity: 'loseAspect.immunity',
-        traits: 'losesAspect.traits'
+        keywords: Symbol('losesAspect.keywords'),
+        immunity: Symbol('loseAspect.immunity'),
+        traits: Symbol('losesAspect.traits')
     }),
     player: Object.freeze({
-        cannotBeFirstPlayer: 'player.cannotBeFirstPlayer',
-        cannotGainChallengeBonus: 'player.cannotGainChallengeBonus',
-        cannotGainDominancePower: 'player.cannotGainDominancePower',
-        cannotRevealPlot: 'player.cannotRevealPlot',
-        cannotWinChallenge: 'player.cannotWinChallenge',
-        cannotWinGame: 'player.cannotWinGame',
-        doesNotReturnUnspentGold: 'player.doesNotReturnUnspentGold'
+        cannotBeFirstPlayer: Symbol('player.cannotBeFirstPlayer'),
+        cannotGainChallengeBonus: Symbol('player.cannotGainChallengeBonus'),
+        cannotGainDominancePower: Symbol('player.cannotGainDominancePower'),
+        cannotRevealPlot: Symbol('player.cannotRevealPlot'),
+        cannotWinChallenge: Symbol('player.cannotWinChallenge'),
+        cannotWinGame: Symbol('player.cannotWinGame'),
+        doesNotReturnUnspentGold: Symbol('player.doesNotReturnUnspentGold')
     }),
     plotModifiers: Object.freeze({
         cannotProvide: function (modifier) {
-            return `plotModifiers.cannotProvide.${modifier}`;
+            return Symbol.for(`plotModifiers.cannotProvide.${modifier}`);
         }
     }),
     powerOptions: Object.freeze({
-        doesNotContribute: 'powerOptions.doesNotContribute'
+        doesNotContribute: Symbol('powerOptions.doesNotContribute')
     })
 });
